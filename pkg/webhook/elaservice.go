6810eac00798de49732ce14584ac5f34cd3e336d
Yanwei Guo <yanweiguo@google.com>
2018-02-07 15:33:25 -0800

    Adds validation for trafficTarget.

--- pkg/webhook/elaservice.go
+++ pkg/webhook/elaservice.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 Google Inc. All Rights Reserved.
+Copyright 2018 Google LLC. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -23,6 +23,14 @@ import (
 	"github.com/mattbaird/jsonpatch"
 )
 
+const (
+	conflictRevisionsErrorMessage     = "Only one of revision and revisionTemplate can be specificed in traffic field."
+	negativeTargetPercentErrorMessage = "Traffic percent can not be negative."
+	noRevisionsErrorMessage           = "No revision nor revisionTemplate in traffic field provided."
+	noTrafficErrorMessage             = "No traffic field in spec provided."
+	targetPercentSumErrorMessage      = "Traffic percent sum is not equal to 100."
+)
+
 // ValidateElaService is ElaService resource specific validation and mutation handler
 func ValidateElaService(patches *[]jsonpatch.JsonPatchOperation, old GenericCRD, new GenericCRD) error {
 	var oldES *v1alpha1.ElaService
@@ -31,14 +39,48 @@ func ValidateElaService(patches *[]jsonpatch.JsonPatchOperation, old GenericCRD,
 		var ok bool
 		oldES, ok = old.(*v1alpha1.ElaService)
 		if !ok {
-			return fmt.Errorf("Failed to convert old into ElaService")
+			return fmt.Errorf("Failed to convert old into ElaService.")
 		}
 	}
 	glog.Infof("ValidateElaService: OLD ElaService is\n%+v", oldES)
 	newES, ok := new.(*v1alpha1.ElaService)
 	if !ok {
-		return fmt.Errorf("Failed to convert new into ElaService")
+		return fmt.Errorf("Failed to convert new into ElaService.")
 	}
 	glog.Infof("ValidateElaService: NEW ElaService is\n%+v", newES)
+
+	err := validateTrafficTarget(newES)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateTrafficTarget(elaService *v1alpha1.ElaService) error {
+	if elaService.Spec.Traffic == nil {
+		return fmt.Errorf(noTrafficErrorMessage)
+	}
+
+	percentSum := 0
+	for _, trafficTarget := range elaService.Spec.Traffic {
+		revisionLen := len(trafficTarget.Revision)
+		revisionTemplateLen := len(trafficTarget.RevisionTemplate)
+		if revisionLen == 0 && revisionTemplateLen == 0 {
+			return fmt.Errorf(noRevisionsErrorMessage)
+		}
+		if revisionLen != 0 && revisionTemplateLen != 0 {
+			return fmt.Errorf(conflictRevisionsErrorMessage)
+		}
+
+		if trafficTarget.Percent < 0 {
+			return fmt.Errorf(negativeTargetPercentErrorMessage)
+		}
+		percentSum += trafficTarget.Percent
+	}
+
+	if percentSum != 100 {
+		return fmt.Errorf(targetPercentSumErrorMessage)
+	}
 	return nil
 }
