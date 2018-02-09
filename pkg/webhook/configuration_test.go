3c9232808fd62f0847d171b8f798ec545b753255
Yao Wu <wuyao@google.com>
2018-02-09 15:16:56 -0800

    add a test file

---
+++ pkg/webhook/configuration_test.go
@@ -0,0 +1,88 @@
+/*
+Copyright 2018 Google LLC. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package webhook
+
+import (
+	"testing"
+
+	"github.com/google/elafros/pkg/apis/ela/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestValidConfigurationAllowed(t *testing.T) {
+	configuration := createConfiguration(testGeneration)
+
+	err := ValidateConfiguration(nil, &configuration, &configuration)
+
+	if err != nil {
+		t.Fatalf("Valid revision template should pass, but failed with:  %s.", err)
+	}
+}
+
+func TestEmptyConfigurationNotAllowed(t *testing.T) {
+	err := ValidateConfiguration(nil, nil, nil)
+	if err == nil || err.Error() != "Failed to convert new into Configuration" {
+		t.Fatalf("Expected: Failed to convert new into Configuration. Failed with %s", err)
+	}
+}
+
+func TestEmptySpecInConfigurationNotAllowed(t *testing.T) {
+	configuration := v1alpha1.Configuration{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: testNamespace,
+			Name:      rtName,
+		},
+		Spec: v1alpha1.ConfigurationSpec{},
+	}
+
+	err := ValidateConfiguration(nil, &configuration, &configuration)
+
+	if err == nil || err.Error() != errEmptySpecInConfigurationMessage {
+		t.Fatalf("Expected: %s. Failed with %s", errEmptySpecInConfigurationMessage, err)
+	}
+}
+
+func TestEmptyTemplateInSpecNotAllowed(t *testing.T) {
+	configuration := v1alpha1.Configuration{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: testNamespace,
+			Name:      rtName,
+		},
+		Spec: v1alpha1.ConfigurationSpec{
+			Generation: testGeneration,
+			Template:   v1alpha1.Revision{},
+		},
+	}
+
+	err := ValidateConfiguration(nil, &configuration, &configuration)
+
+	if err == nil || err.Error() != errEmptyTemplateInSpecMessage {
+		t.Fatalf("Expected: %s. Failed with %s", errEmptyTemplateInSpecMessage, err)
+	}
+}
+
+func TestNonEmptyStatusInConfiguration(t *testing.T) {
+	configuration := createConfiguration(testGeneration)
+	configuration.Status = v1alpha1.ConfigurationStatus{
+		Latest: "latest version",
+	}
+
+	err := ValidateConfiguration(nil, &configuration, &configuration)
+
+	if err == nil || err.Error() != errNonEmptyStatusInConfigurationMessage {
+		t.Fatalf("Expected: %s. Failed with %s", errNonEmptyStatusInConfigurationMessage, err)
+	}
+}
