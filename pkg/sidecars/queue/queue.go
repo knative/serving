aef729ab382165cc7a761a6b4293f4e746064143
Joseph Burnett <josephburnett79@gmail.com>
2018-02-14 14:12:04 -0500

    Connect stat sink.

--- pkg/sidecars/queue/queue.go
+++ pkg/sidecars/queue/queue.go
@@ -4,43 +4,67 @@ import (
 	"bytes"
 	"elafros/pkg/autoscaler/types"
 	"encoding/gob"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
-	"net/url"
 	"time"
 
 	"github.com/gorilla/websocket"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
-type request struct {
-	w http.ResponseWriter
-	r *http.Request
-	c chan struct{}
-}
-
-var requestChan = make(chan *request)
 var statChan = make(chan *types.Stat)
+var kubeClient *kubernetes.Clientset
+var statSink *websocket.Conn
 
-func init() {
-	svc := os.GetEnv("ELA_AUTOSCALER_SERVICE")
-	if svc == "" {
-		log.Println("No ELA_AUTOSCALER_SERVICE provided.")
+func connectStatSink() {
+	ns := os.GenEnv("ELA_NAMESPACE")
+	if ns == "" {
+		log.Println("No ELA_NAMESPACE provided.")
 		return
 	}
-	// TODO(josephburnett): watch for service.spec.clusterIP
-	// go statReporter()
-}
-
-func statReporter() {
-	conn, _, err := websocket.DefaultDialer.Dial("", nil)
+	rev := os.GetEnv("ELA_REVISION")
+	if rev == "" {
+		log.Println("No ELA_REVISION provided.")
+		return
+	}
+	services := kubeClient.CoreV1().Services(ns)
+	selector := fmt.Sprintf("revision=%s", rev)
+	wi, err := services.Watch(opt)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	defer conn.Close()
+	defer wi.Stop()
+	ch := wi.ResultChan()
+	for {
+		event := <-ch
+		if svc, ok := event.Object(corev1.Service); ok {
+			ip := svc.ClusterIP
+			if ip != "" {
+				conn, _, err := websocket.DefaultDialer.Dial(ip, nil)
+				if err != nil {
+					log.Println(err)
+				} else {
+					log.Println("Connected to stat sink.")
+					statSink = conn
+					return
+				}
+			}
+		}
+	}
+}
+
+func statReporter() {
 	for {
 		s := <-statChan
+		if statSink == nil {
+			log.Println("Stat sink not connected.")
+			return
+		}
 		var b bytes.Buffer
 		enc := gob.NewEncoder(&b)
 		err = enc.Encode(s)
@@ -56,20 +80,6 @@ func statReporter() {
 	}
 }
 
-func consumer() {
-	target, err := url.Parse("http://127.0.0.1:8080")
-	if err != nil {
-		panic(err)
-	}
-	proxy := httputil.NewSingleHostReverseProxy(target)
-	for {
-		r := <-requestChan
-		log.Println("Forwarding a request to the app container at ", time.Now().String())
-		proxy.ServeHTTP(r.w, r.r)
-		close(r.c)
-	}
-}
-
 func handler(w http.ResponseWriter, r *http.Request) {
 	stat := &types.Stat{
 		RequestCount: 1,
@@ -80,14 +90,29 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		r: r,
 		c: make(chan struct{}),
 	}
-	log.Println("Adding a request to the the request channel at ", time.Now().String())
-	requestChan <- req
-	<-req.c
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	log.Println("Forwarding a request to the app container at ", time.Now().String())
+	proxy.ServeHTTP(r.w, r.r)
+	close(r.c)
 }
 
 func main() {
 	log.Println("Queue container is running")
-	go consumer()
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err)
+	}
+	kc, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	kubeClient = kc
+	go connectStatSink()
+	defer func() {
+		if statSink != nil {
+			statSink.Close()
+		}
+	}()
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8012", nil)
 }
