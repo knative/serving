0b75d979c76f07bbd2b74d780e3f1d9e374126c6
Joseph Burnett <josephburnett79@gmail.com>
2018-02-28 12:16:34 -0500

    Add back ela-revision service account.

--- pkg/controller/revision/ela_pod.go
+++ pkg/controller/revision/ela_pod.go
@@ -191,8 +191,9 @@ func MakeElaPodSpec(u *v1alpha1.Revision) *corev1.PodSpec {
 	}
 
 	return &corev1.PodSpec{
-		Volumes:    []corev1.Volume{elaContainerLogVolume, nginxConfigVolume, nginxLogVolume},
-		Containers: []corev1.Container{*elaContainer, queueContainer, nginxContainer, fluentdContainer},
+		Volumes:            []corev1.Volume{elaContainerLogVolume, nginxConfigVolume, nginxLogVolume},
+		Containers:         []corev1.Container{*elaContainer, queueContainer, nginxContainer, fluentdContainer},
+		ServiceAccountName: "ela-revision",
 	}
 }
 
