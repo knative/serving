b1b732ca6277222585f29c36854dc6d1cea8e915
Ian Milligan <ianmllgn@gmail.com>
2018-03-01 10:48:27 -0800

    Add revision/ela_resource.go for common resource helpers.

---
+++ pkg/controller/revision/ela_resource.go
@@ -0,0 +1,15 @@
+package revision
+
+import "github.com/google/elafros/pkg/apis/ela/v1alpha1"
+
+// MakeElaDeploymentLabels constructs the labels we will apply to K8s resources.
+func MakeElaResourceLabels(u *v1alpha1.Revision) map[string]string {
+	labels := map[string]string{
+		elaVersionLabel: u.Name,
+	}
+	// Inherit route label from the revision if present.
+	if route, ok := u.ObjectMeta.Labels[routeLabel]; ok {
+		labels[routeLabel] = route
+	}
+	return labels
+}
