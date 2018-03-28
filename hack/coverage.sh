3c88de66e9f1a56587d6b55cf09caf87ebb83f0e
syzf <syzf@google.com>
2018-03-28 11:04:50 -0700

    use PR number as object name and change ACL
    1) use PR number as object name to enable indexing. Indexing allows
    incremental coverage done on a commit
    
    2) make the object project-private. Without specifying, object will be
    public (which is strange because GCS document says the default is
    project-private)

--- hack/coverage.sh
+++ hack/coverage.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 go test ../pkg/... -coverprofile coverage_profile.txt
-gsutil cp coverage_profile.txt gs://gke-prow/pr-logs/directory/elafros-coverage/
- 
+gsutil cp -a project-private coverage_profile.txt gs://gke-prow/pr-logs/directory/elafros-coverage/$PULL_PULL_SHA
