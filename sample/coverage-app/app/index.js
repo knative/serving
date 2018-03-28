e475df8ee330fb6c9f01af048ea1e74a6d1b81df
syzf <syzf@google.com>
2018-03-28 11:24:10 -0700

    delete experimental code

--- sample/coverage-app/app/index.js
+++
@@ -1,8 +0,0 @@
-exports.function = (req, res) => {
-  if (req.query.name === undefined) {
-    res.status(400).send('Please specify a name query parameter. Example ?name=Max');
-  } else {
-    console.log(req.query.name);
-    res.status(200).send('Hello ' + req.query.name);
-  }
-};
