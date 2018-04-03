f265fff0ee244dc1454757ddeac7b92b61cb1143
Mike Helmick <mike.helmick@gmail.com>
2018-04-03 15:12:59 -0700

    CRD type definitions for the Service object (#412) (#577)
    * add types for Service and generate related code. Issue #412
    
    * add types for Service and generate related code. Issue #412
    
    * Convert tabs to spaces, reformat service_types.go
    
    * add tests for service types
    
    * convert tabs to spaces
    
    * fix spelling mistakes in service definition
    
    * rerun codegen after fixing spelling in json types

--- pkg/apis/ela/v1alpha1/register.go
+++ pkg/apis/ela/v1alpha1/register.go
@@ -51,6 +51,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&ConfigurationList{},
 		&Route{},
 		&RouteList{},
+		&Service{},
+		&ServiceList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
