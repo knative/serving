dabed476b2c75981f121b0d084986e2f51deeb19
Brenda Chan <brchan@pivotal.io>
2018-05-07 15:39:33 -0400

    Use gke 1.10 and enable alpha features for conformance tests
    Signed-off-by: Dave Protasowski <dprotaso@gmail.com>

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -137,7 +137,7 @@ if [[ -z $1 ]]; then
   header "Creating test cluster"
   # Smallest cluster required to run the end-to-end-tests
   CLUSTER_CREATION_ARGS=(
-    --gke-create-args="--enable-autoscaling --min-nodes=1 --max-nodes=${E2E_CLUSTER_NODES} --scopes=cloud-platform"
+    --gke-create-args="--enable-autoscaling --enable-kubernetes-alpha --min-nodes=1 --max-nodes=${E2E_CLUSTER_NODES} --scopes=cloud-platform"
     --gke-shape={\"default\":{\"Nodes\":${E2E_CLUSTER_NODES}\,\"MachineType\":\"${E2E_CLUSTER_MACHINE}\"}}
     --provider=gke
     --deployment=gke
