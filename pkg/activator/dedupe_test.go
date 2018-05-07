52f4c77b0825199f36d9e50d3531c14e2d19eab4
Joseph Burnett <josephburnett79@gmail.com>
2018-05-07 12:54:50 -0400

    First concurrency test.

--- pkg/activator/dedupe_test.go
+++ pkg/activator/dedupe_test.go
@@ -1,7 +1,8 @@
 package activator
 
 import (
-	"fmt"
+	"reflect"
+	"sync"
 	"testing"
 )
 
@@ -33,10 +34,40 @@ func TestSingleRevisionSingleRequest(t *testing.T) {
 	}
 }
 
+func TestSingleRevisionMultipleRequests(t *testing.T) {
+	ep := Endpoint{"ip", 8080}
+	f := newFakeActivator(t,
+		map[revisionId]activationResult{
+			revisionId{"default", "rev1"}: activationResult{
+				endpoint: ep,
+				status:   Status(0),
+				err:      nil,
+			},
+		})
+	d := NewActivationDeduper(f)
+
+	got := concurrentTest(d, f, []revisionId{
+		revisionId{"default", "rev1"},
+		revisionId{"default", "rev1"},
+	})
+
+	want := []activationResult{
+		activationResult{ep, Status(0), nil},
+		activationResult{ep, Status(0), nil},
+	}
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("Unexpected results. Wanted %+v. Got %+v.", want, got)
+	}
+	if len(f.record) != 1 {
+		t.Errorf("Unexpected number of activation requests. Want 1. Got %v.", len(f.record))
+	}
+}
+
 type fakeActivator struct {
 	t         *testing.T
 	responses map[revisionId]activationResult
 	record    []revisionId
+	holds     map[revisionId]chan struct{}
 }
 
 func newFakeActivator(t *testing.T, responses map[revisionId]activationResult) *fakeActivator {
@@ -44,18 +75,58 @@ func newFakeActivator(t *testing.T, responses map[revisionId]activationResult) *
 		t:         t,
 		responses: responses,
 		record:    make([]revisionId, 0),
+		holds:     make(map[revisionId]chan struct{}),
 	}
 }
 
 func (f *fakeActivator) ActiveEndpoint(namespace, name string) (Endpoint, Status, error) {
 	id := revisionId{namespace, name}
 	f.record = append(f.record, id)
-	fmt.Printf("id: %+v", id)
-	fmt.Printf("responses: %+v", f.responses)
 	if result, ok := f.responses[id]; ok {
+		if hold, ok := f.holds[id]; ok {
+			<-hold
+		}
 		return result.endpoint, result.status, result.err
 	} else {
 		f.t.Fatalf("Unexpected call to activator: %v", id)
 		return Endpoint{}, Status(0), nil
 	}
+}
+
+func (f *fakeActivator) hold(id revisionId) {
+	if _, ok := f.holds[id]; !ok {
+		f.holds[id] = make(chan struct{})
+	}
+}
+
+func (f *fakeActivator) release(id revisionId) {
+	if h, ok := f.holds[id]; ok {
+		close(h)
+		delete(f.holds, id)
+	}
+}
+
+func concurrentTest(a Activator, f *fakeActivator, ids []revisionId) []activationResult {
+	for _, id := range ids {
+		f.hold(id)
+	}
+	var start sync.WaitGroup
+	var end sync.WaitGroup
+	results := make([]activationResult, len(ids))
+	for i, id := range ids {
+		start.Add(1)
+		end.Add(1)
+		go func(index int) {
+			start.Done()
+			endpoint, status, err := a.ActiveEndpoint(id.namespace, id.name)
+			results[index] = activationResult{endpoint, status, err}
+			end.Done()
+		}(i)
+	}
+	start.Wait()
+	for _, id := range ids {
+		f.release(id)
+	}
+	end.Wait()
+	return results
 }
