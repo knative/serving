873aa54ffe4b1c4c10e6bc3418b889ac44bae4d3
Zhimin Xiang <zhiminx@google.com>
2018-05-18 10:29:33 -0700

    Reverse back some code for debugging

--- pkg/controller/route/ela_istio_route.go
+++ pkg/controller/route/ela_istio_route.go
@@ -18,6 +18,7 @@ package route
 
 import (
 	"fmt"
+	"regexp"
 
 	"github.com/elafros/elafros/pkg/apis/ela/v1alpha1"
 	istiov1alpha2 "github.com/elafros/elafros/pkg/apis/istio/v1alpha2"
@@ -35,6 +36,15 @@ func makeIstioRouteSpec(u *v1alpha1.Route, tt *v1alpha1.TrafficTarget, ns string
 		Destination: istiov1alpha2.IstioService{
 			Name:      controller.GetElaK8SServiceName(u),
 			Namespace: ns,
+		},
+		Match: istiov1alpha2.Match{
+			Request: istiov1alpha2.MatchRequest{
+				Headers: istiov1alpha2.Headers{
+					Authority: istiov1alpha2.MatchString{
+						Regex: regexp.QuoteMeta(domain),
+					},
+				},
+			},
 		},
 		Route: destinationWeights,
 	}
