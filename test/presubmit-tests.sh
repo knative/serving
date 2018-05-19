5e1d338e7a18615fdd52422688a31d7d6cc16ed6
Adriano Cunha <adrcunha@google.com>
2018-05-18 23:26:06 -0700

    Use git diff instead of git log to get the list of changed files
    git log includes sync merges, which might not affect the changed files in the PR.

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -38,7 +38,7 @@ cd ${ELAFROS_ROOT_DIR}
 # Skip presubmit tests if only markdown files were changed.
 if [[ -n "${PULL_PULL_SHA}" ]]; then
   # On a presubmit job
-  changes="$(git log -m -1 --name-only --pretty="format:" ${PULL_PULL_SHA})"
+  changes="$(git diff --name-only ${PULL_PULL_SHA} ${PULL_BASE_SHA})"
   no_presubmit_pattern="${NO_PRESUBMIT_FILES[*]}"
   no_presubmit_pattern="\(${no_presubmit_pattern// /\\|}\)$"
   echo -e "Changed files in commit ${PULL_PULL_SHA}:\n${changes}"
