2c65d93abb3534e3607f76a6baf02ec7ce26153c
Stephen <36011612+steuhs@users.noreply.github.com>
2018-06-04 13:52:00 -0700

    DO NOT MERGE - testing code coverage pre-submit

--- pkg/logging/logger_test.go
+++ pkg/logging/logger_test.go
@@ -23,28 +23,28 @@ import (
 	"go.uber.org/zap"
 )
 
-func TestContext(t *testing.T) {
-	logger1 := zap.NewNop().Sugar()
-	logger2 := zap.NewExample().Sugar()
-
-	// Happy path tests
-	ctx := WithLogger(context.Background(), logger1)
-	checkFromContext(ctx, logger1, t)
-
-	ctx = WithLogger(ctx, logger2)
-	checkFromContext(ctx, logger2, t)
-
-	ctx = WithLogger(ctx, logger1)
-	checkFromContext(ctx, logger1, t)
-
-	// Empty logger
-	ctx = context.Background()
-	checkFromContext(ctx, fallbackLogger, t)
-
-	// Logger with a wrong type
-	ctx = context.WithValue(context.Background(), loggerKey{}, zap.NewNop())
-	checkFromContext(ctx, fallbackLogger, t)
-}
+//func TestContext(t *testing.T) {
+//	logger1 := zap.NewNop().Sugar()
+//	logger2 := zap.NewExample().Sugar()
+//
+//	// Happy path tests
+//	ctx := WithLogger(context.Background(), logger1)
+//	checkFromContext(ctx, logger1, t)
+//
+//	ctx = WithLogger(ctx, logger2)
+//	checkFromContext(ctx, logger2, t)
+//
+//	ctx = WithLogger(ctx, logger1)
+//	checkFromContext(ctx, logger1, t)
+//
+//	// Empty logger
+//	ctx = context.Background()
+//	checkFromContext(ctx, fallbackLogger, t)
+//
+//	// Logger with a wrong type
+//	ctx = context.WithValue(context.Background(), loggerKey{}, zap.NewNop())
+//	checkFromContext(ctx, fallbackLogger, t)
+//}
 
 func checkFromContext(ctx context.Context, want *zap.SugaredLogger, t *testing.T) {
 	if got := FromContext(ctx); want != got {
