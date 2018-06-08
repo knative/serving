5fb7042f7fc4e3fa8f4c2f44e9bbc2bd0a87bdfe
Nicole Thurmond <kcnicole@google.com>
2018-06-08 17:13:41 -0400

    Testing autoscale 1-N-0-N

--- test/e2e/autoscale_test.go
+++ test/e2e/autoscale_test.go
@@ -33,6 +33,10 @@ const (
 	autoscaleExpectedOutput = "39999983"
 )
 
+var (
+	initialScaleToZeroThreshold string
+)
+
 func isExpectedOutput() func(body string) (bool, error) {
 	return func(body string) (bool, error) {
 		return strings.Contains(body, autoscaleExpectedOutput), nil
@@ -41,13 +45,13 @@ func isExpectedOutput() func(body string) (bool, error) {
 
 func isDeploymentScaledUp() func(d *v1beta1.Deployment) (bool, error) {
 	return func(d *v1beta1.Deployment) (bool, error) {
-		return d.Status.ReadyReplicas > 1, nil
+		return d.Status.ReadyReplicas >= 1, nil
 	}
 }
 
-func isDeploymentScaledDown() func(d *v1beta1.Deployment) (bool, error) {
+func isDeploymentScaledToZero() func(d *v1beta1.Deployment) (bool, error) {
 	return func(d *v1beta1.Deployment) (bool, error) {
-		return d.Status.ReadyReplicas <= 1, nil
+		return d.Status.ReadyReplicas == 0, nil
 	}
 }
 
@@ -73,8 +77,40 @@ func generateTrafficBurst(clients *test.Clients, names test.ResourceNames, num i
 	}
 }
 
-func TestAutoscaleUpDownUp(t *testing.T) {
+func getAutoscalerConfigMap(clients *test.Clients) (*v1.ConfigMap, error) {
+	return clients.Kube.CoreV1().ConfigMaps("knative-serving-system").Get("config-autoscaler", metav1.GetOptions{})
+}
+
+func setScaleToZeroThreshold(clients *test.Clients, threshold string) error {
+	configMap, err := getAutoscalerConfigMap(clients)
+	if err != nil {
+		return err
+	}
+	configMap.Data["scale-to-zero-threshold"] = threshold
+	_, err = clients.Kube.CoreV1().ConfigMaps("knative-serving-system").Update(configMap)
+	return err
+}
+
+func setup(t *testing.T) *test.Clients {
 	clients := Setup(t)
+
+	configMap, err := getAutoscalerConfigMap(clients)
+	if err != nil {
+		log.Printf("Unable to retrieve the autoscale configMap. Assuming a ScaleToZero value of '5m'. %v", err)
+		initialScaleToZeroThreshold = "5m"
+	} else {
+		initialScaleToZeroThreshold = configMap.Data["scale-to-zero-threshold"]
+	}
+	return clients
+}
+
+func tearDown(clients *test.Clients, names test.ResourceNames) {
+	setScaleToZeroThreshold(clients, initialScaleToZeroThreshold)
+	TearDown(clients, names)
+}
+
+func TestAutoscaleUpDownUp(t *testing.T) {
+	clients := setup(t)
 
 	imagePath := strings.Join(
 		[]string{
@@ -87,8 +123,8 @@ func TestAutoscaleUpDownUp(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to create Route and Configuration: %v", err)
 	}
-	test.CleanupOnInterrupt(func() { TearDown(clients, names) })
-	defer TearDown(clients, names)
+	test.CleanupOnInterrupt(func() { tearDown(clients, names) })
+	defer tearDown(clients, names)
 
 	log.Println(`When the Revision can have traffic routed to it,
 	            the Route is marked as Ready.`)
@@ -144,10 +180,20 @@ func TestAutoscaleUpDownUp(t *testing.T) {
 
 	log.Println(`The autoscaler successfully scales down when devoid of
 		    traffic.`)
+
+	log.Println(`Manually setting ScaleToZeroThreshold to '1m' to facilitate
+		    faster testing.`)
+
+	err = setScaleToZeroThreshold(clients, "1m")
+	if err != nil {
+		t.Fatalf(`Unable to set ScaleToZeroThreshold to '1m'. This will
+		          cause the test to time out. Failing fast instead. %v`, err)
+	}
+
 	err = test.WaitForDeploymentState(
 		clients.Kube.ExtensionsV1beta1().Deployments(NamespaceName),
 		deploymentName,
-		isDeploymentScaledDown())
+		isDeploymentScaledToZero())
 	if err != nil {
 		log.Fatalf(`Unable to observe the Deployment named %s scaling
 		           down. %s`, deploymentName, err)
@@ -156,19 +202,15 @@ func TestAutoscaleUpDownUp(t *testing.T) {
 	// Account for the case where scaling up uses all available pods.
 	log.Println("Wait until there are pods available to scale into.")
 	pc := clients.Kube.CoreV1().Pods(NamespaceName)
-	pods, err := pc.List(metav1.ListOptions{})
-	podCount := 0
-	if err != nil {
-		log.Printf("Unable to get pod count. Defaulting to 1.")
-		podCount = 1
-	} else {
-		podCount = len(pods.Items)
-	}
 	err = test.WaitForPodListState(
 		pc,
 		func(p *v1.PodList) (bool, error) {
-			return len(p.Items) < podCount, nil
+			return len(p.Items) == 0, nil
 		})
+
+	log.Println("Scaled down, resetting ScaleToZeroThreshold.")
+
+	setScaleToZeroThreshold(clients, initialScaleToZeroThreshold)
 
 	log.Println(`The autoscaler spins up additional replicas once again when
 	            traffic increases.`)
