baf4b24a29674d42994fdb9e7cc4c54d43a214c0
Markus Thömmes <markusthoemmes@me.com>
2018-06-13 21:24:17 +0200

    Improve concurrent request bucketing in queue-proxy. (#1091)
    * Improve concurrent request bucketing in queue-proxy.
    
    Instead of counting all requests that arrived in a certain bucket as concurrent, the queue proxy now reports the actual maximum concurrency that was present inside of one bucket.
    
    If for example 3 requests arrive at once, the maximum concurrency will be 3 for that bucket. If another arises while the 3 remaining are still open, maximum concurrency is 4. Closing requests results in a decrement on the concurrency immediatly (versus draining the outgoing request queue on quantization which results in the behavior described above).
    
    Closes #1060
    
    * Fix edge case and add a specific test.
    
    * Unify channels and add buffer again.
    
    * Update config documentation.
    
    * Adjust channel typing.

--- cmd/queue/main.go
+++ cmd/queue/main.go
@@ -52,9 +52,7 @@ const (
 	// reporting so that latency in the stat pipeline doesn't
 	// interfere with request handling.
 	statReportingQueueLength = 10
-	// Add enough buffer to keep track of as many requests as can
-	// be handled in a quantum of time. Because the request out
-	// channel isn't drained until the end of a quantum of time.
+	// Add enough buffer to not block request serving on stats collection
 	requestCountingQueueLength = 100
 	// Number of seconds the /quitquitquit handler should wait before
 	// returning.  The purpose is to kill the container alive a little
@@ -75,8 +73,7 @@ var (
 	elaAutoscaler            string
 	elaAutoscalerPort        string
 	statChan                 = make(chan *autoscaler.Stat, statReportingQueueLength)
-	reqInChan                = make(chan queue.Poke, requestCountingQueueLength)
-	reqOutChan               = make(chan queue.Poke, requestCountingQueueLength)
+	reqChan                  = make(chan queue.ReqEvent, requestCountingQueueLength)
 	kubeClient               *kubernetes.Clientset
 	statSink                 *websocket.Conn
 	proxy                    *httputil.ReverseProxy
@@ -157,9 +154,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// Metrics for autoscaling
-	reqInChan <- queue.Poke{}
+	reqChan <- queue.ReqIn
 	defer func() {
-		reqOutChan <- queue.Poke{}
+		reqChan <- queue.ReqOut
 	}()
 	if *concurrencyModel == string(v1alpha1.RevisionRequestConcurrencyModelSingle) {
 		// Enforce single concurrency and breaking
@@ -272,8 +269,7 @@ func main() {
 	bucketTicker := time.NewTicker(*concurrencyQuantumOfTime).C
 	reportTicker := time.NewTicker(time.Second).C
 	queue.NewStats(podName, queue.Channels{
-		ReqInChan:        reqInChan,
-		ReqOutChan:       reqOutChan,
+		ReqChan:          reqChan,
 		QuantizationChan: bucketTicker,
 		ReportChan:       reportTicker,
 		StatChan:         statChan,
