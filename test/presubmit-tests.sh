04ce38d72a20c5e3c97731541954751b527e3396
Adriano Cunha <adrcunha@google.com>
2018-06-18 00:40:15 -0700

    Install dep before running verify-codegen.sh

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -39,6 +39,8 @@ function cleanup() {
 function build_tests() {
   header "Running build tests"
   go build ./cmd/... ./sample/... ./pkg/...
+  # kubekins images don't have dep installed by default
+  go get -u github.com/golang/dep/cmd/dep
   ./hack/verify-codegen.sh
 }
 
