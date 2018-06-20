f30c73c0870e8a36c38bfb884b5d90ed93413a97
Shash Reddy <sreddy@pivotal.io>
2018-06-20 17:57:11 -0400

    Add webhook test coverage

--- pkg/webhook/webhook_test.go
+++ pkg/webhook/webhook_test.go
@@ -17,12 +17,16 @@ package webhook
 
 import (
 	"context"
+	"crypto/tls"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
 	"reflect"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/knative/serving/pkg"
 
 	"go.uber.org/zap"
@@ -464,6 +468,118 @@ func TestUpdatingWebhook(t *testing.T) {
 	currentWebhook, _ := ac.client.AdmissionregistrationV1beta1().MutatingWebhookConfigurations().Get(ac.options.WebhookName, metav1.GetOptions{})
 	if reflect.DeepEqual(currentWebhook.Webhooks, webhook.Webhooks) {
 		t.Fatalf("Expected webhook to be updated")
+	}
+}
+
+func TestRegistrationForAlreadyExistingWebhook(t *testing.T) {
+	_, ac := newNonRunningTestAdmissionController(t, newDefaultOptions())
+	webhook := &admissionregistrationv1beta1.MutatingWebhookConfiguration{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: ac.options.WebhookName,
+		},
+		Webhooks: []admissionregistrationv1beta1.Webhook{
+			{
+				Name:         ac.options.WebhookName,
+				Rules:        []admissionregistrationv1beta1.RuleWithOperations{{}},
+				ClientConfig: admissionregistrationv1beta1.WebhookClientConfig{},
+			},
+		},
+	}
+	createWebhook(ac, webhook)
+
+	ac.options.RegistrationDelay = 1 * time.Millisecond
+	stopCh := make(chan struct{})
+	errCh := make(chan error)
+
+	go func() {
+		errCh <- ac.Run(stopCh)
+	}()
+
+	err := <-errCh
+	if err == nil {
+		t.Fatal("Expected webhook controller to fail")
+	}
+
+	if !strings.Contains(err.Error(), "configmaps") {
+		t.Fatal("Expected error msg to contain configmap key missing error")
+	}
+}
+
+func TestCertConfigurationForAlreadyGeneratedSecret(t *testing.T) {
+	secretName := "test-secret"
+	ns := "test-namespace"
+	opts := newDefaultOptions()
+	opts.SecretName = secretName
+	opts.ServiceNamespace = ns
+	kubeClient, ac := newNonRunningTestAdmissionController(t, opts)
+
+	ctx := context.TODO()
+	newSecret, err := generateSecret(ctx, secretName, ns)
+	if err != nil {
+		t.Fatalf("Failed to generate secret: %v", err)
+	}
+	_, err = kubeClient.CoreV1().Secrets(ns).Create(newSecret)
+	if err != nil {
+		t.Fatalf("Failed to create secret: %v", err)
+	}
+
+	createNamespace(t, ac.client, metav1.NamespaceSystem)
+	createTestConfigMap(t, ac.client)
+
+	tlsConfig, caCert, err := configureCerts(ctx, kubeClient, &ac.options)
+	if err != nil {
+		t.Fatalf("Failed to configure secret: %v", err)
+	}
+	expectedCert, err := tls.X509KeyPair(newSecret.Data[secretServerCert], newSecret.Data[secretServerKey])
+	if err != nil {
+		t.Fatalf("Failed to create cert from x509 key pair: %v", err)
+	}
+
+	if tlsConfig == nil {
+		t.Fatal("Expected TLS config not to be nil")
+	}
+	if len(tlsConfig.Certificates) < 1 {
+		t.Fatalf("Expected TLS Config Cert to be set")
+	}
+
+	if diff := cmp.Diff(expectedCert.Certificate, tlsConfig.Certificates[0].Certificate, cmp.AllowUnexported()); diff != "" {
+		t.Fatalf("Unexpected cert diff (-want, +got) %v", diff)
+	}
+	if diff := cmp.Diff(newSecret.Data[secretCACert], caCert, cmp.AllowUnexported()); diff != "" {
+		t.Fatalf("Unexpected CA cert diff (-want, +got) %v", diff)
+	}
+}
+
+func TestCertConfigurationForGeneratedSecret(t *testing.T) {
+	secretName := "test-secret"
+	ns := "test-namespace"
+	opts := newDefaultOptions()
+	opts.SecretName = secretName
+	opts.ServiceNamespace = ns
+	kubeClient, ac := newNonRunningTestAdmissionController(t, opts)
+
+	ctx := context.TODO()
+	createNamespace(t, ac.client, metav1.NamespaceSystem)
+	createTestConfigMap(t, ac.client)
+
+	tlsConfig, caCert, err := configureCerts(ctx, kubeClient, &ac.options)
+	if err != nil {
+		t.Fatalf("Failed to configure certificates: %v", err)
+	}
+
+	if tlsConfig == nil {
+		t.Fatal("Expected TLS config not to be nil")
+	}
+	if len(tlsConfig.Certificates) < 1 {
+		t.Fatalf("Expected TLS Certfificate to be set on webhook server")
+	}
+
+	p, _ := pem.Decode(caCert)
+	if p == nil {
+		t.Fatalf("Expected PEM encoded CA cert ")
+	}
+	if p.Type != "CERTIFICATE" {
+		t.Fatalf("Expectet type to be CERTIFICATE but got %s", string(p.Type))
 	}
 }
 
