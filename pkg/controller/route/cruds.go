f2b238a19af0a63d5a112725615352c42f9241ed
Scott Nichols <32305648+n3wscott@users.noreply.github.com>
2018-07-25 08:56:35 -0700

    fixing domainInternal disappearing after a second reconcile. (#1672)
    * fixing domainInternal disapearing after a second reconcile.
    
    * not my domain.
    
    * Only one place to set.

--- pkg/controller/route/cruds.go
+++ pkg/controller/route/cruds.go
@@ -81,7 +81,6 @@ func (c *Controller) reconcilePlaceholderService(ctx context.Context, route *v1a
 			return err
 		}
 		logger.Infof("Created service %s", name)
-		route.Status.DomainInternal = resourcenames.K8sServiceFullname(route)
 		c.Recorder.Eventf(route, corev1.EventTypeNormal, "Created", "Created service %q", name)
 	} else if err != nil {
 		return err
@@ -98,6 +97,8 @@ func (c *Controller) reconcilePlaceholderService(ctx context.Context, route *v1a
 			}
 		}
 	}
+	// Update the route's domain internal field
+	route.Status.DomainInternal = resourcenames.K8sServiceFullname(route)
 
 	// TODO(mattmoor): This is where we'd look at the state of the Service and
 	// reflect any necessary state into the Route.
