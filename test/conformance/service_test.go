ddb3e85e628b37d1186438a57b6e88ea6957b877
srinivashegde86 <srinivashegde86@gmail.com>
2018-07-25 17:58:35 -0700

    Log the service object when created from conformance tests (#1705)

--- test/conformance/service_test.go
+++ test/conformance/service_test.go
@@ -139,7 +139,7 @@ func TestRunLatestService(t *testing.T) {
 	test.CleanupOnInterrupt(func() { tearDownService(clients, names) }, logger)
 
 	logger.Info("Creating a new Service")
-	svc, err := clients.Services.Create(test.LatestService(test.Flags.Namespace, names, imagePaths[0]))
+	svc, err := test.CreateLatestService(logger, clients, names, imagePaths[0])
 	if err != nil {
 		t.Fatalf("Failed to create Service: %v", err)
 	}
