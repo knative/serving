c316e597c42fbd1b98904b2b58cb9b58c10f8537
Mark Peek <mark@peek.org>
2018-07-31 22:14:36 -0700

    Fix some spelling errors detected by misspell (#1753)

--- test/service.go
+++ test/service.go
@@ -23,7 +23,7 @@ import (
 )
 
 // CreateLatestService creates a service in namespace with the name names.Service
-// that uses the image specifed by imagePath
+// that uses the image specified by imagePath
 func CreateLatestService(logger *zap.SugaredLogger, clients *Clients, names ResourceNames, imagePath string) (*v1alpha1.Service, error) {
 	service := LatestService(Flags.Namespace, names, imagePath)
 	LogResourceObject(logger, ResourceObjects{Service: service})
