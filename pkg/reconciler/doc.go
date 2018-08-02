369c63348fb4b6af7078ee0be2408779af6b9594
Matt Moore <mattmoor@google.com>
2018-08-02 17:09:27 +0000

    Add a package comment to pkg/reconciler

--- pkg/reconciler/doc.go
+++ pkg/reconciler/doc.go
@@ -14,4 +14,17 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package reconciler defines implementations of the Reconciler interface
+// defined at github.com/knative/pkg/controller.Reconciler.  These implement
+// the basic workhorse functionality of controllers, while leaving the
+// shared controller implementation to manage things like the workqueue.
+//
+// Despite defining a Reconciler, each of the packages here are expected to
+// expose a controller constructor like:
+//    func NewController(...) *controller.Impl { ... }
+// These constructors will:
+// 1. Construct the Reconciler,
+// 2. Construct a controller.Impl with that Reconciler,
+// 3. Wire the assorted informers this Reconciler watches to call appropriate
+//   enqueue methods on the controller.
 package reconciler
