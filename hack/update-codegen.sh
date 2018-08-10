662b4e4009f114c60dbdc7db949f0935d14e79a7
Matt Moore <mattmoor@google.com>
2018-08-10 11:37:55 -0700

    Define the KPA resource. (#1825)
    This defines the initial outline of our KPA resource.  You can think of this resources as the common API that our Revision controller speaks that may be backed by a variety of KPA controllers.
    
    Over time, the Revision controller will become fairly ignorant of autoscaling semantics, and simply instantiate one of these.  For more information, see the linked issue.
    
    Progress towards: https://github.com/knative/serving/issues/1655

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -27,7 +27,7 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SERVING_ROOT}; ls -d -1 ./vendor/k8s.io/code-g
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
   github.com/knative/serving/pkg/client github.com/knative/serving/pkg/apis \
-  "serving:v1alpha1" \
+  "serving:v1alpha1 autoscaling:v1alpha1" \
   --go-header-file ${SERVING_ROOT}/hack/boilerplate/boilerplate.go.txt
 
 # Depends on generate-groups.sh to install bin/deepcopy-gen
