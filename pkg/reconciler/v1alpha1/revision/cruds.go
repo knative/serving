84d891bad1d01c55cf49238e9f4ebec6b002cbf2
Matt Moore <mattmoor@google.com>
2018-08-22 09:58:58 -0700

    Make Revision controller insensitive to ServingState. (#1899)
    This makes the behavior of the Revision controller largely agnostic of the ServingState.  Revision is still the source of truth for ServingState, and it is mutable, but the only real effect is to apply it to the KPA.

--- pkg/reconciler/v1alpha1/revision/cruds.go
+++ pkg/reconciler/v1alpha1/revision/cruds.go
@@ -35,20 +35,14 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
-	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	vpav1alpha1 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/poc.autoscaling.k8s.io/v1alpha1"
 )
 
 func (c *Reconciler) createDeployment(ctx context.Context, rev *v1alpha1.Revision) (*appsv1.Deployment, error) {
 	logger := logging.FromContext(ctx)
 
-	// TODO(mattmoor): REMOVE ME
-	var replicaCount int32 = 1
-	if rev.Spec.ServingState == v1alpha1.RevisionServingStateReserve {
-		replicaCount = 0
-	}
 	deployment := resources.MakeDeployment(rev, c.getLoggingConfig(), c.getNetworkConfig(),
-		c.getObservabilityConfig(), c.getAutoscalerConfig(), c.getControllerConfig(), replicaCount)
+		c.getObservabilityConfig(), c.getAutoscalerConfig(), c.getControllerConfig())
 
 	// Resolve tag image references to digests.
 	if err := c.getResolver().Resolve(deployment); err != nil {
@@ -75,19 +69,6 @@ func (c *Reconciler) checkAndUpdateDeployment(ctx context.Context, rev *v1alpha1
 	// deployment.Spec = desiredDeployment.Spec
 	// d, err := c.KubeClientSet.AppsV1().Deployments(deployment.Namespace).Update(deployment)
 	// return d, WasChanged, err
-}
-
-func (c *Reconciler) deleteDeployment(ctx context.Context, deployment *appsv1.Deployment) error {
-	logger := logging.FromContext(ctx)
-
-	err := c.KubeClientSet.AppsV1().Deployments(deployment.Namespace).Delete(deployment.Name, fgDeleteOptions)
-	if apierrs.IsNotFound(err) {
-		return nil
-	} else if err != nil {
-		logger.Errorf("deployments.Delete for %q failed: %s", deployment.Name, err)
-		return err
-	}
-	return nil
 }
 
 func (c *Reconciler) createKPA(ctx context.Context, rev *v1alpha1.Revision) (*kpa.PodAutoscaler, error) {
@@ -140,39 +121,10 @@ func (c *Reconciler) checkAndUpdateService(ctx context.Context, rev *v1alpha1.Re
 	return d, WasChanged, err
 }
 
-func (c *Reconciler) deleteService(ctx context.Context, svc *corev1.Service) error {
-	logger := logging.FromContext(ctx)
-
-	err := c.KubeClientSet.CoreV1().Services(svc.Namespace).Delete(svc.Name, fgDeleteOptions)
-	if apierrs.IsNotFound(err) {
-		return nil
-	} else if err != nil {
-		logger.Errorf("service.Delete for %q failed: %s", svc.Name, err)
-		return err
-	}
-	return nil
-}
-
 func (c *Reconciler) createVPA(ctx context.Context, rev *v1alpha1.Revision) (*vpav1alpha1.VerticalPodAutoscaler, error) {
 	vpa := resources.MakeVPA(rev)
 
 	return c.vpaClient.PocV1alpha1().VerticalPodAutoscalers(vpa.Namespace).Create(vpa)
-}
-
-func (c *Reconciler) deleteVPA(ctx context.Context, vpa *vpav1alpha1.VerticalPodAutoscaler) error {
-	logger := logging.FromContext(ctx)
-	if !c.getAutoscalerConfig().EnableVPA {
-		return nil
-	}
-
-	err := c.vpaClient.PocV1alpha1().VerticalPodAutoscalers(vpa.Namespace).Delete(vpa.Name, fgDeleteOptions)
-	if apierrs.IsNotFound(err) {
-		return nil
-	} else if err != nil {
-		logger.Errorf("vpa.Delete for %q failed: %v", vpa.Name, err)
-		return err
-	}
-	return nil
 }
 
 func (c *Reconciler) getNetworkConfig() *config.Network {
--- pkg/reconciler/v1alpha1/revision/reconcile_resources.go
+++ pkg/reconciler/v1alpha1/revision/reconcile_resources.go
@@ -24,6 +24,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 
 	commonlogkey "github.com/knative/pkg/logging/logkey"
+	kpav1alpha1 "github.com/knative/serving/pkg/apis/autoscaling/v1alpha1"
 	"github.com/knative/serving/pkg/apis/serving/v1alpha1"
 	"github.com/knative/serving/pkg/logging"
 	"github.com/knative/serving/pkg/logging/logkey"
@@ -48,65 +49,44 @@ func (c *Reconciler) reconcileDeployment(ctx context.Context, rev *v1alpha1.Revi
 	logger := logging.FromContext(ctx).With(zap.String(commonlogkey.Deployment, deploymentName))
 
 	deployment, getDepErr := c.deploymentLister.Deployments(ns).Get(deploymentName)
-	switch rev.Spec.ServingState {
-	case v1alpha1.RevisionServingStateActive, v1alpha1.RevisionServingStateReserve:
-		// When Active or Reserved, deployment should exist and have a particular specification.
-		if apierrs.IsNotFound(getDepErr) {
-			// Deployment does not exist. Create it.
-			rev.Status.MarkDeploying("Deploying")
-			var err error
-			deployment, err = c.createDeployment(ctx, rev)
-			if err != nil {
-				logger.Errorf("Error creating deployment %q: %v", deploymentName, err)
-				return err
-			}
-			logger.Infof("Created deployment %q", deploymentName)
-		} else if getDepErr != nil {
-			logger.Errorf("Error reconciling deployment %q: %v", deploymentName, getDepErr)
-			return getDepErr
-		} else {
-			// Deployment exist. Update the replica count based on the serving state if necessary
-			var changed Changed
-			var err error
-			deployment, changed, err = c.checkAndUpdateDeployment(ctx, rev, deployment)
-			if err != nil {
-				logger.Errorf("Error updating deployment %q: %v", deploymentName, err)
-				return err
-			}
-			if changed == WasChanged {
-				logger.Infof("Updated deployment %q", deploymentName)
-				rev.Status.MarkDeploying("Updating")
-			}
-		}
-
-		// Now that we have a Deployment, determine whether there is any relevant
-		// status to surface in the Revision.
-		if hasDeploymentTimedOut(deployment) {
-			rev.Status.MarkProgressDeadlineExceeded(fmt.Sprintf(
-				"Unable to create pods for more than %d seconds.", resources.ProgressDeadlineSeconds))
-			c.Recorder.Eventf(rev, corev1.EventTypeNormal, "ProgressDeadlineExceeded",
-				"Revision %s not ready due to Deployment timeout", rev.Name)
-		}
-		return nil
-
-	case v1alpha1.RevisionServingStateRetired:
-		// When Retired, we remove the underlying Deployment.
-		if apierrs.IsNotFound(getDepErr) {
-			// If it does not exist, then we have nothing to do.
-			return nil
-		}
-		if err := c.deleteDeployment(ctx, deployment); err != nil {
-			logger.Errorf("Error deleting deployment %q: %v", deploymentName, err)
+	// When Active or Reserved, deployment should exist and have a particular specification.
+	if apierrs.IsNotFound(getDepErr) {
+		// Deployment does not exist. Create it.
+		rev.Status.MarkDeploying("Deploying")
+		var err error
+		deployment, err = c.createDeployment(ctx, rev)
+		if err != nil {
+			logger.Errorf("Error creating deployment %q: %v", deploymentName, err)
 			return err
 		}
-		logger.Infof("Deleted deployment %q", deploymentName)
-		rev.Status.MarkInactive(fmt.Sprintf("Revision %q is Inactive.", rev.Name))
-		return nil
-
-	default:
-		logger.Errorf("Unknown serving state: %v", rev.Spec.ServingState)
-		return nil
+		logger.Infof("Created deployment %q", deploymentName)
+	} else if getDepErr != nil {
+		logger.Errorf("Error reconciling deployment %q: %v", deploymentName, getDepErr)
+		return getDepErr
+	} else {
+		// Deployment exist. Update the replica count based on the serving state if necessary
+		var changed Changed
+		var err error
+		deployment, changed, err = c.checkAndUpdateDeployment(ctx, rev, deployment)
+		if err != nil {
+			logger.Errorf("Error updating deployment %q: %v", deploymentName, err)
+			return err
+		}
+		if changed == WasChanged {
+			logger.Infof("Updated deployment %q", deploymentName)
+			rev.Status.MarkDeploying("Updating")
+		}
 	}
+
+	// Now that we have a Deployment, determine whether there is any relevant
+	// status to surface in the Revision.
+	if hasDeploymentTimedOut(deployment) {
+		rev.Status.MarkProgressDeadlineExceeded(fmt.Sprintf(
+			"Unable to create pods for more than %d seconds.", resources.ProgressDeadlineSeconds))
+		c.Recorder.Eventf(rev, corev1.EventTypeNormal, "ProgressDeadlineExceeded",
+			"Revision %s not ready due to Deployment timeout", rev.Name)
+	}
+	return nil
 }
 
 func (c *Reconciler) reconcileKPA(ctx context.Context, rev *v1alpha1.Revision) error {
@@ -137,7 +117,18 @@ func (c *Reconciler) reconcileKPA(ctx context.Context, rev *v1alpha1.Revision) e
 		}
 	}
 
-	// TODO(mattmoor): Update status based on the KPA status.
+	// Reflect the KPA status in our own.
+	cond := kpa.Status.GetCondition(kpav1alpha1.PodAutoscalerConditionReady)
+	switch {
+	case cond == nil:
+		// TODO(mattmoor): rev.Status.MarkActivating("Deploying", "")
+	case cond.Status == corev1.ConditionUnknown:
+		// TODO(mattmoor): rev.Status.MarkActivating(cond.Reason, cond.Message)
+	case cond.Status == corev1.ConditionFalse:
+		rev.Status.MarkInactive(cond.Message)
+	case cond.Status == corev1.ConditionTrue:
+		// TODO(mattmoor): rev.Status.MarkActive()
+	}
 	return nil
 }
 
@@ -149,93 +140,73 @@ func (c *Reconciler) reconcileService(ctx context.Context, rev *v1alpha1.Revisio
 	rev.Status.ServiceName = serviceName
 
 	service, err := c.serviceLister.Services(ns).Get(serviceName)
-	switch rev.Spec.ServingState {
-	case v1alpha1.RevisionServingStateActive:
-		// When Active, the Service should exist and have a particular specification.
-		if apierrs.IsNotFound(err) {
-			// If it does not exist, then create it.
-			rev.Status.MarkDeploying("Deploying")
-			service, err = c.createService(ctx, rev, resources.MakeK8sService)
-			if err != nil {
-				logger.Errorf("Error creating Service %q: %v", serviceName, err)
-				return err
-			}
-			logger.Infof("Created Service %q", serviceName)
-		} else if err != nil {
-			logger.Errorf("Error reconciling Active Service %q: %v", serviceName, err)
-			return err
-		} else {
-			// If it exists, then make sure if looks as we expect.
-			// It may change if a user edits things around our controller, which we
-			// should not allow, or if our expectations of how the service should look
-			// changes (e.g. we update our controller with new sidecars).
-			var changed Changed
-			service, changed, err = c.checkAndUpdateService(ctx, rev, resources.MakeK8sService, service)
-			if err != nil {
-				logger.Errorf("Error updating Service %q: %v", serviceName, err)
-				return err
-			}
-			if changed == WasChanged {
-				logger.Infof("Updated Service %q", serviceName)
-				rev.Status.MarkDeploying("Updating")
-			}
-		}
-
-		// We cannot determine readiness from the Service directly.  Instead, we look up
-		// the backing Endpoints resource and check it for healthy pods.  The name of the
-		// Endpoints resource matches the Service it backs.
-		endpoints, err := c.endpointsLister.Endpoints(ns).Get(serviceName)
-		if apierrs.IsNotFound(err) {
-			// If it isn't found, then we need to wait for the Service controller to
-			// create it.
-			logger.Infof("Endpoints not created yet %q", serviceName)
-			rev.Status.MarkDeploying("Deploying")
-			return nil
-		} else if err != nil {
-			logger.Errorf("Error checking Active Endpoints %q: %v", serviceName, err)
+	// When Active, the Service should exist and have a particular specification.
+	if apierrs.IsNotFound(err) {
+		// If it does not exist, then create it.
+		rev.Status.MarkDeploying("Deploying")
+		service, err = c.createService(ctx, rev, resources.MakeK8sService)
+		if err != nil {
+			logger.Errorf("Error creating Service %q: %v", serviceName, err)
 			return err
 		}
-		// If the endpoints resource indicates that the Service it sits in front of is ready,
-		// then surface this in our Revision status as resources available (pods were scheduled)
-		// and container healthy (endpoints should be gated by any provided readiness checks).
-		if getIsServiceReady(endpoints) {
-			rev.Status.MarkResourcesAvailable()
-			rev.Status.MarkContainerHealthy()
-			// TODO(mattmoor): How to ensure this only fires once?
-			c.Recorder.Eventf(rev, corev1.EventTypeNormal, "RevisionReady",
-				"Revision becomes ready upon endpoint %q becoming ready", serviceName)
-		} else {
-			// If the endpoints is NOT ready, then check whether it is taking unreasonably
-			// long to become ready and if so mark our revision as having timed out waiting
-			// for the Service to become ready.
-			revisionAge := time.Now().Sub(getRevisionLastTransitionTime(rev))
-			if revisionAge >= serviceTimeoutDuration {
-				rev.Status.MarkServiceTimeout()
-				// TODO(mattmoor): How to ensure this only fires once?
-				c.Recorder.Eventf(rev, corev1.EventTypeWarning, "RevisionFailed",
-					"Revision did not become ready due to endpoint %q", serviceName)
-			}
-		}
-		return nil
-
-	case v1alpha1.RevisionServingStateReserve, v1alpha1.RevisionServingStateRetired:
-		// When Reserve or Retired, we remove the underlying Service.
-		if apierrs.IsNotFound(err) {
-			// If it does not exist, then we have nothing to do.
-			return nil
-		}
-		if err := c.deleteService(ctx, service); err != nil {
-			logger.Errorf("Error deleting Service %q: %v", serviceName, err)
+		logger.Infof("Created Service %q", serviceName)
+	} else if err != nil {
+		logger.Errorf("Error reconciling Active Service %q: %v", serviceName, err)
+		return err
+	} else {
+		// If it exists, then make sure if looks as we expect.
+		// It may change if a user edits things around our controller, which we
+		// should not allow, or if our expectations of how the service should look
+		// changes (e.g. we update our controller with new sidecars).
+		var changed Changed
+		service, changed, err = c.checkAndUpdateService(ctx, rev, resources.MakeK8sService, service)
+		if err != nil {
+			logger.Errorf("Error updating Service %q: %v", serviceName, err)
 			return err
 		}
-		logger.Infof("Deleted Service %q", serviceName)
-		rev.Status.MarkInactive(fmt.Sprintf("Revision %q is Inactive.", rev.Name))
-		return nil
-
-	default:
-		logger.Errorf("Unknown serving state: %v", rev.Spec.ServingState)
-		return nil
+		if changed == WasChanged {
+			logger.Infof("Updated Service %q", serviceName)
+			rev.Status.MarkDeploying("Updating")
+		}
 	}
+
+	// We cannot determine readiness from the Service directly.  Instead, we look up
+	// the backing Endpoints resource and check it for healthy pods.  The name of the
+	// Endpoints resource matches the Service it backs.
+	endpoints, err := c.endpointsLister.Endpoints(ns).Get(serviceName)
+	if apierrs.IsNotFound(err) {
+		// If it isn't found, then we need to wait for the Service controller to
+		// create it.
+		logger.Infof("Endpoints not created yet %q", serviceName)
+		rev.Status.MarkDeploying("Deploying")
+		return nil
+	} else if err != nil {
+		logger.Errorf("Error checking Active Endpoints %q: %v", serviceName, err)
+		return err
+	}
+
+	// If the endpoints resource indicates that the Service it sits in front of is ready,
+	// then surface this in our Revision status as resources available (pods were scheduled)
+	// and container healthy (endpoints should be gated by any provided readiness checks).
+	if getIsServiceReady(endpoints) {
+		rev.Status.MarkResourcesAvailable()
+		rev.Status.MarkContainerHealthy()
+		// TODO(mattmoor): How to ensure this only fires once?
+		c.Recorder.Eventf(rev, corev1.EventTypeNormal, "RevisionReady",
+			"Revision becomes ready upon endpoint %q becoming ready", serviceName)
+	} else {
+		// If the endpoints is NOT ready, then check whether it is taking unreasonably
+		// long to become ready and if so mark our revision as having timed out waiting
+		// for the Service to become ready.
+		revisionAge := time.Now().Sub(getRevisionLastTransitionTime(rev))
+		if revisionAge >= serviceTimeoutDuration {
+			rev.Status.MarkServiceTimeout()
+			// TODO(mattmoor): How to ensure this only fires once?
+			c.Recorder.Eventf(rev, corev1.EventTypeWarning, "RevisionFailed",
+				"Revision did not become ready due to endpoint %q", serviceName)
+		}
+	}
+	return nil
 }
 
 func (c *Reconciler) reconcileFluentdConfigMap(ctx context.Context, rev *v1alpha1.Revision) error {
@@ -275,6 +246,7 @@ func (c *Reconciler) reconcileFluentdConfigMap(ctx context.Context, rev *v1alpha
 	return nil
 }
 
+// TODO(#1876): Move this into the KPA's scope.
 func (c *Reconciler) reconcileVPA(ctx context.Context, rev *v1alpha1.Revision) error {
 	logger := logging.FromContext(ctx)
 	if !c.getAutoscalerConfig().EnableVPA {
@@ -285,45 +257,23 @@ func (c *Reconciler) reconcileVPA(ctx context.Context, rev *v1alpha1.Revision) e
 	vpaName := resourcenames.VPA(rev)
 
 	// TODO(mattmoor): Switch to informer lister once it can reliably be sunk.
-	vpa, err := c.vpaClient.PocV1alpha1().VerticalPodAutoscalers(ns).Get(vpaName, metav1.GetOptions{})
-	switch rev.Spec.ServingState {
-	case v1alpha1.RevisionServingStateActive:
-		// When Active, the VPA should exist and have a particular specification.
-		if apierrs.IsNotFound(err) {
-			// If it does not exist, then create it.
-			vpa, err = c.createVPA(ctx, rev)
-			if err != nil {
-				logger.Errorf("Error creating VPA %q: %v", vpaName, err)
-				return err
-			}
-			logger.Infof("Created VPA %q", vpaName)
-		} else if err != nil {
-			logger.Errorf("Error reconciling Active VPA %q: %v", vpaName, err)
-			return err
-		} else {
-			// TODO(mattmoor): Should we checkAndUpdate the VPA, or would it
-			// suffer similar problems to Deployment?
-		}
-
-		// TODO(mattmoor): We don't predicate the Revision's readiness on any readiness
-		// properties of the autoscaler, but perhaps we should.
-		return nil
-
-	case v1alpha1.RevisionServingStateReserve, v1alpha1.RevisionServingStateRetired:
-		// When Reserve or Retired, we remove the underlying VPA.
-		if apierrs.IsNotFound(err) {
-			// If it does not exist, then we have nothing to do.
-			return nil
-		}
-		if err := c.deleteVPA(ctx, vpa); err != nil {
-			logger.Errorf("Error deleting VPA %q: %v", vpaName, err)
+	_, err := c.vpaClient.PocV1alpha1().VerticalPodAutoscalers(ns).Get(vpaName, metav1.GetOptions{})
+	// When Active, the VPA should exist and have a particular specification.
+	if apierrs.IsNotFound(err) {
+		// If it does not exist, then create it.
+		_, err = c.createVPA(ctx, rev)
+		if err != nil {
+			logger.Errorf("Error creating VPA %q: %v", vpaName, err)
 			return err
 		}
-		logger.Infof("Deleted VPA %q", vpaName)
-		return nil
-
-	default:
-		logger.Errorf("Unknown serving state: %v", rev.Spec.ServingState)
-		return nil
+		logger.Infof("Created VPA %q", vpaName)
+	} else if err != nil {
+		logger.Errorf("Error reconciling Active VPA %q: %v", vpaName, err)
+		return err
+	} else {
+		// TODO(mattmoor): Should we checkAndUpdate the VPA, or would it
+		// suffer similar problems to Deployment?
 	}
+
+	return nil
 }
