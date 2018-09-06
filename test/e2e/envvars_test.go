7cb78574e3b31d91d881e28a4a77aca9850a8705
Nader Ziada <nziada@pivotal.io>
2018-09-06 11:36:31 -0400

    switch env var e2e test to use service

--- test/e2e/envvars_test.go
+++ test/e2e/envvars_test.go
@@ -25,6 +25,8 @@ import (
 
 	pkgTest "github.com/knative/pkg/test"
 	"github.com/knative/pkg/test/logging"
+	"github.com/knative/serving/pkg/apis/serving/v1alpha1"
+	serviceresourcenames "github.com/knative/serving/pkg/reconciler/v1alpha1/service/resources/names"
 	"github.com/knative/serving/test"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -37,13 +39,38 @@ func TestEnvVars(t *testing.T) {
 
 	var imagePath = test.ImagePath("envvars")
 
-	logger.Infof("Creating a new Route and Configuration")
-	names, err := CreateRouteAndConfig(clients, logger, imagePath)
-	if err != nil {
-		t.Fatalf("Failed to create Route and Configuration: %v", err)
-	}
+	var names test.ResourceNames
+	names.Service = test.AppendRandomString("yashiki", logger)
+
 	test.CleanupOnInterrupt(func() { TearDown(clients, names, logger) }, logger)
 	defer TearDown(clients, names, logger)
+
+	logger.Info("Creating a new Service")
+	svc, err := test.CreateLatestService(logger, clients, names, imagePath)
+	if err != nil {
+		t.Fatalf("Failed to create Service: %v", err)
+	}
+	names.Route = serviceresourcenames.Route(svc)
+	names.Config = serviceresourcenames.Configuration(svc)
+
+	var revisionName string
+	logger.Info("The Service will be updated with the name of the Revision once it is created")
+	err = test.WaitForServiceState(clients.ServingClient, names.Service, func(s *v1alpha1.Service) (bool, error) {
+		if s.Status.LatestCreatedRevisionName != names.Revision {
+			revisionName = s.Status.LatestCreatedRevisionName
+			return true, nil
+		}
+		return false, nil
+	}, "ServiceUpdatedWithRevision")
+	if err != nil {
+		t.Fatalf("Service %s was not updated with the new revision: %v", names.Service, err)
+	}
+	names.Revision = revisionName
+
+	logger.Info("When the Service reports as Ready, everything should be ready.")
+	if err := test.WaitForServiceState(clients.ServingClient, names.Service, test.IsServiceReady, "ServiceIsReady"); err != nil {
+		t.Fatalf("The Service %s was not marked as Ready to serve traffic to Revision %s: %v", names.Service, names.Revision, err)
+	}
 
 	logger.Infof("When the Revision can have traffic routed to it, the Route is marked as Ready.")
 	if err := test.WaitForRouteState(clients.ServingClient, names.Route, test.IsRouteReady, "RouteIsReady"); err != nil {
@@ -56,7 +83,7 @@ func TestEnvVars(t *testing.T) {
 	}
 	domain := route.Status.Domain
 
-	envVarsExpectedOutput := fmt.Sprintf("Here are our env vars service: - configuration:%s - revision:%s", names.Config, names.Revision)
+	envVarsExpectedOutput := fmt.Sprintf("Here are our env vars service:%s - configuration:%s - revision:%s", names.Service, names.Config, names.Revision)
 
 	_, err = pkgTest.WaitForEndpointState(
 		clients.KubeClient,
