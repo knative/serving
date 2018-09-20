009e8c9a4a7e0e273d66b12e505662ba01d3e8ec
Scott Nichols <32305648+n3wscott@users.noreply.github.com>
2018-09-20 16:15:19 -0700

    Update serving to use new FieldError.Also (#2067)
    * all tests work again.
    
    * update resources.
    
    * update pkg

--- vendor/github.com/knative/pkg/client/informers/externalversions/factory.go
+++ vendor/github.com/knative/pkg/client/informers/externalversions/factory.go
@@ -25,7 +25,6 @@ import (
 
 	versioned "github.com/knative/pkg/client/clientset/versioned"
 	authentication "github.com/knative/pkg/client/informers/externalversions/authentication"
-	duck "github.com/knative/pkg/client/informers/externalversions/duck"
 	internalinterfaces "github.com/knative/pkg/client/informers/externalversions/internalinterfaces"
 	istio "github.com/knative/pkg/client/informers/externalversions/istio"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -175,16 +174,11 @@ type SharedInformerFactory interface {
 	WaitForCacheSync(stopCh <-chan struct{}) map[reflect.Type]bool
 
 	Authentication() authentication.Interface
-	Duck() duck.Interface
 	Networking() istio.Interface
 }
 
 func (f *sharedInformerFactory) Authentication() authentication.Interface {
 	return authentication.New(f, f.namespace, f.tweakListOptions)
-}
-
-func (f *sharedInformerFactory) Duck() duck.Interface {
-	return duck.New(f, f.namespace, f.tweakListOptions)
 }
 
 func (f *sharedInformerFactory) Networking() istio.Interface {
