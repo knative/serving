0997a741a6e9e1ebea6812015f4b02008fd4fd11
Ivan Mikushin <imikushin@vmware.com>
2018-09-25 16:37:33 -0700

    sort FieldError paths alphabetically

--- pkg/apis/serving/v1alpha1/service_validation_test.go
+++ pkg/apis/serving/v1alpha1/service_validation_test.go
@@ -100,14 +100,20 @@ func TestServiceValidation(t *testing.T) {
 		},
 		want: &apis.FieldError{
 			Message: "expected exactly one, got both",
-			Paths:   []string{"spec.runLatest", "spec.pinned"},
+			Paths: []string{
+				"spec.pinned",
+				"spec.runLatest",
+			},
 		},
 	}, {
 		name: "invalid neither type",
 		s:    &Service{},
 		want: &apis.FieldError{
 			Message: "expected exactly one, got neither",
-			Paths:   []string{"spec.runLatest", "spec.pinned"},
+			Paths: []string{
+				"spec.pinned",
+				"spec.runLatest",
+			},
 		},
 	}, {
 		name: "invalid runLatest",
