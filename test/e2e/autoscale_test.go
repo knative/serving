017442a445535d9486367c1c74f495907d2edabc
Markus Thömmes <markusthoemmes@me.com>
2018-10-08 12:52:31 +0200

    Print body for failure interpreted as string.

--- test/e2e/autoscale_test.go
+++ test/e2e/autoscale_test.go
@@ -92,7 +92,7 @@ func generateTraffic(clients *test.Clients, logger *logging.BaseLogger, concurre
 					if res.StatusCode != http.StatusOK {
 						logger.Errorf("non 200 response %v", res.StatusCode)
 						logger.Errorf("response headers: %v", res.Header)
-						logger.Errorf("response body: %v", res.Body)
+						logger.Errorf("response body: %v", string(res.Body[:]))
 						continue
 					}
 					mux.Lock()
