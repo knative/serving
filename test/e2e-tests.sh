ba291f4fd4a22d112fbdee0404525a64841e3103
Adriano Cunha <adrcunha@google.com>
2018-10-10 17:11:29 -0700

    Increase E2E tests timeout to 20 minutes
    Currently 10 minutes may not be enough, for example as in #1701.
    
    Backport of #1702.

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -139,7 +139,7 @@ function run_e2e_tests() {
   kubectl label namespace $2 istio-injection=enabled --overwrite
   local options=""
   (( EMIT_METRICS )) && options="-emitmetrics"
-  report_go_test -v -tags=e2e -count=1 ./test/$1 -dockerrepo gcr.io/knative-tests/test-images/$3 ${options}
+  report_go_test -v -tags=e2e -count=1 -timeout=20m ./test/$1 -dockerrepo gcr.io/knative-tests/test-images/$3 ${options}
 
   local result=$?
   [[ ${result} -ne 0 ]] && dump_stack_info
