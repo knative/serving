5ed93af1045498fbc5af43b6d9b585521b6dfb46
Jon Johnson <jonjohnson@google.com>
2018-10-17 19:50:46 +0000

    Remove obsolete unit tests

--- pkg/webhook/webhook_test.go
+++ pkg/webhook/webhook_test.go
@@ -236,30 +236,6 @@ func TestValidRouteNoChanges(t *testing.T) {
 	expectPatches(t, resp.Patch, []jsonpatch.JsonPatchOperation{})
 }
 
-func TestInvalidOldRoute(t *testing.T) {
-	_, ac := newNonRunningTestAdmissionController(t, newDefaultOptions())
-	new := createRoute(1, testRouteName)
-	newBytes, err := json.Marshal(new)
-	if err != nil {
-		t.Errorf("Marshal(%v) = %v", new, err)
-	}
-	oldBytes := []byte(`{"bad": "field"}`)
-	resp := ac.admit(TestContextWithLogger(t), createUpdateRouteRaw(oldBytes, newBytes))
-	expectFailsWith(t, resp, `unknown field "bad"`)
-}
-
-func TestInvalidNewRoute(t *testing.T) {
-	_, ac := newNonRunningTestAdmissionController(t, newDefaultOptions())
-	old := createRoute(1, testRouteName)
-	oldBytes, err := json.Marshal(old)
-	if err != nil {
-		t.Errorf("Marshal(%v) = %v", old, err)
-	}
-	newBytes := []byte(`{"sepc": {}}`)
-	resp := ac.admit(TestContextWithLogger(t), createUpdateRouteRaw(oldBytes, newBytes))
-	expectFailsWith(t, resp, `unknown field "sepc"`)
-}
-
 func TestValidRouteChanges(t *testing.T) {
 	_, ac := newNonRunningTestAdmissionController(t, newDefaultOptions())
 	old := createRoute(1, testRouteName)
