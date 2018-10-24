eef2a3913d5b4c002dc5504b5991091e779b3b38
Joseph Burnett <josephburnett79@gmail.com>
2018-10-23 20:08:29 -0400

    Increase E2E test timeout to 40 minutes.

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -81,7 +81,7 @@ kubectl create namespace serving-tests
 options=""
 (( EMIT_METRICS )) && options="-emitmetrics"
 report_go_test \
-  -v -tags=e2e -count=1 -timeout=20m \
+  -v -tags=e2e -count=1 -timeout=40m \
   ./test/conformance ./test/e2e \
   ${options} || fail_test
 
