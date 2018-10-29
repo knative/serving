cfc65602a5d83ed8c982e0debdfa3a78c7ab8366
Markus Thömmes <markusthoemmes@me.com>
2018-10-29 18:04:33 +0100

    Simplify internal activator header usage. (#2327)
    * Simplify internal activator header usage.
    
    * Remove the superfluous header in the handler where its used.
    
    * Roll back headerpruning.
    
    * Rollback header.go change.

--- pkg/activator/activator.go
+++ pkg/activator/activator.go
@@ -21,10 +21,6 @@ const (
 	K8sServiceName = "activator-service"
 	// RequestCountHTTPHeader is the header key for number of tries
 	RequestCountHTTPHeader string = "knative-activator-num-retries"
-	// ResponseCountHTTPHeader is the header key for number of tries.
-	// TODO this header is used to pass state between component of the
-	// activator, it is not used outside the activator and should be removed
-	ResponseCountHTTPHeader string = "knative-internal-activator-attempts"
 	// RevisionHeaderName is the header key for revision name
 	RevisionHeaderName string = "knative-serving-revision"
 	// RevisionHeaderNamespace is the header key for revision's namespace
