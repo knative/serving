7c4f3c5be32bc74addeae53e2f954fb5f790d90f
Matt Moore <mattmoor@google.com>
2018-11-16 10:39:18 -0800

    Convert the Revision controller to use functional options to reduce boilerplate. (#2447)
    * Refactor the Service following #2447
    
    This starts to factor out functional options for setting up our table tests, which reduces the verbosity a fair amount.
    
    I've broken off Service from a broader set of changes in #2447 to make things a bit more manageable to review.
    
    * Simplify some of the object construction in Revision controller. (#2446)
    
    An initial cleanup while looking into: https://github.com/knative/serving/issues/2442

--- pkg/reconciler/v1alpha1/testing/functional.go
+++ pkg/reconciler/v1alpha1/testing/functional.go
@@ -17,8 +17,11 @@ limitations under the License.
 package testing
 
 import (
+	"fmt"
 	"time"
 
+	"github.com/knative/pkg/apis"
+	"github.com/knative/pkg/apis/duck"
 	duckv1alpha1 "github.com/knative/pkg/apis/duck/v1alpha1"
 	kpav1alpha1 "github.com/knative/serving/pkg/apis/autoscaling/v1alpha1"
 	"github.com/knative/serving/pkg/apis/serving/v1alpha1"
@@ -30,6 +33,47 @@ import (
 
 // BuildOption enables further configuration of a Build.
 type BuildOption func(*unstructured.Unstructured)
+
+// WithSucceededTrue updates the status of the provided unstructured Build object with the
+// expected success condition.
+func WithSucceededTrue(orig *unstructured.Unstructured) {
+	cp := orig.DeepCopy()
+	cp.Object["status"] = map[string]interface{}{"conditions": []duckv1alpha1.Condition{{
+		Type:   duckv1alpha1.ConditionSucceeded,
+		Status: corev1.ConditionTrue,
+	}}}
+	duck.FromUnstructured(cp, orig) // prevent panic in b.DeepCopy()
+}
+
+// WithSucceededUnknown updates the status of the provided unstructured Build object with the
+// expected in-flight condition.
+func WithSucceededUnknown(reason, message string) BuildOption {
+	return func(orig *unstructured.Unstructured) {
+		cp := orig.DeepCopy()
+		cp.Object["status"] = map[string]interface{}{"conditions": []duckv1alpha1.Condition{{
+			Type:    duckv1alpha1.ConditionSucceeded,
+			Status:  corev1.ConditionUnknown,
+			Reason:  reason,
+			Message: message,
+		}}}
+		duck.FromUnstructured(cp, orig) // prevent panic in b.DeepCopy()
+	}
+}
+
+// WithSucceededFalse updates the status of the provided unstructured Build object with the
+// expected failure condition.
+func WithSucceededFalse(reason, message string) BuildOption {
+	return func(orig *unstructured.Unstructured) {
+		cp := orig.DeepCopy()
+		cp.Object["status"] = map[string]interface{}{"conditions": []duckv1alpha1.Condition{{
+			Type:    duckv1alpha1.ConditionSucceeded,
+			Status:  corev1.ConditionFalse,
+			Reason:  reason,
+			Message: message,
+		}}}
+		duck.FromUnstructured(cp, orig) // prevent panic in b.DeepCopy()
+	}
+}
 
 // ServiceOption enables further configuration of a Service.
 type ServiceOption func(*v1alpha1.Service)
@@ -46,6 +90,116 @@ var (
 		},
 	}
 )
+
+// WithRunLatestRollout configures the Service to use a "runLatest" rollout.
+func WithRunLatestRollout(s *v1alpha1.Service) {
+	s.Spec = v1alpha1.ServiceSpec{
+		RunLatest: &v1alpha1.RunLatestType{
+			Configuration: configSpec,
+		},
+	}
+}
+
+// WithPinnedRollout configures the Service to use a "pinned" rollout,
+// which is pinned to the named revision.
+func WithPinnedRollout(name string) ServiceOption {
+	return func(s *v1alpha1.Service) {
+		s.Spec = v1alpha1.ServiceSpec{
+			Pinned: &v1alpha1.PinnedType{
+				RevisionName:  name,
+				Configuration: configSpec,
+			},
+		}
+	}
+}
+
+// WithReleaseRollout configures the Service to use a "release" rollout,
+// which spans the provided revisions.
+func WithReleaseRollout(names ...string) ServiceOption {
+	return func(s *v1alpha1.Service) {
+		s.Spec = v1alpha1.ServiceSpec{
+			Release: &v1alpha1.ReleaseType{
+				Revisions:     names,
+				Configuration: configSpec,
+			},
+		}
+	}
+}
+
+// WithManualRollout configures the Service to use a "manual" rollout.
+func WithManualRollout(s *v1alpha1.Service) {
+	s.Spec = v1alpha1.ServiceSpec{
+		Manual: &v1alpha1.ManualType{},
+	}
+}
+
+// WithInitSvcConditions initializes the Service's conditions.
+func WithInitSvcConditions(s *v1alpha1.Service) {
+	s.Status.InitializeConditions()
+}
+
+// WithManualStatus configures the Service to have the appropriate
+// status for a "manual" rollout type.
+func WithManualStatus(s *v1alpha1.Service) {
+	s.Status.SetManualStatus()
+}
+
+// WithReadyRoute reflects the Route's readiness in the Service resource.
+func WithReadyRoute(s *v1alpha1.Service) {
+	s.Status.PropagateRouteStatus(v1alpha1.RouteStatus{
+		Conditions: []duckv1alpha1.Condition{{
+			Type:   "Ready",
+			Status: "True",
+		}},
+	})
+}
+
+// WithFailedRoute reflects a Route's failure in the Service resource.
+func WithFailedRoute(reason, message string) ServiceOption {
+	return func(s *v1alpha1.Service) {
+		s.Status.PropagateRouteStatus(v1alpha1.RouteStatus{
+			Conditions: []duckv1alpha1.Condition{{
+				Type:    "Ready",
+				Status:  "False",
+				Reason:  reason,
+				Message: message,
+			}},
+		})
+	}
+}
+
+// WithReadyConfig reflects the Configuration's readiness in the Service
+// resource.  This must coincide with the setting of Latest{Created,Ready}
+// to the provided revision name.
+func WithReadyConfig(name string) ServiceOption {
+	return func(s *v1alpha1.Service) {
+		s.Status.PropagateConfigurationStatus(v1alpha1.ConfigurationStatus{
+			LatestCreatedRevisionName: name,
+			LatestReadyRevisionName:   name,
+			Conditions: []duckv1alpha1.Condition{{
+				Type:   "Ready",
+				Status: "True",
+			}},
+		})
+	}
+}
+
+// WithFailedConfig reflects the Configuration's failure in the Service
+// resource.  The failing revision's name is reflected in LatestCreated.
+func WithFailedConfig(name, reason, message string) ServiceOption {
+	return func(s *v1alpha1.Service) {
+		s.Status.PropagateConfigurationStatus(v1alpha1.ConfigurationStatus{
+			LatestCreatedRevisionName: name,
+			Conditions: []duckv1alpha1.Condition{{
+				Type:   "Ready",
+				Status: "False",
+				Reason: reason,
+				Message: fmt.Sprintf("Revision %q failed with message: %q.",
+					name, message),
+			}},
+		})
+	}
+}
 
 // RouteOption enables further configuration of a Route.
 type RouteOption func(*v1alpha1.Route)
@@ -147,6 +301,11 @@ func WithRevConcurrencyModel(ss v1alpha1.RevisionRequestConcurrencyModelType) Re
 	}
 }
 
+// WithLogURL sets the .Status.LogURL to the expected value.
+func WithLogURL(r *v1alpha1.Revision) {
+	r.Status.LogURL = "http://logger.io/test-uid"
+}
+
 // WithCreationTimestamp sets the Revision's timestamp to the provided time.
 // TODO(mattmoor): Ideally this could be a more generic Option and use meta.Accessor,
 // but unfortunately Go's type system cannot support that.
@@ -166,6 +325,53 @@ func WithNoBuild(r *v1alpha1.Revision) {
 			Reason: "NoBuild",
 		}},
 	})
+}
+
+// WithOngoingBuild propagates the status of an in-progress Build to the Revision's status.
+func WithOngoingBuild(r *v1alpha1.Revision) {
+	r.Status.PropagateBuildStatus(duckv1alpha1.KResourceStatus{
+		Conditions: []duckv1alpha1.Condition{{
+			Type:   duckv1alpha1.ConditionSucceeded,
+			Status: corev1.ConditionUnknown,
+		}},
+	})
+}
+
+// WithSuccessfulBuild propagates the status of a successful Build to the Revision's status.
+func WithSuccessfulBuild(r *v1alpha1.Revision) {
+	r.Status.PropagateBuildStatus(duckv1alpha1.KResourceStatus{
+		Conditions: []duckv1alpha1.Condition{{
+			Type:   duckv1alpha1.ConditionSucceeded,
+			Status: corev1.ConditionTrue,
+		}},
+	})
+}
+
+// WithFailedBuild propagates the status of a failed Build to the Revision's status.
+func WithFailedBuild(reason, message string) RevisionOption {
+	return func(r *v1alpha1.Revision) {
+		r.Status.PropagateBuildStatus(duckv1alpha1.KResourceStatus{
+			Conditions: []duckv1alpha1.Condition{{
+				Type:    duckv1alpha1.ConditionSucceeded,
+				Status:  corev1.ConditionFalse,
+				Reason:  reason,
+				Message: message,
+			}},
+		})
+	}
+}
+
+// WithEmptyLTTs clears the LastTransitionTime fields on all of the conditions of the
+// provided Revision.
+func WithEmptyLTTs(r *v1alpha1.Revision) {
+	conds := r.Status.Conditions
+	for i, c := range conds {
+		// The LTT defaults and is long enough ago that we expire waiting
+		// on the Endpoints to become ready.
+		c.LastTransitionTime = apis.VolatileTime{}
+		conds[i] = c
+	}
+	r.Status.SetConditions(conds)
 }
 
 // WithLastPinned updates the "last pinned" annotation to the provided timestamp.
@@ -188,6 +394,38 @@ func MarkActive(r *v1alpha1.Revision) {
 	r.Status.MarkActive()
 }
 
+// MarkInactive calls .Status.MarkInactive on the Revision.
+func MarkInactive(reason, message string) RevisionOption {
+	return func(r *v1alpha1.Revision) {
+		r.Status.MarkInactive(reason, message)
+	}
+}
+
+// MarkActivating calls .Status.MarkActivating on the Revision.
+func MarkActivating(reason, message string) RevisionOption {
+	return func(r *v1alpha1.Revision) {
+		r.Status.MarkActivating(reason, message)
+	}
+}
+
+// MarkDeploying calls .Status.MarkDeploying on the Revision.
+func MarkDeploying(reason string) RevisionOption {
+	return func(r *v1alpha1.Revision) {
+		r.Status.MarkDeploying(reason)
+	}
+}
+
+// MarkProgressDeadlineExceeded calls the method of the same name on the Revision
+// with the message we expect the Revision Reconciler to pass.
+func MarkProgressDeadlineExceeded(r *v1alpha1.Revision) {
+	r.Status.MarkProgressDeadlineExceeded("Unable to create pods for more than 120 seconds.")
+}
+
+// MarkServiceTimeout calls .Status.MarkServiceTimeout on the Revision.
+func MarkServiceTimeout(r *v1alpha1.Revision) {
+	r.Status.MarkServiceTimeout()
+}
+
 // MarkContainerMissing calls .Status.MarkContainerMissing on the Revision.
 func MarkContainerMissing(rev *v1alpha1.Revision) {
 	rev.Status.MarkContainerMissing("It's the end of the world as we know it")
@@ -205,8 +443,42 @@ func MarkRevisionReady(r *v1alpha1.Revision) {
 // KPAOption enables further configuration of the PodAutoscaler.
 type KPAOption func(*kpav1alpha1.PodAutoscaler)
 
+// WithTraffic updates the KPA to reflect it receiving traffic.
+func WithTraffic(kpa *kpav1alpha1.PodAutoscaler) {
+	kpa.Status.MarkActive()
+}
+
+// WithBufferedTraffic updates the KPA to reflect that it has received
+// and buffered traffic while it is being activated.
+func WithBufferedTraffic(reason, message string) KPAOption {
+	return func(kpa *kpav1alpha1.PodAutoscaler) {
+		kpa.Status.MarkActivating(reason, message)
+	}
+}
+
+// WithNoTraffic updates the KPA to reflect the fact that it is not
+// receiving traffic.
+func WithNoTraffic(reason, message string) KPAOption {
+	return func(kpa *kpav1alpha1.PodAutoscaler) {
+		kpa.Status.MarkInactive(reason, message)
+	}
+}
+
 // K8sServiceOption enables further configuration of the Kubernetes Service.
 type K8sServiceOption func(*corev1.Service)
 
+// MutateK8sService changes the service in a way that must be reconciled.
+func MutateK8sService(svc *corev1.Service) {
+	// An effective hammer ;-P
+	svc.Spec = corev1.ServiceSpec{}
+}
+
 // EndpointsOption enables further configuration of the Kubernetes Endpoints.
 type EndpointsOption func(*corev1.Endpoints)
+
+// WithSubsets adds subsets to the body of a Revision, enabling us to refer readiness.
+func WithSubsets(ep *corev1.Endpoints) {
+	ep.Subsets = []corev1.EndpointSubset{{
+		Addresses: []corev1.EndpointAddress{{IP: "127.0.0.1"}},
+	}}
+}
