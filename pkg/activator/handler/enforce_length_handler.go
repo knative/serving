79278f3a3ff879c7beb02e50c7a9bbb92e694186
Tanzeeb Khalili <tkhalili@pivotal.io>
2018-11-23 14:03:33 -0500

    Enforce max content length for streaming requests in activator

--- pkg/activator/handler/enforce_length_handler.go
+++ pkg/activator/handler/enforce_length_handler.go
@@ -14,8 +14,26 @@ limitations under the License.
 package handler
 
 import (
+	"io"
 	"net/http"
 )
+
+func limitReadCloser(rc io.ReadCloser, l int64) io.ReadCloser {
+	return &readCloser{io.LimitReader(rc, l), rc}
+}
+
+type readCloser struct {
+	Reader io.Reader
+	Closer io.Closer
+}
+
+func (lrc *readCloser) Read(b []byte) (int, error) {
+	return lrc.Reader.Read(b)
+}
+
+func (lrc *readCloser) Close() error {
+	return lrc.Closer.Close()
+}
 
 // EnforceMaxContentLengthHandler prevents uploads larger than `MaxContentLengthBytes`
 type EnforceMaxContentLengthHandler struct {
@@ -24,10 +42,14 @@ type EnforceMaxContentLengthHandler struct {
 }
 
 func (h *EnforceMaxContentLengthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// If we have a ContentLength, we can fail early
 	if r.ContentLength > h.MaxContentLengthBytes {
 		w.WriteHeader(http.StatusRequestEntityTooLarge)
 		return
 	}
+
+	// Enforce MaxContentLengthBytes
+	r.Body = limitReadCloser(r.Body, h.MaxContentLengthBytes)
 
 	h.NextHandler.ServeHTTP(w, r)
 }
--- pkg/activator/handler/enforce_length_handler_test.go
+++ pkg/activator/handler/enforce_length_handler_test.go
@@ -13,51 +13,104 @@ limitations under the License.
 package handler
 
 import (
-	"bytes"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
 func TestEnforceMaxContentLengthHandler(t *testing.T) {
 	payload := "SAMPLE PAYLOAD"
 
+	// httptest.NewRequest will set ContentLength for strings.NewReader
+	fixed := func(p string) io.Reader { return strings.NewReader(p) }
+	stream := func(p string) io.Reader { return ioutil.NopCloser(strings.NewReader(p)) }
+
 	examples := []struct {
 		label     string
 		maxUpload int
+		request   io.Reader
+		response  string
 		status    int
 	}{
 		{
-			label:     "under",
+			label:     "under with ContentLength",
 			maxUpload: len(payload) + 1,
+			request:   fixed(payload),
 			status:    http.StatusOK,
+			response:  payload,
 		},
 		{
-			label:     "equal",
+			label:     "equal with ContentLength",
 			maxUpload: len(payload),
+			request:   fixed(payload),
 			status:    http.StatusOK,
+			response:  payload,
 		},
 		{
-			label:     "over",
+			label:     "over with ContentLength",
 			maxUpload: len(payload) - 1,
+			request:   fixed(payload),
 			status:    http.StatusRequestEntityTooLarge,
+			response:  "",
+		},
+		{
+			label:     "under without ContentLength",
+			maxUpload: len(payload) + 1,
+			request:   stream(payload),
+			status:    http.StatusOK,
+			response:  payload,
+		},
+		{
+			label:     "equal without ContentLength",
+			maxUpload: len(payload),
+			request:   stream(payload),
+			status:    http.StatusOK,
+			response:  payload,
+		},
+		{
+			label:     "over without ContentLength",
+			maxUpload: len(payload) - 1,
+			request:   stream(payload),
+			status:    http.StatusOK,
+			response:  payload[0 : len(payload)-1],
 		},
 	}
 
 	for _, e := range examples {
 		t.Run(e.label, func(t *testing.T) {
+			// Return 200 and request body
 			baseHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(http.StatusOK)
+
+				if r.Body != nil {
+					body, err := ioutil.ReadAll(r.Body)
+					if err != nil {
+						t.Fatalf("Error request reading body")
+					}
+					w.Write(body)
+				}
 			})
 			handler := EnforceMaxContentLengthHandler{NextHandler: baseHandler, MaxContentLengthBytes: int64(e.maxUpload)}
 
 			resp := httptest.NewRecorder()
-			req := httptest.NewRequest("POST", "http://example.com", bytes.NewBufferString(payload))
+			req := httptest.NewRequest("POST", "http://example.com", e.request)
 
 			handler.ServeHTTP(resp, req)
 
+			gotBody, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("Error request reading body")
+			}
+
+			if e.response != string(gotBody) {
+				t.Errorf("Unexpected response body. Want %q, got %q", e.response, gotBody)
+			}
+
 			if resp.Code != e.status {
-				t.Errorf("Unexpected response status for payload %q. Want %d, got %d", payload, e.status, resp.Code)
+				t.Errorf("Unexpected response status. Want %d, got %d", e.status, resp.Code)
 			}
 		})
 	}
