db32f4536718c91f591c95795db81ea7627f275d
Nghia Tran <nghia@google.com>
2018-11-30 15:09:47 -0500

    Make e2e tests @ HEAD wait for knative-ingressgateway if exists.

--- test/cluster.sh
+++ test/cluster.sh
@@ -111,6 +111,9 @@ function install_knative_serving() {
 
   wait_until_pods_running knative-serving || return 1
   wait_until_pods_running istio-system || return 1
+  if kubectl get -n istio-system knative-ingressgateway; then
+    wait_until_service_has_external_ip istio-system istio-ingressgateway
+  fi
   wait_until_service_has_external_ip istio-system istio-ingressgateway
 }
 
