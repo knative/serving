dc3e74f382eafec69c3c9873f6780dbc402b213e
Victor Agababov <vagababov@users.noreply.github.com>
2018-12-10 14:51:15 -0800

    Some minor lint errors removal and, imo, better error messages. (#2673)
    * Some minor lint errors removal and, imo, better error messages.
    
    * Fix the typo in the comment

--- pkg/apis/serving/v1alpha1/route_validation_test.go
+++ pkg/apis/serving/v1alpha1/route_validation_test.go
@@ -322,14 +322,14 @@ func TestTrafficTargetValidation(t *testing.T) {
 			RevisionName: "foo",
 			Percent:      -5,
 		},
-		want: apis.ErrInvalidValue("-5", "percent"),
+		want: apis.ErrOutOfBoundsValue("-5", "0", "100", "percent"),
 	}, {
 		name: "invalid percent too high",
 		tt: &TrafficTarget{
 			RevisionName: "foo",
 			Percent:      101,
 		},
-		want: apis.ErrInvalidValue("101", "percent"),
+		want: apis.ErrOutOfBoundsValue("101", "0", "100", "percent"),
 	}}
 
 	for _, test := range tests {
