c5e55a65f9cc4ddf624ad498f5a2383fb211021d
Victor Agababov <vagababov@gmail.com>
2018-12-10 12:32:08 -0800

    Clean up the Diagnose.go file, which had some very shady code in it.
    - Clean up all the logging
    - Clean up how the timer operation was done -- channels and timers exist
      for a reason.

--- test/e2e/diagnose.go
+++ test/e2e/diagnose.go
@@ -1,7 +1,6 @@
 package e2e
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/knative/pkg/test/logging"
@@ -10,25 +9,29 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// DiagnoseMeEvery queries the k8s controller and reports the pod stats to the logger,
+// every `duration` period.
 func DiagnoseMeEvery(duration time.Duration, clients *test.Clients, logger *logging.BaseLogger) chan struct{} {
 	stopChan := make(chan struct{})
 	go func() {
+		c := time.NewTicker(duration)
 		for {
 			select {
+			case <-c.C:
+				diagnoseMe(clients, logger)
+				continue
 			case <-stopChan:
+				c.Stop()
 				return
-			default:
 			}
-			DiagnoseMe(clients, logger)
-			time.Sleep(duration)
 		}
 	}()
 	return stopChan
 }
 
-func DiagnoseMe(clients *test.Clients, logger *logging.BaseLogger) {
+func diagnoseMe(clients *test.Clients, logger *logging.BaseLogger) {
 	if clients == nil || clients.KubeClient == nil || clients.KubeClient.Kube == nil {
-		logger.Errorf(fmt.Sprintf("could not diagnose: nil kube client"))
+		logger.Error("Could not diagnose: nil kube client")
 		return
 	}
 
@@ -43,22 +46,20 @@ func DiagnoseMe(clients *test.Clients, logger *logging.BaseLogger) {
 func checkCurrentPodCount(clients *test.Clients, logger *logging.BaseLogger) {
 	revs, err := clients.ServingClient.Revisions.List(metav1.ListOptions{})
 	if err != nil {
-		logger.Errorf(fmt.Sprintf("could not check current pod count: %v", err))
+		logger.Errorf("Could not check current pod count: %v", err)
 		return
 	}
 	for _, r := range revs.Items {
 		deploymentName := r.Name + "-deployment"
 		dep, err := clients.KubeClient.Kube.AppsV1().Deployments(test.ServingNamespace).Get(deploymentName, metav1.GetOptions{})
 		if err != nil {
-			logger.Errorf(fmt.Sprintf("could not get deployment %v", deploymentName))
+			logger.Errorf("Could not get deployment %v", deploymentName)
 			continue
 		}
-		want := dep.Status.Replicas
-		have := dep.Status.ReadyReplicas
-		if have != want {
-			logger.Infof("deployment %v has %v pods. wants %v.", deploymentName, have, want)
+		if want, have := dep.Status.Replicas, dep.Status.ReadyReplicas; have != want {
+			logger.Infof("Deployment %v has %v pods. wants %v.", deploymentName, have, want)
 		} else {
-			logger.Infof("deployment %v has %v pods.", deploymentName, have)
+			logger.Infof("Deployment %v has %v pods.", deploymentName, have)
 		}
 	}
 }
@@ -67,9 +68,10 @@ func checkUnschedulablePods(clients *test.Clients, logger *logging.BaseLogger) {
 	kube := clients.KubeClient.Kube
 	pods, err := kube.CoreV1().Pods(test.ServingNamespace).List(metav1.ListOptions{})
 	if err != nil {
-		logger.Errorf(fmt.Sprintf("could not check unschedulable pods: %v", err))
+		logger.Errorf("Could not check unschedulable pods: %v", err)
 		return
 	}
+
 	totalPods := 0
 	unschedulablePods := 0
 	for _, p := range pods.Items {
@@ -82,7 +84,6 @@ func checkUnschedulablePods(clients *test.Clients, logger *logging.BaseLogger) {
 		}
 	}
 	if unschedulablePods != 0 {
-		logger.Errorf(fmt.Sprintf("%v out of %v pods are unschedulable. insufficient cluster capacity?",
-			unschedulablePods, totalPods))
+		logger.Errorf("%v out of %v pods are unschedulable. Insufficient cluster capacity?", unschedulablePods, totalPods)
 	}
 }
