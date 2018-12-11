b31d0b7a41392eb331c38f6782f9e79b3e6dc205
Nghia Tran <tcnghia@gmail.com>
2018-12-10 17:07:16 -0800

    Add a cluster local gateway to our Istio templates. (#2613)
    VirtualServices exposed to this gateway will not be accessible outside
    of the cluster -- however, they would still be accessible to pods
    without sidecar injection.

--- third_party/istio-1.0.2/download-istio.sh
+++ third_party/istio-1.0.2/download-istio.sh
@@ -12,6 +12,20 @@ cp ../istio-pilot-hpa.yaml install/kubernetes/helm/istio/charts/pilot/templates/
 # Copy CRDs template
 cp install/kubernetes/helm/istio/templates/crds.yaml ../istio-crds.yaml
 
+# Create a custom cluster local gateway, based on the Istio custom-gateway template.
+helm template --namespace=istio-system \
+  --set gateways.custom-gateway.cpu.targetAverageUtilization=60 \
+  --set gateways.custom-gateway.labels.app='cluster-local-gateway' \
+  --set gateways.custom-gateway.labels.istio='cluster-local-gateway' \
+  --set gateways.custom-gateway.type='ClusterIP' \
+  --set gateways.istio-ingressgateway.enabled=false \
+  --set gateways.istio-egressgateway.enabled=false \
+  --set gateways.istio-ilbgateway.enabled=false \
+  install/kubernetes/helm/istio \
+  -f install/kubernetes/helm/istio/values-istio-gateways.yaml \
+  | sed -e "s/custom-gateway/cluster-local-gateway/g" -e "s/customgateway/clusterlocalgateway/g" \
+  > cluster-local-gateway.yaml
+
 # A template with sidecar injection enabled.
 helm template --namespace=istio-system \
   --set sidecarInjectorWebhook.enabled=true \
@@ -26,6 +40,7 @@ helm template --namespace=istio-system \
   --set pilot.autoscaleMax=10 \
   --set pilot.cpu.targetAverageUtilization=60 \
   install/kubernetes/helm/istio > ../istio.yaml
+cat cluster-local-gateway.yaml >> ../istio.yaml
 
 # A liter template, with no sidecar injection.  We could probably remove
 # more from this template.
@@ -38,6 +53,7 @@ helm template --namespace=istio-system \
   `# Disable mixer policy check, since in our template we set no policy.` \
   --set global.disablePolicyChecks=true \
   install/kubernetes/helm/istio > ../istio-lean.yaml
+cat cluster-local-gateway.yaml >> ../istio-lean.yaml
 
 # Clean up.
 cd ..
