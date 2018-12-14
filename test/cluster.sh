dba29b78e2b7ee894946d991ecfbb9685a9de29d
Markus Thömmes <markusthoemmes@me.com>
2018-12-14 13:12:20 +0100

    Upload images compatible to Dockerhub, simplify test flag code.

--- test/cluster.sh
+++ test/cluster.sh
@@ -152,8 +152,9 @@ function uninstall_knative_serving() {
 # Publish all e2e test images in ${REPO_ROOT_DIR}/test/test_images/
 function publish_test_images() {
   echo ">> Publishing test images"
+  # TODO(markusthoemmes): Call upload-test-images.sh?
   local image_dirs="$(find ${REPO_ROOT_DIR}/test/test_images -mindepth 1 -maxdepth 1 -type d)"
   for image_dir in ${image_dirs}; do
-    ko publish -P "github.com/knative/serving/test/test_images/$(basename ${image_dir})" || return 1
+    ko publish -B "github.com/knative/serving/test/test_images/$(basename ${image_dir})" || return 1
   done
 }
