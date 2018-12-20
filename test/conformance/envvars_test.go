4a13dd65700100374cedec83f393327d205db34f
dushyanthsc <43390008+dushyanthsc@users.noreply.github.com>
2018-12-20 15:01:35 -0800

    Conformance test coverage for secrets and configs via env var (#2751)
    This changeset adds conformance test to verify propagation of secrets
    and configs(via ConfigMaps) to service container as environment variables.
    
    Test re-uses test_images/environment image to fetch environment variables from
    the service. In the process runtime_conformance_helper is renamed to
    conformance_helper and fetchEnvInfo() is refactored to support this
    change.

--- test/conformance/envvars_test.go
+++ test/conformance/envvars_test.go
@@ -27,11 +27,11 @@ import (
 	"github.com/knative/serving/test"
 )
 
-//TestShouldEnvVars verifies environment variables that are declared as "SHOULD be set" in runtime-contract
+// TestShouldEnvVars verifies environment variables that are declared as "SHOULD be set" in runtime-contract
 func TestShouldEnvVars(t *testing.T) {
 	logger := logging.GetContextLogger("TestShouldEnvVars")
-	var names test.ResourceNames
-	resp, err := fetchEnvInfo(t, logger, test.EnvImageEnvVarsPath, &names)
+	clients := setup(t)
+	resp, names, err := fetchEnvInfo(clients, logger, test.EnvImageEnvVarsPath, &test.Options{})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -51,11 +51,11 @@ func TestShouldEnvVars(t *testing.T) {
 	}
 }
 
-//TestMustEnvVars verifies environment variables that are declared as "MUST be set" in runtime-contract
+// TestMustEnvVars verifies environment variables that are declared as "MUST be set" in runtime-contract
 func TestMustEnvVars(t *testing.T) {
 	logger := logging.GetContextLogger("TestMustEnvVars")
-	var names test.ResourceNames
-	resp, err := fetchEnvInfo(t, logger, test.EnvImageEnvVarsPath, &names)
+	clients := setup(t)
+	resp, _, err := fetchEnvInfo(clients, logger, test.EnvImageEnvVarsPath, &test.Options{})
 	if err != nil {
 		t.Fatal(err)
 	}
