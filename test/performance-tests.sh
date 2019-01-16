8f7aa6d29db91e1a32efbf4871bda2d6ce0a8725
Srinivas Hegde <srinivashegde@google.com>
2019-01-16 12:57:15 -0800

    Add metadata.json to performance tests

--- test/performance-tests.sh
+++ test/performance-tests.sh
@@ -24,6 +24,19 @@ function teardown() {
   uninstall_knative_serving
 }
 
+function save_metadata() {
+  local md="${ARTIFACTS}/metadata.json"
+  touch "$md"
+  # Save some uber data about tools and versions in metadata.json
+  echo -e "{" >> "${md}"
+  echo -e "\t\"Region\": \"${E2E_CLUSTER_REGION}\"," >> "${md}"
+  echo -e "\t\"Zone\": \"${E2E_CLUSTER_ZONE}\"," >> "${md}"
+  echo -e "\t\"Machine\": \"${E2E_CLUSTER_MACHINE}\"," >> "${md}"
+  echo -e "\t\"MinNodes\": \"${E2E_MIN_CLUSTER_NODES}\"," >> "${md}"
+  echo -e "\t\"MaxNodes\": \"${E2E_MAX_CLUSTER_NODES}\"" >> "${md}"
+  echo -e "}" >> "${md}"
+}
+
 initialize $@
 
 header "Setting up environment"
@@ -34,13 +47,15 @@ set +o pipefail
 
 # Build Knative, but don't install the default "no monitoring" version
 build_knative_from_source
-install_knative_serving "${ISTIO_CRD_YAML}" "${ISTIO_YAML}" "${SERVING_YAML}" \
-    || fail_test "Knative Serving installation failed"
+install_knative_serving "${ISTIO_CRD_YAML}" "${ISTIO_YAML}" "${SERVING_YAML}" || fail_test "Knative Serving installation failed"
 publish_test_images || fail_test "one or more test images weren't published"
 
 # Run the tests
 # We use a plain `go test` because `go_test_e2e()` calls bazel to generate
 # the test summary, thus overwriting our generated performance summary
 go test -v -count=1 -tags=performance -timeout=5m ./test/performance || fail_test
+
+# Save some metadata about the test cluster
+save_metadata
 
 success
