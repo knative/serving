1392a8f3d42702027d86017474f2b588b70b799d
Markus Thömmes <markusthoemmes@me.com>
2019-01-21 16:31:06 +0100

    Adjust another test.

--- pkg/reconciler/v1alpha1/route/route_test.go
+++ pkg/reconciler/v1alpha1/route/route_test.go
@@ -449,11 +449,9 @@ func TestCreateRouteWithMultipleTargets(t *testing.T) {
 func TestCreateRouteWithOneTargetReserve(t *testing.T) {
 	_, servingClient, controller, _, servingInformer, _ := newTestReconciler(t)
 	// A standalone inactive revision
-	rev := getTestRevisionWithCondition("test-rev",
-		duckv1alpha1.Condition{
-			Type:   v1alpha1.RevisionConditionActive,
-			Status: corev1.ConditionFalse,
-		})
+	rev := getTestRevision("test-rev")
+	rev.Status.MarkInactive("NoTraffic", "no message")
+
 	servingClient.ServingV1alpha1().Revisions(testNamespace).Create(rev)
 	servingInformer.Serving().V1alpha1().Revisions().Informer().GetIndexer().Add(rev)
 
