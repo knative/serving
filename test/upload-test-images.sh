facd97719bf4268e935d968392fc90ed34f27816
Markus Thömmes <markusthoemmes@me.com>
2019-02-04 19:22:13 +0100

    Use ko's --tags option to publish tags.

--- test/upload-test-images.sh
+++ test/upload-test-images.sh
@@ -20,17 +20,16 @@ function upload_test_images() {
   echo ">> Publishing test images"
   local image_dirs="$(find $(dirname $0)/test_images -mindepth 1 -maxdepth 1 -type d)"
   local docker_tag=$1
+  local tag_option=""
+  if [ -n "${docker_tag}" ]; then
+    tag_option="--tags $docker_tag"
+  fi
 
   for image_dir in ${image_dirs}; do
       local image_name="$(basename ${image_dir})"
       local image="github.com/knative/serving/test/test_images/${image_name}"
-      ko publish -B ${image}
-      if [ -n "${docker_tag}" ]; then
-          image=${KO_DOCKER_REPO}/${image_name}
-          local digest=$(gcloud container images list-tags --filter="tags:latest" --format='get(digest)' ${image})
-          echo "Tagging ${image}@${digest} with ${docker_tag}"
-          gcloud -q container images add-tag ${image}@${digest} ${image}:${docker_tag}
-      fi
+
+      ko publish -B "${image}" "${tag_option}"
   done
 }
 
