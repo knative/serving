32e4672b2dd542a4402ecd5837394d5ba6ec41ce
Victor Agababov <vagababov@gmail.com>
2019-02-08 11:36:41 -0800

    Add one more case to cover @latest as part of the traffic split. (#3104)
    * Add one more case to cover @latest as part of the traffic split.
    
    Followup to the main implementation of #2819.
    - fix the test comment, to reflect the reality
    - add a case where candidate is replaced with @latest
    - traffic distribution is validated.
    
    * update the test
    
    * intermediate checkpoint state
    
    * checkpoint
    
    * checkpoint
    
    * make sure stuff works
    
    * restore util.go
    
    * update comment
    
    * Update test/conformance/service_test.go
    
    Co-Authored-By: vagababov <vagababov@users.noreply.github.com>
    
    * Update test/conformance/util.go
    
    Co-Authored-By: vagababov <vagababov@users.noreply.github.com>
    
    * address comments

--- test/service.go
+++ test/service.go
@@ -66,7 +66,8 @@ func validateCreatedServiceStatus(clients *Clients, names *ResourceNames) error 
 	})
 }
 
-func getResourceObjects(clients *Clients, names ResourceNames) (*ResourceObjects, error) {
+// GetResourceObjects obtains the services resources from the k8s API server.
+func GetResourceObjects(clients *Clients, names ResourceNames) (*ResourceObjects, error) {
 	routeObject, err := clients.ServingClient.Routes.Get(names.Route, metav1.GetOptions{})
 	if err != nil {
 		return nil, err
@@ -130,7 +131,7 @@ func CreateReleaseServiceWithLatest(
 	}
 
 	logger.Info("Getting latest objects Created by Service.")
-	return getResourceObjects(clients, *names)
+	return GetResourceObjects(clients, *names)
 }
 
 // CreateRunLatestServiceReady creates a new RunLatest Service in state 'Ready'. This function expects Service and Image name passed in through 'names'.
@@ -163,7 +164,7 @@ func CreateRunLatestServiceReady(logger *logging.BaseLogger, clients *Clients, n
 	}
 
 	logger.Info("Getting latest objects Created by Service.")
-	return getResourceObjects(clients, *names)
+	return GetResourceObjects(clients, *names)
 }
 
 // CreateReleaseService creates a service in namespace with the name names.Service and names.Image,
