bdf0ddf1b8059c79e5a47ba05002b5316d631aea
Nghia Tran <nghia@google.com>
2019-02-14 13:20:21 -0800

    Fix client panic.

--- test/test_images/websocket_client/client.go
+++ test/test_images/websocket_client/client.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 The Knative Authors
+Copyright 2019 The Knative Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -54,7 +54,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			"Host": []string{targetHost},
 		},
 	)
-	defer conn.Close()
+	if conn != nil {
+		defer conn.Close()
+	}
 	if err != nil {
 		if resp != nil {
 			body := new(bytes.Buffer)
