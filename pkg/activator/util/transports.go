8229ab177cffa578da0ca762592d589c77db703f
Matt Moore <mattmoor@google.com>
2019-03-05 04:19:17 +0000

    Remove several pieces of code we no longer need.
    When I previously introduced the network probing in the activator it replaced the need for the "request rewinder" and the "retrying transport", which have been inactive since it went in.
    
    This also removes the handler in the activator that enforces a limit on the request's content-length, which was introduced in #1292 (alongside the Rewinder) to bound the size of the request we're willing to buffer into memory.  Now that we don't "rewind" the request, we no longer have cause to limit the payload size.
    
    Fixes: https://github.com/knative/serving/issues/3359

--- pkg/activator/util/transports.go
+++ pkg/activator/util/transports.go
@@ -15,14 +15,8 @@ package util
 
 import (
 	"net/http"
-	"strconv"
-
-	"k8s.io/apimachinery/pkg/util/wait"
-
-	"github.com/knative/serving/pkg/activator"
 
 	h2cutil "github.com/knative/serving/pkg/http/h2c"
-	"go.uber.org/zap"
 )
 
 // RoundTripperFunc implementation roundtrips a request.
@@ -47,76 +41,3 @@ func NewHTTPTransport(v1 http.RoundTripper, v2 http.RoundTripper) http.RoundTrip
 
 // AutoTransport uses h2c for HTTP2 requests and falls back to `http.DefaultTransport` for all others
 var AutoTransport = NewHTTPTransport(http.DefaultTransport, h2cutil.DefaultTransport)
-
-// RetryCond implementationr returns true if the request is to be retried.
-type RetryCond func(*http.Response) bool
-
-// RetryStatus will filter responses matching `status`.
-func RetryStatus(status int) RetryCond {
-	return func(resp *http.Response) bool {
-		return resp.StatusCode == status
-	}
-}
-
-type retryRoundTripper struct {
-	logger          *zap.SugaredLogger
-	transport       http.RoundTripper
-	backoffSettings wait.Backoff
-	retryConditions []RetryCond
-}
-
-// NewRetryRoundTripper retries a request on error or retry condition, using the given `retry` strategy
-func NewRetryRoundTripper(rt http.RoundTripper, l *zap.SugaredLogger, b wait.Backoff, conditions ...RetryCond) http.RoundTripper {
-	return &retryRoundTripper{
-		logger:          l,
-		transport:       rt,
-		backoffSettings: b,
-		retryConditions: conditions,
-	}
-}
-
-func (rrt *retryRoundTripper) RoundTrip(r *http.Request) (resp *http.Response, err error) {
-	// The request body cannot be read multiple times for retries.
-	// The workaround is to clone the request body into a byte reader
-	// so the body can be read multiple times.
-	if r.Body != nil {
-		rrt.logger.Debugf("Wrapping body in a rewinder.")
-		r.Body = NewRewinder(r.Body)
-	}
-
-	attempts := 0
-	wait.ExponentialBackoff(rrt.backoffSettings, func() (bool, error) {
-		rrt.logger.Debugf("Retrying")
-
-		attempts++
-		r.Header.Add(activator.RequestCountHTTPHeader, strconv.Itoa(attempts))
-		resp, err = rrt.transport.RoundTrip(r)
-
-		if err != nil {
-			rrt.logger.Errorf("Error making a request: %s", err)
-			return false, nil
-		}
-
-		for _, retryCond := range rrt.retryConditions {
-			if retryCond(resp) {
-				resp.Body.Close()
-				return false, nil
-			}
-		}
-		return true, nil
-	})
-
-	if err == nil {
-		rrt.logger.Infof("Finished after %d attempt(s). Response code: %d", attempts, resp.StatusCode)
-
-		if resp.Header == nil {
-			resp.Header = make(http.Header)
-		}
-
-		resp.Header.Add(activator.RequestCountHTTPHeader, strconv.Itoa(attempts))
-	} else {
-		rrt.logger.Errorf("Failed after %d attempts. Last error: %v", attempts, err)
-	}
-
-	return
-}
