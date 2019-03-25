db05296482a1edd5f824ae119e0c055e48a844fc
Matt Moore <mattmoor@google.com>
2019-03-25 15:29:49 -0700

    Tie Activator health to the readiness of the metrics socket. (#3519)
    Fixes: https://github.com/knative/serving/issues/3508
    Fixes: https://github.com/knative/serving/issues/2857

---
+++ pkg/activator/handler/healthz_handler.go
@@ -0,0 +1,44 @@
+/*
+Copyright 2019 The Knative Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package handler
+
+import (
+	"net/http"
+	"strings"
+)
+
+func isKubeletProbe(r *http.Request) bool {
+	// Since K8s 1.8, prober requests have
+	//   User-Agent = "kube-probe/{major-version}.{minor-version}".
+	return strings.HasPrefix(r.Header.Get("User-Agent"), "kube-probe/")
+}
+
+// HealthHandler handles responding to kubelet probes with a provided health check.
+type HealthHandler struct {
+	HealthCheck func() error
+	NextHandler http.Handler
+}
+
+func (h *HealthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if isKubeletProbe(r) {
+		if err := h.HealthCheck(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+		return
+	}
+
+	h.NextHandler.ServeHTTP(w, r)
+}
