82a88922218f3cf843f4e19d2fbc2cb2d3158751
Victor Agababov <vagababov@gmail.com>
2019-03-29 14:31:24 -0700

    add more tests

--- pkg/reconciler/v1alpha1/serverlessservice/serverlessservice_test.go
+++ pkg/reconciler/v1alpha1/serverlessservice/serverlessservice_test.go
@@ -239,6 +239,48 @@ func TestReconcile(t *testing.T) {
 		WantEvents: []string{
 			Eventf(corev1.EventTypeWarning, "UpdateFailed", "Failed to update status: inducing failure for update serverlessservices"),
 		},
+	}, {
+		Name:    "ronin-pub-service/fail5",
+		Key:     "ronin-pub-service/fail5",
+		WantErr: true,
+		Objects: []runtime.Object{
+			sks("ronin-pub-service", "fail5", withPubService),
+			svcpub("ronin-pub-service", "fail5", WithK8sSvcOwnersRemoved),
+			svcpriv("ronin-pub-service", "fail5"),
+			endpointspub("ronin-pub-service", "fail5", WithSubsets),
+			endpointspriv("ronin-pub-service", "fail5", WithSubsets),
+		},
+		WantEvents: []string{
+			Eventf(corev1.EventTypeWarning, "UpdateFailed", `InternalError: SKS: "fail5" does not own Service: "fail5-pub"`),
+		},
+	}, {
+		Name:    "ronin-priv-service/fail6",
+		Key:     "ronin-priv-service/fail6",
+		WantErr: true,
+		Objects: []runtime.Object{
+			sks("ronin-priv-service", "fail6", withPubService),
+			svcpub("ronin-priv-service", "fail6"),
+			svcpriv("ronin-priv-service", "fail6", WithK8sSvcOwnersRemoved),
+			endpointspub("ronin-priv-service", "fail6", WithSubsets),
+			endpointspriv("ronin-priv-service", "fail6", WithSubsets),
+		},
+		WantEvents: []string{
+			Eventf(corev1.EventTypeWarning, "UpdateFailed", `InternalError: SKS: "fail6" does not own Service: "fail6-priv"`),
+		},
+	}, {
+		Name:    "ronin-pub-eps/fail7",
+		Key:     "ronin-pub-eps/fail7",
+		WantErr: true,
+		Objects: []runtime.Object{
+			sks("ronin-pub-eps", "fail7", withPubService),
+			svcpub("ronin-pub-eps", "fail7"),
+			svcpriv("ronin-pub-eps", "fail7"),
+			endpointspub("ronin-pub-eps", "fail7", WithSubsets, WithEndpointsOwnersRemoved),
+			endpointspriv("ronin-pub-eps", "fail7", WithSubsets),
+		},
+		WantEvents: []string{
+			Eventf(corev1.EventTypeWarning, "UpdateFailed", `InternalError: SKS: "fail7" does not own Endpoints: "fail7-pub"`),
+		},
 	}}
 
 	defer ClearAllLoggers()
