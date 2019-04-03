fae790f6fbd6c9395e7da4610d0c00700845533a
Matt Moore (via sockpuppet) <mattmoor+sockpuppet@google.com>
2019-04-03 16:15:42 +0000

    golang format tools
    Produced via:
      `gofmt -s -w $(find -path './vendor' -prune -o -type f -name '*.go' -print))`
      `goimports -w $(find -name '*.go' | grep -v vendor)`

--- pkg/reconciler/v1alpha1/revision/resources/deploy_test.go
+++ pkg/reconciler/v1alpha1/revision/resources/deploy_test.go
@@ -46,8 +46,8 @@ var (
 		VolumeMounts:             []corev1.VolumeMount{varLogVolumeMount},
 		Lifecycle:                userLifecycle,
 		TerminationMessagePolicy: corev1.TerminationMessageFallbackToLogsOnError,
-		Stdin: false,
-		TTY:   false,
+		Stdin:                    false,
+		TTY:                      false,
 		Env: []corev1.EnvVar{{
 			Name:  "PORT",
 			Value: "8080",
@@ -154,7 +154,7 @@ var (
 	}
 
 	defaultPodSpec = &corev1.PodSpec{
-		Volumes: []corev1.Volume{varLogVolume},
+		Volumes:                       []corev1.Volume{varLogVolume},
 		TerminationGracePeriodSeconds: refInt64(45),
 	}
 
