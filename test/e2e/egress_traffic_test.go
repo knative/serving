b18b7d5fbb0fa9638a4b756d50e90d590e864b65
tzununbekov <t.zununbekov@gmail.com>
2019-04-12 14:09:17 +0600

    Egress traffic e2e test

---
+++ test/e2e/egress_traffic_test.go
@@ -0,0 +1,43 @@
+package e2e
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/knative/serving/test"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+const (
+	targetHostEnvName = "TARGET_HOST"
+	targetHostDomain  = "www.google.com"
+)
+
+func TestEgressTraffic(t *testing.T) {
+	t.Parallel()
+	clients := Setup(t)
+
+	names := test.ResourceNames{
+		Service: test.ObjectNameForTest(t),
+		Image:   "httpproxy",
+	}
+	envVars := []corev1.EnvVar{{
+		Name:  targetHostEnvName,
+		Value: targetHostDomain,
+	}}
+	defer test.TearDown(clients, names)
+	test.CleanupOnInterrupt(func() { test.TearDown(clients, names) })
+
+	service, err := test.CreateRunLatestServiceReady(t, clients, &names, &test.Options{EnvVars: envVars})
+	if err != nil {
+		t.Fatalf("Failed to create a service: %v", err)
+	}
+	response, err := sendRequest(t, clients, test.ServingFlags.ResolvableDomain, service.Route.Status.Domain)
+	if err != nil {
+		t.Fatalf("Failed to send request to httpproxy: %v", err)
+	}
+	if got, want := response.StatusCode, http.StatusOK; got != want {
+		t.Errorf("httpbin response StatusCode = %v, want %v", got, want)
+	}
+}
