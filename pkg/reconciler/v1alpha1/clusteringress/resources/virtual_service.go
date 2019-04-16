597a4fe3a09e88bef146366f72298ed879cfc816
Matt Moore <mattmoor@google.com>
2019-04-15 19:37:00 -0700

    Add per-route AppendHeaders support in ClusterIngress. (#3763)
    This adds the support to cluster ingress, but doesn't yet take advantage of it
    in Route because the new fields break downgrade testing, and other
    ClusterIngress providers will need to add support for this in order to remain
    compatible.

--- pkg/reconciler/v1alpha1/clusteringress/resources/virtual_service.go
+++ pkg/reconciler/v1alpha1/clusteringress/resources/virtual_service.go
@@ -98,17 +98,38 @@ func makeVirtualServiceRoute(hosts []string, http *v1alpha1.HTTPClusterIngressPa
 	for _, host := range expandedHosts(hosts) {
 		matches = append(matches, makeMatch(host, http.Path))
 	}
-	weights := []v1alpha3.DestinationWeight{}
+	weights := []v1alpha3.HTTPRouteDestination{}
 	for _, split := range http.Splits {
-		weights = append(weights, v1alpha3.DestinationWeight{
+
+		var h *v1alpha3.Headers
+		if len(split.AppendHeaders) > 0 {
+			h = &v1alpha3.Headers{
+				Request: &v1alpha3.HeaderOperations{
+					Add: split.AppendHeaders,
+				},
+			}
+		}
+
+		weights = append(weights, v1alpha3.HTTPRouteDestination{
 			Destination: v1alpha3.Destination{
 				Host: network.GetServiceHostname(
 					split.ServiceName, split.ServiceNamespace),
 				Port: makePortSelector(split.ServicePort),
 			},
-			Weight: split.Percent,
+			Weight:  split.Percent,
+			Headers: h,
 		})
 	}
+
+	var h *v1alpha3.Headers
+	if len(http.AppendHeaders) > 0 {
+		h = &v1alpha3.Headers{
+			Request: &v1alpha3.HeaderOperations{
+				Add: http.AppendHeaders,
+			},
+		}
+	}
+
 	return &v1alpha3.HTTPRoute{
 		Match:   matches,
 		Route:   weights,
@@ -117,8 +138,12 @@ func makeVirtualServiceRoute(hosts []string, http *v1alpha1.HTTPClusterIngressPa
 			Attempts:      http.Retries.Attempts,
 			PerTryTimeout: http.Retries.PerTryTimeout.Duration.String(),
 		},
-		AppendHeaders:    http.AppendHeaders,
-		WebsocketUpgrade: true,
+		// TODO(mattmoor): Remove AppendHeaders when 1.1 is a hard dependency.
+		// AppendHeaders is deprecated in Istio 1.1 in favor of Headers,
+		// however, 1.0.x doesn't support Headers.
+		DeprecatedAppendHeaders: http.AppendHeaders,
+		Headers:                 h,
+		WebsocketUpgrade:        true,
 	}
 }
 
