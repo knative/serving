28118d24871f27719d96978d2779ecf6dccaf8a5
Joan Edwards <42442393+joaedwar@users.noreply.github.com>
2019-04-29 13:17:26 +0100

    Increase 'queueDepth' value to allow auoscaler to scale up
    Running a performance test with a high number of clients reveals issues with autoscaler. Autoscaler fails to scale up due to low queue size. Make 'queueDepth' relative to the concurrency level but much longer than it is now.

--- cmd/queue/main.go
+++ cmd/queue/main.go
@@ -235,9 +235,10 @@ func main() {
 
 	// If containerConcurrency == 0 then concurrency is unlimited.
 	if containerConcurrency > 0 {
-		// We set the queue depth to be equal to the container concurrency but at least 10 to
+		// We set the queue depth to be equal to the container concurrency * 100 to
 		// allow the autoscaler to get a strong enough signal.
-		queueDepth := containerConcurrency
+		//
+		queueDepth := containerConcurrency * 100
 		if queueDepth < 10 {
 			queueDepth = 10
 		}
