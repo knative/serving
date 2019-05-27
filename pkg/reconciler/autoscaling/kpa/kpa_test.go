8f5d48a73a344c5f9ccc2360c0e036a6a5ed3ee0
Joshua Rider <47116457+joshrider@users.noreply.github.com>
2019-05-27 16:37:29 -0400

    Revisions should wait for minScale replicas to report ready (#3493)
    * revisions become ready with minScale reached
    
    * remove redundant condition
    
    * revert accidental spacing changes
    
    * cleanup test helpers
    
    * cleanup tests
    
    * remove copypasted comment

--- pkg/reconciler/autoscaling/kpa/kpa_test.go
+++ pkg/reconciler/autoscaling/kpa/kpa_test.go
@@ -19,6 +19,7 @@ package kpa
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"sync"
 	"testing"
 	"time"
@@ -51,6 +52,7 @@ import (
 	"github.com/knative/serving/pkg/reconciler/autoscaling/kpa/resources"
 	aresources "github.com/knative/serving/pkg/reconciler/autoscaling/resources"
 	revisionresources "github.com/knative/serving/pkg/reconciler/revision/resources"
+	presources "github.com/knative/serving/pkg/resources"
 	perrors "github.com/pkg/errors"
 	fakedynamic "k8s.io/client-go/dynamic/fake"
 
@@ -540,6 +542,32 @@ func TestReconcile(t *testing.T) {
 		},
 		WantStatusUpdates: []clientgotesting.UpdateActionImpl{{
 			Object: kpa(testNamespace, testRevision, markActive, WithPAStatusService(testRevision)),
+		}},
+	}, {
+		Name: "kpa does not become ready without minScale endpoints",
+		Key:  key,
+		Objects: []runtime.Object{
+			kpa(testNamespace, testRevision, withMinScale(2)),
+			sks(testNamespace, testRevision, WithDeployRef(deployName), WithSKSReady),
+			metricsSvc(testNamespace, testRevision, withSvcSelector(usualSelector)),
+			expectedDeploy,
+			makeSKSPrivateEndpoints(1, testNamespace, testRevision),
+		},
+		WantStatusUpdates: []clientgotesting.UpdateActionImpl{{
+			Object: kpa(testNamespace, testRevision, markActivating, withMinScale(2), WithPAStatusService(testRevision)),
+		}},
+	}, {
+		Name: "kpa becomes ready with minScale endpoints",
+		Key:  key,
+		Objects: []runtime.Object{
+			kpa(testNamespace, testRevision, markActivating, withMinScale(2), WithPAStatusService(testRevision)),
+			sks(testNamespace, testRevision, WithDeployRef(deployName), WithSKSReady),
+			metricsSvc(testNamespace, testRevision, withSvcSelector(usualSelector)),
+			expectedDeploy,
+			makeSKSPrivateEndpoints(2, testNamespace, testRevision),
+		},
+		WantStatusUpdates: []clientgotesting.UpdateActionImpl{{
+			Object: kpa(testNamespace, testRevision, markActive, withMinScale(2), WithPAStatusService(testRevision)),
 		}},
 	}, {
 		Name: "sks does not exist",
@@ -1512,10 +1540,23 @@ func makeSKSPrivateEndpoints(num int, ns, n string) *corev1.Endpoints {
 }
 
 func addEndpoint(ep *corev1.Endpoints) *corev1.Endpoints {
-	ep.Subsets = []corev1.EndpointSubset{{
-		Addresses: []corev1.EndpointAddress{{IP: "127.0.0.1"}},
-	}}
+	if ep.Subsets == nil {
+		ep.Subsets = []corev1.EndpointSubset{{
+			Addresses: []corev1.EndpointAddress{},
+		}}
+	}
+
+	ep.Subsets[0].Addresses = append(ep.Subsets[0].Addresses, corev1.EndpointAddress{IP: "127.0.0.1"})
 	return ep
+}
+
+func withMinScale(minScale int) PodAutoscalerOption {
+	return func(pa *asv1a1.PodAutoscaler) {
+		pa.Annotations = presources.UnionMaps(
+			pa.Annotations,
+			map[string]string{autoscaling.MinScaleAnnotationKey: strconv.Itoa(minScale)},
+		)
+	}
 }
 
 type testConfigStore struct {
