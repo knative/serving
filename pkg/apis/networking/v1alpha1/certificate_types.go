62560c9f3b91d0328a50e1f2c149b373ac820b43
Zhimin Xiang <zhiminx@google.com>
2019-05-31 20:16:48 +0000

    add HTTP01 challenge into CertificateStatus

--- pkg/apis/networking/v1alpha1/certificate_types.go
+++ pkg/apis/networking/v1alpha1/certificate_types.go
@@ -21,6 +21,7 @@ import (
 	duckv1beta1 "github.com/knative/pkg/apis/duck/v1beta1"
 	"github.com/knative/pkg/kmeta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 // +genclient
@@ -93,4 +94,27 @@ type CertificateStatus struct {
 	// by this resource in spec.secretName.
 	// +optional
 	NotAfter *metav1.Time `json:"notAfter,omitempty"`
+
+	// HTTP01Challenges is a list of HTTP01 challenges that need to be fulfilled
+	// in order to get the TLS certificate..
+	HTTP01Challenges []HTTP01Challenge `json:"http01Challenges,omitempty"`
+}
+
+// HTTP01Challenge defines the status of a HTTP01 challenge that a certificate needs
+// to fulfill.
+type HTTP01Challenge struct {
+	// DNSName is the DNS name to be verified by this HTTP01 challenge.
+	DNSName string `json:"dnsName,omitempty"`
+
+	// ServiceName is the name of the service to serve HTTP01 challenge requests.
+	ServiceName string `json:"serviceName,omitempty"`
+
+	// ServiceNamespace is the namespace of the service to serve HTTP01 challenge requests.
+	ServiceNamespace string `json:"serviceNamespace,omitempty"`
+
+	// ServicePort is the port of the service to serve HTTP01 challenge requests.
+	ServicePort intstr.IntOrString `json:"servicePort,omitempty"`
+
+	// Path is the specifi URI path of HTT01 challenge requests.
+	Path string `json:"path,omitempty"`
 }
