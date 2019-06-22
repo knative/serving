d5db036c402201a61d98d493c7876517cdaaab52
Victor Agababov <vagababov@gmail.com>
2019-06-21 21:04:59 -0700

    Fix the bug where we might have ended up with 0 target concurrency. (#4469)
    The code as it was, would multiply target currency by target utilization.
    Since TU is effectively 1, this was never a problem.
    But now that we're trying to lower it, it could have been that we'd have numbers <1, which after int() casting would
    effectively become 0.
    This fixes the problem in both places (the CFGMAP setting) and the computation (min of 1, will be ensured)>
    
    /assign @mattmoor @markusthoemmes

--- pkg/reconciler/autoscaling/resources/concurrency_test.go
+++ pkg/reconciler/autoscaling/resources/concurrency_test.go
@@ -20,23 +20,45 @@ import (
 	"testing"
 
 	"github.com/knative/serving/pkg/apis/autoscaling/v1alpha1"
+	"github.com/knative/serving/pkg/autoscaler"
 
 	. "github.com/knative/serving/pkg/testing"
 )
 
-func TestResolveTargetConcurrency(t *testing.T) {
+func TestResolveConcurrency(t *testing.T) {
 	cases := []struct {
-		name string
-		pa   *v1alpha1.PodAutoscaler
-		want float64
+		name   string
+		pa     *v1alpha1.PodAutoscaler
+		cfgOpt func(autoscaler.Config) *autoscaler.Config
+		want   float64
 	}{{
 		name: "defaults",
 		pa:   pa(),
 		want: 100,
 	}, {
+		name: "with container concurrency 10 and TU=80%",
+		pa:   pa(WithContainerConcurrency(10)),
+		cfgOpt: func(c autoscaler.Config) *autoscaler.Config {
+			c.ContainerConcurrencyTargetFraction = 0.8
+			return &c
+		},
+		want: 8,
+	}, {
+		name: "with container concurrency 1 and TU=80%",
+		pa:   pa(WithContainerConcurrency(1)),
+		cfgOpt: func(c autoscaler.Config) *autoscaler.Config {
+			c.ContainerConcurrencyTargetFraction = 0.8
+			return &c
+		},
+		want: 1, // Not permitting less than 1.
+	}, {
 		name: "with container concurrency 1",
 		pa:   pa(WithContainerConcurrency(1)),
 		want: 1,
+	}, {
+		name: "with container concurrency 10",
+		pa:   pa(WithContainerConcurrency(10)),
+		want: 10,
 	}, {
 		name: "with target annotation 1",
 		pa:   pa(WithTargetAnnotation("1")),
@@ -53,7 +75,11 @@ func TestResolveTargetConcurrency(t *testing.T) {
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			if got := ResolveTargetConcurrency(tc.pa, config); got != tc.want {
+			cfg := config
+			if tc.cfgOpt != nil {
+				cfg = tc.cfgOpt(*cfg)
+			}
+			if got := ResolveConcurrency(tc.pa, cfg); got != tc.want {
 				t.Errorf("ResolveTargetConcurrency(%v, %v) = %v, want %v", tc.pa, config, got, tc.want)
 			}
 		})
