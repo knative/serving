3a9e84794b5513a62e38beb559abcbd43fe22513
Victor Agababov <vagababov@gmail.com>
2019-06-23 12:43:59 -0700

    Use the upstream ChildName version from pkg (#4464)

--- pkg/resources/meta.go
+++ pkg/resources/meta.go
@@ -16,11 +16,6 @@ limitations under the License.
 
 package resources
 
-import (
-	"crypto/md5"
-	"fmt"
-)
-
 // CopyMap makes a copy of the map.
 func CopyMap(a map[string]string) map[string]string {
 	ret := make(map[string]string, len(a))
@@ -56,23 +51,4 @@ func FilterMap(in map[string]string, filter func(string) bool) map[string]string
 		ret[k] = v
 	}
 	return ret
-}
-
-// The longest name supported by the K8s is 63.
-// These constants
-const (
-	longest = 63
-	md5Len  = 32
-	head    = longest - md5Len
-)
-
-// ChildName generates a name for the resource based upong the parent resource and suffix.
-// If the concatenated name is longer than K8s permits the name is hashed and truncated to permit
-// construction of the resource, but still keeps it unique.
-func ChildName(parent, suffix string) string {
-	n := parent
-	if len(parent) > (longest - len(suffix)) {
-		n = fmt.Sprintf("%s%x", parent[:head-len(suffix)], md5.Sum([]byte(parent)))
-	}
-	return n + suffix
 }
