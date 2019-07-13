f0e4daf2695af792d4ee2740a8bb352383010333
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2019-07-13 17:46:23 +0900

    Set defaults for all containers passed in thorugh revision

--- pkg/apis/serving/v1beta1/revision_validation_test.go
+++ pkg/apis/serving/v1beta1/revision_validation_test.go
@@ -208,13 +208,13 @@ func TestRevisionSpecValidation(t *testing.T) {
 		},
 		want: apis.ErrDisallowedFields("containers[0].lifecycle"),
 	}, {
-		name: "missing image",
+		name: "missing container",
 		rs: &RevisionSpec{
 			PodSpec: corev1.PodSpec{
 				Containers: []corev1.Container{},
 			},
 		},
-		want: apis.ErrMissingField("containers[0].image"),
+		want: apis.ErrMissingField("containers"),
 	}, {
 		name: "too many containers",
 		rs: &RevisionSpec{
@@ -284,7 +284,6 @@ func TestRevisionSpecValidation(t *testing.T) {
 			if test.wc != nil {
 				ctx = test.wc(ctx)
 			}
-			test.rs.SetDefaults(ctx)
 			got := test.rs.Validate(ctx)
 			if !cmp.Equal(test.want.Error(), got.Error()) {
 				t.Errorf("Validate (-want, +got) = %v",
