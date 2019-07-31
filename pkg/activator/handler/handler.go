3d6aaa27833d7f3bc8d22b59a222aaddff123a90
Victor Agababov <vagababov@gmail.com>
2019-07-31 15:09:06 -0700

    Add safety catches

--- pkg/activator/handler/handler.go
+++ pkg/activator/handler/handler.go
@@ -100,12 +100,20 @@ func withOrigProto(or *http.Request) prober.Preparer {
 	}
 }
 
-func (a *activationHandler) probeEndpoint(logger *zap.SugaredLogger, r *http.Request, target *url.URL) (bool, int) {
+func (a *activationHandler) probeEndpoint(logger *zap.SugaredLogger, r *http.Request, target *url.URL, revID activator.RevisionID) (bool, int) {
 	var (
 		attempts int
 		st       = time.Now()
 		url      = target.String()
 	)
+
+	// This opportunistically caches the probes, so
+	// a few concurrent requests might result in concurrent probes
+	// but requests coming after won't.
+	if !a.throttler.ShouldProbe(revID) {
+		return true, 0
+	}
+	logger.Debugf("Actually will be probing %s", url)
 
 	err := wait.PollImmediate(100*time.Millisecond, a.probeTimeout, func() (bool, error) {
 		attempts++
@@ -124,6 +132,9 @@ func (a *activationHandler) probeEndpoint(logger *zap.SugaredLogger, r *http.Req
 			logger.Warn("Pod probe unsuccessful")
 			return false, nil
 		}
+
+		// Cache probe success.
+		a.throttler.MarkProbe(revID)
 		return true, nil
 	})
 
@@ -136,7 +147,6 @@ func (a *activationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	name := pkghttp.LastHeaderValue(r.Header, activator.RevisionHeaderName)
 	start := time.Now()
 	revID := activator.RevisionID{Namespace: namespace, Name: name}
-
 	logger := a.logger.With(zap.String(logkey.Key, revID.String()))
 
 	revision, err := a.revisionLister.Revisions(namespace).Get(name)
@@ -178,7 +188,7 @@ func (a *activationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		a.logger.Debugf("Waiting for throttler took %v time", time.Since(tryStart))
 
 		probeCtx, probeSpan := trace.StartSpan(r.Context(), "probe")
-		success, attempts := a.probeEndpoint(logger, r.WithContext(probeCtx), target)
+		success, attempts := a.probeEndpoint(logger, r.WithContext(probeCtx), target, revID)
 		probeSpan.End()
 
 		var httpStatus int
--- pkg/activator/testing/roundtripper.go
+++ pkg/activator/testing/roundtripper.go
@@ -21,6 +21,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"sync"
+	"sync/atomic"
 
 	"knative.dev/serving/pkg/network"
 	"knative.dev/serving/pkg/queue"
@@ -48,6 +49,8 @@ type FakeRoundTripper struct {
 	// Response to non-probe requests
 	RequestResponse *FakeResponse
 	responseMux     sync.Mutex
+
+	NumProbes int32
 }
 
 func defaultProbeResponse() *FakeResponse {
@@ -91,6 +94,7 @@ func (rt *FakeRoundTripper) popResponse() *FakeResponse {
 // RT is a RoundTripperFunc
 func (rt *FakeRoundTripper) RT(req *http.Request) (*http.Response, error) {
 	if req.Header.Get(network.ProbeHeaderName) != "" {
+		atomic.AddInt32(&rt.NumProbes, 1)
 		resp := rt.popResponse()
 		if resp.Err != nil {
 			return nil, resp.Err
