c6f7bc48faa2c882539d56719d5b23fdd653ea5c
Zhimin Xiang <zhiminx@google.com>
2019-08-07 19:25:56 -0700

    Refactor ingress controller to use access model to reconcile Secret (#5042)
    * Add SecretAccessor and use it in ingress reconciler
    
    * fix typo
    
    * create a new type of error to indicate NotOwnResource case
    
    * fix typo
    
    * address comments
    
    * address comments

---
+++ pkg/reconciler/accessor/core/secret_test.go
@@ -0,0 +1,210 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package core
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"golang.org/x/sync/errgroup"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	kubefake "k8s.io/client-go/kubernetes/fake"
+	corev1listers "k8s.io/client-go/listers/core/v1"
+	"knative.dev/pkg/controller"
+	fakekubeclient "knative.dev/pkg/injection/clients/kubeclient/fake"
+	logtesting "knative.dev/pkg/logging/testing"
+	"knative.dev/pkg/ptr"
+	kaccessor "knative.dev/serving/pkg/reconciler/accessor"
+
+	. "knative.dev/pkg/reconciler/testing"
+)
+
+var (
+	ownerObj = &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "ownerObj",
+			Namespace: "default",
+			UID:       "abcd",
+		},
+	}
+
+	ownerRef = metav1.OwnerReference{
+		Kind:       ownerObj.Kind,
+		Name:       ownerObj.Name,
+		UID:        ownerObj.UID,
+		Controller: ptr.Bool(true),
+	}
+
+	origin = &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            "secret",
+			Namespace:       "default",
+			OwnerReferences: []metav1.OwnerReference{ownerRef},
+		},
+		Data: map[string][]byte{
+			"test-secret": []byte("origin"),
+		},
+	}
+
+	desired = &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            "secret",
+			Namespace:       "default",
+			OwnerReferences: []metav1.OwnerReference{ownerRef},
+		},
+		Data: map[string][]byte{
+			"test-secret": []byte("desired"),
+		},
+	}
+
+	notOwnedSecret = &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "secret",
+			Namespace: "default",
+		},
+		Data: map[string][]byte{
+			"test-secret": []byte("origin"),
+		},
+	}
+)
+
+type FakeAccessor struct {
+	client       kubernetes.Interface
+	secretLister corev1listers.SecretLister
+}
+
+func (f *FakeAccessor) GetKubeClient() kubernetes.Interface {
+	return f.client
+}
+
+func (f *FakeAccessor) GetSecretLister() corev1listers.SecretLister {
+	return f.secretLister
+}
+
+func TestReconcileSecretCreate(t *testing.T) {
+	defer logtesting.ClearAll()
+	ctx, _ := SetupFakeContext(t)
+	ctx, cancel := context.WithCancel(ctx)
+	grp := errgroup.Group{}
+	defer func() {
+		cancel()
+		if err := grp.Wait(); err != nil {
+			t.Errorf("Wait() = %v", err)
+		}
+	}()
+
+	kubeClient := fakekubeclient.Get(ctx)
+
+	h := NewHooks()
+	h.OnCreate(&kubeClient.Fake, "secrets", func(obj runtime.Object) HookResult {
+		got := obj.(*corev1.Secret)
+		if diff := cmp.Diff(got, desired); diff != "" {
+			t.Logf("Unexpected Secret (-want, +got): %v", diff)
+			return HookIncomplete
+		}
+		return HookComplete
+	})
+
+	accessor := setup(ctx, []*corev1.Secret{}, kubeClient, t)
+	ReconcileSecret(ctx, ownerObj, desired, accessor)
+
+	if err := h.WaitForHooks(3 * time.Second); err != nil {
+		t.Errorf("Failed to Reconcile Secret: %v", err)
+	}
+}
+
+func TestReconcileSecretUpdate(t *testing.T) {
+	defer logtesting.ClearAll()
+	ctx, _ := SetupFakeContext(t)
+	ctx, cancel := context.WithCancel(ctx)
+	grp := errgroup.Group{}
+	defer func() {
+		cancel()
+		if err := grp.Wait(); err != nil {
+			t.Errorf("Wait() = %v", err)
+		}
+	}()
+
+	kubeClient := fakekubeclient.Get(ctx)
+	accessor := setup(ctx, []*corev1.Secret{origin}, kubeClient, t)
+
+	h := NewHooks()
+	h.OnUpdate(&kubeClient.Fake, "secrets", func(obj runtime.Object) HookResult {
+		got := obj.(*corev1.Secret)
+		if diff := cmp.Diff(got, desired); diff != "" {
+			t.Logf("Unexpected Secret (-want, +got): %v", diff)
+			return HookIncomplete
+		}
+		return HookComplete
+	})
+
+	ReconcileSecret(ctx, ownerObj, desired, accessor)
+	if err := h.WaitForHooks(3 * time.Second); err != nil {
+		t.Errorf("Failed to Reconcile Secret: %v", err)
+	}
+}
+
+func TestNotOwnedFailure(t *testing.T) {
+	defer logtesting.ClearAll()
+	ctx, _ := SetupFakeContext(t)
+	ctx, cancel := context.WithCancel(ctx)
+	grp := errgroup.Group{}
+	defer func() {
+		cancel()
+		if err := grp.Wait(); err != nil {
+			t.Errorf("Wait() = %v", err)
+		}
+	}()
+
+	kubeClient := fakekubeclient.Get(ctx)
+	accessor := setup(ctx, []*corev1.Secret{notOwnedSecret}, kubeClient, t)
+
+	_, err := ReconcileSecret(ctx, ownerObj, desired, accessor)
+	if err == nil {
+		t.Error("Expected to get error when calling ReconcileSecret, but got no error.")
+	}
+	if !kaccessor.IsNotOwned(err) {
+		t.Errorf("Expected to get NotOwnedError but got %v", err)
+	}
+}
+
+func setup(ctx context.Context, secrets []*corev1.Secret,
+	kubeClient kubernetes.Interface, t *testing.T) *FakeAccessor {
+
+	fake := kubefake.NewSimpleClientset()
+	informer := informers.NewSharedInformerFactory(fake, 0)
+	secretInformer := informer.Core().V1().Secrets()
+
+	for _, secret := range secrets {
+		fake.CoreV1().Secrets(secret.Namespace).Create(secret)
+		secretInformer.Informer().GetIndexer().Add(secret)
+	}
+
+	if err := controller.StartInformers(ctx.Done(), secretInformer.Informer()); err != nil {
+		t.Fatalf("failed to start secret informer: %v", err)
+	}
+
+	return &FakeAccessor{
+		client:       kubeClient,
+		secretLister: secretInformer.Lister(),
+	}
+}
--- pkg/reconciler/ingress/ingress.go
+++ pkg/reconciler/ingress/ingress.go
@@ -52,9 +52,12 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/client-go/kubernetes"
 	corev1listers "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	sharedclientset "knative.dev/pkg/client/clientset/versioned"
+	kaccessor "knative.dev/serving/pkg/reconciler/accessor"
+	coreaccessor "knative.dev/serving/pkg/reconciler/accessor/core"
 	istioaccessor "knative.dev/serving/pkg/reconciler/accessor/istio"
 )
 
@@ -95,7 +98,10 @@ type BaseIngressReconciler struct {
 	StatusManager StatusManager
 }
 
-var _ istioaccessor.VirtualServiceAccessor = (*BaseIngressReconciler)(nil)
+var (
+	_ coreaccessor.SecretAccessor          = (*BaseIngressReconciler)(nil)
+	_ istioaccessor.VirtualServiceAccessor = (*BaseIngressReconciler)(nil)
+)
 
 // NewBaseIngressReconciler creates a new BaseIngressReconciler
 func NewBaseIngressReconciler(ctx context.Context, agentName, finalizer string, cmw configmap.Watcher) *BaseIngressReconciler {
@@ -356,43 +362,18 @@ func (r *BaseIngressReconciler) reconcileIngress(ctx context.Context, ra Reconci
 
 func (r *BaseIngressReconciler) reconcileCertSecrets(ctx context.Context, ia v1alpha1.IngressAccessor, desiredSecrets []*corev1.Secret) error {
 	for _, certSecret := range desiredSecrets {
-		if err := r.reconcileCertSecret(ctx, ia, certSecret); err != nil {
+		// We track the origin and desired secrets so that desired secrets could be synced accordingly when the origin TLS certificate
+		// secret is refreshed.
+		r.Tracker.Track(resources.SecretRef(certSecret.Namespace, certSecret.Name), ia)
+		r.Tracker.Track(resources.SecretRef(
+			certSecret.Labels[networking.OriginSecretNamespaceLabelKey],
+			certSecret.Labels[networking.OriginSecretNameLabelKey]), ia)
+		if _, err := coreaccessor.ReconcileSecret(ctx, ia, certSecret, r); err != nil {
+			if kaccessor.IsNotOwned(err) {
+				ia.GetStatus().MarkResourceNotOwned("Secret", certSecret.Name)
+			}
 			return err
 		}
-	}
-	return nil
-}
-
-func (r *BaseIngressReconciler) reconcileCertSecret(ctx context.Context, ia v1alpha1.IngressAccessor, desired *corev1.Secret) error {
-	// We track the origin and desired secrets so that desired secrets could be synced accordingly when the origin TLS certificate
-	// secret is refreshed.
-	r.Tracker.Track(resources.SecretRef(desired.Namespace, desired.Name), ia)
-	r.Tracker.Track(resources.SecretRef(desired.Labels[networking.OriginSecretNamespaceLabelKey], desired.Labels[networking.OriginSecretNameLabelKey]), ia)
-
-	logger := logging.FromContext(ctx)
-	existing, err := r.SecretLister.Secrets(desired.Namespace).Get(desired.Name)
-	if apierrs.IsNotFound(err) {
-		_, err = r.KubeClientSet.CoreV1().Secrets(desired.Namespace).Create(desired)
-		if err != nil {
-			logger.Errorw("Failed to create Certificate Secret", zap.Error(err))
-			r.Recorder.Eventf(ia, corev1.EventTypeWarning, "CreationFailed",
-				"Failed to create Secret %s/%s: %v", desired.Namespace, desired.Name, err)
-			return err
-		}
-		r.Recorder.Eventf(ia, corev1.EventTypeNormal, "Created", "Created Secret %s/%s", desired.Namespace, desired.Name)
-	} else if err != nil {
-		return err
-	} else if !equality.Semantic.DeepEqual(existing.Data, desired.Data) {
-		// Don't modify the informers copy
-		copy := existing.DeepCopy()
-		copy.Data = desired.Data
-		_, err = r.KubeClientSet.CoreV1().Secrets(copy.Namespace).Update(copy)
-		if err != nil {
-			logger.Errorw("Failed to update target secret", zap.Error(err))
-			r.Recorder.Eventf(ia, corev1.EventTypeWarning, "UpdateFailed", "Failed to update Secret %s/%s: %v", desired.Namespace, desired.Name, err)
-			return err
-		}
-		r.Recorder.Eventf(ia, corev1.EventTypeNormal, "Updated", "Updated Secret %s/%s", copy.Namespace, copy.Name)
 	}
 	return nil
 }
@@ -404,6 +385,9 @@ func (r *BaseIngressReconciler) reconcileVirtualServices(ctx context.Context, ia
 	kept := sets.NewString()
 	for _, d := range desired {
 		if _, err := istioaccessor.ReconcileVirtualService(ctx, ia, d, r); err != nil {
+			if kaccessor.IsNotOwned(err) {
+				ia.GetStatus().MarkResourceNotOwned("VirtualService", d.Name)
+			}
 			return err
 		}
 		kept.Insert(d.Name)
@@ -529,6 +513,16 @@ func (r *BaseIngressReconciler) reconcileGateway(ctx context.Context, ia v1alpha
 	}
 	r.Recorder.Eventf(ia, corev1.EventTypeNormal, "Updated", "Updated Gateway %s/%s", gateway.Namespace, gateway.Name)
 	return nil
+}
+
+// GetKubeClient returns the client to access k8s resources.
+func (r *BaseIngressReconciler) GetKubeClient() kubernetes.Interface {
+	return r.KubeClientSet
+}
+
+// GetSecretLister returns the lister for Secret.
+func (r *BaseIngressReconciler) GetSecretLister() corev1listers.SecretLister {
+	return r.SecretLister
 }
 
 // GetSharedClient returns the client to access shared resources.
