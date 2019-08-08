f1a4734c4554e7d74f5b27c825ab48794c61c67b
Srinivas Hegde <srinivashegde@google.com>
2019-08-08 10:11:19 -0700

    Add common setup for mako

--- test/performance/mako/sidecar.go
+++ test/performance/mako/sidecar.go
@@ -16,9 +16,43 @@ limitations under the License.
 
 package mako
 
+import (
+	"context"
+
+	"github.com/golang/protobuf/proto"
+        "github.com/google/mako/helpers/go/quickstore"
+        qpb "github.com/google/mako/helpers/proto/quickstore/quickstore_go_proto"
+)
+
 const (
 	// SidecarAddress is the address of the Mako sidecar to which we locally
 	// write results, and it authenticates and publishes them to Mako after
 	// assorted preprocessing.
-	SidecarAddress = "localhost:9813"
+	sidecarAddress = "localhost:9813"
 )
+
+// SetupMako sets up the mako client for the provided benchmarkKey.
+// It will add a few common tags and allows each benchmark to add custm tags as well.
+// It returns the mako client handle to sotre metrics, a method to close the connection
+// to mako server once done and error if case of failures.
+func SetupMako(ctx context.Context, benchmarkKey string, extraTags []string)(*quickstore.Quickstore, func(context.Context), error) {
+	tags := []string{"master"}
+        if commitID, err := changeset.Get(); err == nil {
+                tags = append(tags, commitID)
+        } else {
+		return nil, err
+        }
+
+	// Get the Kubernetes version from the API server.
+	if version, err := kubeclient.Get(ctx).Discovery().ServerVersion(); err == nil {
+		tags = append(tags, version)
+	} else {
+		return nil, err
+	}
+
+	tags = append(tags, extraTags...)
+        return quickstore.NewAtAddress(ctx, &qpb.QuickstoreInput{
+                BenchmarkKey: proto.String(benchmarkKey),
+                Tags: tags,
+	}, sidecarAddress)
+}
