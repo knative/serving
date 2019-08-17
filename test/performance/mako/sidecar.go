8c6e61f9835502d6263ac403b135ff5f57378c52
Matt Moore <mattmoor@google.com>
2019-08-17 11:37:31 -0700

    Generalize the benchmark tagging. (#5193)
    * Generalize the benchmark tagging.
    
    This moves the K8s version tagging into `mako.Setup` and adds a Go version tag.
    
    You can see a sample run of the dataplane-probe (now with K8s tagging!) here:
    https://mako.dev/run?run_key=6230979358228480&~kd=1&~id=1&~qp=1&~a=1&~ke=1&~ie=1&~qe=1&~ae=1
    
    * Address review comments.

--- test/performance/mako/sidecar.go
+++ test/performance/mako/sidecar.go
@@ -18,10 +18,17 @@ package mako
 
 import (
 	"context"
+	"runtime"
+	"strings"
+
+	"knative.dev/pkg/injection/clients/kubeclient"
 
 	"github.com/google/mako/helpers/go/quickstore"
 	qpb "github.com/google/mako/helpers/proto/quickstore/quickstore_go_proto"
+	"k8s.io/client-go/rest"
 	"knative.dev/pkg/changeset"
+	"knative.dev/pkg/controller"
+	"knative.dev/pkg/injection"
 )
 
 const (
@@ -31,21 +38,49 @@ const (
 	sidecarAddress = "localhost:9813"
 )
 
+// EscapeTag replaces characters that Mako doesn't accept with ones it does.
+func EscapeTag(tag string) string {
+	return strings.ReplaceAll(tag, ".", "_")
+}
+
 // SetupMako sets up the mako client for the provided benchmarkKey.
 // It will add a few common tags and allows each benchmark to add custm tags as well.
 // It returns the mako client handle to sotre metrics, a method to close the connection
 // to mako server once done and error if case of failures.
-func Setup(ctx context.Context, extraTags ...string) (*quickstore.Quickstore, func(context.Context), error) {
-	tags := make([]string, 0, len(extraTags)+1)
-	if commitID, err := changeset.Get(); err == nil {
-		tags = append(tags, commitID)
-	} else {
-		return nil, nil, err
+func Setup(ctx context.Context, extraTags ...string) (context.Context, *quickstore.Quickstore, func(context.Context), error) {
+	// Get the commit of the benchmarks
+	commitID, err := changeset.Get()
+	if err != nil {
+		return nil, nil, nil, err
 	}
 
-	tags = append(tags, extraTags...)
-	return quickstore.NewAtAddress(ctx, &qpb.QuickstoreInput{
+	// Setup a deployment informer, so that we can use the lister to track
+	// desired and available pod counts.
+	cfg, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	ctx, informers := injection.Default.SetupInformers(ctx, cfg)
+	if err := controller.StartInformers(ctx.Done(), informers...); err != nil {
+		return nil, nil, nil, err
+	}
+
+	// Get the Kubernetes version from the API server.
+	version, err := kubeclient.Get(ctx).Discovery().ServerVersion()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	qs, qclose, err := quickstore.NewAtAddress(ctx, &qpb.QuickstoreInput{
 		BenchmarkKey: MustGetBenchmark(),
-		Tags:         tags,
+		Tags: append(extraTags,
+			"commit="+commitID,
+			"kubernetes="+EscapeTag(version.String()),
+			EscapeTag(runtime.Version()),
+		),
 	}, sidecarAddress)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	return ctx, qs, qclose, nil
 }
