76607ade3e5146e0f9ececfa21fc827f3bb9e660
Srinivas Hegde <srinivashegde@google.com>
2019-08-23 14:00:19 -0700

    Add istio version tag to benchmark runs

--- test/performance/tools/common.sh
+++ test/performance/tools/common.sh
@@ -83,9 +83,12 @@ function create_new_cluster() {
 function update_cluster() {
   name=$1
   zone=$2
+  local istio_version="istio-1.2-latest"
+  # Mako needs to escape '.' in tags. USe '_' instead.
+  local istio_version_escaped=${istio_version//./_}
   echo "Updating cluster with name ${name} in zone ${zone}"
   gcloud container clusters get-credentials ${name} --zone=${zone} --project=${PROJECT_NAME} || abort "Failed to get cluster creds"
-  
+
   echo ">> Delete all existing jobs and test resources"
   kubectl delete job --all
   ko delete -f "${TEST_ROOT_PATH}/$1"
@@ -93,8 +96,8 @@ function update_cluster() {
   pushd .
   cd ${GOPATH}/src/knative.dev
   echo ">> Update istio"
-  kubectl apply -f serving/third_party/istio-1.2-latest/istio-crds.yaml || abort "Failed to apply istio-crds"
-  kubectl apply -f serving/third_party/istio-1.2-latest/istio-lean.yaml || abort "Failed to apply istio-lean"
+  kubectl apply -f serving/third_party/$istio_version/istio-crds.yaml || abort "Failed to apply istio-crds"
+  kubectl apply -f serving/third_party/$istio_version/istio-lean.yaml || abort "Failed to apply istio-lean"
 
   # Overprovision the Istio gateways.
   kubectl patch hpa -n istio-system istio-ingressgateway \
@@ -128,6 +131,7 @@ metadata:
 data:
   # This should only be used by our performance automation.
   environment: prod
+  additionalTags: "istio=$istio_version_escaped"
 EOF
 
   echo ">> Applying all the yamls"
