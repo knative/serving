e392c318579feb68f1141d42454653b7c92dfddf
Martin Gencur <mgencur@redhat.com>
2019-08-26 19:44:37 +0200

    Fix formatting

--- pkg/reconciler/revision/resources/deploy_test.go
+++ pkg/reconciler/revision/resources/deploy_test.go
@@ -53,8 +53,8 @@ var (
 		VolumeMounts:             []corev1.VolumeMount{varLogVolumeMount},
 		Lifecycle:                userLifecycle,
 		TerminationMessagePolicy: corev1.TerminationMessageFallbackToLogsOnError,
-		Stdin: false,
-		TTY:   false,
+		Stdin:                    false,
+		TTY:                      false,
 		Env: []corev1.EnvVar{{
 			Name:  "PORT",
 			Value: "8080",
@@ -173,7 +173,7 @@ var (
 	}
 
 	defaultPodSpec = &corev1.PodSpec{
-		Volumes: []corev1.Volume{varLogVolume},
+		Volumes:                       []corev1.Volume{varLogVolume},
 		TerminationGracePeriodSeconds: refInt64(45),
 	}
 
