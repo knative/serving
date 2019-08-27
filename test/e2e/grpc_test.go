d1d9b9fe7c544f9ba0246adb0a14b17047ec9c7a
Jean-Rémy Bancel <bancel@google.com>
2019-08-27 15:11:55 -0700

    Move from string pointer to plain string.

--- test/e2e/grpc_test.go
+++ test/e2e/grpc_test.go
@@ -193,7 +193,7 @@ func testGRPC(t *testing.T, f grpcTest, fopts ...rtesting.ServiceOption) {
 
 	host := &domain
 	if !test.ServingFlags.ResolvableDomain {
-		host = &pkgTest.Flags.IngressEndpoint
+		host = pkgTest.Flags.IngressEndpoint
 		if pkgTest.Flags.IngressEndpoint == "" {
 			host, err = ingress.GetIngressEndpoint(clients.KubeClient.Kube)
 			if err != nil {
@@ -202,7 +202,7 @@ func testGRPC(t *testing.T, f grpcTest, fopts ...rtesting.ServiceOption) {
 		}
 	}
 
-	f(t, resources, clients, *host, domain)
+	f(t, resources, clients, host, domain)
 }
 
 func TestGRPCUnaryPing(t *testing.T) {
