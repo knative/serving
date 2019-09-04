787f629d6315a4bdd1a5ee2f8a10ea9d577cfeaf
Chao Dai <chaodai@google.com>
2019-09-04 15:41:34 -0700

    also include upgrade tests

--- test/upgrade/main_test.go
+++ test/upgrade/main_test.go
@@ -1,4 +1,4 @@
-// +build e2e
+// +build probe
 
 /*
 Copyright 2019 The Knative Authors
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1beta1
+package upgrade
 
 import (
 	"os"
