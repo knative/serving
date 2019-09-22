7e4a57f43d6388ebb7c22e7ca99cb5de0dea86a7
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2019-09-22 09:00:06 +0900

    Fix code style to more simple

--- pkg/reconciler/ingress/resources/gateway.go
+++ pkg/reconciler/ingress/resources/gateway.go
@@ -280,10 +280,7 @@ func UpdateGateway(gateway *v1alpha3.Gateway, want []v1alpha3.Server, existing [
 
 func isDefaultServer(server *v1alpha3.Server) bool {
 	if server.Port.Name == "https" {
-		if len(server.Hosts) > 0 && server.Hosts[0] == "*" {
-			return true
-		}
-		return false
+		return len(server.Hosts) > 0 && server.Hosts[0] == "*"
 	}
 	return server.Port.Name == "http"
 }
