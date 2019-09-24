2756972e3649608b1599209fe9aac5e89bd3991a
Markus Thömmes <markusthoemmes@me.com>
2019-09-24 14:37:22 +0200

    Add logline that logs a skipped decrease.

--- pkg/autoscaler/autoscaler.go
+++ pkg/autoscaler/autoscaler.go
@@ -218,6 +218,8 @@ func (a *Autoscaler) Scale(ctx context.Context, now time.Time) (desiredPodCount 
 			logger.Infof("Increasing pods from %d to %d.", originalReadyPodsCount, desiredPanicPodCount)
 			a.panicTime = &now
 			a.maxPanicPods = desiredPanicPodCount
+		} else {
+			logger.Infof("Skipping decrease from %d to %d.", originalReadyPodsCount, desiredPanicPodCount)
 		}
 		desiredPodCount = a.maxPanicPods
 	} else {
