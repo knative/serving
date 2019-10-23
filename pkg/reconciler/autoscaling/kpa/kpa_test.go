4af447fe1f5a2578663f8ce1bf39d5702efccb4d
Matt Moore (via sockpuppet) <mattmoor+sockpuppet@google.com>
2019-10-23 20:31:25 +0000

    golang format tools
    Produced via:
      `gofmt -s -w $(find -path './vendor' -prune -o -type f -name '*.go' -print))`
      `goimports -w $(find -name '*.go' | grep -v vendor)`
    /assign mattmoor

--- pkg/reconciler/autoscaling/kpa/kpa_test.go
+++ pkg/reconciler/autoscaling/kpa/kpa_test.go
@@ -278,20 +278,20 @@ func TestReconcile(t *testing.T) {
 	// two constant objects above, which means, that all tests must share
 	// the same namespace and revision name.
 	table := TableTest{{
-		Name: "bad workqueue key, Part I",
-		Key:  "too/many/parts",
+		Name:                    "bad workqueue key, Part I",
+		Key:                     "too/many/parts",
 		SkipNamespaceValidation: true,
 	}, {
-		Name: "bad workqueue key, Part II",
-		Key:  "too-few-parts",
+		Name:                    "bad workqueue key, Part II",
+		Key:                     "too-few-parts",
 		SkipNamespaceValidation: true,
 	}, {
-		Name: "key not found",
-		Key:  "foo/not-found",
+		Name:                    "key not found",
+		Key:                     "foo/not-found",
 		SkipNamespaceValidation: true,
 	}, {
-		Name: "key not found",
-		Key:  "foo/not-found",
+		Name:                    "key not found",
+		Key:                     "foo/not-found",
 		SkipNamespaceValidation: true,
 	}, {
 		Name: "steady state",
