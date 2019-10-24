672e99b90612bd50e3c09b1c67867765f77feed7
jihuin <56172920+jihuin@users.noreply.github.com>
2019-10-24 00:37:05 -0700

    Update istio.go

--- pkg/reconciler/ingress/config/istio.go
+++ pkg/reconciler/ingress/config/istio.go
@@ -110,7 +110,7 @@ func parseGateways(configMap *corev1.ConfigMap, prefix string) ([]Gateway, error
 		parts := strings.SplitN(gatewayName, ".", 2)
         if len(parts) == 1 {
         	namespace = system.Namespace()
-            name      = parts[0] 
+            name      = parts[0]
         } else if len(parts) == 2 {
             namespace = parts[0]
             name      = parts[1]
