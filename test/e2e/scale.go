a9ee50b54dac1874ba5885b0c2b83f38389a665a
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2019-10-29 19:42:52 +0900

    Make sure that all goroutines finish

--- test/e2e/scale.go
+++ test/e2e/scale.go
@@ -17,7 +17,6 @@ limitations under the License.
 package e2e
 
 import (
-	"context"
 	"fmt"
 	"math"
 	"net/url"
@@ -33,6 +32,8 @@ import (
 	v1alpha1testing "knative.dev/serving/pkg/testing/v1alpha1"
 	"knative.dev/serving/test"
 	v1a1test "knative.dev/serving/test/v1alpha1"
+
+	"knative.dev/pkg/test/spoof"
 )
 
 // Latencies is an interface for providing mechanisms for recording timings
@@ -43,6 +44,27 @@ type Latencies interface {
 	// be computed with `time.Since(start)`.  We use this signature to that this
 	// function is suitable for use in a `defer`.
 	Add(name string, start time.Time)
+}
+
+type interrupt struct {
+	stop bool
+}
+
+func (i *interrupt) isServiceReady(s *v1alpha1.Service) (done bool, err error) {
+	if i.stop {
+		return false, fmt.Errorf("interrupted")
+	}
+	if s.Status.URL == nil {
+		return false, nil
+	}
+	return v1a1test.IsServiceReady(s)
+}
+
+func (i *interrupt) proceed(resp *spoof.Response) (done bool, err error) {
+	if i.stop {
+		return false, fmt.Errorf("interrupted")
+	}
+	return true, nil
 }
 
 func ScaleToWithin(t *testing.T, scale int, duration time.Duration, latencies Latencies) {
@@ -71,100 +93,105 @@ func ScaleToWithin(t *testing.T, scale int, duration time.Duration, latencies La
 		// https://golang.org/doc/faq#closures_and_goroutines
 		i := i
 		wg.Go(func() error {
-			worker := func() error {
-				names := test.ResourceNames{
-					Service: test.SubServiceNameForTest(t, fmt.Sprintf("%0[1]*[2]d", width, i)),
-					Image:   "helloworld",
-				}
+			names := test.ResourceNames{
+				Service: test.SubServiceNameForTest(t, fmt.Sprintf("%0[1]*[2]d", width, i)),
+				Image:   "helloworld",
+			}
 
-				// Start the clock for various waypoints towards Service readiness.
-				start := time.Now()
-				// Record the overall completion time regardless of success/failure.
-				defer latencies.Add("time-to-done", start)
+			// Start the clock for various waypoints towards Service readiness.
+			start := time.Now()
+			// Record the overall completion time regardless of success/failure.
+			defer latencies.Add("time-to-done", start)
 
-				svc, err := v1a1test.CreateLatestService(t, clients, names,
-					v1alpha1testing.WithResourceRequirements(corev1.ResourceRequirements{
-						Limits: corev1.ResourceList{
-							corev1.ResourceCPU:    resource.MustParse("10m"),
-							corev1.ResourceMemory: resource.MustParse("50Mi"),
+			svc, err := v1a1test.CreateLatestService(t, clients, names,
+				v1alpha1testing.WithResourceRequirements(corev1.ResourceRequirements{
+					Limits: corev1.ResourceList{
+						corev1.ResourceCPU:    resource.MustParse("10m"),
+						corev1.ResourceMemory: resource.MustParse("50Mi"),
+					},
+					Requests: corev1.ResourceList{
+						corev1.ResourceCPU:    resource.MustParse("10m"),
+						corev1.ResourceMemory: resource.MustParse("20Mi"),
+					},
+				}),
+				v1alpha1testing.WithConfigAnnotations(map[string]string{
+					"autoscaling.knative.dev/maxScale": "1",
+				}),
+				v1alpha1testing.WithReadinessProbe(&corev1.Probe{
+					Handler: corev1.Handler{
+						HTTPGet: &corev1.HTTPGetAction{
+							Path: "/",
 						},
-						Requests: corev1.ResourceList{
-							corev1.ResourceCPU:    resource.MustParse("10m"),
-							corev1.ResourceMemory: resource.MustParse("20Mi"),
-						},
-					}),
-					v1alpha1testing.WithConfigAnnotations(map[string]string{
-						"autoscaling.knative.dev/maxScale": "1",
-					}),
-					v1alpha1testing.WithReadinessProbe(&corev1.Probe{
-						Handler: corev1.Handler{
-							HTTPGet: &corev1.HTTPGetAction{
-								Path: "/",
-							},
-						},
-					}),
-					v1alpha1testing.WithRevisionTimeoutSeconds(10))
+					},
+				}),
+				v1alpha1testing.WithRevisionTimeoutSeconds(10))
 
-				if err != nil {
-					t.Errorf("CreateLatestService() = %v", err)
-					return fmt.Errorf("CreateLatestService() failed: %w", err)
-				}
-				// Record the time it took to create the service.
-				latencies.Add("time-to-create", start)
-				names.Route = serviceresourcenames.Route(svc)
-				names.Config = serviceresourcenames.Configuration(svc)
+			if err != nil {
+				t.Errorf("CreateLatestService() = %v", err)
+				return fmt.Errorf("CreateLatestService() failed: %w", err)
+			}
+			// Record the time it took to create the service.
+			latencies.Add("time-to-create", start)
+			names.Route = serviceresourcenames.Route(svc)
+			names.Config = serviceresourcenames.Configuration(svc)
 
-				// Send it to our cleanup logic (below)
-				cleanupCh <- names
+			// Send it to our cleanup logic (below)
+			cleanupCh <- names
 
-				t.Logf("Wait for %s to become ready.", names.Service)
-				var url *url.URL
-				err = v1a1test.WaitForServiceState(clients.ServingAlphaClient, names.Service, func(s *v1alpha1.Service) (bool, error) {
-					if s.Status.URL == nil {
-						return false, nil
-					}
-					url = s.Status.URL.URL()
-					return v1a1test.IsServiceReady(s)
-				}, "ServiceUpdatedWithURL")
+			t.Logf("Wait for %s to become ready.", names.Service)
+
+			errCh := make(chan error)
+			s := &interrupt{stop: false}
+			go func() {
+				errCh <- v1a1test.WaitForServiceState(clients.ServingAlphaClient, names.Service, s.isServiceReady, "ServiceUpdatedWithURL")
+			}()
+			select {
+			case err = <-errCh:
 				if err != nil {
 					t.Errorf("WaitForServiceState(w/ Domain) = %v", err)
 					return fmt.Errorf("WaitForServiceState(w/ Domain) failed: %w", err)
 				}
-				// Record the time it took to become ready.
-				latencies.Add("time-to-ready", start)
+			case <-time.After(duration):
+				// Stop WaitForServiceState to finish Goroutine
+				s.stop = true
+				return fmt.Errorf("Timed out waiting for service ready")
+			}
+			url := svc.Status.URL.URL()
 
+			// Record the time it took to become ready.
+			latencies.Add("time-to-ready", start)
+
+			s = &interrupt{stop: false}
+			go func() {
 				_, err = pkgTest.WaitForEndpointState(
 					clients.KubeClient,
 					t.Logf,
 					url,
-					v1a1test.RetryingRouteInconsistency(pkgTest.MatchesAllOf(pkgTest.IsStatusOK, pkgTest.MatchesBody(test.HelloWorldText))),
+					v1a1test.RetryingRouteInconsistency(pkgTest.MatchesAllOf(pkgTest.IsStatusOK, pkgTest.MatchesBody(test.HelloWorldText), s.proceed)),
 					"WaitForEndpointToServeText",
 					test.ServingFlags.ResolvableDomain)
+				errCh <- err
+			}()
+			select {
+			case err = <-errCh:
 				if err != nil {
 					t.Errorf("WaitForEndpointState(expected text) = %v", err)
 					return fmt.Errorf("WaitForEndpointState(expected text) failed: %w", err)
 				}
-				// Record the time it took to get back a 200 with the expected text.
-				latencies.Add("time-to-200", start)
-
-				// Start probing the domain until the test is complete.
-				pm.Spawn(url)
-
-				t.Logf("%s is ready.", names.Service)
-				return nil
+			case <-time.After(duration):
+				// Stop WaitForEndpointState to finish Goroutine
+				s.stop = true
+				return fmt.Errorf("Timed out waiting for endpoint ready")
 			}
-			errCh := make(chan error)
-			ctx, cancel := context.WithTimeout(context.Background(), duration)
-			defer cancel()
-			go func() {
-				errCh <- worker()
-			}()
-			select {
-			case <-ctx.Done():
-				return fmt.Errorf("Timed out waiting for service")
-			case err := <-errCh:
-				return err
-			}
+
+			// Record the time it took to get back a 200 with the expected text.
+			latencies.Add("time-to-200", start)
+
+			// Start probing the domain until the test is complete.
+			pm.Spawn(url)
+
+			t.Logf("%s is ready.", names.Service)
+			return nil
 		})
 	}
 
