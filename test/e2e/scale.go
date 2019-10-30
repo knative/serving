31e53fc37612fc91e4abd87173c0c5b2d1fe59ed
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2019-10-30 16:47:17 +0900

    Remove space line

--- test/e2e/scale.go
+++ test/e2e/scale.go
@@ -27,14 +27,13 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	pkgTest "knative.dev/pkg/test"
+	"knative.dev/pkg/test/spoof"
 	"knative.dev/serving/pkg/apis/serving/v1alpha1"
 	"knative.dev/serving/pkg/pool"
 	serviceresourcenames "knative.dev/serving/pkg/reconciler/service/resources/names"
 	v1alpha1testing "knative.dev/serving/pkg/testing/v1alpha1"
 	"knative.dev/serving/test"
 	v1a1test "knative.dev/serving/test/v1alpha1"
-
-	"knative.dev/pkg/test/spoof"
 )
 
 // Latencies is an interface for providing mechanisms for recording timings
