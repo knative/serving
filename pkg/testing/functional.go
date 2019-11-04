0cd89009d5b80b061c86400de0ad6401aec88fe5
Markus Thömmes <markusthoemmes@me.com>
2019-11-04 14:55:08 +0100

    Fix linter warning.

--- pkg/testing/functional.go
+++ pkg/testing/functional.go
@@ -84,13 +84,14 @@ func WithTraffic(pa *asv1a1.PodAutoscaler) {
 	pa.Status.MarkActive()
 }
 
-// WithPAStatusService annotats PA Status with the provided service name.
+// WithPAStatusService annotates PA Status with the provided service name.
 func WithPAStatusService(svc string) PodAutoscalerOption {
 	return func(pa *asv1a1.PodAutoscaler) {
 		pa.Status.ServiceName = svc
 	}
 }
 
+// WithPAMetricsService annotates PA Status with the provided service name.
 func WithPAMetricsService(svc string) PodAutoscalerOption {
 	return func(pa *asv1a1.PodAutoscaler) {
 		pa.Status.MetricsServiceName = svc
