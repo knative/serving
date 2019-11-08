6d8b1f813811f80097cf31fcbf9e92cf1defe8b7
Ben Browning <bbrownin@redhat.com>
2019-11-08 10:44:42 -0500

    Add Kourier as an Ingress option for e2e tests
    This copies the work done for Gloo, except for Kourier. The
    download-kourier.sh script just pulls in Kourier 0.2.2 (the latest)
    for now like Gloo does since we do not test with multiple versions of
    either.

---
+++ third_party/kourier-latest/download-kourier.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# Download Kourier
+KOURIER_VERSION=0.2.2
+KOURIER_YAML=kourier-knative.yaml
+DOWNLOAD_URL=https://raw.githubusercontent.com/3scale/kourier/v${KOURIER_VERSION}/deploy/${KOURIER_YAML}
+
+wget ${DOWNLOAD_URL}
+
+cat ${KOURIER_YAML} \
+  `# Install Kourier into the kourier-system namespace` \
+  | sed 's/namespace: knative-serving/namespace: kourier-system/' \
+  `# Expose Kourier services with LoadBalancer IPs instead of ClusterIP` \
+  | sed 's/ClusterIP/LoadBalancer/' \
+  > kourier.yaml
+
+# Clean up.
+rm ${KOURIER_YAML}
