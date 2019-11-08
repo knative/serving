760149c2a59f15c2069e19e71907166c6c5c7067
Ben Browning <bbrownin@redhat.com>
2019-11-08 11:40:59 -0500

    We need the kourier-system namespace as well
    I missed then when converting things to use the download-kourier.sh
    for generating the kourier.yaml

--- third_party/kourier-latest/download-kourier.sh
+++ third_party/kourier-latest/download-kourier.sh
@@ -9,12 +9,20 @@ DOWNLOAD_URL=https://raw.githubusercontent.com/3scale/kourier/v${KOURIER_VERSION
 
 wget ${DOWNLOAD_URL}
 
+cat <<EOF > kourier.yaml
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: kourier-system
+---
+EOF
+
 cat ${KOURIER_YAML} \
   `# Install Kourier into the kourier-system namespace` \
   | sed 's/namespace: knative-serving/namespace: kourier-system/' \
   `# Expose Kourier services with LoadBalancer IPs instead of ClusterIP` \
   | sed 's/ClusterIP/LoadBalancer/' \
-  > kourier.yaml
+  >> kourier.yaml
 
 # Clean up.
 rm ${KOURIER_YAML}
