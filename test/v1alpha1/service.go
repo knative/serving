d1dcaaf09ec19bd6c06a0a091b87dd20f0998041
Markus Thömmes <markusthoemmes@me.com>
2019-11-12 08:46:36 +0100

    Remove unused code.

--- test/v1alpha1/service.go
+++ test/v1alpha1/service.go
@@ -66,10 +66,6 @@ const (
 	GatewayName = "knative-ingress-gateway"
 )
 
-var (
-	domainName *string
-)
-
 func validateCreatedServiceStatus(clients *test.Clients, names *test.ResourceNames) error {
 	return CheckServiceState(clients.ServingAlphaClient, names.Service, func(s *v1alpha1.Service) (bool, error) {
 		if s.Status.URL == nil || s.Status.URL.Host == "" {
