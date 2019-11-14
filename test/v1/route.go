a8561425930762960bff10b073f4c5eb4a46f6f4
Ben Browning <bbrownin@redhat.com>
2019-11-14 09:47:19 -0500

    Revert "Temporarily enable route retrying logic to isolate test failures"
    This reverts commit e2e02aacb07232b70237244460ef535f810e9f9a.

--- test/v1/route.go
+++ test/v1/route.go
@@ -19,7 +19,6 @@ package v1
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
@@ -117,14 +116,8 @@ func IsRouteNotReady(r *v1.Route) (bool, error) {
 }
 
 // RetryingRouteInconsistency retries common requests seen when creating a new route
-// - 404 until the route is propagated to the proxy
-// - 503 to account for Openshift route inconsistency (https://jira.coreos.com/browse/SRVKS-157)
-// TODO(5573): Remove this.
 func RetryingRouteInconsistency(innerCheck spoof.ResponseChecker) spoof.ResponseChecker {
 	return func(resp *spoof.Response) (bool, error) {
-		if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusServiceUnavailable {
-			return false, nil
-		}
 		// If we didn't match any retryable codes, invoke the ResponseChecker that we wrapped.
 		return innerCheck(resp)
 	}
