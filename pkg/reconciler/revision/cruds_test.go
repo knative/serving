9e229b3492228d38f2b391a361bbcae82c8c5d5c
Markus Thömmes <markusthoemmes@me.com>
2019-12-09 13:57:07 +0100

    Use revision stanza directly to avoid confusion.

--- pkg/reconciler/revision/cruds_test.go
+++ pkg/reconciler/revision/cruds_test.go
@@ -26,55 +26,9 @@ import (
 	corev1informer "k8s.io/client-go/informers/core/v1"
 	kubefake "k8s.io/client-go/kubernetes/fake"
 	"knative.dev/pkg/ptr"
-	"knative.dev/serving/pkg/apis/serving"
 	v1 "knative.dev/serving/pkg/apis/serving/v1"
 	"knative.dev/serving/pkg/apis/serving/v1alpha1"
 )
-
-var (
-	containerName = "my-container-name"
-)
-
-type containerOption func(*corev1.Container)
-type revisionOption func(*v1alpha1.Revision)
-
-func container(container *corev1.Container, opts ...containerOption) corev1.Container {
-	for _, option := range opts {
-		option(container)
-	}
-	return *container
-}
-
-func revision(opts ...revisionOption) *v1alpha1.Revision {
-	defaultRevision := &v1alpha1.Revision{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: "foo",
-			Name:      "bar",
-			UID:       "1234",
-			Labels: map[string]string{
-				serving.ConfigurationLabelKey: "cfg",
-				serving.ServiceLabelKey:       "svc",
-				serving.RouteLabelKey:         "im-a-route",
-			},
-		},
-		Spec: v1alpha1.RevisionSpec{
-			RevisionSpec: v1.RevisionSpec{
-				TimeoutSeconds: ptr.Int64(45),
-				PodSpec: corev1.PodSpec{
-					InitContainers: nil,
-					Containers: []corev1.Container{{
-						Name:  "containerName",
-						Image: "busybox",
-					}},
-				},
-			},
-		},
-	}
-	for _, option := range opts {
-		option(defaultRevision)
-	}
-	return defaultRevision
-}
 
 func TestVerifySecrets(t *testing.T) {
 	tests := []struct {
@@ -84,254 +38,295 @@ func TestVerifySecrets(t *testing.T) {
 		expectedErr string
 	}{{
 		name: "secrets (from volume source) found",
-		rev: revision(
-			func(revision *v1alpha1.Revision) {
-				revision.Spec.GetContainer().VolumeMounts = []corev1.VolumeMount{{
-					Name:      "asdf1",
-					MountPath: "/asdf1",
-				}}
-				container(revision.Spec.GetContainer(),
-					//withTCPReadinessProbe(),
-				)
-				revision.Spec.Volumes = []corev1.Volume{{
-					Name: "asdf1",
-					VolumeSource: corev1.VolumeSource{
-						Secret: &corev1.SecretVolumeSource{
-							SecretName: "asdf1",
-						},
-					},
-				}}
-				revision.ObjectMeta.Labels = map[string]string{}
+		rev: &v1alpha1.Revision{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "foo",
+				Name:      "bar",
 			},
-		),
+			Spec: v1alpha1.RevisionSpec{
+				RevisionSpec: v1.RevisionSpec{
+					PodSpec: corev1.PodSpec{
+						Containers: []corev1.Container{{
+							VolumeMounts: []corev1.VolumeMount{{
+								Name:      "asdf1",
+								MountPath: "/asdf1",
+							}},
+						}},
+						Volumes: []corev1.Volume{{
+							Name: "asdf1",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: "asdf1",
+								},
+							},
+						}},
+					},
+				},
+			},
+		},
 		si: addSecretToInformer(kubefake.NewSimpleClientset(), "asdf1", "foo")(getSecretInformer()),
 	}, {
 		name: "secrets (from volume source) not found",
-		rev: revision(
-			func(revision *v1alpha1.Revision) {
-				revision.Spec.GetContainer().Ports = []corev1.ContainerPort{{
-					ContainerPort: 8888,
-				}}
-				revision.Spec.GetContainer().VolumeMounts = []corev1.VolumeMount{{
-					Name:      "asdf1",
-					MountPath: "/asdf1",
-				}, {
-					Name:      "asdf2",
-					MountPath: "/asdf2",
-				}}
-				container(revision.Spec.GetContainer(),
-					//withTCPReadinessProbe(),
-				)
-				// Adding two secrets that do not exist. We should see both of them in the error
-				revision.Spec.Volumes = []corev1.Volume{{
-					Name: "asdf1",
-					VolumeSource: corev1.VolumeSource{
-						Secret: &corev1.SecretVolumeSource{
-							SecretName: "asdf1",
-						},
-					},
-				}, {
-					Name: "asdf2",
-					VolumeSource: corev1.VolumeSource{
-						Secret: &corev1.SecretVolumeSource{
-							SecretName: "asdf2",
-						},
-					},
-				}}
-				revision.ObjectMeta.Labels = map[string]string{}
+		rev: &v1alpha1.Revision{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "foo",
+				Name:      "bar",
 			},
-		),
+			Spec: v1alpha1.RevisionSpec{
+				RevisionSpec: v1.RevisionSpec{
+					PodSpec: corev1.PodSpec{
+						Containers: []corev1.Container{{
+							VolumeMounts: []corev1.VolumeMount{{
+								Name:      "asdf1",
+								MountPath: "/asdf1",
+							}},
+						}},
+						Volumes: []corev1.Volume{{
+							Name: "asdf1",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: "asdf1",
+								},
+							},
+						}, {
+							Name: "asdf2",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: "asdf2",
+								},
+							},
+						}},
+					},
+				},
+			},
+		},
 		si: getSecretInformer(),
 		expectedErr: `secret "asdf1" not found: spec.volumes[0].volumeSource.secretName
 secret "asdf2" not found: spec.volumes[1].volumeSource.secretName`,
 	}, {
 		name: "secrets (from volume source) not found but optional is true",
-		rev: revision(
-			func(revision *v1alpha1.Revision) {
-				revision.Spec.GetContainer().VolumeMounts = []corev1.VolumeMount{{
-					Name:      "asdf",
-					MountPath: "/asdf",
-				}}
-				container(revision.Spec.GetContainer(),
-					//withTCPReadinessProbe(),
-				)
-				revision.Spec.Volumes = []corev1.Volume{{
-					Name: "asdf",
-					VolumeSource: corev1.VolumeSource{
-						Secret: &corev1.SecretVolumeSource{
-							SecretName: "asdf",
-							Optional:   ptr.Bool(true),
-						},
-					},
-				}}
-				revision.ObjectMeta.Labels = map[string]string{}
+		rev: &v1alpha1.Revision{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "foo",
+				Name:      "bar",
 			},
-		),
+			Spec: v1alpha1.RevisionSpec{
+				RevisionSpec: v1.RevisionSpec{
+					PodSpec: corev1.PodSpec{
+						Containers: []corev1.Container{{
+							VolumeMounts: []corev1.VolumeMount{{
+								Name:      "asdf",
+								MountPath: "/asdf",
+							}},
+						}},
+						Volumes: []corev1.Volume{{
+							Name: "asdf",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: "asdf",
+									Optional:   ptr.Bool(true),
+								},
+							},
+						}},
+					},
+				},
+			},
+		},
 		si: getSecretInformer(),
 	}, {
 		name: "secrets (from volume source projected) found",
-		rev: revision(
-			func(revision *v1alpha1.Revision) {
-				revision.Spec.GetContainer().VolumeMounts = []corev1.VolumeMount{{
-					Name:      "asdf1",
-					MountPath: "/asdf1",
-				}}
-				container(revision.Spec.GetContainer(),
-					//withTCPReadinessProbe(),
-				)
-				revision.Spec.Volumes = []corev1.Volume{{
-					Name: "asdf1",
-					VolumeSource: corev1.VolumeSource{
-						Projected: &corev1.ProjectedVolumeSource{
-							Sources: []corev1.VolumeProjection{{
-								Secret: &corev1.SecretProjection{
-									LocalObjectReference: corev1.LocalObjectReference{
-										Name: "asdf1",
-									},
-								},
-							}},
-						},
-					},
-				}}
-				revision.ObjectMeta.Labels = map[string]string{}
+		rev: &v1alpha1.Revision{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "foo",
+				Name:      "bar",
 			},
-		),
+			Spec: v1alpha1.RevisionSpec{
+				RevisionSpec: v1.RevisionSpec{
+					PodSpec: corev1.PodSpec{
+						Containers: []corev1.Container{{
+							VolumeMounts: []corev1.VolumeMount{{
+								Name:      "asdf1",
+								MountPath: "/asdf1",
+							}},
+						}},
+						Volumes: []corev1.Volume{{
+							Name: "asdf1",
+							VolumeSource: corev1.VolumeSource{
+								Projected: &corev1.ProjectedVolumeSource{
+									Sources: []corev1.VolumeProjection{{
+										Secret: &corev1.SecretProjection{
+											LocalObjectReference: corev1.LocalObjectReference{
+												Name: "asdf1",
+											},
+										},
+									}},
+								},
+							},
+						}},
+					},
+				},
+			},
+		},
 		si: addSecretToInformer(kubefake.NewSimpleClientset(), "asdf1", "foo")(getSecretInformer()),
 	}, {
 		name: "secrets (from volume source projected) not found",
-		rev: revision(
-			func(revision *v1alpha1.Revision) {
-				revision.Spec.GetContainer().VolumeMounts = []corev1.VolumeMount{{
-					Name:      "asdf1",
-					MountPath: "/asdf1",
-				}}
-				container(revision.Spec.GetContainer(),
-					//withTCPReadinessProbe(),
-				)
-				revision.Spec.Volumes = []corev1.Volume{{
-					Name: "asdf1",
-					VolumeSource: corev1.VolumeSource{
-						Projected: &corev1.ProjectedVolumeSource{
-							Sources: []corev1.VolumeProjection{{
-								Secret: &corev1.SecretProjection{
-									LocalObjectReference: corev1.LocalObjectReference{
-										Name: "asdf1",
-									},
-								},
-							}, {
-								Secret: &corev1.SecretProjection{
-									LocalObjectReference: corev1.LocalObjectReference{
-										Name: "asdf2",
-									},
-								},
-							}},
-						},
-					},
-				}}
-				revision.ObjectMeta.Labels = map[string]string{}
+		rev: &v1alpha1.Revision{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "foo",
+				Name:      "bar",
 			},
-		),
+			Spec: v1alpha1.RevisionSpec{
+				RevisionSpec: v1.RevisionSpec{
+					PodSpec: corev1.PodSpec{
+						Containers: []corev1.Container{{
+							VolumeMounts: []corev1.VolumeMount{{
+								Name:      "asdf1",
+								MountPath: "/asdf1",
+							}},
+						}},
+						Volumes: []corev1.Volume{{
+							Name: "asdf1",
+							VolumeSource: corev1.VolumeSource{
+								Projected: &corev1.ProjectedVolumeSource{
+									Sources: []corev1.VolumeProjection{{
+										Secret: &corev1.SecretProjection{
+											LocalObjectReference: corev1.LocalObjectReference{
+												Name: "asdf1",
+											},
+										},
+									}, {
+										Secret: &corev1.SecretProjection{
+											LocalObjectReference: corev1.LocalObjectReference{
+												Name: "asdf2",
+											},
+										},
+									}},
+								},
+							},
+						}},
+					},
+				},
+			},
+		},
 		si: getSecretInformer(),
 		expectedErr: `secret "asdf1" not found: spec.volumes[0].volumeSource.projected.sources[0].secret
 secret "asdf2" not found: spec.volumes[0].volumeSource.projected.sources[1].secret`,
 	}, {
 		name: "secrets (from volume source projected) not found but optional is true",
-		rev: revision(
-			func(revision *v1alpha1.Revision) {
-				revision.Spec.GetContainer().VolumeMounts = []corev1.VolumeMount{{
-					Name:      "asdf1",
-					MountPath: "/asdf1",
-				}}
-				container(revision.Spec.GetContainer(),
-					//withTCPReadinessProbe(),
-				)
-				revision.Spec.Volumes = []corev1.Volume{{
-					Name: "asdf1",
-					VolumeSource: corev1.VolumeSource{
-						Projected: &corev1.ProjectedVolumeSource{
-							Sources: []corev1.VolumeProjection{{
-								Secret: &corev1.SecretProjection{
+		rev: &v1alpha1.Revision{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "foo",
+				Name:      "bar",
+			},
+			Spec: v1alpha1.RevisionSpec{
+				RevisionSpec: v1.RevisionSpec{
+					PodSpec: corev1.PodSpec{
+						Containers: []corev1.Container{{
+							VolumeMounts: []corev1.VolumeMount{{
+								Name:      "asdf1",
+								MountPath: "/asdf1",
+							}},
+						}},
+						Volumes: []corev1.Volume{{
+							Name: "asdf1",
+							VolumeSource: corev1.VolumeSource{
+								Projected: &corev1.ProjectedVolumeSource{
+									Sources: []corev1.VolumeProjection{{
+										Secret: &corev1.SecretProjection{
+											LocalObjectReference: corev1.LocalObjectReference{
+												Name: "asdf1",
+											},
+											Optional: ptr.Bool(true),
+										},
+									}},
+								},
+							},
+						}},
+					},
+				},
+			},
+		},
+		si: getSecretInformer(),
+	}, {
+		name: "secrets (from EnvFrom) found",
+		rev: &v1alpha1.Revision{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "foo",
+				Name:      "bar",
+			},
+			Spec: v1alpha1.RevisionSpec{
+				RevisionSpec: v1.RevisionSpec{
+					PodSpec: corev1.PodSpec{
+						Containers: []corev1.Container{{
+							EnvFrom: []corev1.EnvFromSource{{
+								SecretRef: &corev1.SecretEnvSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: "asdf1",
+									},
+								},
+							}},
+						}},
+					},
+				},
+			},
+		},
+		si: addSecretToInformer(kubefake.NewSimpleClientset(), "asdf1", "foo")(getSecretInformer()),
+	}, {
+		name: "secrets (from EnvFrom) not found",
+		rev: &v1alpha1.Revision{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "foo",
+				Name:      "bar",
+			},
+			Spec: v1alpha1.RevisionSpec{
+				RevisionSpec: v1.RevisionSpec{
+					PodSpec: corev1.PodSpec{
+						Containers: []corev1.Container{{
+							EnvFrom: []corev1.EnvFromSource{{
+								SecretRef: &corev1.SecretEnvSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: "asdf1",
+									},
+								},
+							}, {
+								SecretRef: &corev1.SecretEnvSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: "asdf2",
+									},
+								},
+							}},
+						}},
+					},
+				},
+			},
+		},
+		si: getSecretInformer(),
+		expectedErr: `secret "asdf1" not found: spec.containers[0].envFrom[0].secretRef
+secret "asdf2" not found: spec.containers[0].envFrom[1].secretRef`,
+	}, {
+		name: "secrets (from EnvFrom) not found but optional is true",
+		rev: &v1alpha1.Revision{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "foo",
+				Name:      "bar",
+			},
+			Spec: v1alpha1.RevisionSpec{
+				RevisionSpec: v1.RevisionSpec{
+					PodSpec: corev1.PodSpec{
+						Containers: []corev1.Container{{
+							EnvFrom: []corev1.EnvFromSource{{
+								SecretRef: &corev1.SecretEnvSource{
 									LocalObjectReference: corev1.LocalObjectReference{
 										Name: "asdf1",
 									},
 									Optional: ptr.Bool(true),
 								},
 							}},
-						},
+						}},
 					},
-				}}
-				revision.ObjectMeta.Labels = map[string]string{}
+				},
 			},
-		),
-		si: getSecretInformer(),
-	}, {
-		name: "secrets (from EnvFrom) found",
-		rev: revision(
-			func(revision *v1alpha1.Revision) {
-				revision.Spec.Containers = []corev1.Container{{
-					EnvFrom: []corev1.EnvFromSource{{
-						SecretRef: &corev1.SecretEnvSource{
-							LocalObjectReference: corev1.LocalObjectReference{
-								Name: "asdf1",
-							},
-						},
-					}},
-				}}
-				container(revision.Spec.GetContainer(),
-					//withTCPReadinessProbe(),
-				)
-				revision.ObjectMeta.Labels = map[string]string{}
-			},
-		),
-		si: addSecretToInformer(kubefake.NewSimpleClientset(), "asdf1", "foo")(getSecretInformer()),
-	}, {
-		name: "secrets (from EnvFrom) not found",
-		rev: revision(
-			func(revision *v1alpha1.Revision) {
-				revision.Spec.Containers = []corev1.Container{{
-					EnvFrom: []corev1.EnvFromSource{{
-						SecretRef: &corev1.SecretEnvSource{
-							LocalObjectReference: corev1.LocalObjectReference{
-								Name: "asdf1",
-							},
-						},
-					}, {
-						SecretRef: &corev1.SecretEnvSource{
-							LocalObjectReference: corev1.LocalObjectReference{
-								Name: "asdf2",
-							},
-						},
-					}},
-				}}
-				container(revision.Spec.GetContainer(),
-					//withTCPReadinessProbe(),
-				)
-				revision.ObjectMeta.Labels = map[string]string{}
-			},
-		),
-		si: getSecretInformer(),
-		expectedErr: `secret "asdf1" not found: spec.containers[0].envFrom[0].secretRef
-secret "asdf2" not found: spec.containers[0].envFrom[1].secretRef`,
-	}, {
-		name: "secrets (from EnvFrom) not found but optional is true",
-		rev: revision(
-			func(revision *v1alpha1.Revision) {
-				revision.Spec.Containers = []corev1.Container{{
-					EnvFrom: []corev1.EnvFromSource{{
-						SecretRef: &corev1.SecretEnvSource{
-							LocalObjectReference: corev1.LocalObjectReference{
-								Name: "asdf1",
-							},
-							Optional: ptr.Bool(true),
-						},
-					}},
-				}}
-				container(revision.Spec.GetContainer(),
-					//withTCPReadinessProbe(),
-				)
-				revision.ObjectMeta.Labels = map[string]string{}
-			},
-		),
+		},
 		si: getSecretInformer(),
 	}}
 
