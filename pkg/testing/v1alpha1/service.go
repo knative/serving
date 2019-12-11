cf5efbd4cd1536953140df0c7cdb7838acd71899
Tara Gu <guw@ibm.com>
2019-12-11 12:48:48 -0500

    Remove `SetDefaults` from Service() function
    This change removes `SetDefaults` so that `ServiceOption`'s can override the defaults.
    Also update unit tests to set defaults for services.

--- pkg/testing/v1alpha1/service.go
+++ pkg/testing/v1alpha1/service.go
@@ -49,7 +49,6 @@ func Service(name, namespace string, so ...ServiceOption) *v1alpha1.Service {
 	for _, opt := range so {
 		opt(s)
 	}
-	s.SetDefaults(context.Background())
 	return s
 }
 
@@ -63,8 +62,19 @@ func ServiceWithoutNamespace(name string, so ...ServiceOption) *v1alpha1.Service
 	for _, opt := range so {
 		opt(s)
 	}
+	return s
+}
+
+// DefaultService creates a service with ServiceOptions and with default values set
+func DefaultService(name, namespace string, so ...ServiceOption) *v1alpha1.Service {
+	s := Service(name, namespace, so...)
 	s.SetDefaults(context.Background())
 	return s
+}
+
+// WithServiceDefaults will set the default values on the service.
+func WithServiceDefaults(svc *v1alpha1.Service) {
+	svc.SetDefaults(context.Background())
 }
 
 // WithServiceDeletionTimestamp will set the DeletionTimestamp on the Service.
