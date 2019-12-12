e5c4edeeba41ecdcb49b8690f4a58c4312c6d783
Matt Moore <mattmoor@vmware.com>
2019-12-12 12:48:06 -0800

    Move some shared ingress utilities to pkg/network/ingress
    This also adds direct unit test coverage of what wasn't previously covered.
    
    This is a precursor to trying to factor apart the probing logic from the Istio ingress implementation.

--- pkg/reconciler/ingress/resources/virtual_service.go
+++ pkg/reconciler/ingress/resources/virtual_service.go
@@ -17,8 +17,6 @@ limitations under the License.
 package resources
 
 import (
-	"crypto/md5"
-	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -33,7 +31,7 @@ import (
 	"knative.dev/pkg/system"
 	"knative.dev/serving/pkg/apis/networking/v1alpha1"
 	"knative.dev/serving/pkg/apis/serving"
-	net "knative.dev/serving/pkg/network"
+	"knative.dev/serving/pkg/network/ingress"
 	"knative.dev/serving/pkg/reconciler/ingress/resources/names"
 	"knative.dev/serving/pkg/resources"
 )
@@ -57,7 +55,7 @@ func MakeIngressVirtualService(ia *v1alpha1.Ingress, gateways map[v1alpha1.Ingre
 			OwnerReferences: []metav1.OwnerReference{*kmeta.NewControllerRef(ia)},
 			Annotations:     ia.GetAnnotations(),
 		},
-		Spec: *makeVirtualServiceSpec(ia, gateways, expandedHosts(getHosts(ia))),
+		Spec: *makeVirtualServiceSpec(ia, gateways, ingress.ExpandedHosts(getHosts(ia))),
 	}
 
 	// Populate the Ingress labels.
@@ -83,7 +81,7 @@ func MakeMeshVirtualService(ia *v1alpha1.Ingress) *v1alpha3.VirtualService {
 		Spec: *makeVirtualServiceSpec(ia, map[v1alpha1.IngressVisibility]sets.String{
 			v1alpha1.IngressVisibilityExternalIP:   sets.NewString("mesh"),
 			v1alpha1.IngressVisibilityClusterLocal: sets.NewString("mesh"),
-		}, expandedHosts(keepLocalHostnames(getHosts(ia)))),
+		}, ingress.ExpandedHosts(keepLocalHostnames(getHosts(ia)))),
 	}
 	vs.Labels = resources.FilterMap(ia.GetLabels(), func(k string) bool {
 		return k != serving.RouteLabelKey && k != serving.RouteNamespaceLabelKey
@@ -96,7 +94,7 @@ func MakeMeshVirtualService(ia *v1alpha1.Ingress) *v1alpha3.VirtualService {
 func MakeVirtualServices(ia *v1alpha1.Ingress, gateways map[v1alpha1.IngressVisibility]sets.String) ([]*v1alpha3.VirtualService, error) {
 	// Insert probe header
 	ia = ia.DeepCopy()
-	if _, err := InsertProbe(ia); err != nil {
+	if _, err := ingress.InsertProbe(ia); err != nil {
 		return nil, fmt.Errorf("failed to insert a probe into the Ingress: %w", err)
 	}
 
@@ -116,51 +114,6 @@ func MakeVirtualServices(ia *v1alpha1.Ingress, gateways map[v1alpha1.IngressVisi
 	}
 
 	return vss, nil
-}
-
-// InsertProbe adds a AppendHeader rule so that any request going through a Gateway is tagged with
-// the version of the Ingress currently deployed on the Gateway.
-func InsertProbe(ia *v1alpha1.Ingress) (string, error) {
-	bytes, err := ComputeIngressHash(ia)
-	if err != nil {
-		return "", fmt.Errorf("failed to compute the hash of the Ingress: %w", err)
-	}
-	hash := fmt.Sprintf("%x", bytes)
-
-	for _, rule := range ia.Spec.Rules {
-		for i := range rule.HTTP.Paths {
-			if rule.HTTP.Paths[i].AppendHeaders == nil {
-				rule.HTTP.Paths[i].AppendHeaders = make(map[string]string, 1)
-			}
-			rule.HTTP.Paths[i].AppendHeaders[net.HashHeaderName] = hash
-		}
-	}
-
-	return hash, nil
-}
-
-// ComputeIngressHash computes a hash of the Ingress Spec, Namespace and Name
-func ComputeIngressHash(ia *v1alpha1.Ingress) ([16]byte, error) {
-	bytes, err := json.Marshal(ia.Spec)
-	if err != nil {
-		return [16]byte{}, fmt.Errorf("failed to serialize Ingress: %w", err)
-	}
-	bytes = append(bytes, []byte(ia.GetNamespace())...)
-	bytes = append(bytes, []byte(ia.GetName())...)
-	return md5.Sum(bytes), nil
-}
-
-// HostsPerGateway returns the set of hosts that each Gateway handles
-func HostsPerGateway(ia *v1alpha1.Ingress, gateways map[v1alpha1.IngressVisibility]sets.String) map[string][]string {
-	output := make(map[string][]string)
-	for _, rule := range ia.Spec.Rules {
-		for host := range expandedHosts(sets.NewString(rule.Hosts...)) {
-			for gateway := range gateways[rule.Visibility] {
-				output[gateway] = append(output[gateway], host)
-			}
-		}
-	}
-	return output
 }
 
 func makeVirtualServiceSpec(ia *v1alpha1.Ingress, gateways map[v1alpha1.IngressVisibility]sets.String, hosts sets.String) *istiov1alpha3.VirtualService {
@@ -249,23 +202,6 @@ func keepLocalHostnames(hosts sets.String) sets.String {
 		}
 	}
 	return retained
-}
-
-func expandedHosts(hosts sets.String) sets.String {
-	expanded := sets.NewString()
-	allowedSuffixes := []string{
-		"",
-		"." + network.GetClusterDomainName(),
-		".svc." + network.GetClusterDomainName(),
-	}
-	for _, h := range hosts.List() {
-		for _, suffix := range allowedSuffixes {
-			if strings.HasSuffix(h, suffix) {
-				expanded.Insert(strings.TrimSuffix(h, suffix))
-			}
-		}
-	}
-	return expanded
 }
 
 func makeMatch(host string, pathRegExp string, gateways sets.String) *istiov1alpha3.HTTPMatchRequest {
