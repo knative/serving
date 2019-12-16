be10310249952dd0da1a65269aadd4257700ae75
Tara Gu <guw@ibm.com>
2019-12-16 16:47:08 -0500

    review

--- pkg/testing/v1alpha1/service.go
+++ pkg/testing/v1alpha1/service.go
@@ -67,8 +67,7 @@ func ServiceWithoutNamespace(name string, so ...ServiceOption) *v1alpha1.Service
 
 // DefaultService creates a service with ServiceOptions and with default values set
 func DefaultService(name, namespace string, so ...ServiceOption) *v1alpha1.Service {
-	s := Service(name, namespace, append(so,WithServiceDefaults)...)
-	return s
+	return Service(name, namespace, append(so,WithServiceDefaults)...)
 }
 
 // WithServiceDefaults will set the default values on the service.
