604bdd3afc450e9b74cb648bf4f2367898553296
Matt Moore <mattmoor@vmware.com>
2019-12-23 09:47:40 -0800

    Auto-update dependencies (#6302)
    Produced via:
      `dep ensure -update knative.dev/test-infra knative.dev/pkg knative.dev/caching`
    /assign vagababov
    /cc vagababov

--- vendor/knative.dev/pkg/testutils/clustermanager/perf-tests/pkg/cluster.go
+++ vendor/knative.dev/pkg/testutils/clustermanager/perf-tests/pkg/cluster.go
@@ -230,11 +230,15 @@ func (gc *gkeClient) createClusterWithRetries(gcpProject, name string, config Cl
 		addons = strings.Split(config.Addons, ",")
 	}
 	req := &gke.Request{
+		Project:     gcpProject,
 		ClusterName: name,
 		MinNodes:    config.NodeCount,
 		MaxNodes:    config.NodeCount,
 		NodeType:    config.NodeType,
 		Addons:      addons,
+		// Enable Workload Identity for performance tests because we need to use a Kubernetes service account to act
+		// as a Google cloud service account, which is then used for authentication to the metrics data storage system.
+		EnableWorkloadIdentity: true,
 	}
 	creq, err := gke.NewCreateClusterRequest(req)
 	if err != nil {
