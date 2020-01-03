knative/serving

commit 6434bfbf1c1f79a94acf6d10d324bbe3328508c9

Split out probing and the building of probe targets. (#6233)
* Split out probing and the building of probe targets.

* Addressing the stutter.

* Add TODO.

* Extract common funcs to pkg/network.

* Change signature in accordance with usual informer event handler.

* Add TODO.

* Use better func names NameForPortNumber and PortNumberForName.

* Rename constructor to be more accordance with filename.

* Stop wrapping map in struct for fakeProbeTargetLister.

* Address more nits.

pkg/network/status/status.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package ingress
+package status
 
 import (
 	"context"
@@ -23,7 +23,6 @@ import (
 	"net"
 	"net/http"
 	"reflect"
-	"strconv"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -31,17 +30,12 @@ import (
 	"go.uber.org/zap"
 
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/wait"
-	corev1listers "k8s.io/client-go/listers/core/v1"
-	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 
-	"istio.io/client-go/pkg/apis/networking/v1alpha3"
 	"knative.dev/pkg/network/prober"
 	"knative.dev/serving/pkg/apis/networking/v1alpha1"
-	istiolisters "knative.dev/serving/pkg/client/istio/listers/networking/v1alpha3"
 	"knative.dev/serving/pkg/network"
 	"knative.dev/serving/pkg/network/ingress"
 )
@@ -85,22 +79,21 @@ type workItem struct {
 	podPort      string
 }
 
-// probeTargets represents the target to probe.
-type probeTarget struct {
-	// urlTmpl is an url template to probe.
-	urlTmpl string
-	// targetPort is a port of Gateway pod.
-	targetPort string
+// ProbeTargetLister lists all the targets that requires probing.
+type ProbeTargetLister interface {
+
+	// ListProbeTargets returns the target to be probed as a map from podIP -> port -> urls.
+	ListProbeTargets(ctx context.Context, ingress *v1alpha1.Ingress) (map[string]map[string]sets.String, error)
 }
 
-// StatusManager provides a way to check if a VirtualService is ready
-type StatusManager interface {
-	IsReady(ia *v1alpha1.Ingress, gw map[v1alpha1.IngressVisibility]sets.String) (bool, error)
+// Manager provides a way to check if an Ingress is ready
+type Manager interface {
+	IsReady(ctx context.Context, ia *v1alpha1.Ingress) (bool, error)
 }
 
-// StatusProber provides a way to check if a VirtualService is ready by probing the Envoy pods
+// Prober provides a way to check if a VirtualService is ready by probing the Envoy pods
 // handling that VirtualService.
-type StatusProber struct {
+type Prober struct {
 	logger *zap.SugaredLogger
 
 	// mu guards ingressStates and podStates
@@ -110,9 +103,7 @@ type StatusProber struct {
 
 	workQueue workqueue.RateLimitingInterface
 
-	gatewayLister   istiolisters.GatewayLister
-	endpointsLister corev1listers.EndpointsLister
-	serviceLister   corev1listers.ServiceLister
+	targetLister ProbeTargetLister
 
 	readyCallback func(*v1alpha1.Ingress)
 
@@ -121,23 +112,19 @@ type StatusProber struct {
 	cleanupPeriod    time.Duration
 }
 
-// NewStatusProber creates a new instance of StatusProber
-func NewStatusProber(
+// NewProber creates a new instance of Prober
+func NewProber(
 	logger *zap.SugaredLogger,
-	gatewayLister istiolisters.GatewayLister,
-	endpointsLister corev1listers.EndpointsLister,
-	serviceLister corev1listers.ServiceLister,
-	readyCallback func(*v1alpha1.Ingress)) *StatusProber {
-	return &StatusProber{
+	targetLister ProbeTargetLister,
+	readyCallback func(*v1alpha1.Ingress)) *Prober {
+	return &Prober{
 		logger:        logger,
 		ingressStates: make(map[string]*ingressState),
 		podStates:     make(map[string]*podState),
 		workQueue: workqueue.NewNamedRateLimitingQueue(
 			workqueue.DefaultControllerRateLimiter(),
 			"ProbingQueue"),
-		gatewayLister:    gatewayLister,
-		endpointsLister:  endpointsLister,
-		serviceLister:    serviceLister,
+		targetLister:     targetLister,
 		readyCallback:    readyCallback,
 		probeConcurrency: probeConcurrency,
 		stateExpiration:  stateExpiration,
@@ -145,13 +132,17 @@ func NewStatusProber(
 	}
 }
 
+func ingressKey(ing *v1alpha1.Ingress) string {
+	return fmt.Sprintf("%s/%s", ing.GetNamespace(), ing.GetName())
+}
+
 // IsReady checks if the provided Ingress is ready, i.e. the Envoy pods serving the Ingress
 // have all been updated. This function is designed to be used by the Ingress controller, i.e. it
 // will be called in the order of reconciliation. This means that if IsReady is called on an Ingress,
 // this Ingress is the latest known version and therefore anything related to older versions can be ignored.
 // Also, it means that IsReady is not called concurrently.
-func (m *StatusProber) IsReady(ia *v1alpha1.Ingress, gw map[v1alpha1.IngressVisibility]sets.String) (bool, error) {
-	ingressKey := fmt.Sprintf("%s/%s", ia.GetNamespace(), ia.GetName())
+func (m *Prober) IsReady(ctx context.Context, ia *v1alpha1.Ingress) (bool, error) {
+	ingressKey := ingressKey(ia)
 
 	bytes, err := ingress.ComputeHash(ia)
 	if err != nil {
@@ -186,68 +177,59 @@ func (m *StatusProber) IsReady(ia *v1alpha1.Ingress, gw map[v1alpha1.IngressVisi
 	}
 
 	var workItems []*workItem
-	for gatewayName, hosts := range ingress.HostsPerVisibility(ia, gw) {
-		gateway, err := m.getGateway(gatewayName)
-		if err != nil {
-			return false, fmt.Errorf("failed to get Gateway %q: %w", gatewayName, err)
-		}
-		targetsPerPod, err := m.listGatewayTargetsPerPods(gateway)
-		if err != nil {
-			return false, fmt.Errorf("failed to list the probing URLs of Gateway %q: %w", gatewayName, err)
-		}
-		if len(targetsPerPod) == 0 {
-			continue
-		}
 
-		for ip, targets := range targetsPerPod {
-			// Each Pod backing a Gateway is probed using the different hosts, protocol and ports until
-			// one of the probing calls succeeds. Then, the Pod is considered ready and all pending work items
-			// scheduled for that pod are cancelled.
-			ctx, cancel := context.WithCancel(ingCtx)
-			podState := &podState{
-				successCount: 0,
-				context:      ctx,
-				cancel:       cancel,
-			}
-
-			// Save the podState to be able to cancel it in case of Pod deletion
-			func() {
-				m.mu.Lock()
-				defer m.mu.Unlock()
-				m.podStates[ip] = podState
-			}()
-
-			// Update states and cleanup m.podStates when probing is done or cancelled
-			go func(ip string) {
-				<-podState.context.Done()
-				m.updateStates(ingressState, podState)
-
-				m.mu.Lock()
-				defer m.mu.Unlock()
-				// It is critical to check that the current podState is also the one stored in the map
-				// before deleting it because it could have been replaced if a new version of the ingress
-				// has started being probed.
-				if state, ok := m.podStates[ip]; ok && state == podState {
-					delete(m.podStates, ip)
-				}
-			}(ip)
-
-			for _, host := range hosts.List() {
-				for _, target := range targets {
-					workItem := &workItem{
-						ingressState: ingressState,
-						podState:     podState,
-						url:          fmt.Sprintf(target.urlTmpl, host),
-						podIP:        ip,
-						podPort:      target.targetPort,
-					}
-					workItems = append(workItems, workItem)
-				}
-			}
-		}
-		ingressState.pendingCount += int32(len(targetsPerPod))
+	allProbeTargets, err := m.targetLister.ListProbeTargets(ctx, ia)
+	if err != nil {
+		return false, err
 	}
 
+	for ip, targets := range allProbeTargets {
+		// Each Pod is probed using the different hosts, protocol and ports until
+		// one of the probing calls succeeds. Then, the Pod is considered ready and all pending work items
+		// scheduled for that pod are cancelled.
+		ctx, cancel := context.WithCancel(ingCtx)
+		podState := &podState{
+			successCount: 0,
+			context:      ctx,
+			cancel:       cancel,
+		}
+
+		// Save the podState to be able to cancel it in case of Pod deletion
+		func() {
+			m.mu.Lock()
+			defer m.mu.Unlock()
+			m.podStates[ip] = podState
+		}()
+
+		// Update states and cleanup m.podStates when probing is done or cancelled
+		go func(ip string) {
+			<-podState.context.Done()
+			m.updateStates(ingressState, podState)
+
+			m.mu.Lock()
+			defer m.mu.Unlock()
+			// It is critical to check that the current podState is also the one stored in the map
+			// before deleting it because it could have been replaced if a new version of the ingress
+			// has started being probed.
+			if state, ok := m.podStates[ip]; ok && state == podState {
+				delete(m.podStates, ip)
+			}
+		}(ip)
+
+		for port, urls := range targets {
+			for _, url := range urls.List() {
+				workItem := &workItem{
+					ingressState: ingressState,
+					podState:     podState,
+					url:          url,
+					podIP:        ip,
+					podPort:      port,
+				}
+				workItems = append(workItems, workItem)
+			}
+		}
+		ingressState.pendingCount++
+	}
 	func() {
 		m.mu.Lock()
 		defer m.mu.Unlock()
@@ -260,8 +242,8 @@ func (m *StatusProber) IsReady(ia *v1alpha1.Ingress, gw map[v1alpha1.IngressVisi
 	return len(workItems) == 0, nil
 }
 
-// Start starts the StatusManager background operations
-func (m *StatusProber) Start(done <-chan struct{}) {
+// Start starts the Manager background operations
+func (m *Prober) Start(done <-chan struct{}) {
 	// Start the worker goroutines
 	for i := 0; i < m.probeConcurrency; i++ {
 		go func() {
@@ -280,29 +262,37 @@ func (m *StatusProber) Start(done <-chan struct{}) {
 	}()
 }
 
-// CancelVirtualServiceProbing cancels probing of the provided VirtualService.
-func (m *StatusProber) CancelVirtualServiceProbing(vs *v1alpha3.VirtualService) {
-	key := fmt.Sprintf("%s/%s", vs.Namespace, vs.Name)
+// CancelIngressProbing cancels probing of the provided Ingress
+// TODO(#6270): Use cache.DeletedFinalStateUnknown.
+func (m *Prober) CancelIngressProbing(obj interface{}) {
+	if ing, ok := obj.(*v1alpha1.Ingress); ok {
+		key := ingressKey(ing)
 
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	if state, ok := m.ingressStates[key]; ok {
-		state.cancel()
-		delete(m.ingressStates, key)
+		m.mu.Lock()
+		defer m.mu.Unlock()
+		if state, ok := m.ingressStates[key]; ok {
+			state.cancel()
+			delete(m.ingressStates, key)
+		}
 	}
 }
 
 // CancelPodProbing cancels probing of the provided Pod IP.
-func (m *StatusProber) CancelPodProbing(pod *corev1.Pod) {
-	m.mu.Lock()
-	defer m.mu.Unlock()
-	if state, ok := m.podStates[pod.Status.PodIP]; ok {
-		state.cancel()
+//
+// TODO(#6269): make this cancelation based on Pod x port instead of just Pod.
+func (m *Prober) CancelPodProbing(obj interface{}) {
+	if pod, ok := obj.(*corev1.Pod); ok {
+		m.mu.Lock()
+		defer m.mu.Unlock()
+
+		if state, ok := m.podStates[pod.Status.PodIP]; ok {
+			state.cancel()
+		}
 	}
 }
 
 // expireOldStates removes the states that haven't been accessed in a while.
-func (m *StatusProber) expireOldStates() {
+func (m *Prober) expireOldStates() {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	for key, state := range m.ingressStates {
@@ -315,7 +305,7 @@ func (m *StatusProber) expireOldStates() {
 
 // processWorkItem processes a single work item from workQueue.
 // It returns false when there is no more items to process, true otherwise.
-func (m *StatusProber) processWorkItem() bool {
+func (m *Prober) processWorkItem() bool {
 	obj, shutdown := m.workQueue.Get()
 	if shutdown {
 		return false
@@ -370,7 +360,7 @@ func (m *StatusProber) processWorkItem() bool {
 	return true
 }
 
-func (m *StatusProber) updateStates(ingressState *ingressState, podState *podState) {
+func (m *Prober) updateStates(ingressState *ingressState, podState *podState) {
 	if atomic.AddInt32(&podState.successCount, 1) == 1 {
 		// This is the first successful probe call for the pod, cancel all other work items for this pod
 		podState.cancel()
@@ -380,90 +370,4 @@ func (m *StatusProber) updateStates(ingressState *ingressState, podState *podSta
 			m.readyCallback(ingressState.ia)
 		}
 	}
-}
-
-func (m *StatusProber) getGateway(name string) (*v1alpha3.Gateway, error) {
-	namespace, name, err := cache.SplitMetaNamespaceKey(name)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse Gateway name %q: %w", name, err)
-	}
-	if namespace == "" {
-		return nil, fmt.Errorf("unexpected unqualified Gateway name %q", name)
-	}
-	return m.gatewayLister.Gateways(namespace).Get(name)
-}
-
-// listGatewayPodsURLs returns a map where the keys are the Gateway Pod IPs and the values are the corresponding
-// URL templates and the Gateway Pod Port to be probed.
-func (m *StatusProber) listGatewayTargetsPerPods(gateway *v1alpha3.Gateway) (map[string][]probeTarget, error) {
-	selector := labels.SelectorFromSet(gateway.Spec.Selector)
-
-	services, err := m.serviceLister.List(selector)
-	if err != nil {
-		return nil, fmt.Errorf("failed to list Services: %w", err)
-	}
-	if len(services) == 0 {
-		m.logger.Infof("Skipping Gateway %s/%s because it has no corresponding Service", gateway.Namespace, gateway.Name)
-		return nil, nil
-	}
-	service := services[0]
-
-	endpoints, err := m.endpointsLister.Endpoints(service.Namespace).Get(service.Name)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get Endpoints: %w", err)
-	}
-
-	targetsPerPods := make(map[string][]probeTarget)
-	for _, server := range gateway.Spec.Servers {
-		var urlTmpl string
-		switch server.Port.Protocol {
-		case "HTTP", "HTTP2":
-			if server.Tls == nil || !server.Tls.HttpsRedirect {
-				urlTmpl = "http://%%s:%d/"
-			}
-		case "HTTPS":
-			urlTmpl = "https://%%s:%d/"
-		default:
-			m.logger.Infof("Skipping Server %q because protocol %q is not supported", server.Port.Name, server.Port.Protocol)
-			continue
-		}
-
-		portName, err := findNameForPortNumber(service, int32(server.Port.Number))
-		if err != nil {
-			m.logger.Infof("Skipping Server %q because Service %s/%s doesn't contain a port %d", server.Port.Name, service.Namespace, service.Name, server.Port.Number)
-			continue
-		}
-		for _, sub := range endpoints.Subsets {
-			portNumber, err := findPortNumberForName(sub, portName)
-			if err != nil {
-				m.logger.Infof("Skipping Subset %v because it doesn't contain a port %q", sub.Addresses, portName)
-				continue
-			}
-
-			for _, addr := range sub.Addresses {
-				targetsPerPods[addr.IP] = append(targetsPerPods[addr.IP], probeTarget{urlTmpl: fmt.Sprintf(urlTmpl, server.Port.Number), targetPort: strconv.Itoa(int(portNumber))})
-			}
-		}
-	}
-	return targetsPerPods, nil
-}
-
-// findNameForPortNumber finds the name for a given port as defined by a Service.
-func findNameForPortNumber(svc *corev1.Service, portNumber int32) (string, error) {
-	for _, port := range svc.Spec.Ports {
-		if port.Port == portNumber {
-			return port.Name, nil
-		}
-	}
-	return "", fmt.Errorf("no port with number %d found", portNumber)
-}
-
-// findPortNumberForName resolves a given name to a portNumber as defined by an EndpointSubset.
-func findPortNumberForName(sub corev1.EndpointSubset, portName string) (int32, error) {
-	for _, subPort := range sub.Ports {
-		if subPort.Name == portName {
-			return subPort.Port, nil
-		}
-	}
-	return 0, fmt.Errorf("no port for name %q found", portName)
 }

pkg/network/network.go
@@ -384,3 +384,23 @@ func RewriteHostOut(r *http.Request) {
 		r.Header.Del(OriginalHostHeader)
 	}
 }
+
+// NameForPortNumber finds the name for a given port as defined by a Service.
+func NameForPortNumber(svc *corev1.Service, portNumber int32) (string, error) {
+	for _, port := range svc.Spec.Ports {
+		if port.Port == portNumber {
+			return port.Name, nil
+		}
+	}
+	return "", fmt.Errorf("no port with number %d found", portNumber)
+}
+
+// PortNumberForName resolves a given name to a portNumber as defined by an EndpointSubset.
+func PortNumberForName(sub corev1.EndpointSubset, portName string) (int32, error) {
+	for _, subPort := range sub.Ports {
+		if subPort.Name == portName {
+			return subPort.Port, nil
+		}
+	}
+	return 0, fmt.Errorf("no port for name %q found", portName)
+}

pkg/network/status/status_test.go
@@ -1,0 +1,309 @@
+/*
+Copyright 2019 The Knative Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package status
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/sets"
+	"knative.dev/serving/pkg/apis/networking/v1alpha1"
+	"knative.dev/serving/pkg/network/ingress"
+
+	"go.uber.org/zap/zaptest"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"knative.dev/serving/pkg/network"
+)
+
+var (
+	iaTemplate = &v1alpha1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
+			Name:      "whatever",
+		},
+		Spec: v1alpha1.IngressSpec{
+			Rules: []v1alpha1.IngressRule{{
+				Hosts: []string{
+					"foo.bar.com",
+				},
+				Visibility: v1alpha1.IngressVisibilityExternalIP,
+				HTTP:       &v1alpha1.HTTPIngressRuleValue{},
+			}},
+		},
+	}
+)
+
+func TestProbeLifecycle(t *testing.T) {
+	ia := iaTemplate.DeepCopy()
+	hash, err := ingress.InsertProbe(ia)
+	if err != nil {
+		t.Fatalf("failed to insert probe: %v", err)
+	}
+
+	// Simulate that the latest configuration is not applied yet by returning a different
+	// hash once and then the by returning the expected hash.
+	hashes := make(chan string, 1)
+	hashes <- "not-the-hash-you-are-looking-for"
+	go func() {
+		for {
+			hashes <- hash
+		}
+	}()
+
+	// Dummy handler returning HTTP 500 (it should never be called during probing)
+	dummyRequests := make(chan *http.Request)
+	dummyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		dummyRequests <- r
+		w.WriteHeader(500)
+	})
+
+	// Actual probe handler used in Activator and Queue-Proxy
+	probeHandler := network.NewProbeHandler(dummyHandler)
+
+	// Dummy handler keeping track of received requests, mimicking AppendHeader of K-Network-Hash
+	// and simulate a non-existing host by returning 404.
+	probeRequests := make(chan *http.Request)
+	finalHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.HasPrefix(r.Host, "foo.bar.com") {
+			w.WriteHeader(404)
+			return
+		}
+
+		probeRequests <- r
+		r.Header.Set(network.HashHeaderName, <-hashes)
+		probeHandler.ServeHTTP(w, r)
+	})
+
+	ts := httptest.NewServer(finalHandler)
+	defer ts.Close()
+	url, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Fatalf("failed to parse URL %q: %v", ts.URL, err)
+	}
+	hostname := url.Hostname()
+	port, err := strconv.Atoi(url.Port())
+	if err != nil {
+		t.Fatalf("failed to parse port %q: %v", url.Port(), err)
+	}
+
+	ready := make(chan *v1alpha1.Ingress)
+	prober := NewProber(
+		zaptest.NewLogger(t).Sugar(),
+		fakeProbeTargetLister{
+			hostname: map[string]sets.String{
+				strconv.Itoa(port): sets.NewString("http://foo.bar.com:80"),
+			},
+		},
+		func(ia *v1alpha1.Ingress) {
+			ready <- ia
+		})
+
+	prober.stateExpiration = 2 * time.Second
+	prober.cleanupPeriod = 500 * time.Millisecond
+
+	done := make(chan struct{})
+	defer close(done)
+	prober.Start(done)
+
+	// The first call to IsReady must succeed and return false
+	ok, err := prober.IsReady(context.Background(), ia)
+	if err != nil {
+		t.Fatalf("IsReady failed: %v", err)
+	}
+	if ok {
+		t.Fatalf("IsReady returned %v, want: %v", ok, false)
+	}
+
+	const expHostHeader = "foo.bar.com"
+
+	// Wait for the first (failing) and second (success) requests to be executed and validate Host header
+	for i := 0; i < 2; i++ {
+		req := <-probeRequests
+		if req.Host != expHostHeader {
+			t.Fatalf("Host header = %q, want %q", req.Host, expHostHeader)
+		}
+	}
+
+	// Wait for the probing to eventually succeed
+	<-ready
+
+	// The subsequent calls to IsReady must succeed and return true
+	for i := 0; i < 5; i++ {
+		if ok, err = prober.IsReady(context.Background(), ia); err != nil {
+			t.Fatalf("IsReady failed: %v", err)
+		} else if !ok {
+			t.Fatalf("IsReady returned %v, want: %v", ok, false)
+		}
+
+		time.Sleep(prober.cleanupPeriod)
+	}
+
+	select {
+	// Wait for the cleanup to happen
+	case <-time.After(prober.stateExpiration + prober.cleanupPeriod):
+		break
+	// Validate that no probe requests were issued (cached)
+	case <-probeRequests:
+		t.Fatal("an unexpected probe request was received")
+	// Validate that no requests went through the probe handler
+	case <-dummyRequests:
+		t.Fatal("an unexpected request went through the probe handler")
+	}
+
+	// The state has expired and been removed
+	ok, err = prober.IsReady(context.Background(), ia)
+	if err != nil {
+		t.Fatalf("IsReady failed: %v", err)
+	}
+	if ok {
+		t.Fatalf("IsReady returned %v, want: %v", ok, false)
+	}
+
+	// Wait for the first request (success) to be executed
+	<-probeRequests
+
+	// Wait for the probing to eventually succeed
+	<-ready
+
+	select {
+	// Validate that no requests went through the probe handler
+	case <-dummyRequests:
+		t.Fatal("an unexpected request went through the probe handler")
+	default:
+		break
+	}
+}
+
+func TestCancellation(t *testing.T) {
+	ia := iaTemplate.DeepCopy()
+	// Handler keeping track of received requests and mimicking an Ingress not ready
+	requests := make(chan *http.Request, 100)
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requests <- r
+		w.WriteHeader(404)
+	})
+
+	ts := httptest.NewServer(handler)
+	defer ts.Close()
+	url, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Fatalf("failed to parse URL %q: %v", ts.URL, err)
+	}
+	hostname := url.Hostname()
+	if err != nil {
+		t.Fatalf("failed to parse port %q: %v", url.Port(), err)
+	}
+	port, err := strconv.Atoi(url.Port())
+	if err != nil {
+		t.Fatalf("failed to parse port %q: %v", url.Port(), err)
+	}
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
+			Name:      "gateway",
+		},
+		Status: v1.PodStatus{
+			PodIP: strings.Split(url.Host, ":")[0],
+		},
+	}
+
+	ready := make(chan *v1alpha1.Ingress)
+	prober := NewProber(
+		zaptest.NewLogger(t).Sugar(),
+		fakeProbeTargetLister{
+			hostname: map[string]sets.String{
+				strconv.Itoa(port): sets.NewString("http://foo.bar.com:80"),
+			},
+		},
+		func(ia *v1alpha1.Ingress) {
+			ready <- ia
+		})
+
+	done := make(chan struct{})
+	defer close(done)
+	prober.Start(done)
+
+	ok, err := prober.IsReady(context.Background(), ia)
+	if err != nil {
+		t.Fatalf("IsReady failed: %v", err)
+	}
+	if ok {
+		t.Fatalf("IsReady returned %v, want: %v", ok, false)
+	}
+
+	// Wait for the first probe request
+	<-requests
+
+	// Create a new version of the Ingress
+	ia = ia.DeepCopy()
+	ia.Spec.Rules[0].Hosts[0] = "blabla.net"
+
+	// Check that probing is unsuccessful
+	select {
+	case <-ready:
+		t.Fatal("Probing succeeded while it should not have succeeded")
+	default:
+	}
+
+	ok, err = prober.IsReady(context.Background(), ia)
+	if err != nil {
+		t.Fatalf("IsReady failed: %v", err)
+	}
+	if ok {
+		t.Fatalf("IsReady returned %v, want: %v", ok, false)
+	}
+
+	// Drain requests for the old version
+	for req := range requests {
+		log.Printf("req.Host: %s", req.Host)
+		if strings.HasPrefix(req.Host, "blabla.net") {
+			break
+		}
+	}
+
+	// Cancel Pod probing
+	prober.CancelPodProbing(pod)
+
+	// Check that the requests were for the new version
+	close(requests)
+	for req := range requests {
+		if !strings.HasPrefix(req.Host, "blabla.net") {
+			t.Fatalf("Unexpected Host: want: %s, got: %s", "blabla.net", req.Host)
+		}
+	}
+}
+
+type fakeProbeTargetLister map[string]map[string]sets.String
+
+func (l fakeProbeTargetLister) ListProbeTargets(ctx context.Context, ing *v1alpha1.Ingress) (map[string]map[string]sets.String, error) {
+	targets := map[string]map[string]sets.String{}
+	for ip, m := range l {
+		targets[ip] = map[string]sets.String{}
+		for port := range m {
+			targets[ip][port] = sets.NewString("http://" + ing.Spec.Rules[0].Hosts[0])
+		}
+	}
+	return targets, nil
+}

commit 21aa0af30b03e1d94615b693266adad26a8cef49

Fix Ingress prober bug introduced by #6233 (#6406)
* Process probe targets grouped by IP.

* Add TODO.

pkg/network/status/status.go
@@ -50,6 +50,7 @@ const (
 	cleanupPeriod = 1 * time.Minute
 )
 
+// TODO(https://github.com/knative/serving/issues/6407):  Default timeouts may lead to hanging probes.
 var dialContext = (&net.Dialer{}).DialContext
 
 // ingressState represents the probing progress at the Ingress scope
@@ -184,73 +185,69 @@ func (m *Prober) IsReady(ctx context.Context, ing *v1alpha1.Ingress) (bool, erro
 		cancel:       cancel,
 	}
 
-	var workItems []*workItem
+	workItems := make(map[string][]*workItem)
 
 	allProbeTargets, err := m.targetLister.ListProbeTargets(ctx, ing)
 	if err != nil {
 		return false, err
 	}
 
-	allPodIPs := sets.NewString()
+	// First, group the targets by IPs.
 	for _, target := range allProbeTargets {
-		// Iterate through sorted Pod IPs, for a consistent order.
-		for _, ip := range target.PodIPs.List() {
-			allPodIPs.Insert(ip)
-			// Each Pod is probed using the different hosts, protocol and ports until
-			// one of the probing calls succeeds. Then, the Pod is considered ready and all pending work items
-			// scheduled for that pod are cancelled.
-			ctx, cancel := context.WithCancel(ingCtx)
-			podState := &podState{
-				successCount: 0,
-				context:      ctx,
-				cancel:       cancel,
-			}
-
-			// Save the podState to be able to cancel it in case of Pod deletion
-			func() {
-				m.mu.Lock()
-				defer m.mu.Unlock()
-				m.podStates[ip] = podState
-			}()
-
-			// Update states and cleanup m.podStates when probing is done or cancelled
-			go func(ip string) {
-				<-podState.context.Done()
-				m.updateStates(ingressState, podState)
-
-				m.mu.Lock()
-				defer m.mu.Unlock()
-				// It is critical to check that the current podState is also the one stored in the map
-				// before deleting it because it could have been replaced if a new version of the ingress
-				// has started being probed.
-				if state, ok := m.podStates[ip]; ok && state == podState {
-					delete(m.podStates, ip)
-				}
-			}(ip)
-
+		for ip := range target.PodIPs {
 			for _, url := range target.URLs {
-				workItem := &workItem{
+				workItems[ip] = append(workItems[ip], &workItem{
 					ingressState: ingressState,
-					podState:     podState,
 					url:          url,
 					podIP:        ip,
 					podPort:      target.Port,
-				}
-				workItems = append(workItems, workItem)
+				})
 			}
 		}
 	}
-	ingressState.pendingCount += int32(allPodIPs.Len())
+	for ip, ipWorkItems := range workItems {
+		ctx, cancel := context.WithCancel(ingCtx)
+		podState := &podState{
+			successCount: 0,
+			context:      ctx,
+			cancel:       cancel,
+		}
+
+		// Save the podState to be able to cancel it in case of Pod deletion
+		func() {
+			m.mu.Lock()
+			defer m.mu.Unlock()
+			m.podStates[ip] = podState
+		}()
+
+		// Update states and cleanup m.podStates when probing is done or cancelled
+		go func(ip string) {
+			<-podState.context.Done()
+			m.updateStates(ingressState, podState)
+
+			m.mu.Lock()
+			defer m.mu.Unlock()
+			// It is critical to check that the current podState is also the one stored in the map
+			// before deleting it because it could have been replaced if a new version of the ingress
+			// has started being probed.
+			if state, ok := m.podStates[ip]; ok && state == podState {
+				delete(m.podStates, ip)
+			}
+		}(ip)
+
+		for _, wi := range ipWorkItems {
+			wi.podState = podState
+			m.workQueue.AddRateLimited(wi)
+			m.logger.Infof("Queuing probe for %s, IP: %s:%s (depth: %d)",
+				wi.url, wi.podIP, wi.podPort, m.workQueue.Len())
+		}
+	}
+	ingressState.pendingCount += int32(len(workItems))
 	func() {
 		m.mu.Lock()
 		defer m.mu.Unlock()
 		m.ingressStates[ingressKey] = ingressState
 	}()
-	for _, workItem := range workItems {
-		m.workQueue.AddRateLimited(workItem)
-		m.logger.Infof("Queuing probe for %s, IP: %s:%s (depth: %d)",
-			workItem.url, workItem.podIP, workItem.podPort, m.workQueue.Len())
-	}
 	return len(workItems) == 0, nil
 }
 

test/e2e/istio/probing_test.go
@@ -57,7 +57,6 @@ var (
 )
 
 func TestIstioProbing(t *testing.T) {
-	t.Skip("https://github.com/knative/serving/issues/6400")
 	cancel := logstream.Start(t)
 	defer cancel()
 