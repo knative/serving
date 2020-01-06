1fdc974221061f67d343c72f4bbfd10344ccfb69
Matt Moore <mattmoor@vmware.com>
2020-01-06 14:49:49 -0800

    Try to normalize how Istio is treated in our e2e tests to align with others (#6434)
    * Remove lots of Istio special casing
    
    * Change indentation, use mktemp, add back dropped check.

--- test/e2e-upgrade-tests.sh
+++ test/e2e-upgrade-tests.sh
@@ -45,9 +45,12 @@ function install_latest_release() {
     yaml="serving-core.yaml"
   fi
 
-  install_knative_serving \
-    "${url}/${yaml}" \
-    || fail_test "Knative latest release installation failed"
+  local RELEASE_YAML="$(mktemp)"
+  wget "${url}/${yaml}" -O "${RELEASE_YAML}" \
+      || fail_test "Unable to download latest Knative release."
+
+  install_knative_serving "${RELEASE_YAML}" \
+      || fail_test "Knative latest release installation failed"
   wait_until_pods_running knative-serving
 }
 
