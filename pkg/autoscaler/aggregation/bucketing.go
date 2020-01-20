286a4df4070cd7b764fdf47bdfc5d4a721d2c345
Victor Agababov <vagababov@gmail.com>
2020-01-20 10:50:59 -0800

    Generalize precision func and raise default to 6 digits
    Since there may be edgecases when 3 digits is not enough.
    e.g. big window (1hr) and 1 request, or there might be many pods and just one request
    e.g. after a big drop off in traffic.
    Hence raise it to 6 digits, which should be correct for all intents and purposes.

--- pkg/autoscaler/aggregation/bucketing.go
+++ pkg/autoscaler/aggregation/bucketing.go
@@ -62,8 +62,9 @@ func (t *TimedFloat64Buckets) IsEmpty(now time.Time) bool {
 	return now.Sub(t.lastWrite) > t.window
 }
 
-func roundTo3Digits(f float64) float64 {
-	return math.Floor(f*1000) / 1000
+func roundToNDigits(n int, f float64) float64 {
+	p := math.Pow10(n)
+	return math.Floor(f*p) / p
 }
 
 // WindowAverage returns the average bucket value over the window.
@@ -75,7 +76,7 @@ func (t *TimedFloat64Buckets) WindowAverage(now time.Time) float64 {
 	case d <= 0:
 		// If LastWrite equal or greater than Now
 		// return the current WindowTotal.
-		return roundTo3Digits(t.windowTotal / float64(len(t.buckets)))
+		return roundToNDigits(6 /*precision*/, t.windowTotal/float64(len(t.buckets)))
 	case d < t.window:
 		// If we haven't received metrics for some time, which is less than
 		// the window -- remove the outdated items.
@@ -85,7 +86,7 @@ func (t *TimedFloat64Buckets) WindowAverage(now time.Time) float64 {
 		for i := stIdx + 1; i <= eIdx; i++ {
 			ret -= t.buckets[i%len(t.buckets)]
 		}
-		return roundTo3Digits(ret / float64(len(t.buckets)-(eIdx-stIdx)))
+		return roundToNDigits(6 /*precision*/, ret/float64(len(t.buckets)-(eIdx-stIdx)))
 	default: // Nothing for more than a window time, just 0.
 		return 0.
 	}
