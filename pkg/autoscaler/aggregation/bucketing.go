8c22a2048d717e6cb31905e00228b025d7166aae
Victor Agababov <vagababov@gmail.com>
2020-01-20 11:29:20 -0800

    Use const

--- pkg/autoscaler/aggregation/bucketing.go
+++ pkg/autoscaler/aggregation/bucketing.go
@@ -69,6 +69,7 @@ func roundToNDigits(n int, f float64) float64 {
 
 // WindowAverage returns the average bucket value over the window.
 func (t *TimedFloat64Buckets) WindowAverage(now time.Time) float64 {
+	const precision = 6
 	now = now.Truncate(t.granularity)
 	t.bucketsMutex.RLock()
 	defer t.bucketsMutex.RUnlock()
@@ -76,7 +77,7 @@ func (t *TimedFloat64Buckets) WindowAverage(now time.Time) float64 {
 	case d <= 0:
 		// If LastWrite equal or greater than Now
 		// return the current WindowTotal.
-		return roundToNDigits(6 /*precision*/, t.windowTotal/float64(len(t.buckets)))
+		return roundToNDigits(precision, t.windowTotal/float64(len(t.buckets)))
 	case d < t.window:
 		// If we haven't received metrics for some time, which is less than
 		// the window -- remove the outdated items.
@@ -86,7 +87,7 @@ func (t *TimedFloat64Buckets) WindowAverage(now time.Time) float64 {
 		for i := stIdx + 1; i <= eIdx; i++ {
 			ret -= t.buckets[i%len(t.buckets)]
 		}
-		return roundToNDigits(6 /*precision*/, ret/float64(len(t.buckets)-(eIdx-stIdx)))
+		return roundToNDigits(precision, ret/float64(len(t.buckets)-(eIdx-stIdx)))
 	default: // Nothing for more than a window time, just 0.
 		return 0.
 	}
