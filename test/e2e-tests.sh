cedc55fccca2a8cd93f8f0d9f953d1d4151469f0
Ryan Moe <ryan.moe@gmail.com>
2020-01-30 13:39:28 -0800

    Add conformance test for certificates (#6646)
    * Add conformance test for certificates (#6445)
    
    This test verifies that a secret is created and that
    HTTP01 challenges are configured correctly when creating
    a certificate.
    
    * Address comments
    
    * Improve resource checks
    
    * Verify challenge solver service exists
    * Verify secret contains correct TLS data
    
    * Fix error message formatting and set default cert class
    
    * Add certificateClass flag
    
    * Address formatting comments

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -49,7 +49,7 @@ parallelism=""
 
 # Run conformance and e2e tests.
 go_test_e2e -timeout=30m \
-  ./test/conformance/... \
+  $(go list ./test/conformance/... | grep -v certificate) \
   ./test/e2e \
   ${parallelism} \
   "--resolvabledomain=$(use_resolvable_domain)" "$(use_https)" "$(ingress_class)" || failed=1
@@ -65,6 +65,19 @@ add_trap "kubectl delete -f ./test/config/autotls/certmanager/selfsigned/ --igno
 go_test_e2e -timeout=10m \
   ./test/e2e/autotls || failed=1
 kubectl delete -f ./test/config/autotls/certmanager/selfsigned/
+
+# Certificate conformance tests must be run separately
+kubectl apply -f ./test/config/autotls/certmanager/selfsigned/
+add_trap "kubectl delete -f ./test/config/autotls/certmanager/selfsigned/ --ignore-not-found" SIGKILL SIGTERM SIGQUIT
+go_test_e2e -timeout=10m \
+  ./test/conformance/certificate/nonhttp01 "$(certificate_class)" || failed=1
+kubectl delete -f ./test/config/autotls/certmanager/selfsigned/
+
+kubectl apply -f ./test/config/autotls/certmanager/http01/
+add_trap "kubectl delete -f ./test/config/autotls/certmanager/http01/ --ignore-not-found" SIGKILL SIGTERM SIGQUIT
+go_test_e2e -timeout=10m \
+  ./test/conformance/certificate/http01 "$(certificate_class)" || failed=1
+kubectl delete -f ./test/config/autotls/certmanager/http01/
 
 # Istio E2E tests mutate the cluster and must be ran separately
 if [[ -n "${ISTIO_VERSION}" ]]; then
