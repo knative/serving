67611cf180c7b656819b882b0116e7974ceb9ab2
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-02-12 09:26:34 +0900

    Update istio version to 1.4.4 and 1.3.8
    This patch updates istio version to 1.4.4 and 1.3.8, that have the fix
    for CVE-2020-8595.
    
    Not only for CVE but also v1.4.4 contains some bug fixes and
    improvments so it would be worth to start testing it.
    
    ref:
    - https://istio.io/news/releases/1.4.x/announcing-1.4.4/
    - https://istio.io/news/releases/1.3.x/announcing-1.3.8/

--- third_party/istio-1.3.8/download-istio.sh
+++ third_party/istio-1.3.8/download-istio.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
 
-# Copyright 2019 The Knative Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # Download and unpack Istio
-ISTIO_VERSION=1.4.2
+ISTIO_VERSION=1.3.8
 DOWNLOAD_URL=https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-linux.tar.gz
 
 wget --no-check-certificate $DOWNLOAD_URL
