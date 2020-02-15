6d4d28c6464f637848997278292378a9549f613a
Victor Agababov <vagababov@gmail.com>
2020-02-14 16:35:06 -0800

    nit:

--- pkg/queue/timeout.go
+++ pkg/queue/timeout.go
@@ -33,7 +33,7 @@ import (
 //
 // The new Handler calls h.ServeHTTP to handle each request, but if a
 // call runs for longer than its time limit, the handler responds with
-// a 504 Service Unavailable error and the given message in its body.
+// a 504 Gateway Timeout error and the given message in its body.
 // (If msg is empty, a suitable default message will be sent.)
 // After such a timeout, writes by h to its ResponseWriter will return
 // ErrHandlerTimeout.
