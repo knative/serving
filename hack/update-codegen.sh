23d06786f504193de8e14d19ee964015d76aac75
Weston Haught <whaught@google.com>
2020-02-20 15:26:29 -0800

    Create a patch to keep code generation stable

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -88,3 +88,7 @@ ${GOPATH}/bin/deepcopy-gen \
 
 # Make sure our dependencies are up-to-date
 ${REPO_ROOT_DIR}/hack/update-deps.sh
+
+# Apply Patches
+echo "Applying patches"
+git apply ${REPO_ROOT_DIR}/hack/patches/*.patch
