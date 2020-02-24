e4a6f37f34b6ccd3943177b20f72d6f42144339d
Matt Moore <mattmoor@vmware.com>
2020-02-24 06:59:19 -0800

    Update the nightly contour.
    This reflects the recent refactor to how we pull in the contour yaml.

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -506,10 +506,11 @@ function test_setup() {
   if [[ -n "${CONTOUR_VERSION}" ]]; then
     # we must set these override values to allow the test spoofing client to work with Contour
     # see https://github.com/knative/pkg/blob/release-0.7/test/ingress/ingress.go#L37
-    export GATEWAY_OVERRIDE=envoy-external
-    export GATEWAY_NAMESPACE_OVERRIDE=projectcontour
-    wait_until_pods_running projectcontour || return 1
-    wait_until_service_has_external_ip projectcontour envoy-external
+    export GATEWAY_OVERRIDE=envoy
+    export GATEWAY_NAMESPACE_OVERRIDE=contour-external
+    wait_until_pods_running contour-external || return 1
+    wait_until_pods_running contour-internal || return 1
+    wait_until_service_has_external_ip "${GATEWAY_NAMESPACE_OVERRIDE}" "${GATEWAY_OVERRIDE}"
   fi
 
   if (( INSTALL_MONITORING )); then
