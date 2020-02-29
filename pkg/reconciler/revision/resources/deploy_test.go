939f7771c839b5d70670e4475f48b73d1fcdee3c
savitaashture <sashture@redhat.com>
2020-02-29 18:11:17 +0530

    remove duplicate t.Run from test file

--- pkg/reconciler/revision/resources/deploy_test.go
+++ pkg/reconciler/revision/resources/deploy_test.go
@@ -823,21 +823,7 @@ func TestMakePodSpec(t *testing.T) {
 			})
 			got, err := makePodSpec(test.rev, test.lc, test.tc, test.oc, test.ac, test.cc)
 			if err != nil {
-				t.Fatal("makePodSpec returned errror")
-			}
-			if diff := cmp.Diff(test.want, got, quantityComparer); diff != "" {
-				t.Errorf("makePodSpec (-want, +got) = %v", diff)
-			}
-		})
-
-		t.Run(test.name+"(podspec)", func(t *testing.T) {
-			quantityComparer := cmp.Comparer(func(x, y resource.Quantity) bool {
-				return x.Cmp(y) == 0
-			})
-
-			got, err := makePodSpec(test.rev, test.lc, test.tc, test.oc, test.ac, test.cc)
-			if err != nil {
-				t.Fatal("makePodSpec returned errror")
+				t.Fatalf("makePodSpec returned error: %v", err)
 			}
 			if diff := cmp.Diff(test.want, got, quantityComparer); diff != "" {
 				t.Errorf("makePodSpec (-want, +got) = %v", diff)
@@ -847,16 +833,14 @@ func TestMakePodSpec(t *testing.T) {
 }
 
 func TestMissingProbeError(t *testing.T) {
-	_, err := MakeDeployment(defaultRevision,
+	if _, err := MakeDeployment(defaultRevision,
 		&logging.Config{},
 		&tracingconfig.Config{},
 		&network.Config{},
 		&metrics.ObservabilityConfig{},
 		&autoscalerconfig.Config{},
 		&deployment.Config{},
-	)
-
-	if err == nil {
+	); err == nil {
 		t.Error("expected error from MakeDeployment")
 	}
 }
@@ -950,7 +934,7 @@ func TestMakeDeployment(t *testing.T) {
 			// Tested above so that we can rely on it here for brevity.
 			podSpec, err := makePodSpec(test.rev, test.lc, test.tc, test.oc, test.ac, test.cc)
 			if err != nil {
-				t.Fatal("makePodSpec returned errror")
+				t.Fatalf("makePodSpec returned error: %v", err)
 			}
 			test.want.Spec.Template.Spec = *podSpec
 			got, err := MakeDeployment(test.rev, test.lc, test.tc, test.nc, test.oc, test.ac, test.cc)
