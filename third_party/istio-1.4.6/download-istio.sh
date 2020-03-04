23ad36caf93a6f6ca71ae020fca51fd2fbe5e59f
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-03-04 15:35:47 +0900

    Start testing Istio 1.4.6
    This patch updates Istio version to 1.4.6 for CI. Istio 1.4.6 contains
    security updates as per https://istio.io/news/releases/1.4.x/announcing-1.4.6/

--- third_party/istio-1.4.6/download-istio.sh
+++ third_party/istio-1.4.6/download-istio.sh
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 # Download and unpack Istio
-ISTIO_VERSION=1.4.4
+ISTIO_VERSION=1.4.6
 DOWNLOAD_URL=https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-linux.tar.gz
 
 wget --no-check-certificate $DOWNLOAD_URL
