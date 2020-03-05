fd253212f659c3adb635410659ea95f3ee862097
Knative Prow Robot <41213312+knative-prow-robot@users.noreply.github.com>
2020-03-05 08:54:25 -0800

    Fix our license handling in subdirectories. (#7152)
    Looking at this for `net-istio` I suspected (and confirmed) that we might not properly handle license resolution when the entrypoing is a subdirectory.  This corrects that for serving.

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -49,6 +49,7 @@ dep ensure ${DEP_FLAGS[@]}
 rm -rf $(find vendor/ -name 'OWNERS')
 rm -rf $(find vendor/ -name '*_test.go')
 
-update_licenses third_party/VENDOR-LICENSE "./cmd/*"
+# Do this for every package under "cmd" except kodata and cmd itself.
+update_licenses third_party/VENDOR-LICENSE "$(find ./cmd -type d | grep -v kodata | grep -vE 'cmd$')"
 
 remove_broken_symlinks ./vendor
