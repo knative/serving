9da2fd878beef21077e781d59985012611cf034f
savitaashture <sashture@redhat.com>
2020-03-06 23:56:26 +0530

    update upgrade test to use v1 API (#7160)

--- test/upgrade/service_preupgrade_test.go
+++ test/upgrade/service_preupgrade_test.go
@@ -22,24 +22,23 @@ import (
 	"testing"
 
 	"knative.dev/serving/pkg/apis/autoscaling"
-	v1 "knative.dev/serving/pkg/apis/serving/v1"
 	revisionresourcenames "knative.dev/serving/pkg/reconciler/revision/resources/names"
-	rtesting "knative.dev/serving/pkg/testing/v1alpha1"
+	rtesting "knative.dev/serving/pkg/testing/v1"
 	"knative.dev/serving/test"
 	"knative.dev/serving/test/e2e"
 	v1test "knative.dev/serving/test/v1"
-	v1a1test "knative.dev/serving/test/v1alpha1"
 )
 
-func TestRunLatestServicePreUpgrade(t *testing.T) {
+func TestServicePreUpgrade(t *testing.T) {
 	t.Parallel()
 	clients := e2e.Setup(t)
 
-	var names test.ResourceNames
-	names.Service = serviceName
-	names.Image = test.PizzaPlanet1
+	names := test.ResourceNames{
+		Service: serviceName,
+		Image:   test.PizzaPlanet1,
+	}
 
-	resources, err := v1a1test.CreateRunLatestServiceLegacyReady(t, clients, &names,
+	resources, err := v1test.CreateServiceReady(t, clients, &names,
 		rtesting.WithConfigAnnotations(map[string]string{
 			autoscaling.MinScaleAnnotationKey: "1", //make sure we don't scale to zero during the test
 		}),
@@ -51,15 +50,16 @@ func TestRunLatestServicePreUpgrade(t *testing.T) {
 	assertServiceResourcesUpdated(t, clients, names, url, test.PizzaPlanetText1)
 }
 
-func TestRunLatestServicePreUpgradeAndScaleToZero(t *testing.T) {
+func TestServicePreUpgradeAndScaleToZero(t *testing.T) {
 	t.Parallel()
 	clients := e2e.Setup(t)
 
-	var names test.ResourceNames
-	names.Service = scaleToZeroServiceName
-	names.Image = test.PizzaPlanet1
+	names := test.ResourceNames{
+		Service: scaleToZeroServiceName,
+		Image:   test.PizzaPlanet1,
+	}
 
-	resources, err := v1a1test.CreateRunLatestServiceLegacyReady(t, clients, &names,
+	resources, err := v1test.CreateServiceReady(t, clients, &names,
 		rtesting.WithConfigAnnotations(map[string]string{
 			autoscaling.WindowAnnotationKey: autoscaling.WindowMin.String(), //make sure we scale to zero quickly
 		}),
@@ -85,11 +85,8 @@ func TestBYORevisionPreUpgrade(t *testing.T) {
 		Image:   test.PizzaPlanet1,
 	}
 
-	_, err := v1test.CreateServiceReady(t, clients, &names,
-		func(svc *v1.Service) {
-			svc.Spec.ConfigurationSpec.Template.Name = byoRevName
-		})
-	if err != nil {
+	if _, err := v1test.CreateServiceReady(t, clients, &names,
+		rtesting.WithConfigName(byoRevName)); err != nil {
 		t.Fatalf("Failed to create Service: %v", err)
 	}
 }
