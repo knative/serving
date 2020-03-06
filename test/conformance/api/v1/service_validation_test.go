37984654372e47c44ae58b7a7a78ca09d02e7c75
Weston Haught <whaught@google.com>
2020-03-06 15:54:41 -0800

    Rename validation test.

--- test/conformance/api/v1/service_validation_test.go
+++ test/conformance/api/v1/service_validation_test.go
@@ -2,13 +2,10 @@
 
 /*
 Copyright 2019 The Knative Authors
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,6 +19,7 @@ import (
 	"strings"
 	"testing"
 
+	v1 "knative.dev/serving/pkg/apis/serving/v1"
 	. "knative.dev/serving/pkg/testing/v1"
 	"knative.dev/serving/test"
 	v1test "knative.dev/serving/test/v1"
@@ -31,7 +29,7 @@ const (
 	invalidServiceAccountName = "foo@bar.baz"
 )
 
-func TestServiceAccountValidation(t *testing.T) {
+func TestServiceValidationWithInvalidServiceAccount(t *testing.T) {
 	t.Parallel()
 	clients := test.Setup(t)
 
@@ -51,7 +49,33 @@ func TestServiceAccountValidation(t *testing.T) {
 	if err == nil {
 		t.Fatal("Expected Service creation to fail")
 	}
-	if got, want := err.Error(), "serviceAccountName: spec.template.spec."+invalidServiceAccountName; !strings.Contains(got, want) {
+	if got, want := err.Error(), "error looking up service account knative-serving/"+invalidServiceAccountName; !strings.Contains(got, want) {
+		t.Errorf("Error = %q, want to contain = %q", got, want)
+	}
+}
+
+func TestServiceValidationWithInvalidPodSpec(t *testing.T) {
+	t.Parallel()
+	clients := test.Setup(t)
+
+	names := test.ResourceNames{
+		Service: test.ObjectNameForTest(t),
+		Image:   test.PizzaPlanet1,
+	}
+
+	// Clean up on test failure or interrupt
+	defer test.TearDown(clients, names)
+	test.CleanupOnInterrupt(func() { test.TearDown(clients, names) })
+
+	// Setup initial Service
+	_, err := v1test.CreateServiceReady(t, clients, &names, func(svc *v1.Service) {
+		svc.Spec.Template.Spec.PodSpec.Containers[0].Name = "&InvalidValue"
+	})
+
+	if err == nil {
+		t.Fatal("Expected Service creation to fail")
+	}
+	if got, want := err.Error(), "PodSpec dry run failed"; !strings.Contains(got, want) {
 		t.Errorf("Error = %q, want to contain = %q", got, want)
 	}
 }
