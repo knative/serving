763b7b513cbbe7f12da313aca70a892bcc63072f
Victor Agababov <vagababov@gmail.com>
2020-03-15 22:08:06 -0700

    A trivial optimization to the public EPS listening.
    Make a check that the values did not change before triggering activator rebalancing.
    This is a trivial optimization, whish should reduce the amount of work we're doing
    when activator is not in the path.

--- pkg/activator/net/throttler.go
+++ pkg/activator/net/throttler.go
@@ -592,13 +592,25 @@ func (rt *revisionThrottler) handlePubEpsUpdate(eps *corev1.Endpoints, selfIP st
 	epSet, _ := endpointsToDests(eps, rt.protocol)
 	// We are using List to have the IP addresses sorted for consistent results.
 	epsL := epSet.List()
-	atomic.StoreInt32(&rt.numActivators, int32(len(epsL)))
-	atomic.StoreInt32(&rt.activatorIndex, int32(inferIndex(epsL, selfIP)))
-	// Note that if the revision is served directly or this activator is not
-	// part of the subset it will be `-1/X`. And that's OK, since this activator
-	// should not be receiving requests for the revision.
-	rt.logger.Infof("This activator index is %d/%d", rt.activatorIndex, rt.numActivators)
-	rt.updateCapacity(rt.backendCount)
+	na, ai := int32(len(epsL)), int32(inferIndex(epsL, selfIP))
+
+	// Only update the values if they have changed and are meaningful.
+	// If ai == -1, means this activator is not part of the gang (or SKS
+	// is in serve mode), so the change should not matter, since this activator
+	// should not be receiving the requests anyway.
+	// NB: the line below is the only place where the number of activators
+	// or activator index are changed. Thus we can check the values without
+	// atomic ops here.
+	if ai != -1 && (rt.numActivators != na || ai != rt.activatorIndex) {
+		atomic.StoreInt32(&rt.numActivators, na)
+		atomic.StoreInt32(&rt.activatorIndex, ai)
+		// Note that if the revision is served directly or this activator is not
+		// part of the subset it will be `-1/X`. And that's OK, since this activator
+		// should not be receiving requests for the revision.
+		rt.logger.Infof("This activator index is %d/%d was %d/%d",
+			rt.activatorIndex, rt.numActivators, ai, na)
+		rt.updateCapacity(rt.backendCount)
+	}
 }
 
 // inferIndex returns the index of this activator slice.
