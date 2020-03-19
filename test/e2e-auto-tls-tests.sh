950632942fa7bdc520b320bb85ec8dca56faed51
Chao Dai <chaodai@google.com>
2020-03-19 16:08:15 -0700

    Allow auto tls test scripts taking parameters

--- test/e2e-auto-tls-tests.sh
+++ test/e2e-auto-tls-tests.sh
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
-
 source $(dirname $0)/e2e-common.sh
 
 function knative_setup() {
