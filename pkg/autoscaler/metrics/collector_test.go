d1c1e34f1f32c69eb1f27238e668418596cb210c
Markus Thömmes <markusthoemmes@me.com>
2020-04-07 20:14:00 +0200

    Propagate Metric status changes to the API. (#7525)
    * Store last error instead of status changes.
    
    * Set status in the reconciler accordingly.
    
    * Implement Watch mechanism.
    
    * Use watch mechanism in tests.
    
    * Fix comment.
    
    * Review comments.

--- pkg/autoscaler/metrics/collector_test.go
+++ pkg/autoscaler/metrics/collector_test.go
@@ -23,12 +23,9 @@ import (
 	"time"
 
 	"github.com/google/go-cmp/cmp"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
-	duckv1 "knative.dev/pkg/apis/duck/v1"
 	. "knative.dev/pkg/logging/testing"
 	av1alpha1 "knative.dev/serving/pkg/apis/autoscaling/v1alpha1"
 	"knative.dev/serving/pkg/apis/serving"
@@ -387,11 +384,25 @@ func TestMetricCollectorRecord(t *testing.T) {
 }
 
 func TestMetricCollectorError(t *testing.T) {
+	testMetric := &av1alpha1.Metric{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: fake.TestNamespace,
+			Name:      fake.TestRevision,
+			Labels: map[string]string{
+				serving.RevisionLabelKey: fake.TestRevision,
+			},
+		},
+		Spec: av1alpha1.MetricSpec{
+			ScrapeTarget: fake.TestRevision + "-zhudex",
+		},
+	}
+
+	errOther := errors.New("foo")
+
 	testCases := []struct {
-		name                 string
-		scraper              *testScraper
-		metric               *av1alpha1.Metric
-		expectedMetricStatus duckv1.Status
+		name          string
+		scraper       *testScraper
+		expectedError error
 	}{{
 		name: "Failed to get endpoints scraper error",
 		scraper: &testScraper{
@@ -399,26 +410,7 @@ func TestMetricCollectorError(t *testing.T) {
 				return emptyStat, ErrFailedGetEndpoints
 			},
 		},
-		metric: &av1alpha1.Metric{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: fake.TestNamespace,
-				Name:      fake.TestRevision,
-				Labels: map[string]string{
-					serving.RevisionLabelKey: fake.TestRevision,
-				},
-			},
-			Spec: av1alpha1.MetricSpec{
-				ScrapeTarget: fake.TestRevision + "-zhudex",
-			},
-		},
-		expectedMetricStatus: duckv1.Status{
-			Conditions: duckv1.Conditions{{
-				Type:    av1alpha1.MetricConditionReady,
-				Status:  corev1.ConditionUnknown,
-				Reason:  "NoEndpoints",
-				Message: ErrFailedGetEndpoints.Error(),
-			}},
-		},
+		expectedError: ErrFailedGetEndpoints,
 	}, {
 		name: "Did not receive stat scraper error",
 		scraper: &testScraper{
@@ -426,53 +418,15 @@ func TestMetricCollectorError(t *testing.T) {
 				return emptyStat, ErrDidNotReceiveStat
 			},
 		},
-		metric: &av1alpha1.Metric{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: fake.TestNamespace,
-				Name:      fake.TestRevision,
-				Labels: map[string]string{
-					serving.RevisionLabelKey: fake.TestRevision,
-				},
-			},
-			Spec: av1alpha1.MetricSpec{
-				ScrapeTarget: fake.TestRevision + "-zhudex",
-			},
-		},
-		expectedMetricStatus: duckv1.Status{
-			Conditions: duckv1.Conditions{{
-				Type:    av1alpha1.MetricConditionReady,
-				Status:  corev1.ConditionFalse,
-				Reason:  "DidNotReceiveStat",
-				Message: ErrDidNotReceiveStat.Error(),
-			}},
-		},
+		expectedError: ErrDidNotReceiveStat,
 	}, {
 		name: "Other scraper error",
 		scraper: &testScraper{
 			s: func() (Stat, error) {
-				return emptyStat, errors.New("foo")
+				return emptyStat, errOther
 			},
 		},
-		metric: &av1alpha1.Metric{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: fake.TestNamespace,
-				Name:      fake.TestRevision,
-				Labels: map[string]string{
-					serving.RevisionLabelKey: fake.TestRevision,
-				},
-			},
-			Spec: av1alpha1.MetricSpec{
-				ScrapeTarget: fake.TestRevision + "-zhudex",
-			},
-		},
-		expectedMetricStatus: duckv1.Status{
-			Conditions: duckv1.Conditions{{
-				Type:    av1alpha1.MetricConditionReady,
-				Status:  corev1.ConditionUnknown,
-				Reason:  "CreateOrUpdateFailed",
-				Message: "Collector has failed.",
-			}},
-		},
+		expectedError: errOther,
 	}}
 
 	logger := TestLogger(t)
@@ -480,22 +434,25 @@ func TestMetricCollectorError(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			factory := scraperFactory(test.scraper, nil)
 			coll := NewMetricCollector(factory, logger)
-			coll.CreateOrUpdate(test.metric)
-			key := types.NamespacedName{Namespace: test.metric.Namespace, Name: test.metric.Name}
-
-			var got duckv1.Status
-			wait.PollImmediate(10*time.Millisecond, 2*time.Second, func() (bool, error) {
-				collection, ok := coll.collections[key]
-				if ok {
-					got = collection.currentMetric().Status.Status
-					return equality.Semantic.DeepEqual(got, test.expectedMetricStatus), nil
-				}
-				return false, nil
+			watchCh := make(chan types.NamespacedName)
+			coll.Watch(func(key types.NamespacedName) {
+				watchCh <- key
 			})
-			if !equality.Semantic.DeepEqual(got, test.expectedMetricStatus) {
-				t.Errorf("Got = %#v, want: %#v, diff:\n%q", got, test.expectedMetricStatus, cmp.Diff(got, test.expectedMetricStatus))
+			coll.CreateOrUpdate(testMetric)
+			key := types.NamespacedName{Namespace: testMetric.Namespace, Name: testMetric.Name}
+
+			// Expect an event to be propagated because we're erroring.
+			event := <-watchCh
+			if event != key {
+				t.Fatalf("Event = %v, want %v", event, key)
 			}
-			coll.Delete(test.metric.Namespace, test.metric.Name)
+
+			// Make sure the error is surfaced via 'CreateOrUpdate', which is called in the reconciler.
+			if err := coll.CreateOrUpdate(testMetric); err != test.expectedError {
+				t.Fatalf("CreateOrUpdate = %v, want %v", err, test.expectedError)
+			}
+
+			coll.Delete(testMetric.Namespace, testMetric.Name)
 		})
 	}
 }
