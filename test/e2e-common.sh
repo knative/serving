f8f9f59e692860b84545d75b425ec7824eeeddf0
Chao Dai <chaodai@google.com>
2020-04-16 20:11:18 -0700

    [TESTONLY] update ko to lastest in 0.11

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -25,6 +25,9 @@ E2E_CLUSTER_MACHINE=${E2E_CLUSTER_MACHINE:-n1-standard-8}
 # This script provides helper methods to perform cluster actions.
 source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/e2e-tests.sh
 
+# Install ko for test, this should get us ko@v0.4.0
+GO111MODULE=on go get github.com/google/ko/cmd/ko@v0.4.0
+
 CERT_MANAGER_VERSION="0.9.1"
 ISTIO_VERSION=""
 GLOO_VERSION=""
