1ba872f8c7be7c1b3dbd72390e7007ca0703f5f7
Weston Haught <whaught@google.com>
2020-04-23 13:02:02 -0700

    use the singular WithServiceAnnotation

--- test/e2e/service_validation_test.go
+++ test/e2e/service_validation_test.go
@@ -50,8 +50,8 @@ func TestServiceValidationWithInvalidPodSpec(t *testing.T) {
 
 	// Setup Service
 	t.Logf("Creating a new Service %s", names.Service)
-	service, err := v1test.CreateService(t, clients, names, WithServiceAnnotations(
-		map[string]string{webhook.PodSpecDryRunAnnotation: "enabled"}))
+	service, err := v1test.CreateService(t, clients, names,
+		WithServiceAnnotation(webhook.PodSpecDryRunAnnotation, "enabled"))
 	if err != nil {
 		t.Fatal("Create Service:", err)
 	}
