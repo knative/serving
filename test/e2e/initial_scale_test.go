3727be9be46cbe962b2e93459b3fbee0512f627a
Tara Gu <guw@ibm.com>
2020-05-07 15:32:46 -0400

    add t.Skip()

--- test/e2e/initial_scale_test.go
+++ test/e2e/initial_scale_test.go
@@ -36,6 +36,7 @@ import (
 // the revision level. This test runs after the cluster wide flag allow-zero-initial-scale
 // is set to true.
 func TestInitScaleZero(t *testing.T) {
+	t.Skip()
 	t.Parallel()
 	cancel := logstream.Start(t)
 	defer cancel()
@@ -60,6 +61,7 @@ func TestInitScaleZero(t *testing.T) {
 // TestInitScalePositive tests setting of annotation initialScale to greater than 0 on
 // the revision level.
 func TestInitScalePositive(t *testing.T) {
+	t.Skip()
 	t.Parallel()
 	cancel := logstream.Start(t)
 	defer cancel()
