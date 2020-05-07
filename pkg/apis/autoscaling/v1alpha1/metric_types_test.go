9e7bbf3c7ae85c2c1c111e6d5790e40913d84ff5
Weston Haught <whaught@google.com>
2020-05-07 10:09:45 -0700

    Implement krshaped for autoscaling (#7880)
    * implement krshaped for autoscaling
    
    * merge validation changes from master
    
    * update test got/want
    
    * combine got want lines
    
    * one more

---
+++ pkg/apis/autoscaling/v1alpha1/metric_types_test.go
@@ -0,0 +1,42 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package v1alpha1
+
+import (
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestMetricGetStatus(t *testing.T) {
+	r := &Metric{
+		Status: MetricStatus{},
+	}
+
+	if got, want := r.GetStatus(), &r.Status.Status; got != want {
+		t.Errorf("GotStatus=%v, want=%v", got, want)
+	}
+}
+
+func TestMetricGetObjectMeta(t *testing.T) {
+	r := &Metric{
+		TypeMeta: metav1.TypeMeta{},
+	}
+
+	if got, want := r.GetTypeMeta(), &r.TypeMeta; got != want {
+		t.Errorf("GetTypeMeta=%v, want=%v", got, want)
+	}
+}
