726373a5a5923c7604f031653c6b2b790f0baa13
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-05-23 11:43:38 +0900

    Distinguish among the reason for TLS is not enabled

--- pkg/apis/serving/v1/route_lifecycle.go
+++ pkg/apis/serving/v1/route_lifecycle.go
@@ -126,12 +126,16 @@ func (rs *RouteStatus) MarkCertificateNotOwned(name string) {
 		"There is an existing certificate %s that we don't own.", name)
 }
 
+const (
+	AutoTLSNotEnabledMessage            = "autoTLS is not enabled"
+	TLSNotEnabledForClusterLocalMessage = "TLS is not enabled for cluster-local"
+)
+
 // MarkTLSNotEnabled sets RouteConditionCertificateProvisioned to true when
 // certificate config such as autoTLS is not enabled or private cluster-local service.
-func (rs *RouteStatus) MarkTLSNotEnabled() {
+func (rs *RouteStatus) MarkTLSNotEnabled(msg string) {
 	routeCondSet.Manage(rs).MarkTrueWithReason(RouteConditionCertificateProvisioned,
-		"TLSNotEnabled",
-		"TLS is not enabled")
+		"TLSNotEnabled", msg)
 }
 
 // MarkHTTPDowngrade sets RouteConditionCertificateProvisioned to true when plain
