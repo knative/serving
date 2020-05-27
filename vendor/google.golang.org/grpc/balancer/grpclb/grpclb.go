e19611d182ad7a32a61ede0b7d4af5890a5ed909
Dave Protasowski <dprotaso@gmail.com>
2020-05-27 15:31:25 -0400

    bump k8s deps to v1.17.6

--- vendor/google.golang.org/grpc/balancer/grpclb/grpclb.go
+++ vendor/google.golang.org/grpc/balancer/grpclb/grpclb.go
@@ -27,6 +27,7 @@ package grpclb
 import (
 	"context"
 	"errors"
+	"strconv"
 	"sync"
 	"time"
 
@@ -126,10 +127,11 @@ func (b *lbBuilder) Name() string {
 }
 
 func (b *lbBuilder) Build(cc balancer.ClientConn, opt balancer.BuildOptions) balancer.Balancer {
-	// This generates a manual resolver builder with a fixed scheme. This
-	// scheme will be used to dial to remote LB, so we can send filtered
-	// address updates to remote LB ClientConn using this manual resolver.
-	r := &lbManualResolver{scheme: "grpclb-internal", ccb: cc}
+	// This generates a manual resolver builder with a random scheme. This
+	// scheme will be used to dial to remote LB, so we can send filtered address
+	// updates to remote LB ClientConn using this manual resolver.
+	scheme := "grpclb_internal_" + strconv.FormatInt(time.Now().UnixNano(), 36)
+	r := &lbManualResolver{scheme: scheme, ccb: cc}
 
 	lb := &lbBalancer{
 		cc:              newLBCacheClientConn(cc),
