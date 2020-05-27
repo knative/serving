506bbbac9df177e49b831fcce5f28183e9f22110
Dave Protasowski <dprotaso@gmail.com>
2020-05-26 21:55:57 -0400

    Unpin our update-deps.sh script

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -27,7 +27,7 @@ cd ${ROOT_DIR}
 export GO111MODULE=on
 
 # This controls the release branch we track.
-VERSION="release-0.15"
+VERSION="master"
 
 # The list of dependencies that we track at HEAD and periodically
 # float forward in this repository.
