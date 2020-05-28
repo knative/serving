2b81e7e2b2ee87b80d05917a53309d21f66a4564
Weston Haught <whaught@google.com>
2020-05-27 21:52:45 -0700

    Include default case to ensure all are handled

--- pkg/reconciler/autoscaling/kpa/scaler.go
+++ pkg/reconciler/autoscaling/kpa/scaler.go
@@ -265,7 +265,7 @@ func (ks *scaler) handleScaleToZero(ctx context.Context, pa *pav1alpha1.PodAutos
 		ks.enqueueCB(pa, activationTimeout)
 		return scaleUnknown, false
 	default: // All variants of Activating should be handled above. This is unexpected.
-		logger.Error("PA handled an unexpected activation state.")
+		logger.Error("PA handled an unexpected activation state")
 		return scaleUnknown, false
 	}
 }
