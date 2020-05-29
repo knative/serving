107b0cb917c2ad4442e2d92e4a4c65132291a15a
Julian Friedman <julz.friedman@uk.ibm.com>
2020-05-29 11:25:02 +0100

    Use AsQuantity function from pkg (#8130)

--- pkg/apis/config/defaults.go
+++ pkg/apis/config/defaults.go
@@ -83,10 +83,10 @@ func NewDefaultsConfigFromMap(data map[string]string) (*Defaults, error) {
 		cm.AsInt64("container-concurrency", &nc.ContainerConcurrency),
 		cm.AsInt64("container-concurrency-max-limit", &nc.ContainerConcurrencyMaxLimit),
 
-		asQuantity("revision-cpu-request", &nc.RevisionCPURequest),
-		asQuantity("revision-memory-request", &nc.RevisionMemoryRequest),
-		asQuantity("revision-cpu-limit", &nc.RevisionCPULimit),
-		asQuantity("revision-memory-limit", &nc.RevisionMemoryLimit),
+		cm.AsQuantity("revision-cpu-request", &nc.RevisionCPURequest),
+		cm.AsQuantity("revision-memory-request", &nc.RevisionMemoryRequest),
+		cm.AsQuantity("revision-cpu-limit", &nc.RevisionCPULimit),
+		cm.AsQuantity("revision-memory-limit", &nc.RevisionMemoryLimit),
 	); err != nil {
 		return nil, err
 	}
@@ -154,18 +154,4 @@ func (d *Defaults) UserContainerName(ctx context.Context) string {
 		return ""
 	}
 	return buf.String()
-}
-
-// asQuantity parses the value at key as a *resource.Quantity into the target, if it exists.
-func asQuantity(key string, target **resource.Quantity) cm.ParseFunc {
-	return func(data map[string]string) error {
-		if raw, ok := data[key]; !ok {
-			*target = nil
-		} else if val, err := resource.ParseQuantity(raw); err != nil {
-			return err
-		} else {
-			*target = &val
-		}
-		return nil
-	}
 }
