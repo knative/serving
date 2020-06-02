a784f17b7e1645f39b636b16873de1b120161f20
Victor Agababov <vagababov@gmail.com>
2020-06-02 15:27:20 -0700

    Add pod policy objects.
    This is technically a noop change, but a code refactor, which introduces pod policy selection
    in the load balancing process.
    And the existing two are codified as such.
    Also this slightly improves the code, since it removes one if statement from the serving path!

---
+++ pkg/activator/net/lb_policy_test.go
@@ -0,0 +1,93 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package net
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"knative.dev/serving/pkg/queue"
+)
+
+func TestFirstAvailable(t *testing.T) {
+	t.Run("1 tracker, 1 slot", func(t *testing.T) {
+		podTrackers := []*podTracker{
+			&podTracker{
+				dest: "this-is-nowhere",
+				b: queue.NewBreaker(queue.BreakerParams{
+					QueueDepth:      1,
+					MaxConcurrency:  1,
+					InitialCapacity: 1,
+				}),
+			},
+		}
+
+		ctx := context.Background()
+		cb, tracker := firstAvailableLBPolicy(ctx, podTrackers)
+		defer cb()
+		if tracker == nil {
+			t.Fatal("Tracker was nil")
+		}
+
+		ctx, cancel := context.WithTimeout(ctx, 10*time.Millisecond)
+		defer cancel()
+
+		cb, tracker = firstAvailableLBPolicy(ctx, podTrackers)
+		defer cb()
+		if tracker != nil {
+			t.Fatal("Tracker was not nil")
+		}
+	})
+	t.Run("2 trackers, 1 slot", func(t *testing.T) {
+		podTrackers := []*podTracker{
+			&podTracker{
+				dest: "down-by-the-river",
+				b: queue.NewBreaker(queue.BreakerParams{
+					QueueDepth:      1,
+					MaxConcurrency:  1,
+					InitialCapacity: 1,
+				}),
+			},
+			&podTracker{
+				dest: "heart-of-gold",
+				b: queue.NewBreaker(queue.BreakerParams{
+					QueueDepth:      1,
+					MaxConcurrency:  1,
+					InitialCapacity: 1,
+				}),
+			},
+		}
+
+		ctx := context.Background()
+		cb, tracker := firstAvailableLBPolicy(ctx, podTrackers)
+		defer cb()
+		if tracker == nil {
+			t.Fatal("Tracker was nil")
+		} else if got, want := tracker.dest, podTrackers[0].dest; got != want {
+			t.Errorf("Tracker = %s, want: %s", got, want)
+		}
+
+		cb, tracker = firstAvailableLBPolicy(ctx, podTrackers)
+		defer cb()
+		if tracker == nil {
+			t.Fatal("Tracker was nil")
+		} else if got, want := tracker.dest, podTrackers[1].dest; got != want {
+			t.Errorf("Tracker = %s, want: %s", got, want)
+		}
+	})
+}
