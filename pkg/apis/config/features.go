2949259bd49e39c6d858940d7698ba7779867e41
Jean-Rémy Bancel <dev@bancel.me>
2020-06-25 05:38:28 -0700

    Use `.` as scope separator in config-features keys. (#8469)

--- pkg/apis/config/features.go
+++ pkg/apis/config/features.go
@@ -52,8 +52,8 @@ func NewFeaturesConfigFromMap(data map[string]string) (*Features, error) {
 
 	if err := cm.Parse(data,
 		asFlag("multi-container", &nc.MultiContainer),
-		asFlag("kubernetes/podspec-fieldref", &nc.PodSpecFieldRef),
-		asFlag("kubernetes/podspec-dryrun", &nc.PodSpecDryRun)); err != nil {
+		asFlag("kubernetes.podspec-fieldref", &nc.PodSpecFieldRef),
+		asFlag("kubernetes.podspec-dryrun", &nc.PodSpecDryRun)); err != nil {
 		return nil, err
 	}
 	return nc, nil
