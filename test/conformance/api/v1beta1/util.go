6ca49f8f6a42527ad229a853b5d9dcdc0cd9dc28
Belinda Vennam <beemarie@us.ibm.com>
2020-07-07 13:05:37 -0400

    lower case

--- test/conformance/api/v1beta1/util.go
+++ test/conformance/api/v1beta1/util.go
@@ -349,7 +349,7 @@ func validateImageDigest(imageName string, imageDigest string) (bool, error) {
 }
 
 // WaitForScaleToZero will wait for the specified deployment to scale to 0 replicas.
-// Will wait up to 6 times the ScaleToZeroGracePeriod (30 seconds) before failing.
+// Will wait up to 6 times the scaleToZeroGracePeriod (30 seconds) before failing.
 func WaitForScaleToZero(t pkgTest.TLegacy, deploymentName string, clients *test.Clients) error {
 	t.Helper()
 	t.Logf("Waiting for %q to scale to zero", deploymentName)
