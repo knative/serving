ac11fb9725db66a751f697e2af46d17687396928
Matt Moore <mattmoor@vmware.com>
2020-07-13 10:38:18 -0700

    [master] Auto-update dependencies (#8611)
    Produced via:
      `./hack/update-deps.sh --upgrade && ./hack/update-codegen.sh`
    /assign dprotaso vagababov
    /cc dprotaso vagababov

--- vendor/knative.dev/pkg/apis/duck/v1beta1/status_types.go
+++ vendor/knative.dev/pkg/apis/duck/v1beta1/status_types.go
@@ -25,7 +25,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 
 	"knative.dev/pkg/apis"
-	"knative.dev/pkg/apis/duck"
+	"knative.dev/pkg/apis/duck/ducktypes"
 	"knative.dev/pkg/kmeta"
 )
 
@@ -33,9 +33,6 @@ import (
 
 // Conditions is a simple wrapper around apis.Conditions to implement duck.Implementable.
 type Conditions apis.Conditions
-
-// Conditions is an Implementable "duck type".
-var _ duck.Implementable = (*Conditions)(nil)
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
@@ -84,14 +81,11 @@ func (s *Status) SetConditions(c apis.Conditions) {
 	s.Conditions = Conditions(c)
 }
 
-// In order for Conditions to be Implementable, KResource must be Populatable.
-var _ duck.Populatable = (*KResource)(nil)
-
 // Ensure KResource satisfies apis.Listable
 var _ apis.Listable = (*KResource)(nil)
 
 // GetFullType implements duck.Implementable
-func (*Conditions) GetFullType() duck.Populatable {
+func (*Conditions) GetFullType() ducktypes.Populatable {
 	return &KResource{}
 }
 
