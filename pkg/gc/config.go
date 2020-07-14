ee250bd8a225ce48ca71ba23254b9865f954854b
Weston Haught <whaught@google.com>
2020-07-13 21:46:31 -0700

    remove prefix

--- pkg/gc/config.go
+++ pkg/gc/config.go
@@ -44,19 +44,19 @@ type Config struct {
 
 	// Duration from creation when a Revision should be considered active
 	//  and exempt from GC. Note that GCMaxStaleRevision may override this if set.
-	GCRetainSinceCreateTime time.Duration
+	RetainSinceCreateTime time.Duration
 	// Duration from last active when a Revision should be considered active
 	// and exempt from GC.Note that GCMaxStaleRevision may override this if set.
-	GCRetainSinceLastActiveTime time.Duration
+	RetainSinceLastActiveTime time.Duration
 	// Minimum number of generations of revisions to keep before considering for GC.
 	// Set -1 to disable minimum and fill up to max.
 	// Either min or max must be set.
-	GCMinStaleRevisions int64
+	MinStaleRevisions int64
 	// Maximum number of stale revisions to keep before considering for GC.
 	// regardless of creation or staleness time-bounds
 	// Set -1 to disable this setting.
 	// Either min or max must be set.
-	GCMaxStaleRevisions int64
+	MaxStaleRevisions int64
 }
 
 func defaultConfig() *Config {
@@ -68,10 +68,10 @@ func defaultConfig() *Config {
 		StaleRevisionMinimumGenerations: 20,
 
 		// V2 GC Settings
-		GCRetainSinceCreateTime:     48 * time.Hour,
-		GCRetainSinceLastActiveTime: 15 * time.Hour,
-		GCMinStaleRevisions:         20,
-		GCMaxStaleRevisions:         -1,
+		RetainSinceCreateTime:     48 * time.Hour,
+		RetainSinceLastActiveTime: 15 * time.Hour,
+		MinStaleRevisions:         20,
+		MaxStaleRevisions:         -1,
 	}
 }
 
@@ -87,24 +87,24 @@ func NewConfigFromConfigMapFunc(ctx context.Context) func(configMap *corev1.Conf
 			cm.AsDuration("stale-revision-lastpinned-debounce", &c.StaleRevisionLastpinnedDebounce),
 			cm.AsInt64("stale-revision-minimum-generations", &c.StaleRevisionMinimumGenerations),
 
-			cm.AsDuration("retain-since-create-time", &c.GCRetainSinceCreateTime),
-			cm.AsDuration("retain-since-last-active-time", &c.GCRetainSinceLastActiveTime),
-			cm.AsInt64("min-stale-revisions", &c.GCMinStaleRevisions),
-			cm.AsInt64("max-stale-revisions", &c.GCMaxStaleRevisions),
+			cm.AsDuration("retain-since-create-time", &c.RetainSinceCreateTime),
+			cm.AsDuration("retain-since-last-active-time", &c.RetainSinceLastActiveTime),
+			cm.AsInt64("min-stale-revisions", &c.MinStaleRevisions),
+			cm.AsInt64("max-stale-revisions", &c.MaxStaleRevisions),
 		); err != nil {
 			return nil, fmt.Errorf("failed to parse data: %w", err)
 		}
 
-		if c.GCMaxStaleRevisions >= 0 && c.GCMinStaleRevisions > c.GCMaxStaleRevisions {
+		if c.MaxStaleRevisions >= 0 && c.MinStaleRevisions > c.MaxStaleRevisions {
 			return nil, fmt.Errorf(
 				"min-stale-revisions(%d) must be <= max-stale-revisions(%d)",
-				c.GCMinStaleRevisions, c.GCMaxStaleRevisions)
+				c.MinStaleRevisions, c.MaxStaleRevisions)
 		}
-		if c.GCMinStaleRevisions < 0 {
-			return nil, fmt.Errorf("min-stale-revisions must be non-negative, was: %d", c.GCMinStaleRevisions)
+		if c.MinStaleRevisions < 0 {
+			return nil, fmt.Errorf("min-stale-revisions must be non-negative, was: %d", c.MinStaleRevisions)
 		}
-		if c.GCMaxStaleRevisions < -1 {
-			return nil, fmt.Errorf("max-stale-revisions must be >= -1, was: %d", c.GCMaxStaleRevisions)
+		if c.MaxStaleRevisions < -1 {
+			return nil, fmt.Errorf("max-stale-revisions must be >= -1, was: %d", c.MaxStaleRevisions)
 		}
 
 		if c.StaleRevisionMinimumGenerations < 0 {
