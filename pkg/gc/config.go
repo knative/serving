2e741acbe749efcef8b910fb6c9d9b0892b51d8a
Weston Haught <whaught@google.com>
2020-07-15 00:10:54 -0700

    Updates to GC settings

--- pkg/gc/config.go
+++ pkg/gc/config.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2018 The Knative Authors
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     https://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +16,8 @@ package gc
 import (
 	"context"
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
@@ -29,6 +28,10 @@ import (
 
 const (
 	ConfigName = "config-gc"
+	Forever    = time.Duration(-1)
+	Infinity   = -1
+
+	disabled = "disabled"
 )
 
 type Config struct {
@@ -48,15 +51,12 @@ type Config struct {
 	// Duration from last active when a Revision should be considered active
 	// and exempt from GC.Note that GCMaxStaleRevision may override this if set.
 	RetainSinceLastActiveTime time.Duration
-	// Minimum number of generations of revisions to keep before considering for GC.
-	// Set -1 to disable minimum and fill up to max.
-	// Either min or max must be set.
+	// Minimum number of stale revisions to keep before considering for GC.
 	MinStaleRevisions int64
-	// Maximum number of stale revisions to keep before considering for GC.
+	// Maximum number of non-active revisions to keep before considering for GC.
 	// regardless of creation or staleness time-bounds
 	// Set -1 to disable this setting.
-	// Either min or max must be set.
-	MaxStaleRevisions int64
+	MaxNonActiveRevisions int64
 }
 
 func defaultConfig() *Config {
@@ -68,10 +68,12 @@ func defaultConfig() *Config {
 		StaleRevisionMinimumGenerations: 20,
 
 		// V2 GC Settings
+		// TODO(whaught): consider 'forever' sentinel value for use with max mode.
+		// TODO(whaught): validate positive
 		RetainSinceCreateTime:     48 * time.Hour,
 		RetainSinceLastActiveTime: 15 * time.Hour,
 		MinStaleRevisions:         20,
-		MaxStaleRevisions:         -1,
+		MaxNonActiveRevisions:     1000,
 	}
 }
 
@@ -81,41 +83,75 @@ func NewConfigFromConfigMapFunc(ctx context.Context) func(configMap *corev1.Conf
 	return func(configMap *corev1.ConfigMap) (*Config, error) {
 		c := defaultConfig()
 
+		var retainCreate, retainActive, max string
 		if err := cm.Parse(configMap.Data,
 			cm.AsDuration("stale-revision-create-delay", &c.StaleRevisionCreateDelay),
 			cm.AsDuration("stale-revision-timeout", &c.StaleRevisionTimeout),
 			cm.AsDuration("stale-revision-lastpinned-debounce", &c.StaleRevisionLastpinnedDebounce),
 			cm.AsInt64("stale-revision-minimum-generations", &c.StaleRevisionMinimumGenerations),
 
-			cm.AsDuration("retain-since-create-time", &c.RetainSinceCreateTime),
-			cm.AsDuration("retain-since-last-active-time", &c.RetainSinceLastActiveTime),
+			// v2 settings
+			cm.AsString("retain-since-create-time", &retainCreate),
+			cm.AsString("retain-since-last-active-time", &retainActive),
 			cm.AsInt64("min-stale-revisions", &c.MinStaleRevisions),
-			cm.AsInt64("max-stale-revisions", &c.MaxStaleRevisions),
+			cm.AsString("max-non-active-revisions", &max),
 		); err != nil {
 			return nil, fmt.Errorf("failed to parse data: %w", err)
 		}
 
-		if c.MaxStaleRevisions >= 0 && c.MinStaleRevisions > c.MaxStaleRevisions {
-			return nil, fmt.Errorf(
-				"min-stale-revisions(%d) must be <= max-stale-revisions(%d)",
-				c.MinStaleRevisions, c.MaxStaleRevisions)
-		}
-		if c.MinStaleRevisions < 0 {
-			return nil, fmt.Errorf("min-stale-revisions must be non-negative, was: %d", c.MinStaleRevisions)
-		}
-		if c.MaxStaleRevisions < -1 {
-			return nil, fmt.Errorf("max-stale-revisions must be >= -1, was: %d", c.MaxStaleRevisions)
-		}
-
 		if c.StaleRevisionMinimumGenerations < 0 {
-			return nil, fmt.Errorf("stale-revision-minimum-generations must be non-negative, was: %d", c.StaleRevisionMinimumGenerations)
+			return nil, fmt.Errorf(
+				"stale-revision-minimum-generations must be non-negative, was: %d",
+				c.StaleRevisionMinimumGenerations)
 		}
-
 		if c.StaleRevisionTimeout-c.StaleRevisionLastpinnedDebounce < minRevisionTimeout {
 			logger.Warnf("Got revision timeout of %v, minimum supported value is %v",
 				c.StaleRevisionTimeout, minRevisionTimeout+c.StaleRevisionLastpinnedDebounce)
 			c.StaleRevisionTimeout = minRevisionTimeout + c.StaleRevisionLastpinnedDebounce
 		}
+
+		// validate V2 settings
+		if err := parseForeverOrDuration(retainCreate, &c.RetainSinceCreateTime); err != nil {
+			return nil, fmt.Errorf("failed to parse min-stale-revisions: %w", err)
+		}
+		if err := parseForeverOrDuration(retainActive, &c.RetainSinceLastActiveTime); err != nil {
+			return nil, fmt.Errorf("failed to parse retain-since-last-active-time: %w", err)
+		}
+
+		if max == "" {
+			// keep default value
+		} else if strings.EqualFold(max, disabled) {
+			c.MaxNonActiveRevisions = Infinity
+		} else if parsed, err := strconv.ParseInt(max, 10, 64); err != nil {
+			return nil, fmt.Errorf("failed to parse max-stale-revisions, was: %d", c.MaxNonActiveRevisions)
+		} else {
+			if parsed < 0 {
+				return nil, fmt.Errorf("max-stale-revisions must non-negative or %q, was: %d", disabled, parsed)
+			}
+			c.MaxNonActiveRevisions = parsed
+		}
+
+		if c.MinStaleRevisions < 0 {
+			return nil, fmt.Errorf("min-stale-revisions must be non-negative, was: %d", c.MinStaleRevisions)
+		}
+		if c.MaxNonActiveRevisions >= 0 && c.MinStaleRevisions > c.MaxNonActiveRevisions {
+			return nil, fmt.Errorf("min-stale-revisions(%d) must be <= max-stale-revisions(%d)", c.MinStaleRevisions, c.MaxNonActiveRevisions)
+		}
 		return c, nil
 	}
+}
+
+func parseForeverOrDuration(val string, toSet *time.Duration) error {
+	if val == "" {
+		// keep default value
+	} else if strings.EqualFold(val, disabled) {
+		*toSet = Forever
+	} else if parsed, err := time.ParseDuration(val); err != nil {
+		return err
+	} else if parsed < 0 {
+		return fmt.Errorf("must be non-negative")
+	} else {
+		*toSet = parsed
+	}
+	return nil
 }
--- pkg/gc/config_test.go
+++ pkg/gc/config_test.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2018 The Knative Authors
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     https://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,17 +32,17 @@ func TestOurConfig(t *testing.T) {
 		want *Config
 		data map[string]string
 	}{{
-		name: "Actual config",
+		name: "actual config",
 		fail: false,
 		want: defaultConfig(),
 		data: actual.Data,
 	}, {
-		name: "Example config",
+		name: "example config",
 		fail: false,
 		want: defaultConfig(),
 		data: example.Data,
 	}, {
-		name: "With value overrides",
+		name: "with value overrides",
 		want: &Config{
 			StaleRevisionCreateDelay:        15 * time.Hour,
 			StaleRevisionTimeout:            14 * time.Hour,
@@ -54,7 +51,7 @@ func TestOurConfig(t *testing.T) {
 			RetainSinceCreateTime:           17 * time.Hour,
 			RetainSinceLastActiveTime:       16 * time.Hour,
 			MinStaleRevisions:               5,
-			MaxStaleRevisions:               500,
+			MaxNonActiveRevisions:           500,
 		},
 		data: map[string]string{
 			"stale-revision-create-delay":        "15h",
@@ -64,53 +61,101 @@ func TestOurConfig(t *testing.T) {
 			"retain-since-create-time":           "17h",
 			"retain-since-last-active-time":      "16h",
 			"min-stale-revisions":                "5",
-			"max-stale-revisions":                "500",
+			"max-non-active-revisions":           "500",
 		},
 	}, {
-		name: "Invalid duration",
+		name: "invalid duration",
 		fail: true,
-		want: nil,
 		data: map[string]string{
 			"stale-revision-create-delay": "invalid",
 		},
 	}, {
-		name: "Invalid negative minimum generation",
+		name: "invalid negative minimum generation",
 		fail: true,
-		want: nil,
 		data: map[string]string{
 			"stale-revision-minimum-generations": "-1",
 		},
 	}, {
-		name: "Invalid negative minimum generation v2",
+		name: "invalid minimum generation",
 		fail: true,
-		want: nil,
+		data: map[string]string{
+			"stale-revision-minimum-generations": "invalid",
+		},
+	}, {
+		name: "Invalid negative min stale",
+		fail: true,
 		data: map[string]string{
 			"min-stale-revisions": "-1",
 		},
 	}, {
 		name: "Invalid negative maximum generation",
 		fail: true,
-		want: nil,
 		data: map[string]string{
-			"max-stale-revisions": "-2",
+			"max-non-active-revisions": "-2",
 		},
 	}, {
-		name: "Invalid max less than min",
+		name: "invalid max less than min",
 		fail: true,
-		want: nil,
 		data: map[string]string{
-			"min-stale-revisions": "20",
-			"max-stale-revisions": "10",
+			"min-stale-revisions":      "20",
+			"max-non-active-revisions": "10",
 		},
 	}, {
-		name: "Invalid minimum generation",
+		name: "unparsable create duration",
 		fail: true,
-		want: nil,
 		data: map[string]string{
-			"stale-revision-minimum-generations": "invalid",
+			"retain-since-create-time": "invalid",
 		},
 	}, {
-		name: "Below minimum timeout",
+		name: "negative create duration",
+		fail: true,
+		data: map[string]string{
+			"retain-since-create-time": "-1h",
+		},
+	}, {
+		name: "negative last-active duration",
+		fail: true,
+		data: map[string]string{
+			"retain-since-last-active-time": "-1h",
+		},
+	}, {
+		name: "create delay disabled",
+		want: func() *Config {
+			d := defaultConfig()
+			d.RetainSinceCreateTime = Forever
+			return d
+		}(),
+		data: map[string]string{
+			"retain-since-create-time": disabled,
+		},
+	}, {
+		name: "last-active disabled",
+		want: func() *Config {
+			d := defaultConfig()
+			d.RetainSinceLastActiveTime = Forever
+			return d
+		}(),
+		data: map[string]string{
+			"retain-since-last-active-time": disabled,
+		},
+	}, {
+		name: "max-non-active unparsable",
+		fail: true,
+		data: map[string]string{
+			"max-non-active-revisions": "invalid",
+		},
+	}, {
+		name: "max-non-active disabled",
+		want: func() *Config {
+			d := defaultConfig()
+			d.MaxNonActiveRevisions = Infinity
+			return d
+		}(),
+		data: map[string]string{
+			"max-non-active-revisions": disabled,
+		},
+	}, {
+		name: "below minimum timeout",
 		fail: false,
 		want: &Config{
 			StaleRevisionCreateDelay:        15 * time.Hour,
@@ -120,7 +165,7 @@ func TestOurConfig(t *testing.T) {
 			RetainSinceCreateTime:           48 * time.Hour,
 			RetainSinceLastActiveTime:       15 * time.Hour,
 			MinStaleRevisions:               20,
-			MaxStaleRevisions:               -1,
+			MaxNonActiveRevisions:           1000,
 		},
 		data: map[string]string{
 			"stale-revision-create-delay": "15h",
