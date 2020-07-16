f6f2d0c374cfe08c24ee15c8f038b242f79e6552
Markus Thömmes <markusthoemmes@me.com>
2020-07-16 14:07:33 +0200

    Explain reasoning behind pool implementation to avoid future confusion. (#8647)

--- pkg/network/bufferpool.go
+++ pkg/network/bufferpool.go
@@ -21,18 +21,25 @@ import (
 	"sync"
 )
 
-// bufferPool implements the BufferPool interface to be used in
-// httputil.ReverseProxy. It stores pointers to to slices to
-// further avoid allocations, see https://staticcheck.io/docs/checks#SA6002.
+// bufferPool implements the BufferPool interface to be used in httputil.ReverseProxy.
+//
+// Sadly, because of the httputil.BufferPool interface, we cannot avoid an allocation
+// when returning the slice to the pool. We could choose to not use use pointers of
+// slices and use straight slices instead, but that'd cause an allocation likewise
+// because of the interface{} API. It'd also trigger a staticcheck warning.
+// See https://staticcheck.io/docs/checks#SA6002.
 type bufferPool struct {
 	pool *sync.Pool
 }
 
-// NewBufferPool creates a new BytePool. This is only safe to use in the context
+// NewBufferPool creates a new BufferPool. This is only safe to use in the context
 // of a httputil.ReverseProxy, as the buffers returned via Put are not cleaned
 // explicitly.
 func NewBufferPool() httputil.BufferPool {
 	return &bufferPool{
+		// We don't use the New function of sync.Pool here to avoid an unnecessary
+		// allocation when creating the slices. They are implicitly created in the
+		// Get function below.
 		pool: &sync.Pool{},
 	}
 }
