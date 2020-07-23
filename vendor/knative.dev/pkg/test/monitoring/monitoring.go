e281237b5dcf3c2242fd043cda11a451b718411d
Matt Moore (via sockpuppet) <mattmoor@vmware.com>
2020-07-23 14:16:37 +0000

    [master] Auto-update dependencies
    Produced via:
      `./hack/update-deps.sh --upgrade && ./hack/update-codegen.sh`
    /assign dprotaso vagababov
    /cc dprotaso vagababov

--- vendor/knative.dev/pkg/test/monitoring/monitoring.go
+++ vendor/knative.dev/pkg/test/monitoring/monitoring.go
@@ -18,7 +18,6 @@ package monitoring
 
 import (
 	"fmt"
-	"io"
 	"net"
 	"os"
 	"os/exec"
@@ -60,10 +59,10 @@ func Cleanup(pid int) error {
 }
 
 // PortForward sets up local port forward to the pod specified by the "app" label in the given namespace
-func PortForward(logf logging.FormatLogger, podList *v1.PodList, localPort, remotePort int, namespace string, stdout io.Writer, stderr io.Writer) (int, error) {
+func PortForward(logf logging.FormatLogger, podList *v1.PodList, localPort, remotePort int, namespace string) (int, error) {
 	podName := podList.Items[0].Name
 	portFwdCmd := fmt.Sprintf("kubectl port-forward %s %d:%d -n %s", podName, localPort, remotePort, namespace)
-	portFwdProcess, err := executeCmdBackground(logf, stdout, stderr, portFwdCmd)
+	portFwdProcess, err := executeCmdBackground(logf, portFwdCmd)
 
 	if err != nil {
 		return 0, fmt.Errorf("failed to port forward: %w", err)
@@ -74,13 +73,11 @@ func PortForward(logf logging.FormatLogger, podList *v1.PodList, localPort, remo
 }
 
 // RunBackground starts a background process and returns the Process if succeed
-func executeCmdBackground(logf logging.FormatLogger, stdout io.Writer, stderr io.Writer, format string, args ...interface{}) (*os.Process, error) {
+func executeCmdBackground(logf logging.FormatLogger, format string, args ...interface{}) (*os.Process, error) {
 	cmd := fmt.Sprintf(format, args...)
 	logf("Executing command: %s", cmd)
 	parts := strings.Split(cmd, " ")
 	c := exec.Command(parts[0], parts[1:]...) // #nosec
-	c.Stdout = stdout
-	c.Stderr = stderr
 	if err := c.Start(); err != nil {
 		return nil, fmt.Errorf("%s command failed: %w", cmd, err)
 	}
