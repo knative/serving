3130a5018a071211fa7a7aaf8c7953a9c9b3952f
Matt Moore <mattmoor@vmware.com>
2020-07-23 12:20:57 -0700

    Add logstream to the kingress conformance tests. (#8788)
    Fixes: https://github.com/knative/serving/issues/8766

--- test/conformance/ingress/retry.go
+++ test/conformance/ingress/retry.go
@@ -23,6 +23,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"knative.dev/networking/pkg/apis/networking/v1alpha1"
+	"knative.dev/pkg/test/logstream"
 	"knative.dev/serving/test"
 )
 
@@ -32,6 +33,7 @@ func TestRetry(t *testing.T) {
 		i := i
 		t.Run(fmt.Sprintf("period=%d,attempts=%d", i, i), func(t *testing.T) {
 			t.Parallel()
+			defer logstream.Start(t)()
 			clients := test.Setup(t)
 			// When the period matches, then it shouldn't fail.
 			name, port, cancel := CreateFlakyService(t, clients, i)
@@ -76,6 +78,7 @@ func TestRetry(t *testing.T) {
 
 		t.Run(fmt.Sprintf("period=%d,attempts=%d", i, i-1), func(t *testing.T) {
 			t.Parallel()
+			defer logstream.Start(t)()
 			clients := test.Setup(t)
 			// When the period matches, then it shouldn't fail.
 			name, port, cancel := CreateFlakyService(t, clients, i)
