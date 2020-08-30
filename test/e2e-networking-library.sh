a949e6a98dbabad8fe1580394511f95fc026fc60
Matt Moore <mattmoor@vmware.com>
2020-08-30 14:36:42 -0700

    Get timing data from scale test

--- test/e2e-networking-library.sh
+++ test/e2e-networking-library.sh
@@ -137,7 +137,7 @@ function install_contour() {
   kubectl apply -f ${NET_CONTOUR_YAML_NAME} || return 1
 
   # Disable verbosity until https://github.com/golang/go/issues/40771 is fixed.
-  export GO_TEST_VERBOSITY=standard-quiet
+  # export GO_TEST_VERBOSITY=standard-quiet
 
   UNINSTALL_LIST+=( "${NET_CONTOUR_YAML_NAME}" )
 }
