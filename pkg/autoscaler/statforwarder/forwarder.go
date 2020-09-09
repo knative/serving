87adf4e46ef7fee29bb4abc0ec9b0580f04b9f1a
Yanwei Guo <yanweiguo@google.com>
2020-09-09 11:52:39 -0700

    reviews

--- pkg/autoscaler/statforwarder/forwarder.go
+++ pkg/autoscaler/statforwarder/forwarder.go
@@ -295,7 +295,7 @@ func (f *Forwarder) createProcessor(bkt, holder string) *bucketProcessor {
 	f.processingWg.Add(1)
 	return &bucketProcessor{
 		bkt:    bkt,
-		logger: f.logger,
+		logger: f.logger.With(zap.String("buckjet", bkt)),
 		holder: holder,
 		conn:   websocket.NewDurableSendingConnection(dns, f.logger),
 	}
