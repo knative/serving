f69f2c58b34836e7bf79e3fe1413e919baf7f9c7
Artur Renault <arenault@google.com>
2020-09-16 18:27:41 +0000

    Fix bug in background context

--- test/e2e/autotls/auto_tls_test.go
+++ test/e2e/autotls/auto_tls_test.go
@@ -249,7 +249,7 @@ func createHTTPSClient(t *testing.T, clients *test.Clients, objects *v1test.Reso
 
 func createHTTPClient(t *testing.T, clients *test.Clients, objects *v1test.ResourceObjects) *http.Client {
 	t.Helper()
-	ing, err := clients.NetworkingClient.Ingresses.Get(routenames.Ingress(objects.Route), metav1.GetOptions{})
+	ing, err := clients.NetworkingClient.Ingresses.Get(context.Background(), routenames.Ingress(objects.Route), metav1.GetOptions{})
 	if err != nil {
 		t.Fatalf("Failed to get Ingress %s: %v", routenames.Ingress(objects.Route), err)
 	}
