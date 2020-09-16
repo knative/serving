98fe26074e6949d3e932684f78d02fd17b0175f3
Victor Agababov <vagababov@gmail.com>
2020-09-16 16:45:43 -0700

    Try to restrict a bit the limits and requests we have for AS tests (#9437)
    512m seems a lot, for a delay binary

--- test/e2e/autoscale.go
+++ test/e2e/autoscale.go
@@ -203,10 +203,12 @@ func setup(t *testing.T, class, metric string, target int, targetUtilization flo
 				autoscaling.WindowAnnotationKey: "50s",
 			}), rtesting.WithResourceRequirements(corev1.ResourceRequirements{
 				Limits: corev1.ResourceList{
-					corev1.ResourceMemory: resource.MustParse("512Mi"),
+					corev1.ResourceCPU:    resource.MustParse("100m"),
+					corev1.ResourceMemory: resource.MustParse("128Mi"),
 				},
 				Requests: corev1.ResourceList{
-					corev1.ResourceMemory: resource.MustParse("300Mi"),
+					corev1.ResourceCPU:    resource.MustParse("30m"),
+					corev1.ResourceMemory: resource.MustParse("20Mi"),
 				},
 			}),
 		}, fopts...)...)
