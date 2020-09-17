b578fd822cc92ce39a247c4d05493450a32f1f75
Victor Agababov <vagababov@gmail.com>
2020-09-17 23:37:24 +0000

    0.0

--- test/e2e/autoscale_test.go
+++ test/e2e/autoscale_test.go
@@ -110,7 +110,7 @@ func TestTargetBurstCapacity(t *testing.T) {
 	})
 
 	// Wait for two stable pods.
-	obsScale := 0
+	obsScale := 0.0
 	if err := wait.Poll(250*time.Millisecond, 2*cfg.StableWindow, func() (bool, error) {
 		obsScale, err = numberOfReadyPods(ctx)
 		if err != nil {
