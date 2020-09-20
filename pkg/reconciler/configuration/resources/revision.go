ea782c70607c55e2c2acafb082f473876f67d4b1
Weston Haught <whaught@google.com>
2020-09-20 16:40:33 -0700

    seems fine to me

--- pkg/reconciler/configuration/resources/revision.go
+++ pkg/reconciler/configuration/resources/revision.go
@@ -88,11 +88,10 @@ func updateRevisionAnnotations(rev *v1.Revision, config metav1.Object) {
 		annotations[serving.CreatorAnnotation] = c
 	}
 
-	// DO NOT SUBMIT
-	/*if v, ok := cans[serving.RoutesAnnotationKey]; ok {
+	if v, ok := cans[serving.RoutesAnnotationKey]; ok {
 		annotations[serving.RoutesAnnotationKey] = v
 		rev.SetRoutingState(v1.RoutingStateActive, clock.RealClock{})
-	}*/
+	}
 
 	rev.SetAnnotations(annotations)
 }
