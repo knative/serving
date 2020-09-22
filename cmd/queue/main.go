a44ff89dd7483121fc5159e7e487d53fe343c5f3
Markus Thömmes <markusthoemmes@me.com>
2020-09-22 08:25:26 +0200

    Use sharedmain helper for memstats in queue-proxy.

--- cmd/queue/main.go
+++ cmd/queue/main.go
@@ -31,7 +31,6 @@ import (
 
 	"github.com/kelseyhightower/envconfig"
 	"go.opencensus.io/plugin/ochttp"
-	"go.opencensus.io/stats/view"
 	"go.opencensus.io/trace"
 	"go.uber.org/zap"
 
@@ -39,6 +38,7 @@ import (
 
 	network "knative.dev/networking/pkg"
 	"knative.dev/networking/pkg/apis/networking"
+	"knative.dev/pkg/injection/sharedmain"
 	pkglogging "knative.dev/pkg/logging"
 	"knative.dev/pkg/logging/logkey"
 	"knative.dev/pkg/metrics"
@@ -194,7 +194,6 @@ func knativeProbeHandler(logger *zap.SugaredLogger, healthState *health.State, p
 
 func main() {
 	flag.Parse()
-	ctx := signals.NewContext()
 
 	// If this is set, we run as a standalone binary to probe the queue-proxy.
 	if *readinessProbeTimeout >= 0 {
@@ -208,6 +207,9 @@ func main() {
 		os.Exit(standaloneProbeMain(*readinessProbeTimeout, transport))
 	}
 
+	// Otherwise, we run as the queue-proxy service.
+	ctx := signals.NewContext()
+
 	// Parse the environment.
 	var env config
 	if err := envconfig.Process("", &env); err != nil {
@@ -217,10 +219,9 @@ func main() {
 
 	// Setup the logger.
 	logger, _ := pkglogging.NewLogger(env.ServingLoggingConfig, env.ServingLoggingLevel)
-	logger = logger.Named("queueproxy")
 	defer flush(logger)
 
-	logger = logger.With(
+	logger = logger.Named("queueproxy").With(
 		zap.Object(logkey.Key, pkglogging.NamespacedName(types.NamespacedName{
 			Namespace: env.ServingNamespace,
 			Name:      env.ServingRevision,
@@ -228,11 +229,7 @@ func main() {
 		zap.String(logkey.Pod, env.ServingPod))
 
 	// Report stats on Go memory usage every 30 seconds.
-	msp := metrics.NewMemStatsAll()
-	msp.Start(context.Background(), 30*time.Second)
-	if err := view.Register(msp.DefaultViews()...); err != nil {
-		logger.Fatalw("Error exporting go memstats view", zap.Error(err))
-	}
+	sharedmain.MemStatsOrDie(ctx)
 
 	// Setup reporters and processes to handle stat reporting.
 	promStatReporter, err := queue.NewPrometheusStatsReporter(
