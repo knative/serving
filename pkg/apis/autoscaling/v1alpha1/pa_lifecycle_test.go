5041bccdb64231a1a82a5b1eb4f32f0ee86f200b
Julz Friedman <julz.friedman@uk.ibm.com>
2020-09-24 22:48:17 +0100

    Wire up scaleDownDelay config and annotation

--- pkg/apis/autoscaling/v1alpha1/pa_lifecycle_test.go
+++ pkg/apis/autoscaling/v1alpha1/pa_lifecycle_test.go
@@ -721,6 +721,53 @@ func TestMetric(t *testing.T) {
 	}
 }
 
+func TestScaleDownDelayAnnotation(t *testing.T) {
+	cases := []struct {
+		name      string
+		pa        *PodAutoscaler
+		wantDelay time.Duration
+		wantOK    bool
+	}{{
+		name:      "not present",
+		pa:        pa(map[string]string{}),
+		wantDelay: 0,
+		wantOK:    false,
+	}, {
+		name: "present",
+		pa: pa(map[string]string{
+			autoscaling.ScaleDownDelayAnnotationKey: "120s",
+		}),
+		wantDelay: 120 * time.Second,
+		wantOK:    true,
+	}, {
+		name: "complex",
+		pa: pa(map[string]string{
+			autoscaling.ScaleDownDelayAnnotationKey: "2m33s",
+		}),
+		wantDelay: 153 * time.Second,
+		wantOK:    true,
+	}, {
+		name: "invalid",
+		pa: pa(map[string]string{
+			autoscaling.ScaleDownDelayAnnotationKey: "365d",
+		}),
+		wantDelay: 0,
+		wantOK:    false,
+	}}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			gotDelay, gotOK := tc.pa.ScaleDownDelay()
+			if gotDelay != tc.wantDelay {
+				t.Errorf("ScaleDownDelay = %v, want: %v", gotDelay, tc.wantDelay)
+			}
+			if gotOK != tc.wantOK {
+				t.Errorf("OK = %v, want: %v", gotOK, tc.wantOK)
+			}
+		})
+	}
+}
+
 func TestWindowAnnotation(t *testing.T) {
 	cases := []struct {
 		name       string
