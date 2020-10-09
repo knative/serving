60f190e59bb2ec684537bbd9066f49cf3f70fc35
Matt Moore (via Sockpuppet) <mattmoor@vmware.com>
2020-10-09 14:11:28 +0000

    upgrade to latest dependencies
    Signed-off-by: Matt Moore (via Sockpuppet) <mattmoor@vmware.com>

--- go.mod
+++ go.mod
@@ -33,9 +33,9 @@ require (
 	k8s.io/client-go v11.0.1-0.20190805182717-6502b5e7b1b5+incompatible
 	k8s.io/code-generator v0.18.8
 	k8s.io/kube-openapi v0.0.0-20200410145947-bcb3869e6f29
-	knative.dev/caching v0.0.0-20201008043923-06f743d9ad2b
-	knative.dev/networking v0.0.0-20201008041421-a124f20c3c7c
-	knative.dev/pkg v0.0.0-20201008052621-c0496d5e1b2b
+	knative.dev/caching v0.0.0-20201009023721-0a00448bbad0
+	knative.dev/networking v0.0.0-20201009061021-adc3058ce053
+	knative.dev/pkg v0.0.0-20201009023821-73da5142da72
 	knative.dev/test-infra v0.0.0-20201008221921-aeca2ff2371a
 )
 
--- vendor/knative.dev/networking/test/e2e_flags.go
+++ vendor/knative.dev/networking/test/e2e_flags.go
@@ -40,6 +40,7 @@ type ServingEnvironmentFlags struct {
 	EnableAlphaFeatures bool   // Indicates whether we run tests for alpha features
 	EnableBetaFeatures  bool   // Indicates whether we run tests for beta features
 	SkipTests           string // Indicates the test names we want to skip in alpha or beta features.
+	ClusterSuffix       string // Specifies the cluster DNS suffix to be used in tests.
 }
 
 func initializeServingFlags() *ServingEnvironmentFlags {
@@ -90,6 +91,11 @@ func initializeServingFlags() *ServingEnvironmentFlags {
 		"skip-tests",
 		"",
 		"Set this flag to the tests you want to skip in alpha or beta features. Accepts a comma separated list.")
+
+	flag.StringVar(&f.ClusterSuffix,
+		"cluster-suffix",
+		"cluster.local",
+		"Set this flag to the cluster suffix to be used in tests.")
 
 	return &f
 }
