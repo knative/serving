318b7acddc7e7bd40ff1c084c635f31801c53d05
Matt Moore <mattmoor@vmware.com>
2020-10-09 11:28:21 -0700

    upgrade to latest dependencies (#9758)
    Signed-off-by: Matt Moore (via Sockpuppet) <mattmoor@vmware.com>

--- vendor/knative.dev/pkg/test/helpers/name.go
+++ vendor/knative.dev/pkg/test/helpers/name.go
@@ -23,7 +23,6 @@ import (
 	"unicode"
 
 	"knative.dev/pkg/kmeta"
-	"knative.dev/pkg/test"
 )
 
 const (
@@ -42,13 +41,17 @@ func init() {
 	rand.Seed(seed)
 }
 
+type named interface {
+	Name() string
+}
+
 // ObjectPrefixForTest returns the name prefix for this test's random names.
-func ObjectPrefixForTest(t test.T) string {
+func ObjectPrefixForTest(t named) string {
 	return MakeK8sNamePrefix(strings.TrimPrefix(t.Name(), testNamePrefix))
 }
 
 // ObjectNameForTest generates a random object name based on the test name.
-func ObjectNameForTest(t test.T) string {
+func ObjectNameForTest(t named) string {
 	return kmeta.ChildName(ObjectPrefixForTest(t), string(sep)+RandomString())
 }
 
