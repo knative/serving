c70d50288d15c76a53a87cb12bec31ba48c7f6c0
Markus Thömmes <markusthoemmes@me.com>
2020-10-14 09:51:49 +0200

    Fix format calls

--- test/upgrade/service_postupgrade_test.go
+++ test/upgrade/service_postupgrade_test.go
@@ -41,12 +41,12 @@ func TestServicePostUpgrade(t *testing.T) {
 	// Before updating the service, the route and configuration objects should
 	// not be updated just because there has been an upgrade.
 	if hasGeneration, err := configHasGeneration(clients, serviceName, 1); err != nil {
-		t.Fatalf("Error comparing Configuration generation: %v", err)
+		t.Fatal("Error comparing Configuration generation", err)
 	} else if !hasGeneration {
 		t.Fatal("Configuration is updated after an upgrade.")
 	}
 	if hasGeneration, err := routeHasGeneration(clients, serviceName, 1); err != nil {
-		t.Fatalf("Error comparing Route generation: %v", err)
+		t.Fatal("Error comparing Route generation", err)
 	} else if !hasGeneration {
 		t.Fatal("Route is updated after an upgrade.")
 	}
