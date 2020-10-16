05f83bb55c95e5247998f41a15c5d53b3437c0a1
Julz Friedman <julz.friedman@uk.ibm.com>
2020-10-16 10:56:09 +0100

    Use route rather than routestatus IsReady function

--- pkg/apis/serving/v1/route_lifecycle_test.go
+++ pkg/apis/serving/v1/route_lifecycle_test.go
@@ -163,7 +163,8 @@ func TestRouteIsReady(t *testing.T) {
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			if e, a := tc.isReady, tc.status.IsReady(); e != a {
+			r := Route{Status: tc.status}
+			if e, a := tc.isReady, r.IsReady(); e != a {
 				t.Errorf("%q expected: %v got: %v", tc.name, e, a)
 			}
 		})
