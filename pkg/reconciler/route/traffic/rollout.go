bdba71160a8bf9723848917ac38bc34ac07a7ba8
Victor Agababov <vagababov@gmail.com>
2020-10-28 06:13:40 +0000

    Initial rollout version
    Change-Id: I60a48c1dabd5d8f3fbfb6667ce42eb18cc20c42e

--- pkg/reconciler/route/traffic/rollout.go
+++ pkg/reconciler/route/traffic/rollout.go
@@ -21,7 +21,9 @@ limitations under the License.
 
 package traffic
 
-import "knative.dev/networking/pkg/apis/networking"
+import (
+	"knative.dev/networking/pkg/apis/networking"
+)
 
 // RolloutAnnotationKey is the annotation key for storing
 // the rollout state in the Annotations of the Kingress or Route.Status.
@@ -69,4 +71,95 @@ type RevisionRollout struct {
 	// of total Route traffic, not the relative share of configuration
 	// target percentage.
 	Percent int `json:"percent"`
+}
+
+// Roll processes previous Rollout object and compares to the current
+// rollout state. If there is different, Merge will start or stop the rollout
+// and update `RevisionRollout` objects accoringly.
+// Roll returns true if any changes have been made.
+func (cur *Rollout) Roll(prev *Rollout) bool {
+	if prev == nil {
+		return false
+	}
+
+	// The algorithm below is simplest, but probably not the most performant.
+	// Optimize in the later passes.
+	// Map by tag.
+	ccfg, pcfg := map[string][]*ConfigurationRollout{}, map[string][]*ConfigurationRollout{}
+	for i := range cur.Configurations {
+		cfg := &cur.Configurations[i]
+		ccfg[cfg.Tag] = append(ccfg[cfg.Tag], cfg)
+	}
+	for i := range prev.Configurations {
+		cfg := &prev.Configurations[i]
+		pcfg[cfg.Tag] = append(pcfg[cfg.Tag], cfg)
+	}
+	ret := false
+	for t, cfgs := range ccfg {
+		pcfgs, ok := pcfg[t]
+		// A new tag, rollout to 100% so ignore.
+		if !ok {
+			continue
+		}
+		for i, j := 0, 0; i < len(cfgs) && j < len(pcfgs); {
+			switch {
+			case cfgs[i].ConfigurationName == pcfgs[j].ConfigurationName:
+				// Config might have 0 traffic assigned, if it is a tagged route.
+				if cfgs[i].Percent != 0 {
+					ret = ret || rollConfig(cfgs[i], pcfgs[j])
+				}
+				i++
+				j++
+			case cfgs[i].ConfigurationName < pcfgs[j].ConfigurationName:
+				// A new config, has been added. No action for rollout though.
+				i++
+			default: // cur > prev.
+				// A config has been removed. Again, no action for rollout, since
+				// this will no longer be rolling out even it were.
+				j++
+			}
+		}
+		// Non interlaping configs don't matter since we either roll 100% or remove them.
+	}
+	return ret
+}
+
+func rollConfig(cur, prev *ConfigurationRollout) bool {
+	pc := len(prev.Revisions)
+	// curr will always have just 1 element – the current desired revision.
+	if cur.Revisions[0].RevisionName == prev.Revisions[pc-1].RevisionName {
+		// TODO(vagababov): here would go the logic to compute new percentages for the rollout,
+		// i.e step function, so return value will change, depending on that.
+		// TODO(vagababov): percentage might change, so this should trigger recompute of existing
+		// revision rollouts.
+		return false
+	}
+
+	// Append the new revision, to the list of previous ones, this should start the
+	// rollout.
+	rev := cur.Revisions[0]
+	rev.Percent = 1
+	out := make([]RevisionRollout, 0, len(prev.Revisions)+1)
+	// Go backwards and find first revision with traffic assignment > 0.
+	// Reduce it by one, so we can give that 1% to the new revision.
+	for i := len(prev.Revisions) - 1; i >= 0; i-- {
+		if pp := prev.Revisions[i].Percent; pp > 0 {
+			prev.Revisions[i].Percent--
+			break
+		}
+	}
+
+	// Copy the non 0% objects over.
+	for _, r := range prev.Revisions {
+		if r.Percent == 0 {
+			// Skip the zeroed out items. This can be the 1%->0% from above, but
+			// generally speaking should not happen otherwise, aside from
+			// users modifying the annotation manually.
+			continue
+		}
+		out = append(out, r)
+	}
+	// And replace current with the modified previous version.
+	cur.Revisions = append(out, rev)
+	return true
 }
---
+++ pkg/reconciler/route/traffic/rollout_test.go
@@ -0,0 +1,334 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// rollout.go contains the types and functions to deal with
+// gradual rollout of the new revision for a configuration target.
+// The types in this file are expected to be serialized as strings
+// and used as annotations for progressive rollout logic.
+
+package traffic
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestRoll(t *testing.T) {
+	tests := []struct {
+		name            string
+		prev, cur, want *Rollout
+		wantRoll        bool
+	}{{
+		name: "no prev",
+		cur:  &Rollout{},
+		prev: nil,
+		want: &Rollout{},
+	}, {
+		name: "simplest, same",
+		cur: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "let-it-bleed",
+					Percent:      100,
+				}},
+			}},
+		},
+		prev: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "let-it-bleed",
+					Percent:      100,
+				}},
+			}},
+		},
+		want: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "let-it-bleed",
+					Percent:      100,
+				}},
+			}},
+		},
+	}, {
+		name: "simplest, roll",
+		cur: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "let-it-bleed",
+					Percent:      100,
+				}},
+			}},
+		},
+		prev: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "goat-head-soup",
+					Percent:      100,
+				}},
+			}},
+		},
+		want: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "goat-head-soup",
+					Percent:      99,
+				}, {
+					RevisionName: "let-it-bleed",
+					Percent:      1,
+				}},
+			}},
+		},
+		wantRoll: true,
+	}, {
+		name: "roll with two existing, no deletes",
+		cur: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "sticky-fingers",
+					Percent:      100,
+				}},
+			}},
+		},
+		prev: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "goat-head-soup",
+					Percent:      95,
+				}, {
+					RevisionName: "beggars-banquet",
+					Percent:      5, // 5 should become 4.
+				}},
+			}},
+		},
+		want: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "goat-head-soup",
+					Percent:      95,
+				}, {
+					RevisionName: "beggars-banquet",
+					Percent:      4,
+				}, {
+					RevisionName: "sticky-fingers",
+					Percent:      1,
+				}},
+			}},
+		},
+		wantRoll: true,
+	}, {
+		name: "roll with delete (two fast successive rolls)",
+		cur: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "between-the-buttons",
+					Percent:      100,
+				}},
+			}},
+		},
+		prev: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "goat-head-soup",
+					Percent:      99,
+				}, {
+					RevisionName: "bridges-to-babylon",
+					Percent:      1,
+				}},
+			}},
+		},
+		want: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "goat-head-soup",
+					Percent:      99,
+				}, {
+					RevisionName: "between-the-buttons",
+					Percent:      1,
+				}},
+			}},
+		},
+		wantRoll: true,
+	}, {
+		name: "new tag, no roll", // just attached a tag to an existing route.
+		cur: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "black-on-blue",
+					Percent:      100,
+				}},
+			}, {
+				ConfigurationName: "mick",
+				Tag:               "jagger",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "black-on-blue",
+					Percent:      100,
+				}},
+			}},
+		},
+		prev: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "black-on-blue",
+					Percent:      100,
+				}},
+			}},
+		},
+		want: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "black-on-blue",
+					Percent:      100,
+				}},
+			}, {
+				ConfigurationName: "mick",
+				Tag:               "jagger",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "black-on-blue",
+					Percent:      100,
+				}},
+			}},
+		},
+	}, {
+		name: "deleted config, no roll",
+		cur: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "it's-only-rock-n-roll",
+					Percent:      100,
+				}},
+			}},
+		},
+		prev: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "keith",
+				Percent:           50,
+				Revisions: []RevisionRollout{{
+					RevisionName: "black-on-blue",
+					Percent:      50,
+				}},
+			}, {
+				ConfigurationName: "mick",
+				Percent:           50,
+				Revisions: []RevisionRollout{{
+					RevisionName: "it's-only-rock-n-roll",
+					Percent:      50,
+				}},
+			}},
+		},
+		want: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "it's-only-rock-n-roll",
+					Percent:      100,
+				}},
+			}},
+		},
+	}, {
+		name: "new config, no roll",
+		cur: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "keith",
+				Percent:           50,
+				Revisions: []RevisionRollout{{
+					RevisionName: "black-on-blue",
+					Percent:      50,
+				}},
+			}, {
+				ConfigurationName: "mick",
+				Percent:           50,
+				Revisions: []RevisionRollout{{
+					RevisionName: "it's-only-rock-n-roll",
+					Percent:      50,
+				}},
+			}},
+		},
+		prev: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "mick",
+				Percent:           100,
+				Revisions: []RevisionRollout{{
+					RevisionName: "it's-only-rock-n-roll",
+					Percent:      100,
+				}},
+			}},
+		},
+		want: &Rollout{
+			Configurations: []ConfigurationRollout{{
+				ConfigurationName: "keith",
+				Percent:           50,
+				Revisions: []RevisionRollout{{
+					RevisionName: "black-on-blue",
+					Percent:      50,
+				}},
+			}, {
+				ConfigurationName: "mick",
+				Percent:           50,
+				Revisions: []RevisionRollout{{
+					RevisionName: "it's-only-rock-n-roll",
+					Percent:      50,
+				}},
+			}},
+		},
+	}}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if got, want := tc.cur.Roll(tc.prev), tc.wantRoll; got != want {
+				t.Errorf("Roll = %v, want: %v", got, want)
+			}
+			if got, want := tc.cur, tc.want; !cmp.Equal(got, want) {
+				t.Errorf("Wrong rolled rollout, diff(-want,+got):\n%s", cmp.Diff(want, got))
+			}
+		})
+	}
+}
