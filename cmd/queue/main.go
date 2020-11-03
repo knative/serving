3023376027b1fd0b053c099b48ada853147befc0
Victor Agababov <vagababov@gmail.com>
2020-11-03 20:18:38 +0000

    [WIP] testing updates
    Change-Id: I183368635bff3904eac00f71b8fe70b15acecda7

--- cmd/queue/main.go
+++ cmd/queue/main.go
@@ -144,6 +144,8 @@ func main() {
 	// Report stats on Go memory usage every 30 seconds.
 	metrics.MemStatsOrDie(ctx)
 
+	fmt.Printf("### THIS IS A TEST: %d", 42)
+
 	// Setup reporters and processes to handle stat reporting.
 	promStatReporter, err := queue.NewPrometheusStatsReporter(
 		env.ServingNamespace, env.ServingConfiguration, env.ServingRevision,
