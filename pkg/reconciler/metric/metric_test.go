861357753161f70a68615fcf1c40e485a8cdedec
Victor Agababov <vagababov@gmail.com>
2020-11-06 14:29:08 -0800

    Add more time to the TestReconcileWithCollector test (#10058)
    * Add more time to the TestReconcileWithCollector test
    
    This is a flake in #10056.
    There's nothing noticeably wrong with the test itself, but its timeout
    looks shortish. We usually wait 3 or 5s for reconciliations to happen
    in other controller tests.
    
    Change-Id: Ic6ac1b87b02a78d22a2801d8a28a6c7ba5d26f0e
    
    * more
    
    Change-Id: Ie0f02c930c426c7f06cc73112f83f9eaf76a273b

--- pkg/reconciler/metric/metric_test.go
+++ pkg/reconciler/metric/metric_test.go
@@ -211,14 +211,14 @@ func TestReconcileWithCollector(t *testing.T) {
 
 	scs.AutoscalingV1alpha1().Metrics(m.Namespace).Create(ctx, m, metav1.CreateOptions{})
 
-	if err := wait.PollImmediate(10*time.Millisecond, 2*time.Second, func() (bool, error) {
+	if err := wait.PollImmediate(10*time.Millisecond, 5*time.Second, func() (bool, error) {
 		return collector.createOrUpdateCalls.Load() > 0, nil
 	}); err != nil {
 		t.Fatal("CreateOrUpdate() called 0 times, want non-zero times")
 	}
 
 	scs.AutoscalingV1alpha1().Metrics(m.Namespace).Delete(ctx, m.Name, metav1.DeleteOptions{})
-	if err := wait.PollImmediate(10*time.Millisecond, 2*time.Second, func() (bool, error) {
+	if err := wait.PollImmediate(10*time.Millisecond, 5*time.Second, func() (bool, error) {
 		return collector.deleteCalls.Load() > 0, nil
 	}); err != nil {
 		t.Fatal("Delete() called 0 times, want non-zero times")
