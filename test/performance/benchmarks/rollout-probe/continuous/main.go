366cca6207e6262706ced3821eaf678d32eac867
Victor Agababov <vagababov@gmail.com>
2020-12-23 14:52:04 -0800

    Ensure we execute cleanup (#10434)
    * Ensure we execute cleanup
    
    Bind and explicitly execute the cleanup lambda,
    since if mako storage fails, then we `fatal` and that
    does not execute deferred calls, basically skipping the cleanup.
    
    Change-Id: Ida02c83df376285fb296beff2c55632239e4d9a5
    
    * log
    
    Change-Id: I8fc03c4baa84e0137f1f13491c8b2f6119f4b1b2

--- test/performance/benchmarks/rollout-probe/continuous/main.go
+++ test/performance/benchmarks/rollout-probe/continuous/main.go
@@ -117,6 +117,10 @@ func main() {
 
 	// After a minute, update the Ksvc.
 	updateSvc := time.After(30 * time.Second)
+
+	// Since we might qfatal in the end, this would not execute the deferred calls
+	// thus failing the restore. So bind and execute explicitly.
+	var restoreFn func()
 LOOP:
 	for {
 		select {
@@ -126,7 +130,7 @@ LOOP:
 			break LOOP
 
 		case <-updateSvc:
-			log.Println("Updating the service: ", *target)
+			log.Println("Updating the service:", *target)
 			sc := servingclient.Get(ctx)
 			svc, err := sc.ServingV1().Services(namespace).Get(context.Background(), *target, metav1.GetOptions{})
 			if err != nil {
@@ -137,10 +141,10 @@ LOOP:
 
 			// At the end of the benchmark, restore to the previous value.
 			if prev := svc.Spec.Template.Annotations["autoscaling.knative.dev/minScale"]; prev != "" {
-				defer func() {
+				restoreFn = func() {
 					restore, err := sc.ServingV1().Services(namespace).Get(context.Background(), *target, metav1.GetOptions{})
 					if err != nil {
-						log.Println("Error getting service: ", err)
+						log.Println("Error getting service", err)
 						return
 					}
 					restore = restore.DeepCopy()
@@ -148,7 +152,7 @@ LOOP:
 					_, err = sc.ServingV1().Services(namespace).Update(
 						context.Background(), restore, metav1.UpdateOptions{})
 					log.Printf("Restoring the service to initial minScale = %s, err: %#v", prev, err)
-				}()
+				}
 			}
 			svc.Spec.Template.Annotations["autoscaling.knative.dev/minScale"] = "1"
 			_, err = sc.ServingV1().Services(namespace).Update(context.Background(), svc, metav1.UpdateOptions{})
@@ -190,6 +194,9 @@ LOOP:
 			}
 			q.AddSamplePoint(mako.XTime(rs.Time), v)
 		}
+	}
+	if restoreFn != nil {
+		restoreFn()
 	}
 
 	// Walk over our accumulated per-second error rates and report them as
