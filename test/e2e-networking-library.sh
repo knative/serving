ca96340985c8ed2b89b0d0da2f85f79972838671
Artur Upton Renault <arenault@google.com>
2021-01-05 12:10:39 -0800

    Patch config-istio after the configmap is actually created (#10484)
    * Revert "Revert "Patch config-istio after the configmap is actually created (#10450)" (#10462)"
    
    This reverts commit a190ffa319722f2280edbf72cc7fb2cb1e069747.
    
    * Don't fail with file not found

--- test/e2e-networking-library.sh
+++ test/e2e-networking-library.sh
@@ -69,6 +69,14 @@ function install_istio() {
     sed "s/namespace: \"*${KNATIVE_DEFAULT_NAMESPACE}\"*/namespace: ${SYSTEM_NAMESPACE}/g" ${1} > ${YAML_NAME}
     echo "net-istio patched YAML: $YAML_NAME"
     ko apply -f "${YAML_NAME}" --selector=networking.knative.dev/ingress-provider=istio || return 1
+
+    CONFIGURE_ISTIO=${NET_ISTIO_DIR}/third_party/istio-${ISTIO_VERSION}/extras/configure-istio.sh
+    if [[ -f "$CONFIGURE_ISTIO" ]]; then
+      $CONFIGURE_ISTIO
+    else
+      echo "configure-istio.sh not found; skipping."
+    fi
+
     UNINSTALL_LIST+=( "${YAML_NAME}" )
   fi
 }
