ad49e421e50e7b834be43765cca78e6d2ae5e560
Knative Automation <automation@knative.team>
2021-01-19 02:45:10 +0000

    upgrade to latest dependencies
    bumping knative.dev/caching 3657044...ba2835b:
      > ba2835b upgrade to latest dependencies (# 415)
    bumping knative.dev/networking e223a89...07536a6:
      > 07536a6 Move RewriteHost conformance tests to Beta (# 334)
    bumping knative.dev/pkg 5bb97df...75d66b5:
      > 75d66b5 preserve the stable webhook logger names we had before (# 1994)
      > 22f4898 upgrade to latest dependencies (# 1990)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/networking/test/conformance/ingress/run.go
+++ vendor/knative.dev/networking/test/conformance/ingress/run.go
@@ -49,12 +49,12 @@ var stableTests = map[string]func(t *testing.T){
 var betaTests = map[string]func(t *testing.T){
 	// Add your conformance test for beta features
 	"headers/probe": TestProbeHeaders,
+	"host-rewrite":  TestRewriteHost,
 }
 
 var alphaTests = map[string]func(t *testing.T){
 	// Add your conformance test for alpha features
 	"headers/tags": TestTagHeaders,
-	"host-rewrite": TestRewriteHost,
 }
 
 // RunConformance will run ingress conformance tests
