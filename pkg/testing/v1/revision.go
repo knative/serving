39cc0cf25aabfb15b19c70a9a3aba54f4a485a19
Victor Agababov <vagababov@gmail.com>
2021-01-28 02:04:25 +0000

    Rename back to the non deprecated name
    in case someone uses this field, so we don't break them (we don't think anyone uses really)
    
    Change-Id: Ie7de62c1961d84f3401610febcab11aad702b9df

--- pkg/testing/v1/revision.go
+++ pkg/testing/v1/revision.go
@@ -52,7 +52,7 @@ func WithRevName(name string) RevisionOption {
 // WithServiceName propagates the given service name to the revision status.
 func WithServiceName(sn string) RevisionOption {
 	return func(rev *v1.Revision) {
-		rev.Status.DeprecatedServiceName = sn
+		rev.Status.ServiceName = sn
 	}
 }
 
@@ -139,7 +139,7 @@ func MarkActive(r *v1.Revision) {
 
 // WithK8sServiceName applies sn to the revision status field.
 func WithK8sServiceName(r *v1.Revision) {
-	r.Status.DeprecatedServiceName = r.Name
+	r.Status.ServiceName = r.Name
 }
 
 // MarkInactive calls .Status.MarkInactive on the Revision.
