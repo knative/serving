0055bd00c335888a3c2193c041f2c786c6a92915
Chi Zhang <chizhg@google.com>
2021-02-09 07:32:08 -0800

    Fix the logic to delete kail log files if the tests pass (#10723)

--- test/e2e-upgrade-tests.sh
+++ test/e2e-upgrade-tests.sh
@@ -54,5 +54,5 @@ go_test_e2e -tags=upgrade -timeout=${TIMEOUT} \
 
 # Remove the kail log file if the test flow passes.
 # This is for preventing too many large log files to be uploaded to GCS in CI.
-rm "${KAIL_LOG_FILE}"
+rm "${ARTIFACTS}/k8s.log-$(basename "${E2E_SCRIPT}").txt"
 success
