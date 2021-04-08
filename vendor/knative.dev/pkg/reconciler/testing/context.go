5295c582ff4bde23029e0216c0b55c543440634f
ying liu <cdlliuy@cn.ibm.com>
2021-04-08 12:15:30 +0800

    fix deps

--- vendor/knative.dev/pkg/reconciler/testing/context.go
+++ vendor/knative.dev/pkg/reconciler/testing/context.go
@@ -39,6 +39,7 @@ import (
 )
 
 // SetupFakeContext sets up the the Context and the fake informers for the tests.
+// The optional fs() can be used to edit ctx before the SetupInformer steps
 func SetupFakeContext(t testing.TB, fs ...func(context.Context) context.Context) (context.Context, []controller.Informer) {
 	c, _, is := SetupFakeContextWithCancel(t, fs...)
 	return c, is
@@ -46,6 +47,7 @@ func SetupFakeContext(t testing.TB, fs ...func(context.Context) context.Context)
 
 // SetupFakeContextWithCancel sets up the the Context and the fake informers for the tests
 // The provided context can be canceled using provided callback.
+// The optional fs() can be used to edit ctx before the SetupInformer steps
 func SetupFakeContextWithCancel(t testing.TB, fs ...func(context.Context) context.Context) (context.Context, context.CancelFunc, []controller.Informer) {
 	ctx, c := context.WithCancel(logtesting.TestContextWithLogger(t))
 	ctx = controller.WithEventRecorder(ctx, record.NewFakeRecorder(1000))
