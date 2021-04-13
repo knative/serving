2bfe3de7be5e4e41f407a79582a25b9ecb9060a0
knative-automation <automation@knative.team>
2021-04-12 23:43:59 -0700

    upgrade to latest dependencies (#11168)
    bumping knative.dev/pkg 3a2ae6d...b51994e:
      > b51994e Fix a few comments in reconciler-gen (# 2090)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- go.mod
+++ go.mod
@@ -54,6 +54,6 @@ require (
 	knative.dev/caching v0.0.0-20210412064841-08a0136e4368
 	knative.dev/hack v0.0.0-20210325223819-b6ab329907d3
 	knative.dev/networking v0.0.0-20210412065741-89774aa4adb9
-	knative.dev/pkg v0.0.0-20210409203851-3a2ae6db7097
+	knative.dev/pkg v0.0.0-20210412173742-b51994e3b312
 	sigs.k8s.io/yaml v1.2.0
 )
--- vendor/knative.dev/pkg/codegen/cmd/injection-gen/generators/reconciler_reconciler.go
+++ vendor/knative.dev/pkg/codegen/cmd/injection-gen/generators/reconciler_reconciler.go
@@ -270,13 +270,13 @@ type doReconcile func(ctx {{.contextContext|raw}}, o *{{.type|raw}}) {{.reconcil
 
 // reconcilerImpl implements controller.Reconciler for {{.type|raw}} resources.
 type reconcilerImpl struct {
-	// LeaderAwareFuncs is inlined to help us implement {{.reconcilerLeaderAware|raw}}
+	// LeaderAwareFuncs is inlined to help us implement {{.reconcilerLeaderAware|raw}}.
 	{{.reconcilerLeaderAwareFuncs|raw}}
 
 	// Client is used to write back status updates.
 	Client {{.clientsetInterface|raw}}
 
-	// Listers index properties about resources
+	// Listers index properties about resources.
 	Lister {{.resourceLister|raw}}
 
 	// Recorder is an event recorder for recording Event resources to the
@@ -305,7 +305,7 @@ type reconcilerImpl struct {
 	{{end}}
 }
 
-// Check that our Reconciler implements controller.Reconciler
+// Check that our Reconciler implements controller.Reconciler.
 var _ controller.Reconciler = (*reconcilerImpl)(nil)
 // Check that our generated Reconciler is always LeaderAware.
 var _ {{.reconcilerLeaderAware|raw}}  = (*reconcilerImpl)(nil)
