d73eb9953970cbd1e54719fa927d69aa7d424684
knative-automation <automation@knative.team>
2021-04-13 07:21:00 -0700

    upgrade to latest dependencies (#11173)
    bumping knative.dev/networking 89774aa...a64b4be:
      > a64b4be upgrade to latest dependencies (# 391)
    bumping knative.dev/caching 08a0136...bfe6379:
      > bfe6379 upgrade to latest dependencies (# 452)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/networking/pkg/client/injection/reconciler/networking/v1alpha1/serverlessservice/reconciler.go
+++ vendor/knative.dev/networking/pkg/client/injection/reconciler/networking/v1alpha1/serverlessservice/reconciler.go
@@ -88,13 +88,13 @@ type doReconcile func(ctx context.Context, o *v1alpha1.ServerlessService) reconc
 
 // reconcilerImpl implements controller.Reconciler for v1alpha1.ServerlessService resources.
 type reconcilerImpl struct {
-	// LeaderAwareFuncs is inlined to help us implement reconciler.LeaderAware
+	// LeaderAwareFuncs is inlined to help us implement reconciler.LeaderAware.
 	reconciler.LeaderAwareFuncs
 
 	// Client is used to write back status updates.
 	Client versioned.Interface
 
-	// Listers index properties about resources
+	// Listers index properties about resources.
 	Lister networkingv1alpha1.ServerlessServiceLister
 
 	// Recorder is an event recorder for recording Event resources to the
@@ -116,7 +116,7 @@ type reconcilerImpl struct {
 	skipStatusUpdates bool
 }
 
-// Check that our Reconciler implements controller.Reconciler
+// Check that our Reconciler implements controller.Reconciler.
 var _ controller.Reconciler = (*reconcilerImpl)(nil)
 
 // Check that our generated Reconciler is always LeaderAware.
