101c6f1efa8dcbef7172ffb719dd80c9a087f6b8
Markus Thömmes <markusthoemmes@me.com>
2021-04-30 03:09:55 +0200

    Add complete schemas to all CRDs (#11244)
    * Setup CRD generation machinery and generate initial schemas
    
    * Add filtered PodSpec schema
    
    * Fix embedded types
    
    * Fix things
    
    * Allow preservation for some more types
    
    * Add comments
    
    * Add an explicit comment towards our patched version
    
    * Detach update-codegen from schema generation for now
    
    * Drop incorrect default descriptions
    
    * Drop unnecessary git based rollback
    
    * Simplify further
    
    * Drop now unnecessary comment
    
    * Reorder schema and manual fields
    
    * Drop feature flagged fields
    
    * Remove fat fingering
    
    * Fix leftovers

---
+++ hack/update-schemas.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 The Knative Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Make sure you install the patched version of controller-gen from
+# https://github.com/markusthoemmes/controller-tools/tree/knative-specific
+#
+# We need a patched version because
+# 1. There's a bug that makes our URL types unusable
+#    see https://github.com/kubernetes-sigs/controller-tools/issues/560
+# 2. There's a missing feature to properly generate nested ObjectMeta
+#    see https://github.com/kubernetes-sigs/controller-tools/pull/563
+# 3. We need specialized logic to filter down the surface of PodSpec we allow in Knative.
+controller-gen schemapatch:manifests=config/core/300-resources \
+  output:dir=config/core/300-resources \
+  paths=./pkg/apis/...
--- pkg/apis/serving/v1/revision_types.go
+++ pkg/apis/serving/v1/revision_types.go
@@ -64,6 +64,7 @@ var (
 // RevisionTemplateSpec describes the data a revision should have when created from a template.
 // Based on: https://github.com/kubernetes/api/blob/e771f807/core/v1/types.go#L3179-L3190
 type RevisionTemplateSpec struct {
+	// +kubebuilder:pruning:PreserveUnknownFields
 	// +optional
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
