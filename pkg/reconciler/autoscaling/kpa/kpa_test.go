0e7a41df266a01a5afabb6ad87b568531fbef5d5
Markus Thömmes <markusthoemmes@me.com>
2021-05-04 12:12:27 +0200

    Fix nit

--- pkg/reconciler/autoscaling/kpa/kpa_test.go
+++ pkg/reconciler/autoscaling/kpa/kpa_test.go
@@ -1347,7 +1347,7 @@ func TestReconcileDeciderCreatesAndDeletes(t *testing.T) {
 		if err != nil {
 			return true, err
 		}
-		return newKPA.IsReady() && *newKPA.Status.DesiredScale == 1, nil
+		return newKPA.IsReady() && newKPA.Status.GetDesiredScale() == 1, nil
 	}); err != nil {
 		t.Fatal("PA failed to become ready:", err)
 	}
