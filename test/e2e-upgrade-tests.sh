3a24db787ed3c4939a7bd9bfa7575a9254f484bd
dprotaso <dprotaso@gmail.com>
2021-05-20 14:45:36 -0400

    have GKE spin up 1.19 clusters

--- test/e2e-upgrade-tests.sh
+++ test/e2e-upgrade-tests.sh
@@ -36,9 +36,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/e2e-common.sh"
 # Skip installing istio as an add-on.
 # Temporarily increasing the cluster size for serving tests to rule out
 # resource/eviction as causes of flakiness.
-# Pin to 1.18 since scale test is super flakey on 1.19
-initialize "$@" --skip-istio-addon  --min-nodes=4 --max-nodes=4 --cluster-version=1.18 \
-  --install-latest-release
+initialize --skip-istio-addon  --min-nodes=4 --max-nodes=4 --install-latest-release "$@"
 
 # TODO(#2656): Reduce the timeout after we get this test to consistently passing.
 TIMEOUT=30m
