108155021b085bbf00f6f879832aad50dee44904
dprotaso <dprotaso@gmail.com>
2021-05-31 15:55:35 -0400

    drop dependency on knative.dev/networking/test
    This was breaking our flag parsing

--- test/e2e_flags.go
+++ test/e2e_flags.go
@@ -43,70 +43,26 @@ type ServingEnvironmentFlags struct {
 func initializeServingFlags() *ServingEnvironmentFlags {
 	var f ServingEnvironmentFlags
 
-	// Only define and set flags here. Flag values cannot be read at package init time.
-	if fl := flag.Lookup("resolvabledomain"); fl == nil {
-		// Only define and set flags here. Flag values cannot be read at package init time.
-		flag.BoolVar(&f.ResolvableDomain,
-			"resolvabledomain",
-			false,
-			"Set this flag to true if you have configured the `domainSuffix` on your Route controller to a domain that will resolve to your test cluster.")
-	} else {
-		f.ResolvableDomain = fl.Value.(flag.Getter).Get().(bool)
-	}
+	flag.BoolVar(&f.ResolvableDomain, "resolvabledomain", false,
+		"Set this flag to true if you have configured the `domainSuffix` on your Route controller to a domain that will resolve to your test cluster.")
 
-	if fl := flag.Lookup("customdomain"); fl == nil {
-		flag.StringVar(&f.CustomDomain,
-			"customdomain",
-			"",
-			"Set this flag to the custom domain suffix for domainmapping test.")
-	} else {
-		f.CustomDomain = fl.Value.String()
-	}
+	flag.StringVar(&f.CustomDomain, "customdomain", "",
+		"Set this flag to the custom domain suffix for domainmapping test.")
 
-	if fl := flag.Lookup("https"); fl == nil {
-		flag.BoolVar(&f.HTTPS,
-			"https",
-			false,
-			"Set this flag to true to run all tests with https.")
-	} else {
-		f.HTTPS = fl.Value.(flag.Getter).Get().(bool)
-	}
+	flag.BoolVar(&f.HTTPS, "https", false,
+		"Set this flag to true to run all tests with https.")
 
-	if fl := flag.Lookup("buckets"); fl == nil {
-		flag.IntVar(&f.Buckets,
-			"buckets",
-			1,
-			"Set this flag to the number of reconciler buckets configured.")
-	} else {
-		f.Buckets = fl.Value.(flag.Getter).Get().(int)
-	}
+	flag.IntVar(&f.Buckets, "buckets", 1,
+		"Set this flag to the number of reconciler buckets configured.")
 
-	if fl := flag.Lookup("replicas"); fl == nil {
-		flag.IntVar(&f.Replicas,
-			"replicas",
-			1,
-			"Set this flag to the number of controlplane replicas being run.")
-	} else {
-		f.Replicas = fl.Value.(flag.Getter).Get().(int)
-	}
+	flag.IntVar(&f.Replicas, "replicas", 1,
+		"Set this flag to the number of controlplane replicas being run.")
 
-	if fl := flag.Lookup("enable-alpha"); fl == nil {
-		flag.BoolVar(&f.EnableAlphaFeatures,
-			"enable-alpha",
-			false,
-			"Set this flag to run tests against alpha features")
-	} else {
-		f.EnableAlphaFeatures = fl.Value.(flag.Getter).Get().(bool)
-	}
+	flag.BoolVar(&f.EnableAlphaFeatures, "enable-alpha", false,
+		"Set this flag to run tests against alpha features")
 
-	if fl := flag.Lookup("enable-beta"); fl == nil {
-		flag.BoolVar(&f.EnableBetaFeatures,
-			"enable-beta",
-			false,
-			"Set this flag to run tests against beta features")
-	} else {
-		f.EnableBetaFeatures = fl.Value.(flag.Getter).Get().(bool)
-	}
+	flag.BoolVar(&f.EnableBetaFeatures, "enable-beta", false,
+		"Set this flag to run tests against beta features")
 
 	return &f
 }
