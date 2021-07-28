ab4bddf623a8be0745abd67c807399236754f955
Paul S. Schweigert <paulschw@us.ibm.com>
2021-07-28 11:37:24 -0400

    only allow setting to false

--- pkg/apis/serving/fieldmask.go
+++ pkg/apis/serving/fieldmask.go
@@ -183,7 +183,10 @@ func PodSpecMask(ctx context.Context, in *corev1.PodSpec) *corev1.PodSpec {
 	out.Volumes = in.Volumes
 	out.ImagePullSecrets = in.ImagePullSecrets
 	out.EnableServiceLinks = in.EnableServiceLinks
-	out.AutomountServiceAccountToken = in.AutomountServiceAccountToken
+	// Only allow setting AutomountServiceAccountToken to false
+	if !*in.AutomountServiceAccountToken {
+		out.AutomountServiceAccountToken = in.AutomountServiceAccountToken
+	}
 
 	// Feature fields
 	if cfg.Features.PodSpecAffinity != config.Disabled {
@@ -212,6 +215,10 @@ func PodSpecMask(ctx context.Context, in *corev1.PodSpec) *corev1.PodSpec {
 	out.TerminationGracePeriodSeconds = nil
 	out.ActiveDeadlineSeconds = nil
 	out.DNSPolicy = ""
+	// Only allow setting AutomountServiceAccountToken to false
+	if *in.AutomountServiceAccountToken {
+		out.AutomountServiceAccountToken = nil
+	}
 	out.NodeName = ""
 	out.HostNetwork = false
 	out.HostPID = false
