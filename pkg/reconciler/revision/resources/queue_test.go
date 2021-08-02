b31c73ca3db9e1f01750f803b59b20f708378c04
Paul S. Schweigert <paulschw@us.ibm.com>
2021-08-02 15:05:46 -0400

    test for non-empty values

--- pkg/reconciler/revision/resources/queue_test.go
+++ pkg/reconciler/revision/resources/queue_test.go
@@ -354,6 +354,19 @@ func TestMakeQueueContainer(t *testing.T) {
 				"ENABLE_HTTP2_AUTO_DETECTION": "true",
 			})
 		}),
+	}, {
+		name: "set concurrency state endpoint",
+		rev: revision("bar", "foo",
+			withContainers(containers)),
+		dc: deployment.Config{
+			ProgressDeadline:         5678 * time.Second,
+			ConcurrencyStateEndpoint: "freeze-proxy",
+		},
+		want: queueContainer(func(c *corev1.Container) {
+			c.Env = env(map[string]string{
+				"CONCURRENCY_STATE_ENDPOINT": "freeze-proxy",
+			})
+		}),
 	}}
 
 	for _, test := range tests {
