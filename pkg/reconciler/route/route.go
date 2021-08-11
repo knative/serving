6d44f5f3b0249d775367d6b3b43331a867e94c80
necatican <necaticanyildirim@gmail.com>
2021-08-11 20:12:38 +0300

    Delete orphaned tag certificates after tag deletion

--- pkg/reconciler/route/route.go
+++ pkg/reconciler/route/route.go
@@ -24,6 +24,7 @@ import (
 	"strings"
 	"time"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubelabels "k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/clock"
@@ -33,6 +34,7 @@ import (
 
 	network "knative.dev/networking/pkg"
 	"knative.dev/networking/pkg/apis/networking"
+	"knative.dev/networking/pkg/apis/networking/v1alpha1"
 	netv1alpha1 "knative.dev/networking/pkg/apis/networking/v1alpha1"
 	netclientset "knative.dev/networking/pkg/client/clientset/versioned"
 	networkinglisters "knative.dev/networking/pkg/client/listers/networking/v1alpha1"
@@ -276,6 +278,14 @@ func (c *Reconciler) tls(ctx context.Context, host string, r *v1.Route, traffic 
 	sort.Slice(acmeChallenges, func(i, j int) bool {
 		return acmeChallenges[i].URL.String() < acmeChallenges[j].URL.String()
 	})
+
+	orphanCerts, err := c.getOrphanRouteCerts(r, domainToTagMap)
+	if err == nil {
+		for _, cert := range orphanCerts {
+			c.GetNetworkingClient().NetworkingV1alpha1().Certificates(cert.Namespace).Delete(ctx, cert.Name, metav1.DeleteOptions{})
+		}
+	}
+
 	return tls, acmeChallenges, nil
 }
 
@@ -485,4 +495,30 @@ func wildcardCertMatches(ctx context.Context, domains []string, cert *netv1alpha
 	}
 
 	return true
+}
+
+// Returns a slice of certificates that used to belong route's old tags and are currently not in use.
+func (c *Reconciler) getOrphanRouteCerts(r *v1.Route, domainToTagMap map[string]string) ([]*v1alpha1.Certificate, error) {
+	var unusedCerts []*v1alpha1.Certificate
+	labelSelector := kubelabels.SelectorFromSet(kubelabels.Set{
+		serving.RouteLabelKey: r.Name,
+	})
+
+	certs, err := c.certificateLister.Certificates(r.Namespace).List(labelSelector)
+	if err != nil {
+		return nil, err
+	}
+
+	var shouldKeepCert bool
+	for _, cert := range certs {
+		for _, dn := range cert.Spec.DNSNames {
+			_, shouldKeepCert = domainToTagMap[dn]
+		}
+
+		if !shouldKeepCert {
+			unusedCerts = append(unusedCerts, cert)
+		}
+	}
+
+	return unusedCerts, nil
 }
