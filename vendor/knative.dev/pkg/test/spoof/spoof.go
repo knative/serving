276e11d8c848a50339ef33730a8685edc07b7222
Knative Automation <automation@knative.team>
2021-09-06 13:13:42 +0000

    upgrade to latest dependencies
    bumping knative.dev/networking 5f2e230...7c11d23:
      > 7c11d23 upgrade to latest dependencies (# 498)
    bumping knative.dev/pkg dec98b4...bb433c9:
      > bb433c9 Add tests for retriable errors to the spoof client (# 2277)
      > b09fa45 Declare `DefaultErrorRetryChecker` as `ErrorRetryChecker` type (# 2276)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/pkg/test/spoof/spoof.go
+++ vendor/knative.dev/pkg/test/spoof/spoof.go
@@ -164,7 +164,7 @@ func (sc *SpoofingClient) Do(req *http.Request, errorRetryCheckers ...interface{
 // If no retry checkers are specified `DefaultErrorRetryChecker` will be used.
 func (sc *SpoofingClient) Poll(req *http.Request, inState ResponseChecker, checkers ...interface{}) (*Response, error) {
 	if len(checkers) == 0 {
-		checkers = []interface{}{DefaultErrorRetryChecker}
+		checkers = []interface{}{ErrorRetryChecker(DefaultErrorRetryChecker)}
 	}
 
 	var resp *Response
