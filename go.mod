c38217a264aaf70e8e48215c0ee1a71636064bfe
knative-automation <automation@knative.team>
2021-10-27 09:43:20 -0700

    upgrade to latest dependencies (#12172)
    bumping knative.dev/caching 6facf87...2ffdd31:
      > 2ffdd31 Update actions (# 551)
      > 139fa31 upgrade to latest dependencies (# 550)
      > 540eb6c Update actions (# 549)
    bumping knative.dev/hack 02860f5...a71c865:
      > a71c865 Format go code (# 88)
    bumping knative.dev/networking de9cf66...31eca9c:
      > 31eca9c Update actions (# 539)
      > dea025f upgrade to latest dependencies (# 538)
    bumping knative.dev/pkg 819d556...3b33e02:
      > 3b33e02 upgrade to latest dependencies (# 2329)
      > a8fb292 Update actions (# 2328)
      > 7049a59 Update actions (# 2321)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- go.mod
+++ go.mod
@@ -46,9 +46,9 @@ require (
 	k8s.io/code-generator v0.21.4
 	k8s.io/kube-openapi v0.0.0-20210305001622-591a79e4bda7
 	k8s.io/utils v0.0.0-20210111153108-fddb29f9d009 // indirect
-	knative.dev/caching v0.0.0-20211019132135-6facf87d69eb
-	knative.dev/hack v0.0.0-20211025203920-02860f59a556
-	knative.dev/networking v0.0.0-20211025234619-de9cf667af21
-	knative.dev/pkg v0.0.0-20211025151738-819d556cdaa5
+	knative.dev/caching v0.0.0-20211026205101-2ffdd31aa1e8
+	knative.dev/hack v0.0.0-20211026141922-a71c865b5f66
+	knative.dev/networking v0.0.0-20211027012100-31eca9c93514
+	knative.dev/pkg v0.0.0-20211027105800-3b33e02e5b9c
 	sigs.k8s.io/yaml v1.3.0
 )
--- vendor/knative.dev/networking/pkg/client/injection/informers/networking/v1alpha1/clusterdomainclaim/clusterdomainclaim.go
+++ vendor/knative.dev/networking/pkg/client/injection/informers/networking/v1alpha1/clusterdomainclaim/clusterdomainclaim.go
@@ -50,7 +50,7 @@ func withInformer(ctx context.Context) (context.Context, controller.Informer) {
 }
 
 func withDynamicInformer(ctx context.Context) context.Context {
-	inf := &wrapper{client: client.Get(ctx)}
+	inf := &wrapper{client: client.Get(ctx), resourceVersion: injection.GetResourceVersion(ctx)}
 	return context.WithValue(ctx, Key{}, inf)
 }
 
@@ -66,6 +66,8 @@ func Get(ctx context.Context) v1alpha1.ClusterDomainClaimInformer {
 
 type wrapper struct {
 	client versioned.Interface
+
+	resourceVersion string
 }
 
 var _ v1alpha1.ClusterDomainClaimInformer = (*wrapper)(nil)
@@ -79,10 +81,18 @@ func (w *wrapper) Lister() networkingv1alpha1.ClusterDomainClaimLister {
 	return w
 }
 
+// SetResourceVersion allows consumers to adjust the minimum resourceVersion
+// used by the underlying client.  It is not accessible via the standard
+// lister interface, but can be accessed through a user-defined interface and
+// an implementation check e.g. rvs, ok := foo.(ResourceVersionSetter)
+func (w *wrapper) SetResourceVersion(resourceVersion string) {
+	w.resourceVersion = resourceVersion
+}
+
 func (w *wrapper) List(selector labels.Selector) (ret []*apisnetworkingv1alpha1.ClusterDomainClaim, err error) {
 	lo, err := w.client.NetworkingV1alpha1().ClusterDomainClaims().List(context.TODO(), v1.ListOptions{
-		LabelSelector: selector.String(),
-		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+		LabelSelector:   selector.String(),
+		ResourceVersion: w.resourceVersion,
 	})
 	if err != nil {
 		return nil, err
@@ -95,6 +105,6 @@ func (w *wrapper) List(selector labels.Selector) (ret []*apisnetworkingv1alpha1.
 
 func (w *wrapper) Get(name string) (*apisnetworkingv1alpha1.ClusterDomainClaim, error) {
 	return w.client.NetworkingV1alpha1().ClusterDomainClaims().Get(context.TODO(), name, v1.GetOptions{
-		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+		ResourceVersion: w.resourceVersion,
 	})
 }
