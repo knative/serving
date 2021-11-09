d87041094a7bdc20e55846a55ff9d3e1a72f7b27
knative-automation <automation@knative.team>
2021-11-08 18:37:42 -0800

    upgrade to latest dependencies (#12240)
    bumping knative.dev/networking 6871f98...79a1ce1:
      > 79a1ce1 upgrade to latest dependencies (# 556)
    bumping knative.dev/pkg 51b9e7f...3cc697a:
      > 3cc697a upgrade to latest dependencies (# 2344)
    bumping knative.dev/hack 29f86c2...96aac1c:
      > 96aac1c Add wait_until_object_exists library function (# 96)
    bumping knative.dev/caching bfa1e3f...0c35ec4:
      > 0c35ec4 upgrade to latest dependencies (# 563)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- go.mod
+++ go.mod
@@ -45,9 +45,9 @@ require (
 	k8s.io/code-generator v0.21.4
 	k8s.io/kube-openapi v0.0.0-20210305001622-591a79e4bda7
 	k8s.io/utils v0.0.0-20210111153108-fddb29f9d009 // indirect
-	knative.dev/caching v0.0.0-20211104075903-bfa1e3f2d544
-	knative.dev/hack v0.0.0-20211105231158-29f86c2653b5
-	knative.dev/networking v0.0.0-20211104064801-6871f98f7b4d
-	knative.dev/pkg v0.0.0-20211104101302-51b9e7f161b4
+	knative.dev/caching v0.0.0-20211108065001-0c35ec4809b1
+	knative.dev/hack v0.0.0-20211108170701-96aac1c30be3
+	knative.dev/networking v0.0.0-20211108064904-79a1ce1e1952
+	knative.dev/pkg v0.0.0-20211108064904-3cc697a3cb09
 	sigs.k8s.io/yaml v1.3.0
 )
--- vendor/knative.dev/hack/library.sh
+++ vendor/knative.dev/hack/library.sh
@@ -186,6 +186,32 @@ function wait_until_object_does_not_exist() {
   return 1
 }
 
+# Waits until the given object exists.
+# Parameters: $1 - the kind of the object.
+#             $2 - object's name.
+#             $3 - namespace (optional).
+function wait_until_object_exists() {
+  local KUBECTL_ARGS="get $1 $2"
+  local DESCRIPTION="$1 $2"
+
+  if [[ -n $3 ]]; then
+    KUBECTL_ARGS="get -n $3 $1 $2"
+    DESCRIPTION="$1 $3/$2"
+  fi
+  echo -n "Waiting until ${DESCRIPTION} exists"
+  for i in {1..150}; do  # timeout after 5 minutes
+    if kubectl ${KUBECTL_ARGS} > /dev/null 2>&1; then
+      echo -e "\n${DESCRIPTION} exists"
+      return 0
+    fi
+    echo -n "."
+    sleep 2
+  done
+  echo -e "\n\nERROR: timeout waiting for ${DESCRIPTION} to exist"
+  kubectl ${KUBECTL_ARGS}
+  return 1
+}
+
 # Waits until all pods are running in the given namespace.
 # This function handles some edge cases that `kubectl wait` does not support,
 # and it provides nice debug info on the state of the pod if it failed,
