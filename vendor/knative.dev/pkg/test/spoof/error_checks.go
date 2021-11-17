2c1014288110788cb51e4c7ede16b01de1a4624f
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-11-17 16:54:21 +0900

    Add retry checker for DNS failure from Ingress

--- vendor/knative.dev/pkg/test/spoof/error_checks.go
+++ vendor/knative.dev/pkg/test/spoof/error_checks.go
@@ -21,6 +21,7 @@ package spoof
 import (
 	"errors"
 	"net"
+	"net/http"
 	"strings"
 )
 
@@ -61,4 +62,8 @@ func isConnectionReset(err error) bool {
 
 func isNoRouteToHostError(err error) bool {
 	return err != nil && strings.Contains(err.Error(), "connect: no route to host")
+}
+
+func isResponseDNSError(resp *Response) bool {
+	return resp.StatusCode == http.StatusBadGateway && strings.Contains(string(resp.Body), "no such host")
 }
