215d0134d054e30747d358fc6e64ac24d2096196
Julian Friedman <julz.friedman@uk.ibm.com>
2021-11-25 19:47:17 +0000

    Tidy metrics methods (#12352)
    - Do cache checks as guard clauses to allow out-denting method bodies
    - Unexport methods that aren't used elsewhere
    - Fix generate -> generates typo in comment

--- pkg/metrics/tags_test.go
+++ pkg/metrics/tags_test.go
@@ -62,8 +62,8 @@ func TestContextsErrors(t *testing.T) {
 		strings.Repeat("a", 256), // Longer than 255 characters.
 	}
 	for _, v := range invalidTagValues {
-		if _, err := PodContext(v, v); err == nil {
-			t.Errorf("PodContext(%q) = nil, wanted an error", v)
+		if _, err := podContext(v, v); err == nil {
+			t.Errorf("podContext(%q) = nil, wanted an error", v)
 		}
 		if _, err := PodRevisionContext(v, v, v, v, v, v); err == nil {
 			t.Errorf("PodRevisionContext(%q) = nil, wanted an error", v)
@@ -80,7 +80,7 @@ func TestContexts(t *testing.T) {
 	}{{
 		name: "pod context",
 		ctx: mustCtx(t, func() (context.Context, error) {
-			return PodContext("testpod", "testcontainer")
+			return podContext("testpod", "testcontainer")
 		}),
 		wantTags: map[string]string{
 			LabelPodName:       "testpod",
@@ -173,11 +173,11 @@ func TestContexts(t *testing.T) {
 	}, {
 		name: "pod context augmented with revision",
 		ctx: mustCtx(t, func() (context.Context, error) {
-			ctx, err := PodContext("testpod", "testcontainer")
+			ctx, err := podContext("testpod", "testcontainer")
 			if err != nil {
 				return ctx, err
 			}
-			return AugmentWithRevision(ctx, "testns", "testsvc", "testcfg", "testrev"), nil
+			return augmentWithRevision(ctx, "testns", "testsvc", "testcfg", "testrev"), nil
 		}),
 		wantTags: map[string]string{
 			LabelPodName:       "testpod",
