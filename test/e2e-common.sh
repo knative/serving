7ecb171915cddb5dc385baea516420725c52c2af
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-11-28 21:39:00 +0900

    Revert "Use istio manifest with more resources"
    This reverts commit dcc5fdb6545d16ee3efdedb7aafbfb82dc15862d.

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -230,8 +230,7 @@ function knative_setup() {
     if (( KIND )); then
       $HOME/.istioctl/bin/istioctl install -y --set values.gateways.istio-ingressgateway.type=NodePort --set values.global.proxy.clusterDomain="${CLUSTER_DOMAIN}"
     else
-      #$HOME/.istioctl/bin/istioctl install -y --set values.global.proxy.clusterDomain="${CLUSTER_DOMAIN}"
-      $HOME/.istioctl/bin/istioctl install -y -f third_party/tmp/istio-ci-no-mesh.yaml
+      $HOME/.istioctl/bin/istioctl install -y --set values.global.proxy.clusterDomain="${CLUSTER_DOMAIN}"
     fi
   fi
 
