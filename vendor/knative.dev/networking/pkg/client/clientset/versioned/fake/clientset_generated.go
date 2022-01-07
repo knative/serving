03091748d2795f7bbdd79477c97ff69ef960bf51
knative-automation <automation@knative.team>
2022-01-07 09:01:25 -0800

    upgrade to latest dependencies (#12481)
    bumping knative.dev/networking e40187c...0dbedcd:
      > 0dbedcd upgrade to latest dependencies (# 598)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/networking/pkg/client/clientset/versioned/fake/clientset_generated.go
+++ vendor/knative.dev/networking/pkg/client/clientset/versioned/fake/clientset_generated.go
@@ -74,7 +74,10 @@ func (c *Clientset) Tracker() testing.ObjectTracker {
 	return c.tracker
 }
 
-var _ clientset.Interface = &Clientset{}
+var (
+	_ clientset.Interface = &Clientset{}
+	_ testing.FakeClient  = &Clientset{}
+)
 
 // NetworkingV1alpha1 retrieves the NetworkingV1alpha1Client
 func (c *Clientset) NetworkingV1alpha1() networkingv1alpha1.NetworkingV1alpha1Interface {
