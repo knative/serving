85d1066f0bbebe777719fc24c20af8fcd71f7715
dprotaso <dprotaso@gmail.com>
2022-01-07 09:18:06 -0500

    disable HA

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -111,7 +111,6 @@ fi
 # Note that we use a very high -parallel because each ksvc is run as its own
 # sub-test. If this is not larger than the maximum scale tested then the test
 # simply cannot pass.
-kubectl -n "${SYSTEM_NAMESPACE}" scale deployment "chaosduck" --replicas=0 || fail_test
 go_test_e2e -timeout=20m -parallel=300 ./test/scale ${TEST_OPTIONS} || failed=1
 
 # # Run HPA tests
