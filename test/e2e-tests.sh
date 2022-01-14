bedf8f9267624c618cce3d7377967794867605d0
dprotaso <dprotaso@gmail.com>
2022-01-14 09:51:36 -0500

    clean up comments

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -28,10 +28,6 @@
 source $(dirname $0)/e2e-common.sh
 
 # Script entry point.
-
-# Skip installing istio as an add-on.
-# Temporarily increasing the cluster size for serving tests to rule out
-# resource/eviction as causes of flakiness.
 initialize --skip-istio-addon --min-nodes=4 --max-nodes=4 --enable-ha --cluster-version=1.21 "$@"
 
 # Run the tests
--- test/e2e-upgrade-tests.sh
+++ test/e2e-upgrade-tests.sh
@@ -40,9 +40,6 @@ function stage_test_resources() {
 # Script entry point.
 
 # Skip installing istio as an add-on.
-# Temporarily increasing the cluster size for serving tests to rule out
-# resource/eviction as causes of flakiness.
-# Pin to 1.20 since scale test is super flakey on 1.21
 initialize "$@" --skip-istio-addon  --min-nodes=4 --max-nodes=4 --cluster-version=1.21 \
   --install-latest-release
 
