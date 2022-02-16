84b50a61cdba4c51289ff15804ae49b581590215
Dave Protasowski <dprotaso@gmail.com>
2022-02-16 11:03:40 -0500

    Drop MaxDurationSeconds from the RevisionSpec (#12635)
    We added MaxDurationSeconds (#12322) because the behaviour of
    RevisionSpec.Timeout changed from total duration to time to first byte.
    
    In hindsight changing the behaviour of Timeout was a mistake since
    it goes against the original specification.
    
    Thus we're going to create a path for migration and the first part is
    to remove MaxDurationSeconds from the RevisionSpec.

--- pkg/reconciler/revision/resources/queue_test.go
+++ pkg/reconciler/revision/resources/queue_test.go
@@ -255,19 +255,6 @@ func TestMakeQueueContainer(t *testing.T) {
 			})
 		}),
 	}, {
-		name: "custom maxDurationSeconds",
-		rev: revision("bar", "foo",
-			withContainers(containers),
-			func(revision *v1.Revision) {
-				revision.Spec.MaxDurationSeconds = ptr.Int64(99)
-			},
-		),
-		want: queueContainer(func(c *corev1.Container) {
-			c.Env = env(map[string]string{
-				"MAX_DURATION_SECONDS": "99",
-			})
-		}),
-	}, {
 		name: "default resource config",
 		rev: revision("bar", "foo",
 			withContainers(containers)),
@@ -692,8 +679,7 @@ func TestTCPProbeGeneration(t *testing.T) {
 			SuccessThreshold: 3,
 		},
 		rev: v1.RevisionSpec{
-			TimeoutSeconds:     ptr.Int64(45),
-			MaxDurationSeconds: ptr.Int64(100),
+			TimeoutSeconds: ptr.Int64(45),
 			PodSpec: corev1.PodSpec{
 				Containers: []corev1.Container{{
 					Name: servingContainerName,
@@ -733,8 +719,7 @@ func TestTCPProbeGeneration(t *testing.T) {
 	}, {
 		name: "tcp defaults",
 		rev: v1.RevisionSpec{
-			TimeoutSeconds:     ptr.Int64(45),
-			MaxDurationSeconds: ptr.Int64(100),
+			TimeoutSeconds: ptr.Int64(45),
 			PodSpec: corev1.PodSpec{
 				Containers: []corev1.Container{{
 					Name: servingContainerName,
@@ -790,8 +775,7 @@ func TestTCPProbeGeneration(t *testing.T) {
 			InitialDelaySeconds: 3,
 		},
 		rev: v1.RevisionSpec{
-			TimeoutSeconds:     ptr.Int64(45),
-			MaxDurationSeconds: ptr.Int64(100),
+			TimeoutSeconds: ptr.Int64(45),
 			PodSpec: corev1.PodSpec{
 				Containers: []corev1.Container{{
 					Name: servingContainerName,
@@ -872,7 +856,6 @@ var defaultEnv = map[string]string{
 	"METRICS_COLLECTOR_ADDRESS":        "",
 	"QUEUE_SERVING_PORT":               "8012",
 	"REVISION_TIMEOUT_SECONDS":         "45",
-	"MAX_DURATION_SECONDS":             "100",
 	"SERVING_CONFIGURATION":            "",
 	"SERVING_ENABLE_PROBE_REQUEST_LOG": "false",
 	"SERVING_ENABLE_REQUEST_LOG":       "false",
