47e9f68e7851dfa3747c714fd3a380b54176a3bf
Dave Protasowski <dprotaso@gmail.com>
2022-02-17 15:57:12 -0500

    fix ytt package name (#12644)
    * fix ytt package name
    
    * use correct path

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -566,7 +566,7 @@ function overlay_system_namespace() {
 }
 
 function run_ytt() {
-  run_go_tool github.com/k14s/ytt/cmd/ytt ytt "$@"
+  run_go_tool github.com/vmware-tanzu/carvel-ytt/cmd/ytt ytt "$@"
 }
 
 
