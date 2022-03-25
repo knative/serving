ccc40402ae031b293d1a73ebfebe77e0a3b3338d
knative-automation <automation@knative.team>
2022-03-24 20:15:48 -0400

    upgrade to latest dependencies (#12775)
    bumping knative.dev/networking ec52d71...55757e9:
      > 55757e9 Add HTTPS port variables (# 640)
      > ccab092 Adding code of conduct to reference knative/community's CoC (# 639)
    bumping knative.dev/caching 77295a8...defaf7b:
      > defaf7b Remove shortnames for internal resources (# 616)
      > 7992ea8 upgrade to latest dependencies (# 613)
      > 3067331 Adding code of conduct to reference knative/community's CoC (# 615)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/caching/pkg/client/clientset/versioned/typed/caching/v1alpha1/caching_client.go
+++ vendor/knative.dev/caching/pkg/client/clientset/versioned/typed/caching/v1alpha1/caching_client.go
@@ -19,6 +19,8 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"net/http"
+
 	rest "k8s.io/client-go/rest"
 	v1alpha1 "knative.dev/caching/pkg/apis/caching/v1alpha1"
 	"knative.dev/caching/pkg/client/clientset/versioned/scheme"
@@ -39,12 +41,28 @@ func (c *CachingV1alpha1Client) Images(namespace string) ImageInterface {
 }
 
 // NewForConfig creates a new CachingV1alpha1Client for the given config.
+// NewForConfig is equivalent to NewForConfigAndClient(c, httpClient),
+// where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*CachingV1alpha1Client, error) {
 	config := *c
 	if err := setConfigDefaults(&config); err != nil {
 		return nil, err
 	}
-	client, err := rest.RESTClientFor(&config)
+	httpClient, err := rest.HTTPClientFor(&config)
+	if err != nil {
+		return nil, err
+	}
+	return NewForConfigAndClient(&config, httpClient)
+}
+
+// NewForConfigAndClient creates a new CachingV1alpha1Client for the given config and http client.
+// Note the http client provided takes precedence over the configured transport values.
+func NewForConfigAndClient(c *rest.Config, h *http.Client) (*CachingV1alpha1Client, error) {
+	config := *c
+	if err := setConfigDefaults(&config); err != nil {
+		return nil, err
+	}
+	client, err := rest.RESTClientForConfigAndClient(&config, h)
 	if err != nil {
 		return nil, err
 	}
