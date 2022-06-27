b829926016d150c3c77cac3746b155a4c63e1d76
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2022-06-27 18:51:23 +0900

    Verify SecretPKKey as well

--- pkg/reconciler/revision/reconcile_resources.go
+++ pkg/reconciler/revision/reconcile_resources.go
@@ -222,7 +222,10 @@ func (c *Reconciler) reconcileSecret(ctx context.Context, rev *v1.Revision) erro
 
 	// Verify if secret has been added the data.
 	if _, ok := secret.Data[certificates.SecretCertKey]; !ok {
-		return fmt.Errorf("secret is not ready yet")
+		return fmt.Errorf("public cert in the secret is not ready yet")
+	}
+	if _, ok := secret.Data[certificates.SecretPKKey]; !ok {
+		return fmt.Errorf("private key in the secret is not ready yet")
 	}
 
 	return nil
