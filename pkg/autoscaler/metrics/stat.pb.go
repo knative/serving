cec38634a87e1570605e23ce3823f3214b7fbb9b
dprotaso <dprotaso@gmail.com>
2022-06-29 10:23:16 -0400

    [skip-dot-release] update generated protobuf

--- pkg/autoscaler/metrics/stat.pb.go
+++ pkg/autoscaler/metrics/stat.pb.go
@@ -663,10 +663,7 @@ func (m *Stat) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthStat
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthStat
 			}
 			if (iNdEx + skippy) > l {
@@ -816,10 +813,7 @@ func (m *WireStatMessage) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthStat
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthStat
 			}
 			if (iNdEx + skippy) > l {
@@ -903,10 +897,7 @@ func (m *WireStatMessages) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthStat
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthStat
 			}
 			if (iNdEx + skippy) > l {
