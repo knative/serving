7cafddbca955dae350249a8772f073be1c720037
dprotaso <dprotaso@gmail.com>
2022-07-27 20:50:21 -0400

    disable rate limting on digest resolution

--- pkg/reconciler/revision/controller.go
+++ pkg/reconciler/revision/controller.go
@@ -113,9 +113,9 @@ func newControllerWithOptions(
 	userAgent := fmt.Sprintf("knative/%s (serving)", changeset.Get())
 
 	digestResolveQueue := workqueue.NewNamedRateLimitingQueue(workqueue.NewMaxOfRateLimiter(
-		newItemExponentialFailureRateLimiter(1*time.Second, 1000*time.Second),
+		newItemExponentialFailureRateLimiter(1*time.Millisecond, 1*time.Second),
 		// 10 qps, 100 bucket size.  This is only for retry speed and its only the overall factor (not per item)
-		&workqueue.BucketRateLimiter{Limiter: rate.NewLimiter(rate.Limit(200), 200)},
+		&workqueue.BucketRateLimiter{Limiter: rate.NewLimiter(rate.Limit(10000), 10000)},
 	), "digests")
 
 	resolver := newBackgroundResolver(logger, &digestResolver{client: kubeclient.Get(ctx), transport: transport, userAgent: userAgent}, digestResolveQueue, impl.EnqueueKey)
