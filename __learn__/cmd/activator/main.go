fa982dbfb48ea814f95447bf33b9e281ab206f39
XodiumLuma <28709874+xodiumluma@users.noreply.github.com>
2023-01-02 01:07:35 +1100

    [learn] update imports

--- __learn__/cmd/activator/main.go
+++ __learn__/cmd/activator/main.go
@@ -1,3 +1,56 @@
 package main
 
-// injection related imports
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	"go.uber.org/zap"
+
+	// injection
+	kubeclient "knative.dev/pkg/client/injection/kube/client"
+	"knative.dev/pkg/injection"
+	"knative.dev/serving/pkg/activator"
+	"knative.dev/serving/pkg/http/handler"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+
+	"knative.dev/control-protocol/pkg/certificates"
+	network "knative.dev/networking/pkg"
+	netcfg "knative.dev/networking/pkg/config"
+	netprobe "knative.dev/networking/pkg/http/probe"
+	"knative.dev/pkg/configmap"
+	configmapinformer "knative.dev/pkg/configmap/informer"
+	"knative.dev/pkg/controller"
+	"knative.dev/pkg/injection/sharedmain"
+	pkglogging "knative.dev/pkg/logging"
+	"knative.dev/pkg/logging/logkey"
+	"knative.dev/pkg/metrics"
+	pkgnet "knative.dev/pkg/network"
+	"knative.dev/pkg/profiling"
+	"knative.dev/pkg/signals"
+	"knative.dev/pkg/system"
+	"knative.dev/pkg/tracing"
+	tracingconfig "knative.dev/pkg/tracing/config"
+	"knative.dev/pkg/version"
+	"knative.dev/pkg/websocket"
+	activatorconfig "knative.dev/serving/pkg/activator/config"
+  activatorhandler "knative.dev/serving/pkg/activator/handler"
+  apiconfig "knative.dev/serving/pkg/apis/config"
+  asmetrics "knative.dev/serving/pkg/autoscaler/metrics"
+  pkghttp "knative.ev/serving/pkg/http"
+  "knative.dev/serving/pkg/logging"
+  "knative.dev/serving/pkg/networking"
+)
+
+
