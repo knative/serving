bfee3a11d7976f4968fb321c34fb2a795b894506
Reto Lehmann <retocode@icloud.com>
2023-02-21 09:35:03 +0100

    TEST-ONLY: get istio from unreleased github.com/retocode

--- test/e2e-networking-library.sh
+++ test/e2e-networking-library.sh
@@ -45,7 +45,10 @@ function stage_istio_head() {
   header "Staging Istio YAML (HEAD)"
   local istio_head_dir="${E2E_YAML_DIR}/istio/HEAD/install"
   mkdir -p "${istio_head_dir}"
-  download_net_istio_yamls "${REPO_ROOT_DIR}/third_party/istio-latest/net-istio.yaml" "${istio_head_dir}"
+
+    download_net_istio_yamls \
+      "https://github.com/ReToCode/net-istio/releases/download/internal-encryption/net-istio.yaml" \
+      "${istio_head_dir}"
 }
 
 function stage_istio_latest() {
@@ -54,7 +57,7 @@ function stage_istio_latest() {
   mkdir -p "${istio_latest_dir}"
 
   download_net_istio_yamls \
-    "https://github.com/knative-sandbox/net-istio/releases/download/${LATEST_NET_ISTIO_RELEASE_VERSION}/net-istio.yaml" \
+    "https://github.com/ReToCode/net-istio/releases/download/internal-encryption/net-istio.yaml" \
     "${istio_latest_dir}"
 }
 
