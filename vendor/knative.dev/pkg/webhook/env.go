1b33ae59d7aa14ce3a23406e60170bda3db89371
Knative Automation <automation@knative.team>
2023-04-17 01:38:50 +0000

    upgrade to latest dependencies
    bumping knative.dev/caching 0641348...055e0b0:%0A  > 055e0b0 Update actions (# 743)%0Abumping knative.dev/control-protocol 7879893...dcf76d1:%0A  > dcf76d1 Update actions (# 278)%0Abumping knative.dev/pkg d3d7625...53f04b3:%0A  > 53f04b3 Make minimum TLS version configurable for webhooks (# 2721)%0A  > ce10b06 Update actions (# 2726)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/pkg/webhook/env.go
+++ vendor/knative.dev/pkg/webhook/env.go
@@ -17,6 +17,7 @@ limitations under the License.
 package webhook
 
 import (
+	"crypto/tls"
 	"fmt"
 	"os"
 	"strconv"
@@ -29,6 +30,8 @@ const (
 	webhookNameEnvKey = "WEBHOOK_NAME"
 
 	secretNameEnvKey = "WEBHOOK_SECRET_NAME" //nolint:gosec // This is not a hardcoded credential
+
+	tlsMinVersionEnvKey = "WEBHOOK_TLS_MIN_VERSION"
 )
 
 // PortFromEnv returns the webhook port set by portEnvKey, or default port if env var is not set.
@@ -65,4 +68,17 @@ func SecretNameFromEnv(defaultSecretName string) string {
 		return defaultSecretName
 	}
 	return secret
+}
+
+func TLSMinVersionFromEnv(defaultTLSMinVersion uint16) uint16 {
+	switch tlsMinVersion := os.Getenv(tlsMinVersionEnvKey); tlsMinVersion {
+	case "1.2":
+		return tls.VersionTLS12
+	case "1.3":
+		return tls.VersionTLS13
+	case "":
+		return defaultTLSMinVersion
+	default:
+		panic(fmt.Sprintf("the environment variable %q has to be either '1.2' or '1.3'", tlsMinVersionEnvKey))
+	}
 }
