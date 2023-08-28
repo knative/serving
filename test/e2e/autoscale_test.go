5726ccd4531c5820dfdf36d6a83cb2fde2f0cb75
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2023-08-28 16:17:40 +0900

    Fix

--- test/e2e/autoscale_test.go
+++ test/e2e/autoscale_test.go
@@ -176,7 +176,7 @@ func TestTargetBurstCapacity(t *testing.T) {
 
 	// TODO: Remove this when "activator always stay in path" is eliminated.
 	dataplaneTrustMode := cm.Data[netcfg.DataplaneTrustKey]
-	if dataplaneTrustMode != "" && !strings.EqualFold(dataplaneTrustMode, string(netcfg.TrustDisabled)) && strings.EqualFold(cm.Data[netcfg.InternalEncryptionKey], "true") {
+	if (dataplaneTrustMode != "" && !strings.EqualFold(dataplaneTrustMode, string(netcfg.TrustDisabled))) || strings.EqualFold(cm.Data[netcfg.InternalEncryptionKey], "true") {
 		t.Skip("Skipping TestTargetBurstCapacity as activator always stay in path.")
 	}
 
