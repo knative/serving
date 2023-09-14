1b74859cffb7ec14ddcd3d35c570ef34972d6ca6
Clay Kauzlaric <ckauzlaric@vmware.com>
2023-09-14 10:42:15 -0400

    use new internal encryption keys

--- pkg/reconciler/route/resources/service_test.go
+++ pkg/reconciler/route/resources/service_test.go
@@ -419,7 +419,7 @@ func testConfig() *config.Config {
 			DefaultIngressClass: "test-ingress-class",
 			DomainTemplate:      netcfg.DefaultDomainTemplate,
 			TagTemplate:         netcfg.DefaultTagTemplate,
-			DataplaneTrust:      netcfg.TrustDisabled,
+			KnativeInternalTLS:  netcfg.EncryptionEnabled,
 		},
 		Features: &apiConfig.Features{
 			MultiContainer:               apiConfig.Disabled,
