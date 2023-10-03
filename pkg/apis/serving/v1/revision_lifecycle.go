6b495cee64a38a68a12f9628cde15d2bc91628f6
Gabriel Freites <gfreites@vmware.com>
2023-10-03 12:21:16 +0200

    removing RevisionConditionActive from Revision ConditionSet since we can have Ready Inactive Revisions (scale to zero) * added docs and tests for this and the replicaset failure propagation

--- pkg/apis/serving/v1/revision_lifecycle.go
+++ pkg/apis/serving/v1/revision_lifecycle.go
@@ -55,7 +55,6 @@ const (
 var revisionCondSet = apis.NewLivingConditionSet(
 	RevisionConditionResourcesAvailable,
 	RevisionConditionContainerHealthy,
-	RevisionConditionActive,
 )
 
 // GetConditionSet retrieves the condition set for this resource. Implements the KRShaped interface.
