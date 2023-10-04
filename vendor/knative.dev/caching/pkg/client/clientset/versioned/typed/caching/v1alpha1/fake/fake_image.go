cfe2dfcfdb55243cb4e96b22acec20885a9303f5
Knative Automation <automation@knative.team>
2023-10-04 13:12:57 +0000

    upgrade to latest dependencies
    bumping knative.dev/caching da42414...6c2d45f:
      > 6c2d45f upgrade to latest dependencies (# 792)
      > 808ee92 upgrade to latest dependencies (# 791)
    bumping knative.dev/networking c1cae21...53ba1f4:
      > 53ba1f4 Rename cluster.local to avoid issues with config validation webhook (# 872)
      > 97dab15 upgrade to latest dependencies (# 870)
      > 463dc38 Cleanup SAN constants and Secrets Keys for system-internal-tls certificates (# 861)
      > 05d0964 Align the encryption flags (# 858)
    bumping knative.dev/pkg 1860700...833dd97:
      > 833dd97 prioritize destination CA certs over resolved addressable CA certs (# 2849)
      > 294e777 Add rest.Config to fake context in SetupFakeContext (# 2847)
      > 0736c74 Add SinkAudience to SourceStatus (# 2844)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/caching/pkg/client/clientset/versioned/typed/caching/v1alpha1/fake/fake_image.go
+++ vendor/knative.dev/caching/pkg/client/clientset/versioned/typed/caching/v1alpha1/fake/fake_image.go
@@ -23,7 +23,6 @@ import (
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	testing "k8s.io/client-go/testing"
@@ -36,9 +35,9 @@ type FakeImages struct {
 	ns   string
 }
 
-var imagesResource = schema.GroupVersionResource{Group: "caching.internal.knative.dev", Version: "v1alpha1", Resource: "images"}
+var imagesResource = v1alpha1.SchemeGroupVersion.WithResource("images")
 
-var imagesKind = schema.GroupVersionKind{Group: "caching.internal.knative.dev", Version: "v1alpha1", Kind: "Image"}
+var imagesKind = v1alpha1.SchemeGroupVersion.WithKind("Image")
 
 // Get takes name of the image, and returns the corresponding image object, and an error if there is any.
 func (c *FakeImages) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha1.Image, err error) {
--- vendor/knative.dev/networking/pkg/client/clientset/versioned/typed/networking/v1alpha1/fake/fake_clusterdomainclaim.go
+++ vendor/knative.dev/networking/pkg/client/clientset/versioned/typed/networking/v1alpha1/fake/fake_clusterdomainclaim.go
@@ -23,7 +23,6 @@ import (
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	testing "k8s.io/client-go/testing"
@@ -35,9 +34,9 @@ type FakeClusterDomainClaims struct {
 	Fake *FakeNetworkingV1alpha1
 }
 
-var clusterdomainclaimsResource = schema.GroupVersionResource{Group: "networking.internal.knative.dev", Version: "v1alpha1", Resource: "clusterdomainclaims"}
+var clusterdomainclaimsResource = v1alpha1.SchemeGroupVersion.WithResource("clusterdomainclaims")
 
-var clusterdomainclaimsKind = schema.GroupVersionKind{Group: "networking.internal.knative.dev", Version: "v1alpha1", Kind: "ClusterDomainClaim"}
+var clusterdomainclaimsKind = v1alpha1.SchemeGroupVersion.WithKind("ClusterDomainClaim")
 
 // Get takes name of the clusterDomainClaim, and returns the corresponding clusterDomainClaim object, and an error if there is any.
 func (c *FakeClusterDomainClaims) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha1.ClusterDomainClaim, err error) {
