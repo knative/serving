f7bf116e772c6febec2728e4005db511313367d1
Stavros Kontopoulos <st.kontopoulos@gmail.com>
2023-11-24 10:08:10 +0200

    fixes

--- test/performance/benchmarks/rollout-probe/main.go
+++ test/performance/benchmarks/rollout-probe/main.go
@@ -104,7 +104,7 @@ func main() {
 	}
 
 	// Send 1k QPS for the given duration with a 30s request timeout.
-	rate := vegeta.Rate{Freq: 3600, Per: time.Second}
+	rate := vegeta.Rate{Freq: 1000, Per: time.Second}
 	targeter := vegeta.NewStaticTargeter(t.target)
 	attacker := vegeta.NewAttacker(vegeta.Timeout(30 * time.Second))
 
