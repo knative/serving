b794abbe8e993e03c97f4efe0915ec42fa3806d6
dprotaso <dprotaso@gmail.com>
2024-04-01 19:44:19 -0400

    bump kapp/ytt deps

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -624,10 +624,10 @@ function overlay_system_namespace() {
 }
 
 function run_ytt() {
-  go_run github.com/vmware-tanzu/carvel-ytt/cmd/ytt@v0.44.1 "$@"
+  go_run carvel.dev/ytt/cmd/ytt@v0.48.0 "$@"
 }
 
 
 function run_kapp() {
-  go_run github.com/vmware-tanzu/carvel-kapp/cmd/kapp@v0.54.1 "$@"
+  go_run github.com/vmware-tanzu/carvel-kapp/cmd/kapp@v0.60.0 "$@"
 }
