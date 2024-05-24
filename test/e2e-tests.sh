79da3240c2d9206f66012d4a4367d096a94ac0aa
Izabela Gomes <igomes@vmware.com>
2024-05-24 15:56:48 -0400

    delete CORS tests from serving (#15249)

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -115,15 +115,6 @@ immediate_gc
 go_test_e2e -timeout=2m ./test/e2e/gc ${E2E_TEST_FLAGS} || failed=1
 kubectl replace cm "config-gc" -n ${SYSTEM_NAMESPACE} -f "${TMP_DIR}"/config-gc.yaml
 
-# Run tests with CORS policy enabled for Contour
-if [[ "${INGRESS_CLASS}" == *"contour"* ]]; then
-  toggle_feature cors-policy "allowOrigin:\n - '*'\nallowMethods:\n - GET\n - OPTIONS\n" config-contour || fail_test
-  go_test_e2e -timeout=5m ./test/e2e/corspolicy ${E2E_TEST_FLAGS} || failed=1
-  kubectl patch cm config-contour -n "${SYSTEM_NAMESPACE}" --type=json -p '[{"op": "remove", "path": "/data/cors-policy"}]' || fail_test
-  echo "Waiting 30s for change to get picked up."
-  sleep 30
-fi
-
 # Run scale tests.
 # Note that we use a very high -parallel because each ksvc is run as its own
 # sub-test. If this is not larger than the maximum scale tested then the test
