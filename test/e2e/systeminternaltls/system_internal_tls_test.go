39b1ef707f3f12c7756da5da2e26aa3abe15392d
Reto Lehmann <retocode@icloud.com>
2024-06-04 12:53:59 +0200

    Only run system-internal-tls tests for kourier

--- test/e2e/systeminternaltls/system_internal_tls_test.go
+++ test/e2e/systeminternaltls/system_internal_tls_test.go
@@ -51,9 +51,8 @@ func TestSystemInternalTLS(t *testing.T) {
 		t.Skip("Alpha features not enabled")
 	}
 
-	if !(strings.Contains(test.ServingFlags.IngressClass, "kourier") ||
-		strings.Contains(test.ServingFlags.IngressClass, "istio")) {
-		t.Skip("Skip this test for non-kourier or non-istio ingress.")
+	if !(strings.Contains(test.ServingFlags.IngressClass, "kourier")) {
+		t.Skip("Skip this test for non-kourier ingress.")
 	}
 
 	t.Parallel()
@@ -117,9 +116,8 @@ func TestTLSCertificateRotation(t *testing.T) {
 		t.Skip("Alpha features not enabled")
 	}
 
-	if !(strings.Contains(test.ServingFlags.IngressClass, "kourier") ||
-		strings.Contains(test.ServingFlags.IngressClass, "istio")) {
-		t.Skip("Skip this test for non-kourier or non-istio ingress.")
+	if !(strings.Contains(test.ServingFlags.IngressClass, "kourier")) {
+		t.Skip("Skip this test for non-kourier ingress.")
 	}
 
 	t.Parallel()
