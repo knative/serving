fe0e53f566e76a78d2b64f133fcef0f84e5447df
Knative Automation <automation@knative.team>
2025-01-17 09:51:09 -0500

    upgrade to latest dependencies (#15701)
    bumping knative.dev/pkg 0c2a238...c43477f:
      > c43477f upgrade to latest dependencies (# 3137)
    bumping knative.dev/caching dee364d...565e7cd:
      > 565e7cd upgrade to latest dependencies (# 917)
      > 817ae30 fix generation of our clientset (# 916)
    bumping knative.dev/hack f8be0cc...c142b48:
      > c142b48 Refactor release script to gh CLI (# 408)
    bumping knative.dev/networking 6797968...04878f0:
      > 04878f0 upgrade to latest dependencies (# 1039)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/caching/pkg/client/clientset/versioned/typed/caching/v1alpha1/image.go
+++ vendor/knative.dev/caching/pkg/client/clientset/versioned/typed/caching/v1alpha1/image.go
@@ -20,12 +20,11 @@ package v1alpha1
 
 import (
 	"context"
-	"time"
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
-	rest "k8s.io/client-go/rest"
+	gentype "k8s.io/client-go/gentype"
 	v1alpha1 "knative.dev/caching/pkg/apis/caching/v1alpha1"
 	scheme "knative.dev/caching/pkg/client/clientset/versioned/scheme"
 )
@@ -40,6 +39,7 @@ type ImagesGetter interface {
 type ImageInterface interface {
 	Create(ctx context.Context, image *v1alpha1.Image, opts v1.CreateOptions) (*v1alpha1.Image, error)
 	Update(ctx context.Context, image *v1alpha1.Image, opts v1.UpdateOptions) (*v1alpha1.Image, error)
+	// Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
 	UpdateStatus(ctx context.Context, image *v1alpha1.Image, opts v1.UpdateOptions) (*v1alpha1.Image, error)
 	Delete(ctx context.Context, name string, opts v1.DeleteOptions) error
 	DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error
@@ -52,144 +52,18 @@ type ImageInterface interface {
 
 // images implements ImageInterface
 type images struct {
-	client rest.Interface
-	ns     string
+	*gentype.ClientWithList[*v1alpha1.Image, *v1alpha1.ImageList]
 }
 
 // newImages returns a Images
 func newImages(c *CachingV1alpha1Client, namespace string) *images {
 	return &images{
-		client: c.RESTClient(),
-		ns:     namespace,
+		gentype.NewClientWithList[*v1alpha1.Image, *v1alpha1.ImageList](
+			"images",
+			c.RESTClient(),
+			scheme.ParameterCodec,
+			namespace,
+			func() *v1alpha1.Image { return &v1alpha1.Image{} },
+			func() *v1alpha1.ImageList { return &v1alpha1.ImageList{} }),
 	}
-}
-
-// Get takes name of the image, and returns the corresponding image object, and an error if there is any.
-func (c *images) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha1.Image, err error) {
-	result = &v1alpha1.Image{}
-	err = c.client.Get().
-		Namespace(c.ns).
-		Resource("images").
-		Name(name).
-		VersionedParams(&options, scheme.ParameterCodec).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// List takes label and field selectors, and returns the list of Images that match those selectors.
-func (c *images) List(ctx context.Context, opts v1.ListOptions) (result *v1alpha1.ImageList, err error) {
-	var timeout time.Duration
-	if opts.TimeoutSeconds != nil {
-		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
-	}
-	result = &v1alpha1.ImageList{}
-	err = c.client.Get().
-		Namespace(c.ns).
-		Resource("images").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Watch returns a watch.Interface that watches the requested images.
-func (c *images) Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error) {
-	var timeout time.Duration
-	if opts.TimeoutSeconds != nil {
-		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
-	}
-	opts.Watch = true
-	return c.client.Get().
-		Namespace(c.ns).
-		Resource("images").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Watch(ctx)
-}
-
-// Create takes the representation of a image and creates it.  Returns the server's representation of the image, and an error, if there is any.
-func (c *images) Create(ctx context.Context, image *v1alpha1.Image, opts v1.CreateOptions) (result *v1alpha1.Image, err error) {
-	result = &v1alpha1.Image{}
-	err = c.client.Post().
-		Namespace(c.ns).
-		Resource("images").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(image).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Update takes the representation of a image and updates it. Returns the server's representation of the image, and an error, if there is any.
-func (c *images) Update(ctx context.Context, image *v1alpha1.Image, opts v1.UpdateOptions) (result *v1alpha1.Image, err error) {
-	result = &v1alpha1.Image{}
-	err = c.client.Put().
-		Namespace(c.ns).
-		Resource("images").
-		Name(image.Name).
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(image).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// UpdateStatus was generated because the type contains a Status member.
-// Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
-func (c *images) UpdateStatus(ctx context.Context, image *v1alpha1.Image, opts v1.UpdateOptions) (result *v1alpha1.Image, err error) {
-	result = &v1alpha1.Image{}
-	err = c.client.Put().
-		Namespace(c.ns).
-		Resource("images").
-		Name(image.Name).
-		SubResource("status").
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(image).
-		Do(ctx).
-		Into(result)
-	return
-}
-
-// Delete takes name of the image and deletes it. Returns an error if one occurs.
-func (c *images) Delete(ctx context.Context, name string, opts v1.DeleteOptions) error {
-	return c.client.Delete().
-		Namespace(c.ns).
-		Resource("images").
-		Name(name).
-		Body(&opts).
-		Do(ctx).
-		Error()
-}
-
-// DeleteCollection deletes a collection of objects.
-func (c *images) DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error {
-	var timeout time.Duration
-	if listOpts.TimeoutSeconds != nil {
-		timeout = time.Duration(*listOpts.TimeoutSeconds) * time.Second
-	}
-	return c.client.Delete().
-		Namespace(c.ns).
-		Resource("images").
-		VersionedParams(&listOpts, scheme.ParameterCodec).
-		Timeout(timeout).
-		Body(&opts).
-		Do(ctx).
-		Error()
-}
-
-// Patch applies the patch and returns the patched image.
-func (c *images) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha1.Image, err error) {
-	result = &v1alpha1.Image{}
-	err = c.client.Patch(pt).
-		Namespace(c.ns).
-		Resource("images").
-		Name(name).
-		SubResource(subresources...).
-		VersionedParams(&opts, scheme.ParameterCodec).
-		Body(data).
-		Do(ctx).
-		Into(result)
-	return
 }
