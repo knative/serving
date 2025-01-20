460e0b39266e4f739ea117e0f25fc7535948d8d5
Sascha Schwarze <schwarzs@de.ibm.com>
2025-01-20 23:11:08 +0100

    Ensure ContainerHealthy condition is set back to True (#15503)
    Signed-off-by: Sascha Schwarze <schwarzs@de.ibm.com>
    Co-authored-by: Matthias Diester <matthias.diester@de.ibm.com>

--- pkg/testing/v1/revision.go
+++ pkg/testing/v1/revision.go
@@ -152,9 +152,24 @@ func MarkDeploying(reason string) RevisionOption {
 	}
 }
 
+// MarkContainerHealthyUnknown changes the ContainerHealthy condition to Unknown with the given reason
 func MarkContainerHealthyUnknown(reason string) RevisionOption {
 	return func(r *v1.Revision) {
 		r.Status.MarkContainerHealthyUnknown(reason, "")
+	}
+}
+
+// MarkContainerHealthyFalse changes the ContainerHealthy condition to False with the given reason
+func MarkContainerHealthyFalse(reason string) RevisionOption {
+	return func(r *v1.Revision) {
+		r.Status.MarkContainerHealthyFalse(reason, "")
+	}
+}
+
+// MarkContainerHealthyTrue changes the ContainerHealthy condition to True
+func MarkContainerHealthyTrue() RevisionOption {
+	return func(r *v1.Revision) {
+		r.Status.MarkContainerHealthyTrue()
 	}
 }
 
