45236a33080c7ac43be127145e023584af1fb194
Dave Protasowski <dprotaso@gmail.com>
2025-02-13 04:50:52 -0500

    bump min k8s version to v1.31 (#15774)

--- test/e2e-external-domain-tls-tests.sh
+++ test/e2e-external-domain-tls-tests.sh
@@ -162,7 +162,7 @@ function delete_dns_record() {
 }
 
 # Script entry point.
-initialize "$@" --num-nodes=4 --enable-ha --cluster-version=1.30
+initialize "$@" --num-nodes=4 --enable-ha --cluster-version=1.31
 
 # Run the tests
 header "Running tests"
