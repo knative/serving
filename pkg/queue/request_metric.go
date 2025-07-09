066707d7e6e097fbc8b64826d17ab791ae14e3dd
Dave Protasowski <dprotaso@gmail.com>
2025-07-08 22:52:20 -0400

    port over unit test assertions to OTel

--- pkg/queue/request_metric.go
+++ pkg/queue/request_metric.go
@@ -22,32 +22,47 @@ import (
 
 	"go.opentelemetry.io/otel/metric"
 	semconv "go.opentelemetry.io/otel/semconv/v1.34.0"
+	"k8s.io/utils/clock"
 
 	netheader "knative.dev/networking/pkg/http/header"
 	pkghttp "knative.dev/serving/pkg/http"
 )
 
+var (
+	scopeName     = "knative.dev/serving/pkg/queue"
+	latencyBounds = []float64{0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10}
+)
+
 type appRequestMetricsHandler struct {
 	next    http.Handler
 	breaker *Breaker
+	clock   clock.Clock
 
 	duration metric.Float64Histogram
 	queueLen metric.Int64Gauge
 }
 
 // NewAppRequestMetricsHandler creates an http.Handler that emits request metrics.
-func NewAppRequestMetricsHandler(mp metric.MeterProvider, next http.Handler, b *Breaker) (http.Handler, error) {
+func NewAppRequestMetricsHandler(
+	mp metric.MeterProvider,
+	next http.Handler,
+	b *Breaker,
+) (http.Handler, error) {
 	var (
-		meter   = mp.Meter("knative.dev/serving/pkg/queue")
-		handler = &appRequestMetricsHandler{next: next, breaker: b}
+		meter   = mp.Meter(scopeName)
 		err     error
+		handler = &appRequestMetricsHandler{
+			next:    next,
+			breaker: b,
+			clock:   clock.RealClock{},
+		}
 	)
 
 	handler.duration, err = meter.Float64Histogram(
 		"kn.queueproxy.app.duration",
-		metric.WithDescription("The duration of task execution."),
+		metric.WithDescription("The duration of task execution"),
 		metric.WithUnit("s"),
-		metric.WithExplicitBucketBoundaries(0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10),
+		metric.WithExplicitBucketBoundaries(latencyBounds...),
 	)
 	if err != nil {
 		panic(err)
@@ -67,7 +82,7 @@ func NewAppRequestMetricsHandler(mp metric.MeterProvider, next http.Handler, b *
 
 func (h *appRequestMetricsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	rr := pkghttp.NewResponseRecorder(w, http.StatusOK)
-	startTime := time.Now()
+	startTime := h.clock.Now()
 
 	if h.breaker != nil {
 		h.queueLen.Record(r.Context(), int64(h.breaker.InFlight()))
@@ -80,7 +95,7 @@ func (h *appRequestMetricsHandler) ServeHTTP(w http.ResponseWriter, r *http.Requ
 
 		// If ServeHTTP panics, recover, record the failure and panic again.
 		err := recover()
-		latency := time.Since(startTime)
+		latency := h.clock.Since(startTime)
 
 		status := rr.ResponseCode
 
--- pkg/queue/request_metric_test.go
+++ pkg/queue/request_metric_test.go
@@ -21,21 +21,39 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/metric/metricdata"
+	semconv "go.opentelemetry.io/otel/semconv/v1.34.0"
+	clocktest "k8s.io/utils/clock/testing"
+
 	netheader "knative.dev/networking/pkg/http/header"
+	"knative.dev/pkg/observability/metrics/metricstest"
 )
 
 const targetURI = "http://example.com"
 
 func TestAppRequestMetricsHandlerPanickingHandler(t *testing.T) {
+	// Use the fake clock to set the duration
+	fakeClock := clocktest.NewFakeClock(time.Now())
+
 	reader := metric.NewManualReader()
 	mp := metric.NewMeterProvider(metric.WithReader(reader))
 	baseHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// advance time by one second
+		fakeClock.SetTime(fakeClock.Now().Add(time.Second))
 		panic("no!")
 	})
 	breaker := NewBreaker(BreakerParams{QueueDepth: 10, MaxConcurrency: 10, InitialCapacity: 10})
+
+	// Increment the breaker to report 1 active request
+	breaker.tryAcquirePending()
 	handler, err := NewAppRequestMetricsHandler(mp, baseHandler, breaker)
+
+	handler.(*appRequestMetricsHandler).clock = fakeClock
+
 	if err != nil {
 		t.Fatal("Failed to create handler:", err)
 	}
@@ -46,66 +64,87 @@ func TestAppRequestMetricsHandlerPanickingHandler(t *testing.T) {
 		if err := recover(); err == nil {
 			t.Error("Want ServeHTTP to panic, got nothing.")
 		}
-		// wantTags := map[string]string{
-		// 	metrics.LabelPodName:           "pod",
-		// 	metrics.LabelContainerName:     "queue-proxy",
-		// 	metrics.LabelResponseCode:      "500",
-		// 	metrics.LabelResponseCodeClass: "5xx",
-		// }
-		// wantResource := &resource.Resource{
-		// 	Type: "knative_revision",
-		// 	Labels: map[string]string{
-		// 		metrics.LabelNamespaceName:     "ns",
-		// 		metrics.LabelRevisionName:      "rev",
-		// 		metrics.LabelServiceName:       "svc",
-		// 		metrics.LabelConfigurationName: "cfg",
-		// 	},
-		// }
 
-		// metricstest.AssertMetric(t, metricstest.IntMetric("app_request_count", 1, wantTags).WithResource(wantResource))
-		// metricstest.AssertMetric(t, metricstest.DistributionCountOnlyMetric("app_request_latencies", 1, wantTags).WithResource(wantResource))
+		assertMetrics(t, reader, http.StatusInternalServerError)
 	}()
+
 	handler.ServeHTTP(resp, req)
 }
 
 func TestAppRequestMetricsHandler(t *testing.T) {
+	// Use the fake clock to set the duration
+	fakeClock := clocktest.NewFakeClock(time.Now())
+
 	reader := metric.NewManualReader()
 	mp := metric.NewMeterProvider(metric.WithReader(reader))
-	baseHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+	baseHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fakeClock.SetTime(fakeClock.Now().Add(time.Second))
+	})
 	breaker := NewBreaker(BreakerParams{QueueDepth: 10, MaxConcurrency: 10, InitialCapacity: 10})
+	breaker.tryAcquirePending()
 	handler, err := NewAppRequestMetricsHandler(mp, baseHandler, breaker)
 	if err != nil {
 		t.Fatal("Failed to create handler:", err)
 	}
 
+	handler.(*appRequestMetricsHandler).clock = fakeClock
+
 	resp := httptest.NewRecorder()
 	req := httptest.NewRequest(http.MethodPost, targetURI, bytes.NewBufferString("test"))
 	handler.ServeHTTP(resp, req)
 
-	// wantTags := map[string]string{
-	// 	metrics.LabelPodName:           "pod",
-	// 	metrics.LabelContainerName:     "queue-proxy",
-	// 	metrics.LabelResponseCode:      "200",
-	// 	metrics.LabelResponseCodeClass: "2xx",
-	// }
-	// wantResource := &resource.Resource{
-	// 	Type: "knative_revision",
-	// 	Labels: map[string]string{
-	// 		metrics.LabelNamespaceName:     "ns",
-	// 		metrics.LabelRevisionName:      "rev",
-	// 		metrics.LabelServiceName:       "svc",
-	// 		metrics.LabelConfigurationName: "cfg",
-	// 	},
-	// }
-
-	// metricstest.AssertMetric(t, metricstest.IntMetric("app_request_count", 1, wantTags).WithResource(wantResource))
-	// metricstest.AssertMetric(t, metricstest.DistributionCountOnlyMetric("app_request_latencies", 1, wantTags).WithResource(wantResource))
+	assertMetrics(t, reader, http.StatusOK)
 
 	// A probe request should not be recorded.
 	req.Header.Set(netheader.ProbeKey, "activator")
 	handler.ServeHTTP(resp, req)
-	// metricstest.AssertMetric(t, metricstest.IntMetric("app_request_count", 1, wantTags).WithResource(wantResource))
-	// metricstest.AssertMetric(t, metricstest.DistributionCountOnlyMetric("app_request_latencies", 1, wantTags).WithResource(wantResource))
+
+	// should be no changes so we run the same assertion
+	assertMetrics(t, reader, http.StatusOK)
+}
+
+func assertMetrics(t *testing.T, reader *metric.ManualReader, status int) {
+	t.Helper()
+
+	bucketCounts := [15]uint64{}
+	bucketCounts[9] = 1 // one second bucket
+
+	metricstest.AssertMetrics(
+		t, reader,
+		metricstest.MetricsEqual(
+			scopeName,
+			metricdata.Metrics{
+				Name:        "kn.queueproxy.depth",
+				Unit:        "{item}",
+				Description: "Number of current items in the queue",
+				Data: metricdata.Gauge[int64]{
+					DataPoints: []metricdata.DataPoint[int64]{
+						{Value: 1},
+					},
+				},
+			},
+			metricdata.Metrics{
+				Name:        "kn.queueproxy.app.duration",
+				Unit:        "s",
+				Description: "The duration of task execution",
+				Data: metricdata.Histogram[float64]{
+					Temporality: metricdata.CumulativeTemporality,
+					DataPoints: []metricdata.HistogramDataPoint[float64]{{
+						Bounds:       latencyBounds,
+						Count:        1,
+						Sum:          1,
+						BucketCounts: bucketCounts[:],
+						Min:          metricdata.NewExtrema[float64](1),
+						Max:          metricdata.NewExtrema[float64](1),
+						Attributes: attribute.NewSet(
+							semconv.HTTPResponseStatusCode(status),
+						),
+					}},
+				},
+			},
+		),
+	)
 }
 
 func BenchmarkAppRequestMetricsHandler(b *testing.B) {
