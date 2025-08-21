9fa4ff6f57af2d9223995bf809fa16e3ce50ee46
Knative Automation <automation@knative.team>
2025-08-21 12:13:49 -0400

    upgrade to latest dependencies (#16035)
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/hack/release.sh
+++ vendor/knative.dev/hack/release.sh
@@ -219,10 +219,9 @@ function prepare_dot_release() {
   # Use the original tag (ie. potentially with a knative- prefix) when determining the last version commit sha
   local github_tag="$(gh_tool release list --json tagName --jq '.[].tagName' | grep "${last_version}")"
   local last_release_commit="$(git rev-list -n 1 "${github_tag}")"
-  local last_release_commit_filtered="$(git rev-list --invert-grep --grep '^(?!\s*>).*?\[skip-dot-release\]' -n 1 "${github_tag}")"
+  local last_release_commit_filtered="$(git rev-list --invert-grep --grep '^(?!\s*>).*?\[skip-dot-release\]'-n 1 "${github_tag}")"
   local release_branch_commit="$(git rev-list -n 1 upstream/"${RELEASE_BRANCH}")"
   local release_branch_commit_filtered="$(git rev-list --invert-grep --grep '^(?!\s*>).*?\[skip-dot-release\]' -n 1 upstream/"${RELEASE_BRANCH}")"
-
   [[ -n "${last_release_commit}" ]] || abort "cannot get last release commit"
   [[ -n "${release_branch_commit}" ]] || abort "cannot get release branch last commit"
   [[ -n "${last_release_commit_filtered}" ]] || abort "cannot get filtered last release commit"
