f18364a0edc2afa898fd27ee9a6ee826e41d8680
Dave Protasowski <dprotaso@gmail.com>
2025-09-09 12:55:28 -0400

    Revert "add initialize conditions to MakePA to avoid potential race conditions (#16037)"
    This reverts commit 12777f672fea55de88d6eaa2ab26ae3f4d570a16.

--- pkg/apis/serving/v1/revision_lifecycle_test.go
+++ pkg/apis/serving/v1/revision_lifecycle_test.go
@@ -726,7 +726,6 @@ func TestPropagateAutoscalerStatusReplicas(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			tc.ps.InitializeConditions()
 			r.PropagateAutoscalerStatus(&tc.ps)
 
 			if !cmp.Equal(tc.wantActualReplicas, r.ActualReplicas) {
