1c8d21bb61bd5ff0b2c1446eee39be0be1f21a8d
Mike Baynton <mike@mbaynton.com>
2025-10-06 13:36:11 -0500

    Make queue-proxy's reverse proxy handler extendable (#16097)
    * Make queue-proxy's reverse proxy handler extendable
    
    This builds on #13133 to make it possible to adjust settings on the
    *httputil.ReverseProxy that queue-proxy uses, or to replace it entirely
    with any http.Handler, using the out-of-tree extension pattern.
    
    * Ensure configured Transport is used
    
    If an integrator customizes the Transport in an Options function, we want to
    apply the provided Transport to our default httputil.ReverseProxy
    regardless of which Option sets the Transport and regardless of whether
    or not other Option functions replace our default http.Handler.
    
    Add some tests for this since the interactions beteween
    Options.Transport and Options.ProxyHandler can be subtle.

--- pkg/queue/sharedmain/main.go
+++ pkg/queue/sharedmain/main.go
@@ -22,10 +22,16 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"net"
 	"net/http"
+	"net/http/httputil"
 	"os"
 	"strconv"
 	"time"
+
+	netproxy "knative.dev/networking/pkg/http/proxy"
+	pkghandler "knative.dev/pkg/network/handlers"
+	"knative.dev/serving/pkg/activator"
 
 	"github.com/kelseyhightower/envconfig"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
@@ -145,6 +151,12 @@ type Defaults struct {
 	// If Transport is wrapped, the new RoundTripper should replace the value of Transport.
 	// The new Transport will then be used by other Options (called next) and by QP.
 	Transport http.RoundTripper
+
+	// ProxyHandler provides Options with the QP's main ReverseProxy
+	// The default value is of type *httputil.ReverseProxy
+	// An Option may type assert to *httutil.ReverseProxy and adjust attributes of the ReverseProxy,
+	// or replace the handler entirely, typically to insert additional handler(s) into the chain.
+	ProxyHandler http.Handler
 }
 
 type Option func(*Defaults)
@@ -204,11 +216,9 @@ func Main(opts ...Option) error {
 	}()
 
 	d.Transport = buildTransport(env, tp, mp)
+	proxyHandler := buildProxyHandler(logger, env, d.Transport)
 
-	// allow extensions to read d and return modified context and transport
-	for _, opts := range opts {
-		opts(&d)
-	}
+	applyOptions(&d, proxyHandler, opts...)
 
 	protoStatReporter := queue.NewProtobufStatsReporter(env.ServingPod, reportingPeriod)
 
@@ -232,7 +242,7 @@ func Main(opts ...Option) error {
 	// Enable TLS when certificate is mounted.
 	tlsEnabled := exists(logger, certPath) && exists(logger, keyPath)
 
-	mainHandler, drainer := mainHandler(env, d.Transport, probe, stats, logger, mp, tp)
+	mainHandler, drainer := mainHandler(env, d, probe, stats, logger, mp, tp)
 	adminHandler := adminHandler(d.Ctx, logger, drainer)
 
 	// Enable TLS server when activator server certs are mounted.
@@ -325,6 +335,17 @@ func Main(opts ...Option) error {
 	return nil
 }
 
+func applyOptions(d *Defaults, proxyHandler *httputil.ReverseProxy, opts ...Option) {
+	d.ProxyHandler = proxyHandler
+
+	// allow extensions to read d and return modified context, transport, and proxy handler.
+	for _, opts := range opts {
+		opts(d)
+	}
+
+	proxyHandler.Transport = d.Transport
+}
+
 func exists(logger *zap.SugaredLogger, filename string) bool {
 	_, err := os.Stat(filename)
 	if err != nil && !os.IsNotExist(err) {
@@ -357,6 +378,17 @@ func buildTransport(env config, tp trace.TracerProvider, mp metric.MeterProvider
 		otelhttp.WithTracerProvider(tp),
 		otelhttp.WithMeterProvider(mp),
 	)
+}
+
+func buildProxyHandler(logger *zap.SugaredLogger, env config, transport http.RoundTripper) *httputil.ReverseProxy {
+	target := net.JoinHostPort("127.0.0.1", env.UserPort)
+	httpProxy := pkghttp.NewHeaderPruningReverseProxy(target, pkghttp.NoHostOverride, activator.RevisionHeaders, false /* use HTTP */)
+	httpProxy.Transport = transport
+	httpProxy.ErrorHandler = pkghandler.Error(logger)
+	httpProxy.BufferPool = netproxy.NewBufferPool()
+	httpProxy.FlushInterval = netproxy.FlushInterval
+
+	return httpProxy
 }
 
 func buildBreaker(logger *zap.SugaredLogger, env config) *queue.Breaker {
