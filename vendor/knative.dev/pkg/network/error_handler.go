ceb6b42bb80863b1f50877d96e15ef58350142bc
thiagomedina <thiagomedina001@gmail.com>
2025-10-06 19:40:55 -0300

    run update-codegen

--- vendor/knative.dev/pkg/network/error_handler.go
+++ vendor/knative.dev/pkg/network/error_handler.go
@@ -17,8 +17,6 @@ limitations under the License.
 package network
 
 import (
-	"context"
-	"errors"
 	"net/http"
 	"os"
 
@@ -29,13 +27,8 @@ import (
 // httputil's reverse proxy.
 //
 // Deprecated: Use handler.Error instead.
-
 func ErrorHandler(logger *zap.SugaredLogger) func(http.ResponseWriter, *http.Request, error) {
 	return func(w http.ResponseWriter, req *http.Request, err error) {
-		if errors.Is(err, context.Canceled) {
-			logger.Debugw("request context canceled", zap.Error(err))
-			return
-		}
 		ss := readSockStat(logger)
 		logger.Errorw("error reverse proxying request; sockstat: "+ss, zap.Error(err))
 		http.Error(w, err.Error(), http.StatusBadGateway)
