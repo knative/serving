7a1a2a7aa536de263430eb73402e089a763b2e59
Dave Protasowski <dprotaso@gmail.com>
2025-12-03 04:28:10 -0500

    Re-enable TestShouldHaveHeadersSet conformance test (#16284)
    * Setup e2e runtime tests to use outgoing OTel trace headers
    
    * Tweak logging

--- test/conformance/runtime/main_test.go
+++ test/conformance/runtime/main_test.go
@@ -24,11 +24,25 @@ import (
 	"os"
 	"testing"
 
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+
 	pkgTest "knative.dev/pkg/test"
 )
 
 func TestMain(m *testing.M) {
 	flag.Parse()
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.NeverSample()), // never record or export
+	)
+	// Set the global TracerProvider (so instrumentations use it).
+	otel.SetTracerProvider(tp)
+
+	// 2) Make sure we have a propagator (W3C tracecontext is default, but set explicitly).
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
 	pkgTest.SetupLoggingFlags()
 	os.Exit(m.Run())
 }
