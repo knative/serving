eb4c84f512a242231ef38b4edd82358ba78be514
Mikhail Fedosin <mfedosin@redhat.com>
2026-03-03 17:08:22 +0100

    feat: use knative.dev/pkg/tls for reconciler TLS configuration
    Replace the hand-rolled tlsMinVersionFromEnv helper in the revision
    reconciler with the shared knative.dev/pkg/tls package, allowing TLS
    settings to be configured via TAG_TO_DIGEST_TLS_MIN_VERSION,
    TAG_TO_DIGEST_TLS_MAX_VERSION, TAG_TO_DIGEST_TLS_CIPHER_SUITES, and
    TAG_TO_DIGEST_TLS_CURVE_PREFERENCES environment variables. The default
    minimum version is bumped to TLS 1.3 now that quay.io supports it.
    
    Signed-off-by: Mikhail Fedosin <mfedosin@redhat.com>

--- pkg/reconciler/revision/resolve.go
+++ pkg/reconciler/revision/resolve.go
@@ -18,7 +18,6 @@ package revision
 
 import (
 	"context"
-	"crypto/tls"
 	"crypto/x509"
 	"errors"
 	"fmt"
@@ -30,6 +29,7 @@ import (
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/kubernetes"
+	knativetls "knative.dev/pkg/tls"
 )
 
 type digestResolver struct {
@@ -43,7 +43,7 @@ const (
 	// https://kubernetes.io/docs/tasks/tls/managing-tls-in-a-cluster/#trusting-tls-in-a-cluster
 	k8sCertPath = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 
-	tlsMinVersionEnvKey = "TAG_TO_DIGEST_TLS_MIN_VERSION"
+	tlsEnvPrefix = "TAG_TO_DIGEST_"
 )
 
 // newResolverTransport returns an http.Transport that appends the certs bundle
@@ -62,29 +62,18 @@ func newResolverTransport(path string, maxIdleConns, maxIdleConnsPerHost int) (*
 		return nil, errors.New("failed to append k8s cert bundle to cert pool")
 	}
 
+	cfg, err := knativetls.DefaultConfigFromEnv(tlsEnvPrefix)
+	if err != nil {
+		return nil, err
+	}
+
 	transport := http.DefaultTransport.(*http.Transport).Clone()
 	transport.MaxIdleConns = maxIdleConns
 	transport.MaxIdleConnsPerHost = maxIdleConnsPerHost
-	//nolint:gosec // quay.io still required 1.2 - bump if they've moved up
-	transport.TLSClientConfig = &tls.Config{
-		MinVersion: tlsMinVersionFromEnv(tls.VersionTLS12),
-		RootCAs:    pool,
-	}
+	transport.TLSClientConfig = cfg
+	transport.TLSClientConfig.RootCAs = pool
 
 	return transport, nil
-}
-
-func tlsMinVersionFromEnv(defaultTLSMinVersion uint16) uint16 {
-	switch tlsMinVersion := os.Getenv(tlsMinVersionEnvKey); tlsMinVersion {
-	case "1.2":
-		return tls.VersionTLS12
-	case "1.3":
-		return tls.VersionTLS13
-	case "":
-		return defaultTLSMinVersion
-	default:
-		panic(fmt.Sprintf("the environment variable %q has to be either '1.2' or '1.3'", tlsMinVersionEnvKey))
-	}
 }
 
 // Resolve resolves the image references that use tags to digests.
